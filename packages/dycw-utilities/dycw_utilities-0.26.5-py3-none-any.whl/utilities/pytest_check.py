4920d29c35872f1004760ee7a666e9d8b389acea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:02:58 +0000

    Add package dycw-utilities/0.26.5/dycw_utilities-0.26.5-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.26.5-py3-none-any.whl/utilities/pytest_check.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from collections.abc import Iterator
+from contextlib import contextmanager
+from os import getenv
+
+from pytest_check import check as _check
+
+
+@contextmanager
+def check() -> Iterator[None]:
+    """Context manager running `pytest_check`, but can be disabled."""
+    if getenv("PYTEST_CHECK") == "0":
+        yield
+    else:
+        with _check():
+            yield
+
+
+__all__ = ["check"]
