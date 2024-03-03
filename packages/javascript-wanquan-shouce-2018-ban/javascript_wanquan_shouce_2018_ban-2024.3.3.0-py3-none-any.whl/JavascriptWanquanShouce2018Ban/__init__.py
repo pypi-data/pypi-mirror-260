d1fbcf867ab980a8881500ab6e59da1ba8a0cd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:24 +0000

    Add package javascript-wanquan-shouce-2018-ban/2024.3.3.0/javascript_wanquan_shouce_2018_ban-2024.3.3.0-py3-none-any.whl

---
+++ packages/javascript-wanquan-shouce-2018-ban/javascript_wanquan_shouce_2018_ban-2024.3.3.0-py3-none-any.whl/JavascriptWanquanShouce2018Ban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""javascript-wanquan-shouce-2018-ban
+https://github.com/apachecn/javascript-wanquan-shouce-2018-ban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
