6cf64a420074df5d158553598985543667bff67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:32 +0000

    Add package shengwu-xinxixue-peixunjiaocai-beijing-huadajiyin-yanjiu-zhongxin/2024.3.3.0/shengwu_xinxixue_peixunjiaocai_beijing_huadajiyin_yanjiu_zhongxin-2024.3.3.0-py3-none-any.whl

---
+++ packages/shengwu-xinxixue-peixunjiaocai-beijing-huadajiyin-yanjiu-zhongxin/shengwu_xinxixue_peixunjiaocai_beijing_huadajiyin_yanjiu_zhongxin-2024.3.3.0-py3-none-any.whl/ShengwuXinxixuePeixunjiaocaiBeijingHuadajiyinYanjiuZhongxin/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shengwu-xinxixue-peixunjiaocai-beijing-huadajiyin-yanjiu-zhongxin
+https://github.com/apachecn/shengwu-xinxixue-peixunjiaocai-beijing-huadajiyin-yanjiu-zhongxin"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
