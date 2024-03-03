9e2c4a114ced2e8f9c2f81b352447e99fa2927f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:19 +0000

    Add package excel-modify/1.7/excel_modify-1.7.tar.gz

---
+++ packages/excel-modify/excel_modify-1.7.tar.gz/excel_modify-1.7/Excel_modify/edit_share_comment.py
@@ -0,0 +1,11 @@
+from ctypes import c_char_p, CDLL
+
+
+class EditSharedComment:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self._lib = CDLL('Excel_modify/dll/DrCinCo.dll')
+
+    def update_multiple_cells(self, operation_details):
+        self._lib.UpdateMultipleExcelCells(c_char_p(self.file_path.encode('utf-8')),
+                                           c_char_p(operation_details.encode('utf-8')))
