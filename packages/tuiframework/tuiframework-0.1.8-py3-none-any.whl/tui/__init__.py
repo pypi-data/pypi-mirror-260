c18dd92b5ef89a8c080ca8a25284023a4852bfbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:46 +0000

    Add package tuiframework/0.1.8/tuiframework-0.1.8-py3-none-any.whl

---
+++ packages/tuiframework/tuiframework-0.1.8-py3-none-any.whl/tui/__init__.py
@@ -0,0 +1,7 @@
+from tui.application import tui
+from tui.response import Meta, PageResponse, TitleTemplate
+from tui.version import VERSION
+
+__version__ = VERSION
+
+__all__ = ["tui", "PageResponse", "Meta", "TitleTemplate"]
