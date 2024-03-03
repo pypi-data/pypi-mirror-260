a182ce149f6cbe6f986021716ec4225e696c842d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:30 +0000

    Add package eric-ide/24.3/eric_ide-24.3-py3-none-any.whl

---
+++ packages/eric-ide/eric_ide-24.3-py3-none-any.whl/eric7/Plugins/CheckerPlugins/CodeStyleChecker/Security/Checks/sshNoHostKeyVerification.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2020 - 2024 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module implementing a check for use of mako templates.
+"""
+
+#
+# This is a modified version of the one found in the bandit package.
+#
+# Original Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import ast
+
+
+def getChecks():
+    """
+    Public method to get a dictionary with checks handled by this module.
+
+    @return dictionary containing checker lists containing checker function and
+        list of codes
+    @rtype dict
+    """
+    return {
+        "Call": [
+            (checkSshNoHostKeyVerification, ("S507",)),
+        ],
+    }
+
+
+def checkSshNoHostKeyVerification(reportError, context, config):  # noqa: U100
+    """
+    Function to check for use of mako templates.
+
+    @param reportError function to be used to report errors
+    @type func
+    @param context security context object
+    @type SecurityContext
+    @param config dictionary with configuration data
+    @type dict
+    """
+    if (
+        context.isModuleImportedLike("paramiko")
+        and context.callFunctionName == "set_missing_host_key_policy"
+        and context.node.args
+    ):
+        policyArgument = context.node.args[0]
+
+        policyArgumentValue = None
+        if isinstance(policyArgument, ast.Attribute):
+            policyArgumentValue = policyArgument.attr
+        elif isinstance(policyArgument, ast.Call):
+            policyArgumentValue = policyArgument.func.attr
+
+        if policyArgumentValue in ["AutoAddPolicy", "WarningPolicy"]:
+            reportError(
+                context.node.lineno - 1,
+                context.node.col_offset,
+                "S507",
+                "H",
+                "M",
+            )
