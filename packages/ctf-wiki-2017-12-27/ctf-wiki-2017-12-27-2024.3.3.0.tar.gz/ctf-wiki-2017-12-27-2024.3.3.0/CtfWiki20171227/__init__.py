a7046a45f9d8031ce1561845ea69e118f9a4dfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:21 +0000

    Add package ctf-wiki-2017-12-27/2024.3.3.0/ctf-wiki-2017-12-27-2024.3.3.0.tar.gz

---
+++ packages/ctf-wiki-2017-12-27/ctf-wiki-2017-12-27-2024.3.3.0.tar.gz/ctf-wiki-2017-12-27-2024.3.3.0/CtfWiki20171227/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ctf-wiki-2017-12-27
+https://github.com/apachecn/ctf-wiki-2017-12-27"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
