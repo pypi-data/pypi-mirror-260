fe5ce2955a6871122dc964a78715d26f5834d526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:03:48 +0000

    Add package lm-deezer-bf-dec/0.0.1b4/lm_deezer_bf_dec-0.0.1b4.tar.gz

---
+++ packages/lm-deezer-bf-dec/lm_deezer_bf_dec-0.0.1b4.tar.gz/lm_deezer_bf_dec-0.0.1b4/test.py
@@ -0,0 +1,7 @@
+from lm_deezer_bf_dec import decrypt_track
+
+decrypt_track(
+	'2614673372',
+	'https://e-cdns-proxy-b.dzcdn.net/mobile/1/df21757fa788b15dbe6a8b0c1b22867153bb4f46b1895763ea787566264b88fb8473feb984366fe360bf42e06a2c56c5ea8052a9642485bda8eb57e520705d0d29b9fe716df98d162821e7977fd3dd1c063ef54d67c219821901e1ef8d31ed56',
+	'All This Time - Norah Jones USUM72318640 (MP3_128).mp3'
+)
