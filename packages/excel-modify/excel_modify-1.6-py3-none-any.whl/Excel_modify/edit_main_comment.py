b92035b7e924d81a303e9e41bf4a2eb754416226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:11 +0000

    Add package excel-modify/1.6/excel_modify-1.6-py3-none-any.whl

---
+++ packages/excel-modify/excel_modify-1.6-py3-none-any.whl/Excel_modify/edit_main_comment.py
@@ -0,0 +1,20 @@
+from excel_modify.edit_excel import EditExcel
+
+
+class EditMainComment(EditExcel):
+    def __init__(self, file_path, cell_comment):
+        self._cell_comment = cell_comment
+        super().__init__(file_path, r"xl/comments1.xml")
+
+    def _get_value(self, cell):
+        cell_number = "".join([x for x in str(cell) if x.isnumeric()])
+        comments = self._path_content.find_all('comment')
+        for comment in comments:
+            ref = comment['ref']
+            if ref == self._cell_comment + cell_number:
+                return comment.find('t')
+
+    def set_comment_change(self, cell: str | int, value):
+        old = self._get_value(cell)
+        if old:
+            old.string = value
