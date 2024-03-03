ed1869f7bea02e8c6fbcaa6135b3c53b3808acd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:03:12 +0000

    Add package lino/24.3.0/lino-24.3.0.tar.gz

---
+++ packages/lino/lino-24.3.0.tar.gz/lino-24.3.0/lino/modlib/memo/views.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.views.generic import View
+from lino.core.views import json_response
+
+
+class Suggestions(View):
+
+    def get(self, request):
+        suggesters = settings.SITE.plugins.memo.parser.suggesters
+        trigger = request.GET.get("trigger")
+        query = request.GET.get("query")
+        return json_response(
+            {"suggestions": list(suggesters[trigger].get_suggestions(query))})
