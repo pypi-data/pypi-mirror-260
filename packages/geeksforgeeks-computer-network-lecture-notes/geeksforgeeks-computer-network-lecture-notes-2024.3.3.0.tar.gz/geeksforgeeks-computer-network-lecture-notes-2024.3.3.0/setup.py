94b2200d5253313302b1d5c73ceeb55f58f9971d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:36 +0000

    Add package geeksforgeeks-computer-network-lecture-notes/2024.3.3.0/geeksforgeeks-computer-network-lecture-notes-2024.3.3.0.tar.gz

---
+++ packages/geeksforgeeks-computer-network-lecture-notes/geeksforgeeks-computer-network-lecture-notes-2024.3.3.0.tar.gz/geeksforgeeks-computer-network-lecture-notes-2024.3.3.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import GeeksforgeeksComputerNetworkLectureNotes
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('GeeksforgeeksComputerNetworkLectureNotes'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="geeksforgeeks-computer-network-lecture-notes",
+    version=GeeksforgeeksComputerNetworkLectureNotes.__version__,
+    url="https://github.com/apachecn/geeksforgeeks-computer-network-lecture-notes",
+    author=GeeksforgeeksComputerNetworkLectureNotes.__author__,
+    author_email=GeeksforgeeksComputerNetworkLectureNotes.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="GeeksForGeeks Computer Network Lecture Notes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "geeksforgeeks-computer-network-lecture-notes=GeeksforgeeksComputerNetworkLectureNotes.__main__:main",
+            "GeeksforgeeksComputerNetworkLectureNotes=GeeksforgeeksComputerNetworkLectureNotes.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
