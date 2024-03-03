a62f15e99a46858faaafc900f88002df5b7a009b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:03:09 +0000

    Add package calculus-made-easy-2/2024.3.3.0/calculus-made-easy-2-2024.3.3.0.tar.gz

---
+++ packages/calculus-made-easy-2/calculus-made-easy-2-2024.3.3.0.tar.gz/calculus-made-easy-2-2024.3.3.0/README.md
@@ -0,0 +1,27 @@
+# CALCULUS MADE EASY
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/calculus-made-easy
+docker run -tid -p <port>:80 apachecn0/calculus-made-easy
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install calculus-made-easy
+calculus-made-easy <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g calculus-made-easy
+calculus-made-easy <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/calculus-made-easy-2/calculus-made-easy-2-2024.3.3.0.tar.gz/calculus-made-easy-2-2024.3.3.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import CalculusMadeEasy2
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('CalculusMadeEasy2'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="calculus-made-easy-2",
+    version=CalculusMadeEasy2.__version__,
+    url="https://github.com/apachecn/calculus-made-easy-2",
+    author=CalculusMadeEasy2.__author__,
+    author_email=CalculusMadeEasy2.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="CALCULUS MADE EASY",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "calculus-made-easy-2=CalculusMadeEasy2.__main__:main",
+            "CalculusMadeEasy2=CalculusMadeEasy2.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
