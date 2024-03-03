b2f6b0737e470bdf5ec0698b9508c2ec504167d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:04:15 +0000

    Add package rshare/0.1.23/rshare-0.1.23.tar.gz

---
+++ packages/rshare/rshare-0.1.23.tar.gz/rshare-0.1.23/rshare/rshare.pyi
@@ -0,0 +1,9 @@
+# rshare.pyi
+from typing import List
+import numpy as np
+
+def akversion(name: str, url:str) -> str: ...
+
+def get_title(url: str) -> str: ...
+
+def calculate_moving_average_rs(data: np.ndarray, window_size: int) -> np.ndarray: ...
