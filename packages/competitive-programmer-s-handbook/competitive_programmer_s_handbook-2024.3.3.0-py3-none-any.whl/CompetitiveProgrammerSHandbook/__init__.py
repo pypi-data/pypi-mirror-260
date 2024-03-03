87c9a7bd81d68e8f4ca442b3568f00871ed8df36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:07 +0000

    Add package competitive-programmer-s-handbook/2024.3.3.0/competitive_programmer_s_handbook-2024.3.3.0-py3-none-any.whl

---
+++ packages/competitive-programmer-s-handbook/competitive_programmer_s_handbook-2024.3.3.0-py3-none-any.whl/CompetitiveProgrammerSHandbook/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""competitive-programmer-s-handbook
+https://github.com/apachecn/competitive-programmer-s-handbook"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
