bde1d7d0ef58b1b7006f3850a770fbd9791b3434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:10 +0000

    Add package computer-networks-a-systems-approach-5th-ed/2024.3.3.0/computer_networks_a_systems_approach_5th_ed-2024.3.3.0-py3-none-any.whl

---
+++ packages/computer-networks-a-systems-approach-5th-ed/computer_networks_a_systems_approach_5th_ed-2024.3.3.0-py3-none-any.whl/ComputerNetworksASystemsApproach5thEd/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""computer-networks-a-systems-approach-5th-ed
+https://github.com/apachecn/computer-networks-a-systems-approach-5th-ed"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
