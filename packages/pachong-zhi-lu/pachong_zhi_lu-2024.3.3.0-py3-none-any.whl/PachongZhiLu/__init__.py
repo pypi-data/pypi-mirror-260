828fca43ec338ed52398e0fea7b08e4980464f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:35 +0000

    Add package pachong-zhi-lu/2024.3.3.0/pachong_zhi_lu-2024.3.3.0-py3-none-any.whl

---
+++ packages/pachong-zhi-lu/pachong_zhi_lu-2024.3.3.0-py3-none-any.whl/PachongZhiLu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pachong-zhi-lu
+https://github.com/apachecn/pachong-zhi-lu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
