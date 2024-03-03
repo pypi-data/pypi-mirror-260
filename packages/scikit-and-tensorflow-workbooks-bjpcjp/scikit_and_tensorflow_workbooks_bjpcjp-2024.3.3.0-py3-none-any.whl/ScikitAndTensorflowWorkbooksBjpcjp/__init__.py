e104731e1d434dad583325cc9afa53604e069c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:23 +0000

    Add package scikit-and-tensorflow-workbooks-bjpcjp/2024.3.3.0/scikit_and_tensorflow_workbooks_bjpcjp-2024.3.3.0-py3-none-any.whl

---
+++ packages/scikit-and-tensorflow-workbooks-bjpcjp/scikit_and_tensorflow_workbooks_bjpcjp-2024.3.3.0-py3-none-any.whl/ScikitAndTensorflowWorkbooksBjpcjp/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""scikit-and-tensorflow-workbooks-bjpcjp
+https://github.com/apachecn/scikit-and-tensorflow-workbooks-bjpcjp"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
