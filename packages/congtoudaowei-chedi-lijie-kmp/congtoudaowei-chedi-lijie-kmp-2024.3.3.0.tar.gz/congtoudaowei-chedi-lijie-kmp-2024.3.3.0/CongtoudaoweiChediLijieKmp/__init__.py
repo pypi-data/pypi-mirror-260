c5e2725eaafe9229680cf66056d6430d8a7d2393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:14 +0000

    Add package congtoudaowei-chedi-lijie-kmp/2024.3.3.0/congtoudaowei-chedi-lijie-kmp-2024.3.3.0.tar.gz

---
+++ packages/congtoudaowei-chedi-lijie-kmp/congtoudaowei-chedi-lijie-kmp-2024.3.3.0.tar.gz/congtoudaowei-chedi-lijie-kmp-2024.3.3.0/CongtoudaoweiChediLijieKmp/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""congtoudaowei-chedi-lijie-kmp
+https://github.com/apachecn/congtoudaowei-chedi-lijie-kmp"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
