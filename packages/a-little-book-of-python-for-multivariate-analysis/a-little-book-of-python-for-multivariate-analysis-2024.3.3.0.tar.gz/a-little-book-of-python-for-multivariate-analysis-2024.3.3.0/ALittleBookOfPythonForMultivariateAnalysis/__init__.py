d74b731e2cfb68ba7c43459659cc2dd581687140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:03:13 +0000

    Add package a-little-book-of-python-for-multivariate-analysis/2024.3.3.0/a-little-book-of-python-for-multivariate-analysis-2024.3.3.0.tar.gz

---
+++ packages/a-little-book-of-python-for-multivariate-analysis/a-little-book-of-python-for-multivariate-analysis-2024.3.3.0.tar.gz/a-little-book-of-python-for-multivariate-analysis-2024.3.3.0/ALittleBookOfPythonForMultivariateAnalysis/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""a-little-book-of-python-for-multivariate-analysis
+https://github.com/apachecn/a-little-book-of-python-for-multivariate-analysis"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
