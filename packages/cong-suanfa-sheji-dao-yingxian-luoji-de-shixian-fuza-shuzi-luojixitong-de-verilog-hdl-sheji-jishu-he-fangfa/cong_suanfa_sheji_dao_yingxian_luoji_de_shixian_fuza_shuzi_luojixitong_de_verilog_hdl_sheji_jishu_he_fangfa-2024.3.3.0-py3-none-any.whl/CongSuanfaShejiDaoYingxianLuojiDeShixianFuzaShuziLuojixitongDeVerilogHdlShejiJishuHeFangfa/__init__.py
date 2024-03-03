1d1bd68e46b60c41a85fc8a79a3df30e05d7f05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:13 +0000

    Add package cong-suanfa-sheji-dao-yingxian-luoji-de-shixian-fuza-shuzi-luojixitong-de-verilog-hdl-sheji-jishu-he-fangfa/2024.3.3.0/cong_suanfa_sheji_dao_yingxian_luoji_de_shixian_fuza_shuzi_luojixitong_de_verilog_hdl_sheji_jishu_he_fangfa-2024.3.3.0-py3-none-any.whl

---
+++ packages/cong-suanfa-sheji-dao-yingxian-luoji-de-shixian-fuza-shuzi-luojixitong-de-verilog-hdl-sheji-jishu-he-fangfa/cong_suanfa_sheji_dao_yingxian_luoji_de_shixian_fuza_shuzi_luojixitong_de_verilog_hdl_sheji_jishu_he_fangfa-2024.3.3.0-py3-none-any.whl/CongSuanfaShejiDaoYingxianLuojiDeShixianFuzaShuziLuojixitongDeVerilogHdlShejiJishuHeFangfa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""cong-suanfa-sheji-dao-yingxian-luoji-de-shixian-fuza-shuzi-luojixitong-de-verilog-hdl-sheji-jishu-he-fangfa
+https://github.com/apachecn/cong-suanfa-sheji-dao-yingxian-luoji-de-shixian-fuza-shuzi-luojixitong-de-verilog-hdl-sheji-jishu-he-fangfa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
