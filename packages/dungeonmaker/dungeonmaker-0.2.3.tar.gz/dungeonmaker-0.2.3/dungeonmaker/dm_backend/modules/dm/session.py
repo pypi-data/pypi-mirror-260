29894ece26bc438ef4a4e3cbdf4ca7e41d7cb39a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:04:14 +0000

    Add package dungeonmaker/0.2.3/dungeonmaker-0.2.3.tar.gz

---
+++ packages/dungeonmaker/dungeonmaker-0.2.3.tar.gz/dungeonmaker-0.2.3/dungeonmaker/dm_backend/modules/dm/session.py
@@ -0,0 +1,53 @@
+"""
+Submodule for database connection.
+"""
+from .dba import BaseDatabaseAbstraction, DatabaseAbstractionSelector
+from . import dungeon
+
+_database_abstractions = []
+
+def add_database_abstraction(dba):
+    """
+    Add a database abstraction.
+    """
+    assert isinstance(dba, BaseDatabaseAbstraction)
+    _database_abstractions.append(dba)
+
+database_abstraction = DatabaseAbstractionSelector(_database_abstractions)
+
+
+def get_popular_tab(*, offset : int = 0, amount : int = 20):
+    """
+    Get a default of 20 dungeons with no offset from the most popular dungeons.
+    """
+    return [dungeon.Dungeon(**dungeon_data) for dungeon_data in database_abstraction.sorted_dungeons(offset=offset, amount=amount)]
+
+def get_random_tab(*, amount : int = 20):
+    """
+    Get a default of 20 dungeons of random dungeons. 
+    """
+    return [dungeon.Dungeon(**dungeon_data) for dungeon_data in database_abstraction.random_dungeons(amount=amount)]
+
+def get_newest_tab(*, offset : int = 0, amount : int = 20):
+    """
+    Get a default of 20 dungeons of the newest dungeons. 
+    """
+    return [dungeon.Dungeon(**dungeon_data) for dungeon_data in database_abstraction.sorted_dungeons(offset=offset, amount=amount, field="creation_time", aggregation=[])]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
