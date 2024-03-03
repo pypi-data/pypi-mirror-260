d92ef3e8a06002f1cc6af52ed607bd4f924a45c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:58 +0000

    Add package fake-bge-module-latest/20240303/fake-bge-module-latest-20240303.tar.gz

---
+++ packages/fake-bge-module-latest/fake-bge-module-latest-20240303.tar.gz/fake-bge-module-latest-20240303/bl_keymap_utils/__init__.py
@@ -0,0 +1,9 @@
+import sys
+import typing
+from . import keymap_from_toolbar
+from . import versioning
+from . import io
+from . import keymap_hierarchy
+from . import platform_helpers
+
+GenericType = typing.TypeVar("GenericType")
