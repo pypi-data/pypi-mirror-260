19e62e1134de77fabe68786b5896fb59549712d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:03:08 +0000

    Add package amazon-india-affilate/0.1/amazon-india-affilate-0.1.tar.gz

---
+++ packages/amazon-india-affilate/amazon-india-affilate-0.1.tar.gz/amazon-india-affilate-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amazon-india-affilate',
+    version='0.1',
+    description='A Python library designed to simplify the process of converting Amazon India URLs into affiliate links. This library is not affiliated with Amazon or its official services.',
+    author='Aby Varghese',
+    packages=find_packages(),
+)
