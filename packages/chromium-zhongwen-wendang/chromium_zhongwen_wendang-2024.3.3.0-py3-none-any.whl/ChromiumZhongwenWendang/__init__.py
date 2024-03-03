f69b5fbf74a2312b54fb8275072d620b78d582ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:06 +0000

    Add package chromium-zhongwen-wendang/2024.3.3.0/chromium_zhongwen_wendang-2024.3.3.0-py3-none-any.whl

---
+++ packages/chromium-zhongwen-wendang/chromium_zhongwen_wendang-2024.3.3.0-py3-none-any.whl/ChromiumZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chromium-zhongwen-wendang
+https://github.com/apachecn/chromium-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
