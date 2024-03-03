8e52658f009b788f098fedcfdcf4ac29b42b2546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:03:31 +0000

    Add package SageMakerWrapper/0.1.7/SageMakerWrapper-0.1.7.tar.gz

---
+++ packages/SageMakerWrapper/SageMakerWrapper-0.1.7.tar.gz/SageMakerWrapper-0.1.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SageMakerWrapper",
+    version="0.1.7",
+    packages=find_packages(),
+    install_requires=[
+        "sagemaker",
+        "boto3",
+        "python-dotenv",
+    ],
+    author="Mattias Kockum",
+    author_email="mattias@kockum.net",
+    description="A simple wrapper for AWS Sagemaker",
+    url="https://github.com/MattiasKockum/SageMakerWrapper",
+)
