9f8842e70afc2f9f0f24b594cf4fb9ef8270f780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:03:12 +0000

    Add package lingulio/1.13.0/lingulio-1.13.0.tar.gz

---
+++ packages/lingulio/lingulio-1.13.0.tar.gz/lingulio-1.13.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+VERSION = '1.13.0'
+DESCRIPTION = 'A Python library based on Google Gemini LLM to perform basic and advanced natural language processing (NLP) tasks'
+LONG_DESCRIPTION = 'Basiclingua is a Gemini LLM based Python library that provides functionalities for linguistic tasks such as tokenization, stemming, lemmatization, and many others.'
+
+# Setting up
+setup(
+    name="lingulio",
+    version=VERSION,
+    author="fareed, khan, sad",
+    author_email="fareedhassankhan12@gmail.com, sad@gmail.com",
+    description=DESCRIPTION,
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["google-generativeai", "grpcio", "grpcio-tools"],  # Add any dependencies here
+    keywords=['python', 'NLP'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
