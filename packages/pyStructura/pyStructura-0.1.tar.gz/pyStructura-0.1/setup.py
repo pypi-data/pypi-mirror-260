3f222508a74ce855332023199035d65cc20a7a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:25 +0000

    Add package pyStructura/0.1/pyStructura-0.1.tar.gz

---
+++ packages/pyStructura/pyStructura-0.1.tar.gz/pyStructura-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, Extension
+
+setup(
+    name='pyStructura',
+    version='0.1',
+    author='Sukriti Sharma',
+    author_email='1106.sharma@gmail.com',
+    description='A library for advanced data structures in Python',
+    packages=['pystructura'],
+    install_requires=[],
+    python_requires='>=3.7.4',
+)
