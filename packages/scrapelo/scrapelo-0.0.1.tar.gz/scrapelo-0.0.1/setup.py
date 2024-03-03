9d289b4b90c9074d29db00155ade3fc14b161667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:35 +0000

    Add package scrapelo/0.0.1/scrapelo-0.0.1.tar.gz

---
+++ packages/scrapelo/scrapelo-0.0.1.tar.gz/scrapelo-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+# from pathlib import Path
+# this_directory = Path(__file__).parent
+# long_description = (this_directory / "README.md").read_text()
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Package for scraping data from clubelo.com'
+LONG_DESCRIPTION = '''
+Package provides functions for obtaining ELO scores for clubs provided by clubelo.com.
+As for now, it only enables to get the current ELO of single club
+or the clubs featured on the website for the given country or competition.
+'''
+
+setup(
+        name="scrapelo", 
+        version=VERSION,
+        author="Aleks Kapich",
+        author_email="aleks.kapich@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['requests', 'bs4', 'pandas'],
+        keywords=['python', 'football', 'ELO', 'clubelo', 'scraping', 'data'],
+        classifiers= [
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Other Audience",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
