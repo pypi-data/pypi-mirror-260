0d7c1f4a2cd7f278fd974bfd4451e57d22ccea66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:03:38 +0000

    Add package ncompass/0.0.6.post14/ncompass-0.0.6.post14-py3-none-any.whl

---
+++ packages/ncompass/ncompass-0.0.6.post14-py3-none-any.whl/ncompass/async_utils.py
@@ -0,0 +1,19 @@
+import json
+import aiohttp
+import asyncio
+from ncompass.errors import error_msg
+
+async def async_get_next(ait):
+    try:
+        obj = await ait.__anext__()
+        return False, obj
+    except StopAsyncIteration:
+        return True, None
+
+def get_sync_generator_from_async(async_gen, loop):
+    ait = async_gen.__aiter__()
+    # loop = asyncio.get_event_loop()
+    while True:
+        done, res = loop.run_until_complete(async_get_next(ait))
+        if done: break
+        yield res
