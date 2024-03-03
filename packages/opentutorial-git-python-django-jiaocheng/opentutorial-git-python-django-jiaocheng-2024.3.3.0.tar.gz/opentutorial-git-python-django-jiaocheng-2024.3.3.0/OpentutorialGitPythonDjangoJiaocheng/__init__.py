e10703e521edddabf6c05b7d55ed2e3f3ce0f850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:35 +0000

    Add package opentutorial-git-python-django-jiaocheng/2024.3.3.0/opentutorial-git-python-django-jiaocheng-2024.3.3.0.tar.gz

---
+++ packages/opentutorial-git-python-django-jiaocheng/opentutorial-git-python-django-jiaocheng-2024.3.3.0.tar.gz/opentutorial-git-python-django-jiaocheng-2024.3.3.0/OpentutorialGitPythonDjangoJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""opentutorial-git-python-django-jiaocheng
+https://github.com/apachecn/opentutorial-git-python-django-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
