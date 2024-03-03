182609eabde0f543706c7749e59013baa610bd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:31 +0000

    Add package mongodb-rumen-zhinan/2024.3.3.0/mongodb_rumen_zhinan-2024.3.3.0-py3-none-any.whl

---
+++ packages/mongodb-rumen-zhinan/mongodb_rumen_zhinan-2024.3.3.0-py3-none-any.whl/MongodbRumenZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mongodb-rumen-zhinan
+https://github.com/apachecn/mongodb-rumen-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
