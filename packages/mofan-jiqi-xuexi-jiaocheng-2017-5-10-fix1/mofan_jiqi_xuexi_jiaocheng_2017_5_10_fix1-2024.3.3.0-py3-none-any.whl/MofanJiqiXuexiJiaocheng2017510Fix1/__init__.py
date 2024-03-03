ea761e25b5952a1bfc19b7129be8039396f7be23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:30 +0000

    Add package mofan-jiqi-xuexi-jiaocheng-2017-5-10-fix1/2024.3.3.0/mofan_jiqi_xuexi_jiaocheng_2017_5_10_fix1-2024.3.3.0-py3-none-any.whl

---
+++ packages/mofan-jiqi-xuexi-jiaocheng-2017-5-10-fix1/mofan_jiqi_xuexi_jiaocheng_2017_5_10_fix1-2024.3.3.0-py3-none-any.whl/MofanJiqiXuexiJiaocheng2017510Fix1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mofan-jiqi-xuexi-jiaocheng-2017-5-10-fix1
+https://github.com/apachecn/mofan-jiqi-xuexi-jiaocheng-2017-5-10-fix1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
