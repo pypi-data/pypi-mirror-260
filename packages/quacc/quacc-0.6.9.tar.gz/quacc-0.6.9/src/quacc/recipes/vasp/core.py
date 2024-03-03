c07b73bdaa14e04c146dead29d5beefe72458853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:03:07 +0000

    Add package quacc/0.6.9/quacc-0.6.9.tar.gz

---
+++ packages/quacc/quacc-0.6.9.tar.gz/quacc-0.6.9/src/quacc/recipes/vasp/core.py
@@ -0,0 +1,175 @@
+"""Core recipes for VASP."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from quacc import flow, job
+from quacc.recipes.vasp._base import base_fn
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from ase import Atoms
+
+    from quacc.schemas._aliases.vasp import DoubleRelaxSchema, VaspSchema
+    from quacc.utils.files import Filenames, SourceDirectory
+
+
+@job
+def static_job(
+    atoms: Atoms,
+    preset: str | None = "BulkSet",
+    copy_files: SourceDirectory | dict[SourceDirectory, Filenames] | None = None,
+    **calc_kwargs,
+) -> VaspSchema:
+    """
+    Carry out a single-point calculation.
+
+    Parameters
+    ----------
+    atoms
+        Atoms object
+    preset
+        Preset to use from `quacc.calculators.vasp.presets`.
+    copy_files
+        Files to copy (and decompress) from source to the runtime directory.
+    **calc_kwargs
+        Custom kwargs for the Vasp calculator. Set a value to
+        `None` to remove a pre-existing key entirely. For a list of available
+        keys, refer to [quacc.calculators.vasp.vasp.Vasp][].
+
+    Returns
+    -------
+    VaspSchema
+        Dictionary of results from [quacc.schemas.vasp.vasp_summarize_run][].
+        See the type-hint for the data structure.
+    """
+
+    calc_defaults = {
+        "ismear": -5,
+        "laechg": True,
+        "lcharg": True,
+        "lreal": False,
+        "lwave": True,
+        "nedos": 5001,
+        "nsw": 0,
+    }
+    return base_fn(
+        atoms,
+        preset=preset,
+        calc_defaults=calc_defaults,
+        calc_swaps=calc_kwargs,
+        additional_fields={"name": "VASP Static"},
+        copy_files=copy_files,
+    )
+
+
+@job
+def relax_job(
+    atoms: Atoms,
+    preset: str | None = "BulkSet",
+    relax_cell: bool = True,
+    copy_files: SourceDirectory | dict[SourceDirectory, Filenames] | None = None,
+    **calc_kwargs,
+) -> VaspSchema:
+    """
+    Relax a structure.
+
+    Parameters
+    ----------
+    atoms
+        Atoms object
+    preset
+        Preset to use from `quacc.calculators.vasp.presets`.
+    relax_cell
+        True if a volume relaxation (ISIF = 3) should be performed. False if
+        only the positions (ISIF = 2) should be updated.
+    copy_files
+        Files to copy (and decompress) from source to the runtime directory.
+    **calc_kwargs
+        Custom kwargs for the Vasp calculator. Set a value to
+        `None` to remove a pre-existing key entirely. For a list of available
+        keys, refer to the [quacc.calculators.vasp.vasp.Vasp][] calculator.
+
+    Returns
+    -------
+    VaspSchema
+        Dictionary of results from [quacc.schemas.vasp.vasp_summarize_run][].
+        See the type-hint for the data structure.
+    """
+
+    calc_defaults = {
+        "ediffg": -0.02,
+        "isif": 3 if relax_cell else 2,
+        "ibrion": 2,
+        "isym": 0,
+        "lcharg": False,
+        "lwave": False,
+        "nsw": 200,
+        "symprec": 1e-8,
+    }
+    return base_fn(
+        atoms,
+        preset=preset,
+        calc_defaults=calc_defaults,
+        calc_swaps=calc_kwargs,
+        additional_fields={"name": "VASP Relax"},
+        copy_files=copy_files,
+    )
+
+
+@flow
+def double_relax_flow(
+    atoms: Atoms,
+    preset: str | None = "BulkSet",
+    relax_cell: bool = True,
+    relax1_kwargs: dict[str, Any] | None = None,
+    relax2_kwargs: dict[str, Any] | None = None,
+) -> DoubleRelaxSchema:
+    """
+    Double-relax a structure. This is particularly useful for a few reasons:
+
+    1. To carry out a cheaper pre-relaxation before the high-quality run.
+
+    2. To carry out a GGA calculation before a meta-GGA or hybrid calculation
+    that requires the GGA wavefunction.
+
+    3. To carry out volume relaxations where large changes in volume
+    can require a second relaxation to resolve forces.
+
+    Parameters
+    ----------
+    atoms
+        Atoms object
+    preset
+        Preset to use from `quacc.calculators.vasp.presets`.
+    relax_cell
+        True if a volume relaxation (ISIF = 3) should be performed. False if
+        only the positions (ISIF = 2) should be updated.
+    relax1_kwargs
+        Dictionary of custom kwargs for the first relaxation.
+    relax2_kwargs
+        Dictionary of custom kwargs for the second relaxation.
+
+    Returns
+    -------
+    DoubleRelaxSchema
+        Dictionary of results. See the type-hint for the data structure.
+    """
+    relax1_kwargs = relax1_kwargs or {}
+    relax2_kwargs = relax2_kwargs or {}
+
+    # Run first relaxation
+    summary1 = relax_job(atoms, preset=preset, relax_cell=relax_cell, **relax1_kwargs)
+
+    # Run second relaxation
+    summary2 = relax_job(
+        summary1["atoms"],
+        preset=preset,
+        relax_cell=relax_cell,
+        copy_files={summary1["dir_name"]: ["WAVECAR*"]},
+        **relax2_kwargs,
+    )
+
+    return {"relax1": summary1, "relax2": summary2}
---
+++ packages/quacc/quacc-0.6.9.tar.gz/quacc-0.6.9/src/quacc/schemas/atoms.py
@@ -0,0 +1,154 @@
+"""Schemas for storing metadata about Atoms objects."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+from ase.atoms import Atom, Atoms
+from emmet.core.structure import MoleculeMetadata, StructureMetadata
+from monty.json import jsanitize
+from pymatgen.io.ase import AseAtomsAdaptor
+
+from quacc.atoms.core import (
+    copy_atoms,
+    get_charge_attribute,
+    get_spin_multiplicity_attribute,
+)
+from quacc.utils.dicts import clean_task_doc
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from quacc.schemas._aliases.atoms import AtomsSchema
+
+
+def atoms_to_metadata(
+    atoms: Atoms,
+    charge_and_multiplicity: tuple[int, int] | None = None,
+    get_metadata: bool = True,
+    store_pmg: bool = True,
+    additional_fields: dict[str, Any] | None = None,
+) -> AtomsSchema:
+    """
+    Convert an ASE Atoms object to a dict suitable for storage in MongoDB.
+
+    Parameters
+    ----------
+    atoms
+        ASE Atoms object to store in {"atoms": atoms}
+    charge_and_multiplicity
+        Charge and spin multiplicity of the Atoms object, only used for Molecule
+        metadata.
+    get_metadata
+        Whether to store atoms metadata in the returned dict.
+    store_pmg
+        Whether to store the Pymatgen Structure/Molecule object in {"structure":
+        Structure} or {"molecule": Molecule}, respectively.
+    additional_fields
+        Additional fields to add to the document.
+
+    Returns
+    -------
+    AtomsSchema
+        Dict of metadata about the Atoms object.
+    """
+
+    additional_fields = additional_fields or {}
+    atoms = copy_atoms(atoms)
+    results = {}
+
+    # Set any charge or multiplicity keys
+    if not atoms.pbc.any():
+        _set_charge_and_spin(atoms, charge_and_multiplicity=charge_and_multiplicity)
+
+    # Strip the dummy atoms, if present
+    del atoms[[atom.index for atom in atoms if atom.symbol == "X"]]
+
+    # Get Atoms metadata, if requested. emmet already has built-in tools for
+    # generating pymatgen Structure/Molecule metadata, so we'll just use that.
+    if get_metadata:
+        if atoms.pbc.any():
+            struct = AseAtomsAdaptor().get_structure(atoms)
+            metadata = StructureMetadata().from_structure(struct).model_dump()
+            if store_pmg:
+                results["structure"] = struct
+        else:
+            mol = AseAtomsAdaptor().get_molecule(atoms, charge_spin_check=False)
+            metadata = MoleculeMetadata().from_molecule(mol).model_dump()
+            if store_pmg:
+                results["molecule"] = mol
+    else:
+        metadata = {}
+
+    # Copy the info flags as a separate entry in the DB for easy querying
+    results["atoms_info"] = _quacc_sanitize(atoms.info)
+
+    # Store Atoms object
+    results["atoms"] = atoms
+
+    # Combine the metadata and results dictionaries
+    atoms_doc_unsorted = metadata | results | additional_fields
+
+    return clean_task_doc(atoms_doc_unsorted)
+
+
+def _set_charge_and_spin(
+    atoms: Atoms, charge_and_multiplicity: tuple[int, int] | None = None
+) -> None:
+    """
+    Set the charge and spin multiplicity of an Atoms object.
+
+    Parameters
+    ----------
+    atoms
+        Atoms object
+    charge_and_multiplicity
+        Charge and spin multiplicity of the Atoms object, only used for Molecule
+        metadata.
+
+    Returns
+    -------
+    None
+        Modifies the Atoms object in place.
+    """
+
+    if charge_and_multiplicity:
+        charge = charge_and_multiplicity[0]
+        spin_multiplicity = charge_and_multiplicity[1]
+    else:
+        charge = get_charge_attribute(atoms)
+        spin_multiplicity = get_spin_multiplicity_attribute(atoms)
+
+    if charge is not None:
+        atoms.charge = charge
+    if spin_multiplicity is not None:
+        atoms.spin_multiplicity = spin_multiplicity
+
+
+def _quacc_sanitize(obj: Any) -> Any:
+    """
+    Sanitizes an object for storage in MongoDB.
+
+    This is an analogue of monty's jsanitize function but meant to serialize
+    Atom/Atoms objects as well.
+
+    Parameters
+    ----------
+    obj
+        Object to sanitize
+
+    Returns
+    -------
+    Any
+        Sanitized object
+    """
+    if isinstance(obj, (Atom, Atoms)):
+        obj = atoms_to_metadata(obj)
+    elif isinstance(obj, (list, tuple, np.ndarray)):
+        obj = [_quacc_sanitize(i) for i in obj]
+    elif isinstance(obj, dict):
+        obj = {k.__str__(): _quacc_sanitize(v) for k, v in obj.items()}
+    else:
+        obj = jsanitize(obj)
+    return obj
