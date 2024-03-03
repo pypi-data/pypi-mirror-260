2e5393ca3b8c63517c0684765a824c41d2ed2d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:58 +0000

    Add package fake-bge-module-latest/20240303/fake_bge_module_latest-20240303-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20240303-py3-none-any.whl/bl_i18n_utils/__init__.py
@@ -0,0 +1,11 @@
+import sys
+import typing
+from . import utils
+from . import utils_rtl
+from . import settings
+from . import bl_extract_messages
+from . import merge_po
+from . import utils_cli
+from . import utils_languages_menu
+
+GenericType = typing.TypeVar("GenericType")
