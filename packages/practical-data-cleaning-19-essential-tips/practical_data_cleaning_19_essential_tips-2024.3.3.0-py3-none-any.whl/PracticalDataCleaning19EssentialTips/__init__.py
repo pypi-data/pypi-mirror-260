41e5712896c40264dec7392ac72ee506ef8b7fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:45 +0000

    Add package practical-data-cleaning-19-essential-tips/2024.3.3.0/practical_data_cleaning_19_essential_tips-2024.3.3.0-py3-none-any.whl

---
+++ packages/practical-data-cleaning-19-essential-tips/practical_data_cleaning_19_essential_tips-2024.3.3.0-py3-none-any.whl/PracticalDataCleaning19EssentialTips/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""practical-data-cleaning-19-essential-tips
+https://github.com/apachecn/practical-data-cleaning-19-essential-tips"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
