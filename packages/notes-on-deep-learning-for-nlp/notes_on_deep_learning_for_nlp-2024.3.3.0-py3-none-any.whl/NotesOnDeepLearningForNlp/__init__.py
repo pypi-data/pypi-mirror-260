cb82535852ae94cbbec74ba80a4688a9e7ba88bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:33 +0000

    Add package notes-on-deep-learning-for-nlp/2024.3.3.0/notes_on_deep_learning_for_nlp-2024.3.3.0-py3-none-any.whl

---
+++ packages/notes-on-deep-learning-for-nlp/notes_on_deep_learning_for_nlp-2024.3.3.0-py3-none-any.whl/NotesOnDeepLearningForNlp/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""notes-on-deep-learning-for-nlp
+https://github.com/apachecn/notes-on-deep-learning-for-nlp"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
