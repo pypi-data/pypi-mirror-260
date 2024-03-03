ce7c3203245a09091352869b3b0fd08ae57027a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:25 +0000

    Add package streamfy/0.1.3/streamfy-0.1.3.tar.gz

---
+++ packages/streamfy/streamfy-0.1.3.tar.gz/streamfy-0.1.3/streamfy/__init__.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+import os
+
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "streamfy",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/dist")
+    _component_func = components.declare_component(
+        "streamfy", path=build_dir)
+
+def taginput(data=[], placeholder="", key=None):
+    component_value = _component_func(data=data, placeholder=placeholder, key=key)
+    return component_value
+
+if not _RELEASE:
+    st.subheader("Start")
+    tags = taginput(data = ["A", "B", "C"], placeholder = "Choose letter")
+    st.write(tags)
+    st.subheader("End")
