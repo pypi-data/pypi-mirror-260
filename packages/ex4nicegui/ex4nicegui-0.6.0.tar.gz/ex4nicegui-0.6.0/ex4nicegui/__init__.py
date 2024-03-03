8807ffb1034caef38a77aacf8978f2f8153ed1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:43 +0000

    Add package ex4nicegui/0.6.0/ex4nicegui-0.6.0.tar.gz

---
+++ packages/ex4nicegui/ex4nicegui-0.6.0.tar.gz/ex4nicegui-0.6.0/ex4nicegui/__init__.py
@@ -0,0 +1,46 @@
+from ex4nicegui import reactive as rxui
+from ex4nicegui.utils.signals import (
+    ref_computed,
+    effect,
+    effect_refreshable,
+    to_raw,
+    is_ref,
+    to_ref,
+    to_value,
+    ref,
+    on,
+    event_batch,
+    _TMaybeRef as TMaybeRef,
+    Ref,
+    ReadonlyRef,
+    reactive,
+    deep_ref,
+    is_setter_ref,
+    batch,
+    is_reactive,
+)
+
+
+__all__ = [
+    "is_reactive",
+    "rxui",
+    "ref_computed",
+    "effect",
+    "effect_refreshable",
+    "is_ref",
+    "to_ref",
+    "to_value",
+    "ref",
+    "on",
+    "event_batch",
+    "TMaybeRef",
+    "Ref",
+    "ReadonlyRef",
+    "reactive",
+    "deep_ref",
+    "batch",
+    "to_raw",
+    "is_setter_ref",
+]
+
+__version__ = "0.6.0"
