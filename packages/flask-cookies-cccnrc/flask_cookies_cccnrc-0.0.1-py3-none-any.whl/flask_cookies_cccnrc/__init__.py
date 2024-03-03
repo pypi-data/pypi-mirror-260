7dfc7e6c28287da8ae06daf41d1b7ce99929140d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:04:08 +0000

    Add package flask-cookies-cccnrc/0.0.1/flask_cookies_cccnrc-0.0.1-py3-none-any.whl

---
+++ packages/flask-cookies-cccnrc/flask_cookies_cccnrc-0.0.1-py3-none-any.whl/flask_cookies_cccnrc/__init__.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from flask import Blueprint
+from warnings import warn
+
+
+class Cookies(object):
+    def __init__(self, GTM_ID=None, app=None):
+        if app is not None:
+            self.init_app(app)
+        self.GTM_ID = GTM_ID
+        if not GTM_ID:
+            GTM_ID_WARNING = '''\033[93m You must pass your Google Tag Manager ID to Cookies() initialization or it will \033[4mNOT be connected with your GTM account\033[0m\033[93m: "cookies = Cookies(<put-here-your-GTM-ID>)"\033[0m '''
+            warn(GTM_ID_WARNING)
+            # raise ValueError()
+
+    def init_app(self, app):
+        app.config.setdefault('BOOTSTRAP_LOCAL_SUBDOMAIN', None)
+
+        blueprint = Blueprint(
+            'cookies',
+            __name__,
+            template_folder='templates',
+            static_folder='static',
+            static_url_path=app.static_url_path + '/cookies')
+
+        def pass_gtm_id():
+            '''
+                this is to pass GTM ID without recording it
+                in plain code
+            '''
+            # GTM_ID = current_app.config['GTM_ID']
+            # GTM_ID = 'GTM-NXWSMNRF'
+            return self.GTM_ID
+
+        @app.context_processor
+        def inject_user_gtmID():
+            '''
+                pass the environmental variable GTM ID to all pages
+            '''
+            pass_gtm_id_dict = dict()
+            pass_gtm_id_dict.update(pass_gtm_id=pass_gtm_id)
+            return pass_gtm_id_dict
+
+        app.register_blueprint(blueprint)
