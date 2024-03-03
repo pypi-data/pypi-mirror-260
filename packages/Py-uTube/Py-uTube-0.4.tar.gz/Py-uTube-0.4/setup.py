cc6b666c83c70613aea8e93e8b3db723eba4d1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:04:51 +0000

    Add package Py-uTube/0.4/Py-uTube-0.4.tar.gz

---
+++ packages/Py-uTube/Py-uTube-0.4.tar.gz/Py-uTube-0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="Py-uTube",
+    version="0.4",
+    author="Brhoom",
+    author_email="hetari4all@gmail.com",
+    packages=find_packages(),
+    install_requires=["pytube", "inquirer",
+                      "yaspin", "requests", "rich", "inquirer", "termcolor"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    entry_points={
+        "console_scripts": [
+            "uTube=uTube:cli.app",
+            "utube=uTube:cli.app",
+        ],
+    },
+    python_requires=">=3.6",
+    long_description=description,
+    long_description_content_type="text/markdown",
+
+)
