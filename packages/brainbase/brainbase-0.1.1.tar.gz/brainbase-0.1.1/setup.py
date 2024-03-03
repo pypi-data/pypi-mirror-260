e36905ceebb80d793f0e0098994ccb22fd3d34e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:03:00 +0000

    Add package brainbase/0.1.1/brainbase-0.1.1.tar.gz

---
+++ packages/brainbase/brainbase-0.1.1.tar.gz/brainbase-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='brainbase',
+    version='0.1.1',    
+    description='Brainbase Python SDK',
+    url='https://github.com/brainbasehq',
+    author='Gokhan Egri',
+    author_email='gokhan@usebrainbase.xyz',
+    license='BSD 2-clause',
+    packages=['brainbase'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
