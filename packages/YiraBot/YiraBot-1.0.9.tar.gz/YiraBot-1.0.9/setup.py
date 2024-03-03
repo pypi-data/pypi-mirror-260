e3074f6ef3d559aa69eee4c4988e4cbe358994fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:03:30 +0000

    Add package YiraBot/1.0.9/YiraBot-1.0.9.tar.gz

---
+++ packages/YiraBot/YiraBot-1.0.9.tar.gz/YiraBot-1.0.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YiraBot',
+    version='1.0.9',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').readlines(),
+    author='Owen Orcan',
+    author_email='owenorcan@gmail.com',
+    url='https://github.com/OwenOrcan/Yirabot-Crawler',
+    license='GNU General Public License (Version 3)',
+    description="YiraBot: Simplifying Web Scraping for All. A user-friendly tool for developers and enthusiasts, offering command-line ease and Python integration. Ideal for research, SEO, and data collection.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    entry_points={
+        'console_scripts': [
+            'yirabot=yirabot.yirabot:main',
+        ],
+    },
+)
