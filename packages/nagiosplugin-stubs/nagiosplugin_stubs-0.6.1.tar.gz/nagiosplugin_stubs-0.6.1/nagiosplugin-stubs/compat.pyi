a259332465d24b63a387016f0fb5616ecf4f685e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:51 +0000

    Add package nagiosplugin-stubs/0.6.1/nagiosplugin_stubs-0.6.1.tar.gz

---
+++ packages/nagiosplugin-stubs/nagiosplugin_stubs-0.6.1.tar.gz/nagiosplugin_stubs-0.6.1/nagiosplugin-stubs/compat.pyi
@@ -0,0 +1,9 @@
+from typing import IO
+
+def TemporaryFile(
+    mode: str = ...,
+    encoding: str = ...,
+    suffix: str = ...,
+    prefix: str = ...,
+    dir: str = ...,
+) -> IO[str]: ...
---
+++ packages/nagiosplugin-stubs/nagiosplugin_stubs-0.6.1.tar.gz/nagiosplugin_stubs-0.6.1/nagiosplugin-stubs/resource.pyi
@@ -0,0 +1,8 @@
+from typing import Generator
+
+from nagiosplugin.metric import Metric
+
+class Resource:
+    name: str
+
+    def probe(self) -> list[Metric] | Metric | Generator[Metric, None, None]: ...
