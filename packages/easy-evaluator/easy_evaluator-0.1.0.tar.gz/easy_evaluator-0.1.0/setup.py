88f3f14f37309e87858745091ac32f5fe3ab4a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 03:04:53 +0000

    Add package easy-evaluator/0.1.0/easy_evaluator-0.1.0.tar.gz

---
+++ packages/easy-evaluator/easy_evaluator-0.1.0.tar.gz/easy_evaluator-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easy_evaluator',
+    version='0.1.0',
+    description='A library for easy evaluation of language models',
+    author='Anindyadeep',
+    author_email='proanindyadeep@gmail.com',
+    url='https://github.com/Anindyadeep/easy_eval',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here from requirements.txt
+        "fastapi==0.108.0",
+        "lm-eval==0.4.1"
+    ], 
+    include_package_data=True,
+)
