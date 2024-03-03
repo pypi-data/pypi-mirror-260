c699a061cc10157d249ddd701de71d74caea8697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:25 +0000

    Add package jiexi-juanji-shenjingwangluo-shendu-xuexi-shijian-shouce/2024.3.3.0/jiexi_juanji_shenjingwangluo_shendu_xuexi_shijian_shouce-2024.3.3.0-py3-none-any.whl

---
+++ packages/jiexi-juanji-shenjingwangluo-shendu-xuexi-shijian-shouce/jiexi_juanji_shenjingwangluo_shendu_xuexi_shijian_shouce-2024.3.3.0-py3-none-any.whl/JiexiJuanjiShenjingwangluoShenduXuexiShijianShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiexi-juanji-shenjingwangluo-shendu-xuexi-shijian-shouce
+https://github.com/apachecn/jiexi-juanji-shenjingwangluo-shendu-xuexi-shijian-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
