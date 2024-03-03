54d7c2563b25a3d8ffb1360329e40b850e1cf885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:27 +0000

    Add package shendu-qianghua-xuexi-zongshu-jianlun-jisuanji-weiqi-de-fazhan/2024.3.3.0/shendu_qianghua_xuexi_zongshu_jianlun_jisuanji_weiqi_de_fazhan-2024.3.3.0-py3-none-any.whl

---
+++ packages/shendu-qianghua-xuexi-zongshu-jianlun-jisuanji-weiqi-de-fazhan/shendu_qianghua_xuexi_zongshu_jianlun_jisuanji_weiqi_de_fazhan-2024.3.3.0-py3-none-any.whl/ShenduQianghuaXuexiZongshuJianlunJisuanjiWeiqiDeFazhan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shendu-qianghua-xuexi-zongshu-jianlun-jisuanji-weiqi-de-fazhan
+https://github.com/apachecn/shendu-qianghua-xuexi-zongshu-jianlun-jisuanji-weiqi-de-fazhan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
