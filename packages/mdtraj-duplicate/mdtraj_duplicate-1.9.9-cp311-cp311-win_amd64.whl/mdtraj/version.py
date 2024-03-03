1985af5ea066ce19649659aa117bb9193a8d6e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 14:03:16 +0000

    Add package mdtraj-duplicate/1.9.9/mdtraj_duplicate-1.9.9-cp311-cp311-win_amd64.whl

---
+++ packages/mdtraj-duplicate/mdtraj_duplicate-1.9.9-cp311-cp311-win_amd64.whl/mdtraj/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '1.9.9'
+short_version = '1.9.9'
+full_version = '1.9.9+fd7c66afd513de6228898bc148a3e3e658c0c169'
+git_revision = 'fd7c66afd513de6228898bc148a3e3e658c0c169'
+release = False
