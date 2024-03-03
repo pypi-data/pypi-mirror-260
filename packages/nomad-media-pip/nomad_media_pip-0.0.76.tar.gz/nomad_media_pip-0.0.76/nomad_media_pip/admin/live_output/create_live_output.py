b11dfc14d578cc23ff97b2118d95d26c0737d097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:47 +0000

    Add package nomad-media-pip/0.0.76/nomad_media_pip-0.0.76.tar.gz

---
+++ packages/nomad-media-pip/nomad_media_pip-0.0.76.tar.gz/nomad_media_pip-0.0.76/nomad_media_pip/admin/live_output/create_live_output.py
@@ -0,0 +1,51 @@
+from nomad_media_pip.exceptions.api_exception_handler import _api_exception_handler
+from nomad_media_pip.admin.live_output.get_output_types import _get_output_types
+
+import requests, json
+
+def _create_live_output(AUTH_TOKEN, URL, NAME, OUTPUT_TYPE, ENABLED, AUDIO_BITRATE,
+                        OUTPUT_STREAM_KEY, OUTPUT_URL, SECONDARY_OUTPUT_STREAM_KEY,
+                        SECONDARY_OUTPUT_URL, VIDEO_BITRATE, VIDEO_BITRATE_MODE,
+                        VIDEO_CODEC, VIDEO_FRAMES_PER_SECOND, VIDEO_HEIGHT, VIDEO_WIDTH,
+                        DEBUG):
+
+    API_URL = f"{URL}/liveOutputProfile"
+
+    HEADERS = {
+        'Content-Type': 'application/json',
+        "Authorization": "Bearer " + AUTH_TOKEN
+    }
+
+    OUTPUT_TYPES = _get_output_types(AUTH_TOKEN, URL, DEBUG)
+    OUTPUT_TYPE_OBJECT = next((item for item in OUTPUT_TYPES if item["description"] == OUTPUT_TYPE), None)
+
+    BODY = {
+        "name": NAME,
+        "outputType": OUTPUT_TYPE_OBJECT,
+        "enabled": ENABLED,
+        "audioBitrate": AUDIO_BITRATE,
+        "outputStreamKey": OUTPUT_STREAM_KEY,
+        "outputUrl": OUTPUT_URL,
+        "secondaryOutputStreamKey": SECONDARY_OUTPUT_STREAM_KEY,
+        "secondaryOutputUrl": SECONDARY_OUTPUT_URL,
+        "videoBitrate": VIDEO_BITRATE,
+        "videoBitrateMode": VIDEO_BITRATE_MODE,
+        "videoCodec": VIDEO_CODEC,
+        "videoFramesPerSecond": VIDEO_FRAMES_PER_SECOND,
+        "videoHeight": VIDEO_HEIGHT,
+        "videoWidth": VIDEO_WIDTH
+    }
+
+    if DEBUG:
+        print(f"URL: {API_URL},\nMETHOD: POST,\nBODY: {json.dumps(BODY)}")
+
+    try:
+        RESPONSE = requests.post(API_URL, headers=HEADERS, data=json.dumps(BODY))
+
+        if not RESPONSE.ok:
+            raise Exception()
+        
+        return RESPONSE.json()
+    
+    except:
+        _api_exception_handler(RESPONSE, "Create Live Output Failed")
