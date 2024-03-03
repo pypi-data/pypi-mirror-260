484d0c1a0f253ea24288aba326631ca335915a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-03 15:03:55 +0000

    Add package imessage-hp-package/0.1/imessage_hp_package-0.1.tar.gz

---
+++ packages/imessage-hp-package/imessage_hp_package-0.1.tar.gz/imessage_hp_package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='imessage_hp_package',
+    version='0.1',
+    description='imessage info',
+    # url='https://example.com',
+    author='hp Lin',
+    author_email='L_hp777@163.com',
+    py_modules=['imseeage_hp'],
+)
