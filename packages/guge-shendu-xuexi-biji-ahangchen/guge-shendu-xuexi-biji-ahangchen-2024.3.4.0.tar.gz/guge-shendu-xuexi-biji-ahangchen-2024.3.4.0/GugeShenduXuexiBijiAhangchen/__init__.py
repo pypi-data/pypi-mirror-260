937e8966b6e7303a6b423ede7892c55171618976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:03 +0000

    Add package guge-shendu-xuexi-biji-ahangchen/2024.3.4.0/guge-shendu-xuexi-biji-ahangchen-2024.3.4.0.tar.gz

---
+++ packages/guge-shendu-xuexi-biji-ahangchen/guge-shendu-xuexi-biji-ahangchen-2024.3.4.0.tar.gz/guge-shendu-xuexi-biji-ahangchen-2024.3.4.0/GugeShenduXuexiBijiAhangchen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""guge-shendu-xuexi-biji-ahangchen
+https://github.com/apachecn/guge-shendu-xuexi-biji-ahangchen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
