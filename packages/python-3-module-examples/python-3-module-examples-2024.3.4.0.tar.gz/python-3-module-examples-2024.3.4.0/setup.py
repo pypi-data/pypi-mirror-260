8800745242720afe67c121dafff72362d3dac724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:08 +0000

    Add package python-3-module-examples/2024.3.4.0/python-3-module-examples-2024.3.4.0.tar.gz

---
+++ packages/python-3-module-examples/python-3-module-examples-2024.3.4.0.tar.gz/python-3-module-examples-2024.3.4.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Python3ModuleExamples
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Python3ModuleExamples'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="python-3-module-examples",
+    version=Python3ModuleExamples.__version__,
+    url="https://github.com/apachecn/python-3-module-examples",
+    author=Python3ModuleExamples.__author__,
+    author_email=Python3ModuleExamples.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="Python 3 Module Examples",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "python-3-module-examples=Python3ModuleExamples.__main__:main",
+            "Python3ModuleExamples=Python3ModuleExamples.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
