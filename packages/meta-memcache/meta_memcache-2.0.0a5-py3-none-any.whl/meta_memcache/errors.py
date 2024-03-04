be7ba17cdc366d6c970bd60b04b66da18715d6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 16:03:45 +0000

    Add package meta-memcache/2.0.0a5/meta_memcache-2.0.0a5-py3-none-any.whl

---
+++ packages/meta-memcache/meta_memcache-2.0.0a5-py3-none-any.whl/meta_memcache/errors.py
@@ -0,0 +1,11 @@
+class MemcacheError(Exception):
+    pass
+
+
+class MemcacheServerError(MemcacheError):
+    def __init__(self, server: str, message: str) -> None:
+        self.server = server
+        super().__init__(message)
+
+
+class ServerMarkedDownError(MemcacheServerError): ...
