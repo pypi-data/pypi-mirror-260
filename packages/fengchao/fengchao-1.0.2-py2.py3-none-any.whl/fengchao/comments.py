c7c818774ceef588ad853a3bd7273f62ea9b7cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:20 +0000

    Add package fengchao/1.0.2/fengchao-1.0.2-py2.py3-none-any.whl

---
+++ packages/fengchao/fengchao-1.0.2-py2.py3-none-any.whl/fengchao/comments.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""
+# File       : comments.py
+# Time       ：2024/2/23 13:48
+# Author     ：andy
+# version    ：python 3.9
+"""
+
+LOCAL_URL = "http://192.168.1.233:6000/aigc/local_chat/"
+ONLINE_URL = "http://192.168.1.233:6000/aigc/chat/"
+MODELS_URL = "http://192.168.1.233:6000/aigc/models/"
+PROMPTS_URL = "http://192.168.1.233:6000/aigc/prompts/"
+KGS_URL = "http://192.168.1.233:6000/aigc/kgs"
