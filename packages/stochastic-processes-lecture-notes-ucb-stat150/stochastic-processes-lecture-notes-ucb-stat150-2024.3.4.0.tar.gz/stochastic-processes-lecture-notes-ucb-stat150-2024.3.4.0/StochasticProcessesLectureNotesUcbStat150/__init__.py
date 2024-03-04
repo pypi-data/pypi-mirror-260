8428023c0580f9e74ec97e351a47df2c30171f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:32 +0000

    Add package stochastic-processes-lecture-notes-ucb-stat150/2024.3.4.0/stochastic-processes-lecture-notes-ucb-stat150-2024.3.4.0.tar.gz

---
+++ packages/stochastic-processes-lecture-notes-ucb-stat150/stochastic-processes-lecture-notes-ucb-stat150-2024.3.4.0.tar.gz/stochastic-processes-lecture-notes-ucb-stat150-2024.3.4.0/StochasticProcessesLectureNotesUcbStat150/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""stochastic-processes-lecture-notes-ucb-stat150
+https://github.com/apachecn/stochastic-processes-lecture-notes-ucb-stat150"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
