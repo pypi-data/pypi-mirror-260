c1c274b2c5fc6785547e9977cdce433d606740c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:30 +0000

    Add package state-estimation-for-robotics/2024.3.4.0/state-estimation-for-robotics-2024.3.4.0.tar.gz

---
+++ packages/state-estimation-for-robotics/state-estimation-for-robotics-2024.3.4.0.tar.gz/state-estimation-for-robotics-2024.3.4.0/StateEstimationForRobotics/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""state-estimation-for-robotics
+https://github.com/apachecn/state-estimation-for-robotics"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
