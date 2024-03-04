f2d98bc4cae69dab03902a5af490561bcbb9b65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 16:03:45 +0000

    Add package partcad/0.5.4/partcad-0.5.4-py3-none-any.whl

---
+++ packages/partcad/partcad-0.5.4-py3-none-any.whl/partcad/part_factory_python.py
@@ -0,0 +1,20 @@
+#
+# OpenVMP, 2023
+#
+# Author: Roman Kuzmenko
+# Created: 2023-12-30
+#
+# Licensed under Apache License, Version 2.0.
+#
+
+from . import part_factory_file as pff
+
+
+class PartFactoryPython(pff.PartFactoryFile):
+    def __init__(self, ctx, source_project, target_project, part_config):
+        super().__init__(
+            ctx, source_project, target_project, part_config, extension=".py"
+        )
+
+        self.runtime = self.ctx.get_python_runtime(self.project.python_version)
+        self.runtime.prepare_for_package(source_project)
