3a9e6ec9a08549b28232b58fbdc689fdd0f33c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 16:03:47 +0000

    Add package uptrain/0.6.6/uptrain-0.6.6-py3-none-any.whl

---
+++ packages/uptrain/uptrain-0.6.6-py3-none-any.whl/uptrain/v0/core/classes/distances/__init__.py
@@ -0,0 +1,6 @@
+from .abstract_distance import AbstractDistance
+from .cosine_distance import CosineDistance
+from .l2_distance import L2Distance
+from .norm_ratio import NormRatio
+
+from .distance_resolver import DistanceResolver
