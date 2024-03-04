f53098842c267117c58ea4958716dbad3f5653e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:41 +0000

    Add package atb-lib/0.6.18/atb_lib-0.6.18-py3-none-any.whl

---
+++ packages/atb-lib/atb_lib-0.6.18-py3-none-any.whl/src/modules_version.py
@@ -0,0 +1,16 @@
+﻿import sys
+from importlib.metadata import distributions
+
+
+def modules_version(logger):
+    python_version = sys.version.split()[0]
+    logger.info(f"Python Version: {python_version}")
+
+    installed_packages = distributions()
+    installed_packages_list = sorted([f"{dist.metadata['Name']}=={dist.version}" for dist in installed_packages])
+    logger.info("Installed Packages:")
+    for package in installed_packages_list:
+        for module in ['aio-pika', 'ccxt', 'atb_lib', 'asyncio', 'colorlog']:
+            if module in package:
+                logger.info(f"  {package}")
+                break
