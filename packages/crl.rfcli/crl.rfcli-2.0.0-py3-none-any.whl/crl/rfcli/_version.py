8dc70a38ffdf008d367d48b3a3816ebfbb3765a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 16:03:47 +0000

    Add package crl.rfcli/2.0.0/crl.rfcli-2.0.0-py3-none-any.whl

---
+++ packages/crl.rfcli/crl.rfcli-2.0.0-py3-none-any.whl/crl/rfcli/_version.py
@@ -0,0 +1,12 @@
+__copyright__ = 'Copyright (C) 2019-2024, Nokia'
+
+VERSION = '2.0.0'
+GITHASH = '426650d93bbe549402d7f61750395219a27476b8'
+
+
+def get_version():
+    return VERSION
+
+
+def get_githash():
+    return GITHASH
