40702349cdf35562eea640e017d8bad5fb9a8564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:48 +0000

    Add package pixano/0.5.0/pixano-0.5.0.tar.gz

---
+++ packages/pixano/pixano-0.5.0.tar.gz/pixano-0.5.0/pixano/app/dist/_app/immutable/chunks/datasetStores.CVZVofCF.js
@@ -0,0 +1,14 @@
+import{w as o}from"./entry.XcZdnu9t.js";function T(n){return(n==null?void 0:n.length)!==void 0?n:Array.from(n)}function g(n,d){const c={},u={},f={$$scope:1};let a=n.length;for(;a--;){const t=n[a],s=d[a];if(s){for(const e in t)e in s||(u[e]=1);for(const e in s)f[e]||(c[e]=s[e],f[e]=1);n[a]=s}else for(const e in t)f[e]=1}for(const t in u)t in c||(c[t]=void 0);return c}function E(n){return typeof n=="object"&&n!==null?n:{}}const i=20,S=1;/**
+ * @copyright CEA
+ * @author CEA
+ * @license CECILL
+ *
+ * This software is a collaborative computer program whose purpose is to
+ * generate and explore labeled data for computer vision applications.
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software. You can use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ *
+ * http://www.cecill.info
+ */const l={currentPage:S,pageSize:i},m=o(),b=o([]),D=o(!1),L=o(l),h=o({shouldSave:!1,canSave:!1});export{S as D,E as a,L as b,i as c,m as d,T as e,l as f,g,D as i,b as m,h as s};
---
+++ packages/pixano/pixano-0.5.0.tar.gz/pixano-0.5.0/pixano/app/dist/_app/immutable/chunks/icons.nQXAmEt2.js
@@ -0,0 +1,14 @@
+/**
+ * @copyright CEA
+ * @author CEA
+ * @license CECILL
+ *
+ * This software is a collaborative computer program whose purpose is to
+ * generate and explore labeled data for computer vision applications.
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software. You can use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ *
+ * http://www.cecill.info
+ */const s="M240-240v-480h60v480h-60Zm447-3L453-477l234-234 43 43-191 191 191 191-43 43Z",a="M561-240 320-481l241-241 43 43-198 198 198 198-43 43Z",t="M530-481 332-679l43-43 241 241-241 241-43-43 198-198Z",v="m272-245-43-43 192-192-192-192 43-43 235 235-235 235Zm388 5v-480h60v480h-60Z",g="M796-121 533-384q-30 26-69.959 40.5T378-329q-108.162 0-183.081-75Q120-479 120-585t75-181q75-75 181.5-75t181 75Q632-691 632-584.85 632-542 618-502q-14 40-42 75l264 262-44 44ZM377-389q81.25 0 138.125-57.5T572-585q0-81-56.875-138.5T377-781q-82.083 0-139.542 57.5Q180-666 180-585t57.458 138.5Q294.917-389 377-389Z",_="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z",c="m560-242-43-42 168-168H160v-60h525L516-681l43-42 241 241-240 240Z";export{g as a,_ as b,s as c,a as d,t as e,v as f,c as s};
