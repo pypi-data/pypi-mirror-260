3bf9aabae49860b574457bb48fcb07fbcba76850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:14 +0000

    Add package swebok-2004-zhongwenban/2024.3.4.0/swebok_2004_zhongwenban-2024.3.4.0-py3-none-any.whl

---
+++ packages/swebok-2004-zhongwenban/swebok_2004_zhongwenban-2024.3.4.0-py3-none-any.whl/Swebok2004Zhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""swebok-2004-zhongwenban
+https://github.com/apachecn/swebok-2004-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
