ba260b6ea9d690ac1ec51afc042e1bd2c230bcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:04 +0000

    Add package color-science/0.4.4/color_science-0.4.4-py3-none-any.whl

---
+++ packages/color-science/color_science-0.4.4-py3-none-any.whl/color_science/__init__.py
@@ -0,0 +1,2 @@
+print("You've installed and imported 'color-science', which is a protective placeholder package.")
+print("If you're looking for the color science utilities, you might want to install 'colour-science' instead.")
