fcd5a097b9af8894afa5cd23ef030e767b384e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:07 +0000

    Add package f-for-fun-and-profit-ebook/2024.3.3.0/f-for-fun-and-profit-ebook-2024.3.3.0.tar.gz

---
+++ packages/f-for-fun-and-profit-ebook/f-for-fun-and-profit-ebook-2024.3.3.0.tar.gz/f-for-fun-and-profit-ebook-2024.3.3.0/FForFunAndProfitEbook/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""f-for-fun-and-profit-ebook
+https://github.com/apachecn/f-for-fun-and-profit-ebook"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
