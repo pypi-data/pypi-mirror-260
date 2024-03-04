503651279e665dd8bb75e58ce37a0aeb7217380f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:24 +0000

    Add package djReact/2.0/djReact-2.0.tar.gz

---
+++ packages/djReact/djReact-2.0.tar.gz/djReact-2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="djReact",
+    version='2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "Django>=5.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "djReact = djReact.management.commands.startproject:Command.handle",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
