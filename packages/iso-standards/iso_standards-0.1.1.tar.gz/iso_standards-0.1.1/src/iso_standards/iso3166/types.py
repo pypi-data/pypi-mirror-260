2c2f1133cfbb8b17ca300dc03a20791af2488f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:10 +0000

    Add package iso-standards/0.1.1/iso_standards-0.1.1.tar.gz

---
+++ packages/iso-standards/iso_standards-0.1.1.tar.gz/iso_standards-0.1.1/src/iso_standards/iso3166/types.py
@@ -0,0 +1,46 @@
+from typing import NamedTuple
+
+
+class Iso3166_1(NamedTuple):
+    """ISO 3166-1 lightweight immutable entity."""
+
+    # Country name, short form.
+    short_name: str
+
+    # Alphabetic 2 character code.
+    alpha_2: str
+
+    # Alphabetic 3 character code.
+    alpha_3: str
+
+    # Three-digit numeric code.
+    num_3: str
+
+    # Country name, long form .
+    long_name: str
+
+    # Sovereign country code.
+    parent_code: str = ""
+
+    @property
+    def is_independent(self):
+        return self.parent_code == ""
+
+
+class Iso3166_2(NamedTuple):
+    """ISO 3166-2 lightweight immutable entity."""
+
+    # Subdivision name.
+    name: str
+
+    # Subdivision code.
+    code: str
+
+    # Category.
+    category: str
+
+    # Language code.
+    language_code: str
+
+    # Parent code.
+    parent_code: str = ""
