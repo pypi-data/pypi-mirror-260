49b7bf3de3216a30d3acb7937e368fa796803d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 05:07:15 +0000

    Add package c-yuyan-jinjie/2024.3.4.0/c_yuyan_jinjie-2024.3.4.0-py3-none-any.whl

---
+++ packages/c-yuyan-jinjie/c_yuyan_jinjie-2024.3.4.0-py3-none-any.whl/CYuyanJinjie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""c-yuyan-jinjie
+https://github.com/apachecn/c-yuyan-jinjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
