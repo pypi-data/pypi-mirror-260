9c7954cbe7a647df02b1e64549ec28007f19521d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:08 +0000

    Add package landborn/0.2.1/landborn-0.2.1.tar.gz

---
+++ packages/landborn/landborn-0.2.1.tar.gz/landborn-0.2.1/landborn/__init__.py
@@ -0,0 +1,6 @@
+from .functions import barplot, lineplot, scatterplot, jointplot, swarmplot
+
+__version__ = '0.2.1'
+__author__ = 'Molly Nelson'
+
+print("Package Landborn 0.2.1 Successfully Imported")
