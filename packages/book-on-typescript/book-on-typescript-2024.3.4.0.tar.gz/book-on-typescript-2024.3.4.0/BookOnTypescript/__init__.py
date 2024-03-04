866d4bd82e3542a2e0230635ab8fe321ba48bea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:06:47 +0000

    Add package book-on-typescript/2024.3.4.0/book-on-typescript-2024.3.4.0.tar.gz

---
+++ packages/book-on-typescript/book-on-typescript-2024.3.4.0.tar.gz/book-on-typescript-2024.3.4.0/BookOnTypescript/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""book-on-typescript
+https://github.com/apachecn/book-on-typescript"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
