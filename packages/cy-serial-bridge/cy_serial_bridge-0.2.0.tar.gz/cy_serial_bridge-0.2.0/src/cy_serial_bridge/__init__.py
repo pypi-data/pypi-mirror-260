b753aadb9057cbc2344cf90a0178b43ea754b953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:19 +0000

    Add package cy-serial-bridge/0.2.0/cy_serial_bridge-0.2.0.tar.gz

---
+++ packages/cy-serial-bridge/cy_serial_bridge-0.2.0.tar.gz/cy_serial_bridge-0.2.0/src/cy_serial_bridge/__init__.py
@@ -0,0 +1,24 @@
+"""
+A port of Cypress USB Serial Library (libcyusbserial) in pure python.
+
+This code is still in alpha stage. Many protocols and data format
+details are discovered, but information still needs to be cleaned
+out and API/code/tools need further refactoring.
+
+"""
+
+from cy_serial_bridge import configuration_block, device_discovery, driver
+from cy_serial_bridge.configuration_block import ConfigurationBlock
+from cy_serial_bridge.device_discovery import OpenMode, list_devices, open_device
+from cy_serial_bridge.driver import (
+    CyI2CControllerBridge,
+    CySerialBridgeError,
+    CySPIConfig,
+    CySPIControllerBridge,
+    CySpiMode,
+    I2CArbLostError,
+    I2CBusError,
+    I2CNACKError,
+)
+from cy_serial_bridge.usb_constants import *
+from cy_serial_bridge.utils import ByteSequence
