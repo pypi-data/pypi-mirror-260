b88dc017220ab16f42f49f30e21941ba9674051e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:33 +0000

    Add package brian-lib/0.2.3/brian_lib-0.2.3.tar.gz

---
+++ packages/brian-lib/brian_lib-0.2.3.tar.gz/brian_lib-0.2.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+version = os.getenv('VERSION', '0.1.0') # Usa la versión del tag o 0.1 como predeterminado
+
+# requirements from requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='brian_lib',
+    version=version,
+    packages=find_packages(),
+    install_requires=required,
+    python_requires='>=3.8',
+    summary='A library for data engineering and data science tasks.',
+    author='Brain Food',
+    license='MIT',
+)
