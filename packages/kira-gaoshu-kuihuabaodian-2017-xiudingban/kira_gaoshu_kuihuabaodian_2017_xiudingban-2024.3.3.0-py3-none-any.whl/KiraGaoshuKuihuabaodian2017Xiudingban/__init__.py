16341331d828b0dc1d7072847de8a4292da22133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:11 +0000

    Add package kira-gaoshu-kuihuabaodian-2017-xiudingban/2024.3.3.0/kira_gaoshu_kuihuabaodian_2017_xiudingban-2024.3.3.0-py3-none-any.whl

---
+++ packages/kira-gaoshu-kuihuabaodian-2017-xiudingban/kira_gaoshu_kuihuabaodian_2017_xiudingban-2024.3.3.0-py3-none-any.whl/KiraGaoshuKuihuabaodian2017Xiudingban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kira-gaoshu-kuihuabaodian-2017-xiudingban
+https://github.com/apachecn/kira-gaoshu-kuihuabaodian-2017-xiudingban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
