7afb4d8d4e81e1ddc6e612d2f87b7a82e734e4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:35 +0000

    Add package x97-things-every-programmer-should-know/2024.3.3.0/x97-things-every-programmer-should-know-2024.3.3.0.tar.gz

---
+++ packages/x97-things-every-programmer-should-know/x97-things-every-programmer-should-know-2024.3.3.0.tar.gz/x97-things-every-programmer-should-know-2024.3.3.0/X97ThingsEveryProgrammerShouldKnow/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x97-things-every-programmer-should-know
+https://github.com/apachecn/x97-things-every-programmer-should-know"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
