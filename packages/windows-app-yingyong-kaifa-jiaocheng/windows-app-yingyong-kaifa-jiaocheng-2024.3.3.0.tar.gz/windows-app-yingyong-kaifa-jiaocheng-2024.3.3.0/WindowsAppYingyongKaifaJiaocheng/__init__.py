4c207a7acb438f21fa05c899648907dca4f8b887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:33 +0000

    Add package windows-app-yingyong-kaifa-jiaocheng/2024.3.3.0/windows-app-yingyong-kaifa-jiaocheng-2024.3.3.0.tar.gz

---
+++ packages/windows-app-yingyong-kaifa-jiaocheng/windows-app-yingyong-kaifa-jiaocheng-2024.3.3.0.tar.gz/windows-app-yingyong-kaifa-jiaocheng-2024.3.3.0/WindowsAppYingyongKaifaJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""windows-app-yingyong-kaifa-jiaocheng
+https://github.com/apachecn/windows-app-yingyong-kaifa-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
