da99af28c519c4c19ac2e3abd8d867a2240c469d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:13:50 +0000

    Add package yong-python-xue-weijifen/2024.3.3.0/yong_python_xue_weijifen-2024.3.3.0-py3-none-any.whl

---
+++ packages/yong-python-xue-weijifen/yong_python_xue_weijifen-2024.3.3.0-py3-none-any.whl/YongPythonXueWeijifen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yong-python-xue-weijifen
+https://github.com/apachecn/yong-python-xue-weijifen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
