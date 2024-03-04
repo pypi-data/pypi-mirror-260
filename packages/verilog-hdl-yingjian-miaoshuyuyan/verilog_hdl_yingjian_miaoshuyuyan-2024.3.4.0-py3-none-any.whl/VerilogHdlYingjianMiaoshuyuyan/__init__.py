39e7b47e3b841d33b0e908bcc8889aa1556308bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:27 +0000

    Add package verilog-hdl-yingjian-miaoshuyuyan/2024.3.4.0/verilog_hdl_yingjian_miaoshuyuyan-2024.3.4.0-py3-none-any.whl

---
+++ packages/verilog-hdl-yingjian-miaoshuyuyan/verilog_hdl_yingjian_miaoshuyuyan-2024.3.4.0-py3-none-any.whl/VerilogHdlYingjianMiaoshuyuyan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""verilog-hdl-yingjian-miaoshuyuyan
+https://github.com/apachecn/verilog-hdl-yingjian-miaoshuyuyan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
