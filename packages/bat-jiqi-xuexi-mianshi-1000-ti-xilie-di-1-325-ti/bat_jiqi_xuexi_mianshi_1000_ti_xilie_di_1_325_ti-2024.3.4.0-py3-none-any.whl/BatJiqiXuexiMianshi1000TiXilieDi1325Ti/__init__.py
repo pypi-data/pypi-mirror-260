5c601203800b12f4c748d1332bf731391b6d0b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 05:07:10 +0000

    Add package bat-jiqi-xuexi-mianshi-1000-ti-xilie-di-1-325-ti/2024.3.4.0/bat_jiqi_xuexi_mianshi_1000_ti_xilie_di_1_325_ti-2024.3.4.0-py3-none-any.whl

---
+++ packages/bat-jiqi-xuexi-mianshi-1000-ti-xilie-di-1-325-ti/bat_jiqi_xuexi_mianshi_1000_ti_xilie_di_1_325_ti-2024.3.4.0-py3-none-any.whl/BatJiqiXuexiMianshi1000TiXilieDi1325Ti/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bat-jiqi-xuexi-mianshi-1000-ti-xilie-di-1-325-ti
+https://github.com/apachecn/bat-jiqi-xuexi-mianshi-1000-ti-xilie-di-1-325-ti"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
