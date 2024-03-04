b429732b0b5e3f66df080f14ce4b1fd7c67af05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:21 +0000

    Add package gei-ios-kaifazhe-de-sketch-rumenjiaocheng/2024.3.3.0/gei_ios_kaifazhe_de_sketch_rumenjiaocheng-2024.3.3.0-py3-none-any.whl

---
+++ packages/gei-ios-kaifazhe-de-sketch-rumenjiaocheng/gei_ios_kaifazhe_de_sketch_rumenjiaocheng-2024.3.3.0-py3-none-any.whl/GeiIosKaifazheDeSketchRumenjiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gei-ios-kaifazhe-de-sketch-rumenjiaocheng
+https://github.com/apachecn/gei-ios-kaifazhe-de-sketch-rumenjiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
