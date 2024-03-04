3a1eed9680de7bb1190929d00d696fde45719003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:10 +0000

    Add package python-zhongwen-wendang-3-7-rumenjiaocheng/2024.3.4.0/python_zhongwen_wendang_3_7_rumenjiaocheng-2024.3.4.0-py3-none-any.whl

---
+++ packages/python-zhongwen-wendang-3-7-rumenjiaocheng/python_zhongwen_wendang_3_7_rumenjiaocheng-2024.3.4.0-py3-none-any.whl/PythonZhongwenWendang37Rumenjiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""python-zhongwen-wendang-3-7-rumenjiaocheng
+https://github.com/apachecn/python-zhongwen-wendang-3-7-rumenjiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
