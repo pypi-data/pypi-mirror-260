5872fe91f1084d444ad3c5bc6877ada42c8c4c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:16 +0000

    Add package stbook/0.0.1/stbook-0.0.1-py3-none-any.whl

---
+++ packages/stbook/stbook-0.0.1-py3-none-any.whl/stbook/code_completion.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import ollama
+from code_editor import code_editor
+
+try:
+    models = ollama.list()["models"]
+except:
+    st.warning("Ollama is not available. Please install it using `pip install ollama`, https://ollama.com")
+    st.stop()
+
+
+def generate_completion(code):
+    sys_message = 'You are an AI code completion system. Generate code to complete the given Python code.'
+    ret = ollama.generate(model='deepseek-coder:6.7b', prompt=code, system=sys_message)['response']
+    st.session_state.in_code = f'{code}{ret}'
+    st.code(st.session_state.in_code)
+
+
+def auto_complete():
+    if 'in_code' not in st.session_state: st.session_state.in_code = 'Type to complete...'
+    res = code_editor(st.session_state.in_code)
+    if len(res['id']) != 0 and (res['type'] == 'submit' or res['type'] == 'selection'):
+        generate_completion(res['text'])
+    else: st.session_state.in_code = 'Type to complete...'
