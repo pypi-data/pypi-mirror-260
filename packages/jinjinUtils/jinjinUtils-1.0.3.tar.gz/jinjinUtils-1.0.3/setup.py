28dc6c0e02ef745794deabf7066ffea84a10cdda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:05 +0000

    Add package jinjinUtils/1.0.3/jinjinUtils-1.0.3.tar.gz

---
+++ packages/jinjinUtils/jinjinUtils-1.0.3.tar.gz/jinjinUtils-1.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools  # 导入setuptools打包工具
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jinjinUtils",  # 用自己的名替换其中的YOUR_USERNAME_
+    version="1.0.3",  # 包版本号，便于维护版本,保证每次发布都是版本都是唯一的
+    author="wcj",  # 作者，可以写自己的姓名
+    author_email="745381270@qq.com",  # 作者联系方式，可写自己的邮箱地址
+    description="一些小工具",  # 包的简述
+    long_description=long_description,  # 包的详细介绍，一般在README.md文件内
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/wang_chaojin/jinjinutils.git",  # 自己项目地址，比如github的项目地址
+    packages=setuptools.find_packages(),
+    # entry_points={
+    #     "console_scripts" : ['mwjApiTest = mwjApiTest.manage:run']
+    # }, #安装成功后，在命令行输入mwjApiTest 就相当于执行了mwjApiTest.manage.py中的run了
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[  
+        "pymysql"
+    ],  
+    python_requires='>=3.6',  # 对python的最低版本要求
+)
+
+
+# python setup.py sdist bdist_wheel  
+# twine upload dist/*
