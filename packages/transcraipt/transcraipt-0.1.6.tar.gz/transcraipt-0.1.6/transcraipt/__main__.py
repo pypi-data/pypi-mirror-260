6be4f6ae631f3559a769dba941569c927692c388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:30 +0000

    Add package transcraipt/0.1.6/transcraipt-0.1.6.tar.gz

---
+++ packages/transcraipt/transcraipt-0.1.6.tar.gz/transcraipt-0.1.6/transcraipt/__main__.py
@@ -0,0 +1,8 @@
+# This is the __main__.py file within the transcraipt package
+
+# Import the main function from transcript.py
+from transcript import main
+
+# Call the main function
+if __name__ == '__main__':
+    main()
