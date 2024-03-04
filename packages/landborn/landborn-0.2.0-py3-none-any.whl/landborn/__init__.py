e65dca223675976df4ab61c3996a904eef45f8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:05 +0000

    Add package landborn/0.2.0/landborn-0.2.0-py3-none-any.whl

---
+++ packages/landborn/landborn-0.2.0-py3-none-any.whl/landborn/__init__.py
@@ -0,0 +1,6 @@
+from .functions import barplot, lineplot, scatterplot, jointplot, swarmplot
+
+__version__ = '0.2.0'
+__author__ = 'Molly Nelson'
+
+print("Package Landborn 0.2.0 Successfully Imported")
