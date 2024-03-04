2e3424c3d12c7089e7d31841a4d82b13243b9e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:35 +0000

    Add package spring-data-zhongwen-banben/2024.3.4.0/spring_data_zhongwen_banben-2024.3.4.0-py3-none-any.whl

---
+++ packages/spring-data-zhongwen-banben/spring_data_zhongwen_banben-2024.3.4.0-py3-none-any.whl/SpringDataZhongwenBanben/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""spring-data-zhongwen-banben
+https://github.com/apachecn/spring-data-zhongwen-banben"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
