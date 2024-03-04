a8499cbc8ad0b9bc73e235b9320c2d5354740099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:22 +0000

    Add package quasimoto/0.2.1/quasimoto-0.2.1-py3-none-any.whl

---
+++ packages/quasimoto/quasimoto-0.2.1-py3-none-any.whl/quasimoto/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=9bcedc43070f45384f7b98092917a200
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A lossless audio generator."
+PKG_NAME = "quasimoto"
+VERSION = "0.2.1"
