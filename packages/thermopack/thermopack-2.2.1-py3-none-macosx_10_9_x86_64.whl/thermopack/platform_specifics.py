cb2e5402203344046f8eea924e1a5cf03ae7e423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:47 +0000

    Add package thermopack/2.2.1/thermopack-2.2.1-py3-none-macosx_10_9_x86_64.whl

---
+++ packages/thermopack/thermopack-2.2.1-py3-none-macosx_10_9_x86_64.whl/thermopack/platform_specifics.py
@@ -0,0 +1,17 @@
+# Module for platform specific stuff. Automatically generated.
+# Timestamp : 2024-03-04T08:49:22.245107
+
+
+DIFFERENTIAL_RETURN_MODE = 'v2'
+
+
+def get_platform_specifics():
+    pf_specifics = {}
+    pf_specifics["os_id"] = "darwin"
+    pf_specifics["prefix"] = "__"
+    pf_specifics["module"] = "_MOD_"
+    pf_specifics["postfix"] = ""
+    pf_specifics["postfix_no_module"] = "_"
+    pf_specifics["dyn_lib"] = "libthermopack.dynlib"
+    pf_specifics["diff_return_mode"] = "v2"
+    return pf_specifics
