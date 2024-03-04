d56adcd102237ec5549ed098b2bf30c2cb3853bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:28 +0000

    Add package vue-js-2-0-wanquan-rumen-jilu/2024.3.3.0/vue-js-2-0-wanquan-rumen-jilu-2024.3.3.0.tar.gz

---
+++ packages/vue-js-2-0-wanquan-rumen-jilu/vue-js-2-0-wanquan-rumen-jilu-2024.3.3.0.tar.gz/vue-js-2-0-wanquan-rumen-jilu-2024.3.3.0/VueJs20WanquanRumenJilu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""vue-js-2-0-wanquan-rumen-jilu
+https://github.com/apachecn/vue-js-2-0-wanquan-rumen-jilu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
