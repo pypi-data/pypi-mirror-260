ff50e74cc0bfc7784e5e5223a313460448962837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:15 +0000

    Add package zeytea/0.1.0/zeytea-0.1.0.tar.gz

---
+++ packages/zeytea/zeytea-0.1.0.tar.gz/zeytea-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeytea',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'cann_calculator==1.0.0',
+        'lilzey_generator==0.1.0',  # Gerekli bağımlılıkları buraya ekleyin
+    ],
+    entry_points={
+        'console_scripts': [
+            'zeytea-game=zeytea.main:main',
+        ],
+    },
+    author='Lilzey',
+    author_email='lilzey0101@gmail.com',
+    description='A simple number guessing game',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+    project_urls={
+        'Source': 'https://github.com/hw010101/zeytea',
+    },
+)
