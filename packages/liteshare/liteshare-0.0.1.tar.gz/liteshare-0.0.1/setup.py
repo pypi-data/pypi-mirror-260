4a596a29b89f29666db034592307a3aae49792c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:42 +0000

    Add package liteshare/0.0.1/liteshare-0.0.1.tar.gz

---
+++ packages/liteshare/liteshare-0.0.1.tar.gz/liteshare-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="liteshare",
+    version="0.0.1",
+    author="bignene123",
+    author_email="bignene@foxmail.com",
+    description="A quant community.",
+    long_description="A quant community.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/bignene123/liteshare",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
