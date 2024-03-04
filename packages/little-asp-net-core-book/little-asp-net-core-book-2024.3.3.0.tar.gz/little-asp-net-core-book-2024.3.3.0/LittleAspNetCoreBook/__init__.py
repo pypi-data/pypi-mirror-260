9ea9ac4081695679353b70c333535f55d08eaa1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:45 +0000

    Add package little-asp-net-core-book/2024.3.3.0/little-asp-net-core-book-2024.3.3.0.tar.gz

---
+++ packages/little-asp-net-core-book/little-asp-net-core-book-2024.3.3.0.tar.gz/little-asp-net-core-book-2024.3.3.0/LittleAspNetCoreBook/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""little-asp-net-core-book
+https://github.com/apachecn/little-asp-net-core-book"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
