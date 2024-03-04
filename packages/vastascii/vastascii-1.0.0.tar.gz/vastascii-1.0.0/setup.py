dba738d595e8e4cd67c0adb2c268e6d640ceb9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:24 +0000

    Add package vastascii/1.0.0/vastascii-1.0.0.tar.gz

---
+++ packages/vastascii/vastascii-1.0.0.tar.gz/vastascii-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vastascii',
+    version='1.0.0',
+    description='Convert images to ASCII art and display them in a Tkinter window or in the terminal',
+    author='SiliconY',
+    author_email='yangsilicon9@gmail.com',
+    readme = "README.md",
+    packages=find_packages(),
+    install_requires=['requests', 'Pillow'],
+)
