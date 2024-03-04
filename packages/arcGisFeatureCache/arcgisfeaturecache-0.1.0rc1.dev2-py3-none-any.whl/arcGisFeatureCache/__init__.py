a8a6f332460e3b1a822bd79919a52f8d77fd5c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 05:07:12 +0000

    Add package arcGisFeatureCache/0.1.0rc1.dev2/arcgisfeaturecache-0.1.0rc1.dev2-py3-none-any.whl

---
+++ packages/arcGisFeatureCache/arcgisfeaturecache-0.1.0rc1.dev2-py3-none-any.whl/arcGisFeatureCache/__init__.py
@@ -0,0 +1,15 @@
+from arcGisFeatureCache.featureService import (
+    ArcGisFeatureLayer,
+    ArcGisFeatureService,
+    ArcGisScraper,
+    get_feature_service,
+)
+
+__version__ = "0.1.0rc1.dev2"
+
+__all__ = [
+    "ArcGisFeatureService",
+    "ArcGisFeatureLayer",
+    "ArcGisScraper",
+    "get_feature_service",
+]
