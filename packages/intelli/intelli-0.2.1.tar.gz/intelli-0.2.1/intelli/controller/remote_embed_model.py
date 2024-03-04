8c0df779ce9c99b3bd22dccfcf2b9d6b7ab2a12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:14 +0000

    Add package intelli/0.2.1/intelli-0.2.1.tar.gz

---
+++ packages/intelli/intelli-0.2.1.tar.gz/intelli-0.2.1/intelli/controller/remote_embed_model.py
@@ -0,0 +1,33 @@
+from intelli.model.input.embed_input import EmbedInput
+from intelli.wrappers.geminiai_wrapper import GeminiAIWrapper
+from intelli.wrappers.mistralai_wrapper import MistralAIWrapper
+from intelli.wrappers.openai_wrapper import OpenAIWrapper
+
+
+class RemoteEmbedModel:
+    def __init__(self, provider_name, api_key):
+        self.provider_name = provider_name.lower()
+        providers = {
+            'openai': OpenAIWrapper,
+            'mistral': MistralAIWrapper,
+            'gemini': GeminiAIWrapper,
+        }
+        if self.provider_name in providers:
+            self.provider = providers[self.provider_name](api_key)
+        else:
+            raise Exception(f"Provider {provider_name} not supported.")
+
+    def get_embeddings(self, embed_input):
+        if not isinstance(embed_input, EmbedInput):
+            raise Exception("embed_input must be an instance of EmbedInput.")
+
+        if self.provider_name == 'openai':
+            params = embed_input.get_openai_inputs()
+        elif self.provider_name == 'mistral':
+            params = embed_input.get_mistral_inputs()
+        elif self.provider_name == 'gemini':
+            params = embed_input.get_gemini_inputs()
+        else:
+            raise Exception("Invalid provider name.")
+
+        return self.provider.get_embeddings(params)
