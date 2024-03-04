cbcba0089b217350f0c7e19a849b29f7ae5c6312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:32 +0000

    Add package learning-deep-architectures-for-ai-zhongwenban-chuyi/2024.3.3.0/learning-deep-architectures-for-ai-zhongwenban-chuyi-2024.3.3.0.tar.gz

---
+++ packages/learning-deep-architectures-for-ai-zhongwenban-chuyi/learning-deep-architectures-for-ai-zhongwenban-chuyi-2024.3.3.0.tar.gz/learning-deep-architectures-for-ai-zhongwenban-chuyi-2024.3.3.0/LearningDeepArchitecturesForAiZhongwenbanChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""learning-deep-architectures-for-ai-zhongwenban-chuyi
+https://github.com/apachecn/learning-deep-architectures-for-ai-zhongwenban-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
