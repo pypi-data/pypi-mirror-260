9169a630a16d8d5ec04fff1ca7b96004f5b44bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:06:59 +0000

    Add package pyedautils/0.0.8/pyedautils-0.0.8.tar.gz

---
+++ packages/pyedautils/pyedautils-0.0.8.tar.gz/pyedautils-0.0.8/pyedautils/geopy.py
@@ -0,0 +1,162 @@
+from geopy.geocoders import Nominatim
+import pgeocode
+import requests
+import pandas as pd
+import time
+from typing import List, Union, Tuple
+import math
+
+class GeocodingError(Exception):
+    pass
+
+def convert_wsg84_to_lv95(lat: float, long: float) -> List[float]:
+    """
+    Converts WGS84 latitude and longitude coordinates to Swiss coordinate system LV95.
+
+    Args:
+        lat (float): Latitude in decimal degrees.
+        long (float): Longitude in decimal degrees.
+
+    Returns:
+        List[float]: A list containing x and y coordinates in LV95 system, e.g., [xcoord, ycoord].
+    """
+    if not (5.2 <= long <= 11) or not (45.4 <= lat <= 48.2):
+        raise GeocodingError("Coordinates not in range for Swiss coordinate system LV95")
+
+    latitude = str(lat)
+    longitude = str(long)
+    query = f'http://geodesy.geo.admin.ch/reframe/wgs84tolv95?easting={longitude}&northing={latitude}'
+    try:
+        r = requests.get(query).json()
+        coord_list_lv95 = r.get("coordinates", [])
+        return coord_list_lv95
+    except Exception as e:
+        raise GeocodingError(f"Failed to convert WGS84 to LV95: {e}") from e
+
+def get_altitude_lv95(coord_list_lv95: List[float]) -> float:
+    """
+    Returns altitude in meters above sea level for the given LV95 coordinates.
+    The geo.admin.ch api gets used.
+
+    Args:
+        coord_list_lv95 (List[float]): LV95 coordinates as [xcoord, ycoord].
+
+    Returns:
+        float: Altitude in meters above sea level.
+    """
+    query = f'https://api3.geo.admin.ch/rest/services/height?easting={coord_list_lv95[0]}&northing={coord_list_lv95[1]}'
+    try:       
+        r = requests.get(query).json()
+        altitude = float(r.get("height", 0))
+        return altitude
+    except Exception as e:
+        raise GeocodingError(f"Failed to get altitude for LV95 coordinates, Error: {e}") from e
+
+def get_altitude_lat_long(lat: float, long: float) -> float:
+    """
+    Returns altitude in meters above sea level for the given WGS84 coordinates.
+    The opentopodata.org api gets used.
+
+    Args:
+        lat (float): Latitude in decimal degrees.
+        long (float): Longitude in decimal degrees.
+
+    Returns:
+        float: Altitude in meters above sea level.
+    """
+    switzerland_lat_min = 45.67
+    switzerland_lat_max = 47.92
+    switzerland_long_min = 5.7
+    switzerland_long_max = 10.7
+    
+    if(not((switzerland_lat_min <= lat <= switzerland_lat_max) and (switzerland_long_min <= long <= switzerland_long_max))):
+        raise ValueError("Coordinates not in range for Swiss coordinate system")
+    
+    latitude = str(lat)
+    longitude = str(long)
+    query = f'https://api.opentopodata.org/v1/eudem25m?locations={latitude},{longitude}'
+    try:
+        r = requests.get(query).json()
+        if(pd.json_normalize(r, 'results')['elevation'][0] == None):
+            altitude = 0
+        else:
+            altitude = float(pd.json_normalize(r, 'results')['elevation'].values[0])
+        time.sleep(1)  # API rate limiting
+        return round(altitude,1)
+    except Exception as e:
+        raise GeocodingError(f"Failed to get altitude for WGS84 coordinates: {e}") from e
+
+def get_lat_long_address(address: str) -> Union[List[float], None]:
+    """
+    Returns latitude and longitude coordinates for the given address.
+
+    Args:
+        address (str): The address to geocode.
+
+    Returns:
+        Union[List[float], None]: A list containing latitude and longitude coordinates, e.g., [latitude, longitude], or None if the address could not be geocoded.
+    """
+    try:
+        nom = Nominatim(user_agent="myPythonScript")
+        for _ in range(3):
+            n = nom.geocode(address)
+            if n is not None:
+                break
+            time.sleep(1)  # API rate limiting
+        else:
+            raise GeocodingError("Failed to geocode address after 3 attempts")
+
+        return [n.latitude, n.longitude]
+    except Exception as e:
+        raise GeocodingError(f"Failed to geocode address, Error: {e}") from e
+
+def get_coordindates_ch_plz(plz: int) -> Tuple[float, float]:
+    """
+    Returns latitude and longitude for a Swiss postal code.
+
+    Args:
+        plz (int): Postal Code (Postleitzahl)
+
+    Returns:
+        tuple (lat: float, lon: float)
+    """
+    try:
+        nomi = pgeocode.Nominatim('ch') 
+        data=nomi.query_postal_code(plz)
+        coordinates = data.latitude.astype(float), data.longitude.astype(float)
+        if(str(coordinates[0]) == "nan"):
+            raise GeocodingError(f"Failed to get lat/long from plz {plz}")
+    except Exception:
+        raise GeocodingError(f"Failed to get lat/long for plz {plz}")
+     
+    return coordinates
+
+def get_distance_between_two_points(coord1, coord2):
+    """
+    Calculates the distance between two points on the Earth's surface given their latitude and longitude coordinates.
+    
+    Args:
+        coord1 (tuple): Latitude and longitude of the first point in degrees, as a tuple (lat1, lon1).
+        coord2 (tuple): Latitude and longitude of the second point in degrees, as a tuple (lat2, lon2).
+        
+    Returns:
+        float: Distance between the two points in kilometers.
+    
+    """
+    # Radius of Earth in km
+    R = 6373.0
+    
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1 = math.radians(coord1[0]), math.radians(coord1[1])
+    lat2, lon2 = math.radians(coord2[0]), math.radians(coord2[1])
+    
+    # Calculate differences in longitude and latitude
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    
+    # Apply Haversine formula to calculate distance
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    
+    return round(distance, 3)
---
+++ packages/pyedautils/pyedautils-0.0.8.tar.gz/pyedautils-0.0.8/tests/test_geopy.py
@@ -0,0 +1,58 @@
+# # -*- coding: utf-8 -*-
+
+import unittest
+from pyedautils.geopy import get_altitude_lat_long, get_lat_long_address, get_altitude_lv95, convert_wsg84_to_lv95, GeocodingError, get_coordindates_ch_plz, get_distance_between_two_points
+
+class TestGeopyhelpers(unittest.TestCase):
+    """Tests for "geopyhelpers.py"."""
+    
+    def test_get_altitude_lat_long(self):
+        """Test get_altitude() with different lat/long pairs"""
+        self.assertIn(round(get_altitude_lat_long(47.01450, 8.30620),0), range(438,445))
+        with self.assertRaises(ValueError):
+            get_altitude_lat_long(57.01450, 8.30620)
+        with self.assertRaises(ValueError):
+            get_altitude_lat_long(100, 10)
+        with self.assertRaises(ValueError):
+            get_altitude_lat_long(-100, 10)
+        with self.assertRaises(ValueError):
+            get_altitude_lat_long(10, -200)
+        with self.assertRaises(ValueError):
+            get_altitude_lat_long(10, 200)
+
+    def test_get_lat_long_address(self):
+        """Test get_lat_long() with different addresses"""
+        self.assertEqual(round(get_lat_long_address("Technikumstrasse 21, 6048 Horw")[0], 1), 47.0)
+        self.assertEqual(round(get_lat_long_address("Technikumstrasse 21, 6048 Horw")[1], 1), 8.3)
+        with self.assertRaises(GeocodingError):
+            get_lat_long_address("Highway to hell, Horw, Switzerland")        
+        
+    def test_convert_wsg84_to_lv95(self):
+        """Test convert_wsg84_to_lv95() with different coodrinates"""
+        self.assertIn(round(convert_wsg84_to_lv95(47.01331, 8.30612)[0], 0), range(2665959,2665962))
+        self.assertIn(round(convert_wsg84_to_lv95(47.01450, 8.30620)[1], 0), range(1207300,1207450))
+        with self.assertRaises(GeocodingError):
+            convert_wsg84_to_lv95(57.01450, 8.30620)
+        with self.assertRaises(GeocodingError):
+            convert_wsg84_to_lv95(47.01450, 15.30620)
+    
+    def test_get_altitude_lv95(self):
+        """Test get_altitude_ch_lv95() with different lat/long pairs"""
+        self.assertIn(round(get_altitude_lv95([2665949.0,1207341.8]),0), range(438,445))
+        self.assertIn(round(get_altitude_lv95(convert_wsg84_to_lv95(47.01450, 8.30620)),0), range(438,445))
+        
+        
+    def test_find_coordindates_ch_plz(self):
+        self.assertAlmostEqual(get_coordindates_ch_plz(6048)[0], 47.01, delta=0.1)
+        self.assertAlmostEqual(get_coordindates_ch_plz(6048)[1], 8.3, delta=0.1)
+        with self.assertRaises(GeocodingError):
+            get_coordindates_ch_plz(424242)
+            
+    def test_get_distance_between_two_points(self):
+        self.assertEqual(round(get_distance_between_two_points(get_coordindates_ch_plz(6048), get_coordindates_ch_plz(3800)), 0), 50)
+        self.assertAlmostEqual(get_distance_between_two_points(get_coordindates_ch_plz(6048), get_lat_long_address("20 W 34th, New York")), 6323.833, delta = 10)
+        with self.assertRaises(GeocodingError):
+            get_distance_between_two_points(get_coordindates_ch_plz(5555555), get_coordindates_ch_plz(3800))
+            
+if __name__ == '__main__':
+    unittest.main()
