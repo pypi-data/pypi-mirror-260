2b8698178bca0148ed2563facba828148579fd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:04:15 +0000

    Add package benimang/0.5.18/benimang-0.5.18.tar.gz

---
+++ packages/benimang/benimang-0.5.18.tar.gz/benimang-0.5.18/test/test_crypto.py
@@ -0,0 +1,10 @@
+# import pytest
+# from beni import bcrypto
+
+
+# @pytest.mark.asyncio
+# async def test():
+#     password = 'mima'
+#     content = 'hello'
+#     # magicContent = bcrypto.encode(content, password)
+#     # assert bcrypto.decode(magicContent, password) == content
