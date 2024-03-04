6f94e9d8b3330b799223e4a9a8680ddacf1abbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:08:06 +0000

    Add package internet-of-things-101-intel-edison/2024.3.4.0/internet_of_things_101_intel_edison-2024.3.4.0-py3-none-any.whl

---
+++ packages/internet-of-things-101-intel-edison/internet_of_things_101_intel_edison-2024.3.4.0-py3-none-any.whl/InternetOfThings101IntelEdison/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""internet-of-things-101-intel-edison
+https://github.com/apachecn/internet-of-things-101-intel-edison"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
