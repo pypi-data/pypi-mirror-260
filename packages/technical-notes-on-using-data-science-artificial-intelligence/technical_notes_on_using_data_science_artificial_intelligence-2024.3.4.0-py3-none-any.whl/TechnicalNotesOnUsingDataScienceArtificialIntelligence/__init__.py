49cc829c66dc1170acbd94d609b9db53b50bd28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:16 +0000

    Add package technical-notes-on-using-data-science-artificial-intelligence/2024.3.4.0/technical_notes_on_using_data_science_artificial_intelligence-2024.3.4.0-py3-none-any.whl

---
+++ packages/technical-notes-on-using-data-science-artificial-intelligence/technical_notes_on_using_data_science_artificial_intelligence-2024.3.4.0-py3-none-any.whl/TechnicalNotesOnUsingDataScienceArtificialIntelligence/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""technical-notes-on-using-data-science-artificial-intelligence
+https://github.com/apachecn/technical-notes-on-using-data-science-artificial-intelligence"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
