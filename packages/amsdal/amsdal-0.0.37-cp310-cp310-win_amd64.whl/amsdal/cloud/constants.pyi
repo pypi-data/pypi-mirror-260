66130aae6fd84a9344371bb2466254553432f443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 16:03:46 +0000

    Add package amsdal/0.0.37/amsdal-0.0.37-cp310-cp310-win_amd64.whl

---
+++ packages/amsdal/amsdal-0.0.37-cp310-cp310-win_amd64.whl/amsdal/cloud/constants.pyi
@@ -0,0 +1,8 @@
+from _typeshed import Incomplete
+
+JWT_PUBLIC_KEY: str
+ENCRYPT_PUBLIC_KEY: str
+SYNC_KEY: bytes
+IS_AMSDAL_SANDBOX_ENVIRONMENT: Incomplete
+AMSDAL_ENV_SUBDOMAIN: Incomplete
+BASE_AUTH_URL: Incomplete
