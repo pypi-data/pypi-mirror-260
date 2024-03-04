9d950960f6a719e3b7b64acc40f581d675ff60c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:13:50 +0000

    Add package dns-client/0.2.1/dns-client-0.2.1.tar.gz

---
+++ packages/dns-client/dns-client-0.2.1.tar.gz/dns-client-0.2.1/dns_client/__init__.py
@@ -0,0 +1,21 @@
+"""Python client library for sending DNS queries."""
+
+__author__ = "Sergey M"
+__version__ = "0.2.1"
+
+from .client import DNSClient
+from .errors import ConnectionError, DNSError, SocketError
+from .protocol import Header, Packet, Question, Record, RecordClass, RecordType
+
+__all__: tuple[str, ...] = (
+    "DNSError",
+    "DNSClient",
+    "ConnectionError",
+    "Header",
+    "Packet",
+    "Question",
+    "Record",
+    "RecordClass",
+    "RecordType",
+    "SocketError",
+)
