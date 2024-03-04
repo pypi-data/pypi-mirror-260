fdc66c53086bdeb4fa910962c76afa18a5d5ecb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:04:13 +0000

    Add package TCCreator/0.0.9/TCCreator-0.0.9.tar.gz

---
+++ packages/TCCreator/TCCreator-0.0.9.tar.gz/TCCreator-0.0.9/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 14:55:19 2023
+
+@author: QianYang
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="TCCreator",
+    version="0.0.9",
+    packages=find_packages(),
+    DESCRIPTION = 'Creat TC adn CKL with anotated/blank CRF and metadata file',
+)
