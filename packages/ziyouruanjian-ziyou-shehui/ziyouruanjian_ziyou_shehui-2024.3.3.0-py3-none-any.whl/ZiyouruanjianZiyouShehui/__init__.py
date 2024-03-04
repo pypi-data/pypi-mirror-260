54cd6ce8aa2488fc43b293a7f75d6055dedc0936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:04 +0000

    Add package ziyouruanjian-ziyou-shehui/2024.3.3.0/ziyouruanjian_ziyou_shehui-2024.3.3.0-py3-none-any.whl

---
+++ packages/ziyouruanjian-ziyou-shehui/ziyouruanjian_ziyou_shehui-2024.3.3.0-py3-none-any.whl/ZiyouruanjianZiyouShehui/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziyouruanjian-ziyou-shehui
+https://github.com/apachecn/ziyouruanjian-ziyou-shehui"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
