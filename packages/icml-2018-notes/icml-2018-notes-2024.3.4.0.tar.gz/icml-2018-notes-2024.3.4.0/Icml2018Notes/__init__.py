351577abb9576fe7b56f7a7f6d1bebcc6fef9d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:10 +0000

    Add package icml-2018-notes/2024.3.4.0/icml-2018-notes-2024.3.4.0.tar.gz

---
+++ packages/icml-2018-notes/icml-2018-notes-2024.3.4.0.tar.gz/icml-2018-notes-2024.3.4.0/Icml2018Notes/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""icml-2018-notes
+https://github.com/apachecn/icml-2018-notes"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
