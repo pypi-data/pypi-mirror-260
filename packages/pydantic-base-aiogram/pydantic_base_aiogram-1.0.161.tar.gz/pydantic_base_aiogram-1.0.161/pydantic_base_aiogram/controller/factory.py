cfd246c8178ac62fe41b37e08ffc350551ea25df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:25 +0000

    Add package pydantic-base-aiogram/1.0.161/pydantic_base_aiogram-1.0.161.tar.gz

---
+++ packages/pydantic-base-aiogram/pydantic_base_aiogram-1.0.161.tar.gz/pydantic_base_aiogram-1.0.161/pydantic_base_aiogram/controller/factory.py
@@ -0,0 +1,42 @@
+from abc import ABC
+from typing import Iterable, Optional, Type, Union
+from datetime import date
+from pydantic import BaseModel
+from pydantic.fields import ModelField
+from enum import Enum
+
+from pydantic_base_aiogram.field_factory import FieldFactory
+from pydantic_base_aiogram.types import FileType, OptionalAlbum, OptionalFile, Album
+from pydantic_base_aiogram.controller.optional_file import OptionalFileController
+
+from .type_base_message_text import TypeBaseMessageTextController
+from .date_message_text import DateMessageTextController
+from .cq_checkbox import CQCheckboxController
+from .custom_data_str import CustomDataStrController
+from .bool import BoolController
+from .file import FileController
+
+
+class ControllerFactory(FieldFactory, ABC):
+    CONVERT_DIALECTS = {
+        str: TypeBaseMessageTextController,
+        Enum: CQCheckboxController,
+        Union[str, Enum]: CustomDataStrController,
+        bool: BoolController,
+        date: DateMessageTextController,
+        FileType: FileController,
+        Album: FileController,
+        OptionalFile: OptionalFileController,
+        OptionalAlbum: OptionalFileController,
+    }
+
+    def create4models(self, field: ModelField, models: list[Type[BaseModel]], kwargs: dict):
+        res, _ = super().create4models(field, models, kwargs)
+        return res
+
+    def create4type(self, field: ModelField, parents: Optional[Iterable[str]] = None, force_type: Optional[type] = None, **kwargs):
+        if field.field_info.extra.get('getter'):
+            force_type = Union[str, Enum]
+
+        return super().create4type(field, parents, force_type, **kwargs)
+
