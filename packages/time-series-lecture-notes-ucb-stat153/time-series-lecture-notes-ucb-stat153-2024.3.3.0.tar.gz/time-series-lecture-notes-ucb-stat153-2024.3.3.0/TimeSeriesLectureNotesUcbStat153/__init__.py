609cc884993e52e2cb8f067b5e81628fb003f229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:44 +0000

    Add package time-series-lecture-notes-ucb-stat153/2024.3.3.0/time-series-lecture-notes-ucb-stat153-2024.3.3.0.tar.gz

---
+++ packages/time-series-lecture-notes-ucb-stat153/time-series-lecture-notes-ucb-stat153-2024.3.3.0.tar.gz/time-series-lecture-notes-ucb-stat153-2024.3.3.0/TimeSeriesLectureNotesUcbStat153/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""time-series-lecture-notes-ucb-stat153
+https://github.com/apachecn/time-series-lecture-notes-ucb-stat153"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
