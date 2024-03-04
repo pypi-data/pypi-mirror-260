7ec8533309ac1858a493901c6d6cb8b1ad058121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:03 +0000

    Add package EasyFTP/0.0.1b0/EasyFTP-0.0.1b0.tar.gz

---
+++ packages/EasyFTP/EasyFTP-0.0.1b0.tar.gz/EasyFTP-0.0.1b0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EasyFTP',
+    version='0.0.1b',
+    description='Easy usage of FTP operation',
+    author='ZustFancake',
+    author_email='ZustFancake@dimigo.hs.kr',
+    url='https://github.com/ZustFancake/lab/blob/main/python/EasyFTP.py',
+    install_requires=['ftplib'],
+    packages=find_packages(exclude=[]),
+    keywords=['zustfancake', 'ftp', 'easyftp', 'EasyFTP', 'pypi'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
