0dc7354418b2a053796904f7d2f20d0e6f634d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:49 +0000

    Add package yitai-fang-guanwang-wendang-zhongwenban/2024.3.3.0/yitai_fang_guanwang_wendang_zhongwenban-2024.3.3.0-py3-none-any.whl

---
+++ packages/yitai-fang-guanwang-wendang-zhongwenban/yitai_fang_guanwang_wendang_zhongwenban-2024.3.3.0-py3-none-any.whl/YitaiFangGuanwangWendangZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yitai-fang-guanwang-wendang-zhongwenban
+https://github.com/apachecn/yitai-fang-guanwang-wendang-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
