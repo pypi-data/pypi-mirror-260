ccf254a75197c5d7e246c52c4989602ce42db46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:09 +0000

    Add package exceltosqlserver/0.2.5/exceltosqlserver-0.2.5-py3-none-any.whl

---
+++ packages/exceltosqlserver/exceltosqlserver-0.2.5-py3-none-any.whl/exceltosqlserver/_api.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Tuesday, December 29th 2020
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+
+from .exceltosqlserver import ExcelToDB
+from .exceltosqlserver import hostname
+from .exceltosqlserver import local_ip
+
+__all__ = ['ExcelToDB', 'hostname', 'local_ip']
