af06a6c50f13f1198ce4c12de830725f47c040e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:30 +0000

    Add package shenruqianchu-java-web/2024.3.3.0/shenruqianchu_java_web-2024.3.3.0-py3-none-any.whl

---
+++ packages/shenruqianchu-java-web/shenruqianchu_java_web-2024.3.3.0-py3-none-any.whl/ShenruqianchuJavaWeb/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenruqianchu-java-web
+https://github.com/apachecn/shenruqianchu-java-web"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
