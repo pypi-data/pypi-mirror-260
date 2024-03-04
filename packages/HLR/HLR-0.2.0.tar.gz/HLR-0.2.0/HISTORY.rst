294871de6a79fec5739064d531a7ed8bb02790fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:42 +0000

    Add package HLR/0.2.0/HLR-0.2.0.tar.gz

---
+++ packages/HLR/HLR-0.2.0.tar.gz/HLR-0.2.0/HISTORY.rst
@@ -0,0 +1,29 @@
+=======
+History
+=======
+
+0.1.0 (2023-02-24)
+------------------
+
+* First release on PyPI.
+
+0.1.4 (2023-03-9)
+------------------
+
+* Fixed pairwise correlations threshold for multicollinearity assumption testing (0.3 -> 0.7)
+* Fixed partial regression plots fixed figure size
+* Added titles to diagnostic plots
+* Fixed the VIF to match with SPSS output by adding the constant to X
+
+0.1.5 (2023-04-6)
+------------------
+
+* Added standardised beta coefficients to model output
+* Added partial and semi-partial correlations (unique variance) to model output
+* Fixed F-change degrees of freedom calculation
+* Fixed F-change p-value calculation
+
+0.2.0 (2024-03-2)
+------------------
+
+* Overall project restructuring for optimisation
---
+++ packages/HLR/HLR-0.2.0.tar.gz/HLR-0.2.0/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['scipy>=1.7.3',
+                'pandas>=1.3.5',
+                'matplotlib>=3.5.2',
+                'seaborn>=0.11',
+                'statsmodels>=0.13',
+                'pingouin>=0.5.3']
+test_requirements = ['pytest>=3']
+
+setup(
+    author="Toomas Erik Anijärv",
+    author_email='toomaserikanijarv@gmail.com',
+    python_requires='>=3.9',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description="HLR - Hierarchical Linear Regression for Python",
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description_content_type='text/markdown',
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='HLR',
+    name='HLR',
+    packages=find_packages(include=['HLR', 'HLR.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/teanijarv/HLR',
+    version='0.2.0',
+    zip_safe=False,
+)
