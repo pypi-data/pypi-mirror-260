a8c1b8251873d2975d8fb86a125fa33bc421d413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:06:50 +0000

    Add package ire.py/0.0.0/ire.py-0.0.0.tar.gz

---
+++ packages/ire.py/ire.py-0.0.0.tar.gz/ire.py-0.0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ire.py",
+    version="0.0.0",
+    description="Export tables and plots from Jupyter notebooks, along with metadata for embedding interactive tables in downstream apps.",
+)
