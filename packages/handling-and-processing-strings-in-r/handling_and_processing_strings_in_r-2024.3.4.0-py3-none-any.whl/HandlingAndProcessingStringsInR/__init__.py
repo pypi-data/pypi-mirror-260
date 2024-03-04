25093c7223c39c7785c6b6c2e210add7956cbf3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:06 +0000

    Add package handling-and-processing-strings-in-r/2024.3.4.0/handling_and_processing_strings_in_r-2024.3.4.0-py3-none-any.whl

---
+++ packages/handling-and-processing-strings-in-r/handling_and_processing_strings_in_r-2024.3.4.0-py3-none-any.whl/HandlingAndProcessingStringsInR/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""handling-and-processing-strings-in-r
+https://github.com/apachecn/handling-and-processing-strings-in-r"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
