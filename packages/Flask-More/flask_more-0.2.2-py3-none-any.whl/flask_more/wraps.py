7891aa36b240dbd4c76a318ccc5250ad38ec06ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:27 +0000

    Add package Flask-More/0.2.2/flask_more-0.2.2-py3-none-any.whl

---
+++ packages/Flask-More/flask_more-0.2.2-py3-none-any.whl/flask_more/wraps.py
@@ -0,0 +1,52 @@
+from enum import Enum
+from functools import partial, wraps
+from typing import Any, Callable, Dict, List, Optional, Union
+
+from flask import jsonify
+from pydantic import BaseModel
+
+from flask_more.validate import validator
+
+
+def api(
+    f: Optional[Callable] = None,
+    *,
+    rsp_model: Optional[BaseModel] = None,
+    status: int = 200,
+    tags: Optional[List[Union[str, Enum]]] = None,
+    summary: Optional[str] = None,
+    description: Optional[str] = None,
+    response_description: str = "Successful Response",
+) -> Any:
+    if f is None:
+        return partial(
+            api,
+            rsp_model=rsp_model,
+            status=status,
+            tags=tags,
+            summary=summary,
+            description=description,
+            response_description=response_description,
+        )
+
+    api_desc = {
+        "tags": tags,
+        "summary": summary,
+        "description": description,
+        "response_description": response_description,
+    }
+    setattr(f, "__openapi__", api_desc)
+
+    @wraps(f)
+    def wrapper(*args: List, **kwargs: Dict) -> Any:
+        errors, validated = validator(f, *args, **kwargs)  # type: ignore
+        if errors["details"]:
+            return jsonify(errors), 400
+
+        kwargs = {**kwargs, **validated}
+
+        r = f(*args, **kwargs)  # type: ignore
+
+        return r, status
+
+    return wrapper
