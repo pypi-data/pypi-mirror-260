94336eb1ac06426a9c19999839e24071c1de3e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:35 +0000

    Add package xuexi-angularjs-1-x/2024.3.3.0/xuexi-angularjs-1-x-2024.3.3.0.tar.gz

---
+++ packages/xuexi-angularjs-1-x/xuexi-angularjs-1-x-2024.3.3.0.tar.gz/xuexi-angularjs-1-x-2024.3.3.0/XuexiAngularjs1X/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xuexi-angularjs-1-x
+https://github.com/apachecn/xuexi-angularjs-1-x"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
