080ea789f4376651d60afbe8d618524a6c4067cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:05 +0000

    Add package hadoop-the-definitive-guide-4e-zhongwenban/2024.3.4.0/hadoop_the_definitive_guide_4e_zhongwenban-2024.3.4.0-py3-none-any.whl

---
+++ packages/hadoop-the-definitive-guide-4e-zhongwenban/hadoop_the_definitive_guide_4e_zhongwenban-2024.3.4.0-py3-none-any.whl/HadoopTheDefinitiveGuide4eZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hadoop-the-definitive-guide-4e-zhongwenban
+https://github.com/apachecn/hadoop-the-definitive-guide-4e-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
