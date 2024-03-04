d4991a3b1f5b5f18c2a9076920218a2f6fae76a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 05:07:11 +0000

    Add package word2vec-zhong-de-shuxue/2024.3.4.0/word2vec_zhong_de_shuxue-2024.3.4.0-py3-none-any.whl

---
+++ packages/word2vec-zhong-de-shuxue/word2vec_zhong_de_shuxue-2024.3.4.0-py3-none-any.whl/Word2vecZhongDeShuxue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""word2vec-zhong-de-shuxue
+https://github.com/apachecn/word2vec-zhong-de-shuxue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
