4c9daa4cb1f12431effba32dc8726c0d3a59d0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:41 +0000

    Add package solidity-guanfang-wendang-zhongwenban/2024.3.3.0/solidity_guanfang_wendang_zhongwenban-2024.3.3.0-py3-none-any.whl

---
+++ packages/solidity-guanfang-wendang-zhongwenban/solidity_guanfang_wendang_zhongwenban-2024.3.3.0-py3-none-any.whl/SolidityGuanfangWendangZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""solidity-guanfang-wendang-zhongwenban
+https://github.com/apachecn/solidity-guanfang-wendang-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
