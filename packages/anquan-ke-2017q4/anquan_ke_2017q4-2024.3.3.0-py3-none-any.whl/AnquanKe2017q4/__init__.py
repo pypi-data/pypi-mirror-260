a39d44ff6b7bb9598d5ad056c728ddd3509f641d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:11 +0000

    Add package anquan-ke-2017q4/2024.3.3.0/anquan_ke_2017q4-2024.3.3.0-py3-none-any.whl

---
+++ packages/anquan-ke-2017q4/anquan_ke_2017q4-2024.3.3.0-py3-none-any.whl/AnquanKe2017q4/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""anquan-ke-2017q4
+https://github.com/apachecn/anquan-ke-2017q4"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
