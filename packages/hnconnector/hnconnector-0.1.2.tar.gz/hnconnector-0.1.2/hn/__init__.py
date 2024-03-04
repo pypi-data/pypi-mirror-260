e5b5ac01b4baf443eb7187fcbe19b608b565aee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:27 +0000

    Add package hnconnector/0.1.2/hnconnector-0.1.2.tar.gz

---
+++ packages/hnconnector/hnconnector-0.1.2.tar.gz/hnconnector-0.1.2/hn/__init__.py
@@ -0,0 +1,7 @@
+"""An unofficial Hacker News API Client Library in Python
+
+.. author:: mfiro
+
+"""
+
+from hn.client import Client
---
+++ packages/hnconnector/hnconnector-0.1.2.tar.gz/hnconnector-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hnconnector',
+    version='0.1.2',
+    packages=find_packages(),
+    description='An unofficial Hacker News API Client Library in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='mfiro',
+    author_email='mfiro@protonmail.com',
+    url='https://github.com/mfiro/hnconnector',
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
