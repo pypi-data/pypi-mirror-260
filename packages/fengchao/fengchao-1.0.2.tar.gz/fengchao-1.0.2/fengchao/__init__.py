0e8b89443404bee108ea376482ad9cef5b7228e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:20 +0000

    Add package fengchao/1.0.2/fengchao-1.0.2.tar.gz

---
+++ packages/fengchao/fengchao-1.0.2.tar.gz/fengchao-1.0.2/fengchao/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+"""
+# File       : __init__.py.py
+# Time       ：2024/2/23 13:34
+# Author     ：andy
+# version    ：python 3.9
+"""
+from .core import FengChao, LOCAL_URL, ONLINE_URL, MODELS_URL, PROMPTS_URL, KGS_URL
+from .__version__ import __version__
