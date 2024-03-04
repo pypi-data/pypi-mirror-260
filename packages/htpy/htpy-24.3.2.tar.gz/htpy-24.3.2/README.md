628023a4ae26400fbdca10be4faa937852e98236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:45 +0000

    Add package htpy/24.3.2/htpy-24.3.2.tar.gz

---
+++ packages/htpy/htpy-24.3.2.tar.gz/htpy-24.3.2/README.md
@@ -0,0 +1,17 @@
+
+# htpy - HTML in Python
+This project makes it possible to express HTML directly as Python code.
+
+## Development environment
+1) Create a virtualenv: ```python -m venv .venv```
+2) Install dependencies: ```pip install .[dev]```
+3) Run tests/checks: ```./test```
+
+## Examples
+
+### Flask with htmx
+See [examples/htmx_flask.py](https://github.com/pelme/htpy/blob/main/examples/htmx_flask.py). Run with:
+```
+$ pip install flask
+$ python examples/htmx_flask.py
+```
---
+++ packages/htpy/htpy-24.3.2.tar.gz/htpy-24.3.2/tests/test_escaping.py
@@ -0,0 +1,31 @@
+from markupsafe import Markup
+
+from htpy import div
+
+
+def test_escape_children() -> None:
+    result = str(div['>"'])
+    assert result == '<div>&gt;"</div>'
+
+
+def test_escape_kwarg_attribute() -> None:
+    assert str(div(id='"hello"')) == '<div id="&quot;hello&quot;"></div>'
+
+
+def test_escape_dict_attribute() -> None:
+    assert str(div({'<"': '"hello"'})) == '<div &lt;&quot;="&quot;hello&quot;"></div>'
+
+
+def test_safe_children() -> None:
+    result = str(div[Markup("<hello></hello>")])
+    assert result == "<div><hello></hello></div>"
+
+
+def test_safe_kwarg_attribute() -> None:
+    result = str(div(id=Markup("<hi>")))
+    assert result == '<div id="<hi>"></div>'
+
+
+def test_safe_dict_attribute() -> None:
+    result = str(div({Markup('<"'): Markup('"hello"')}))
+    assert result == '<div <"=""hello""></div>'
