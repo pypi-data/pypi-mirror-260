d6269b8f1c6bcad5118c5ee1daef4520a37a0f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:03 +0000

    Add package econometrics-streamlined-applied-and-e-aware/2024.3.3.0/econometrics_streamlined_applied_and_e_aware-2024.3.3.0-py3-none-any.whl

---
+++ packages/econometrics-streamlined-applied-and-e-aware/econometrics_streamlined_applied_and_e_aware-2024.3.3.0-py3-none-any.whl/EconometricsStreamlinedAppliedAndEAware/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""econometrics-streamlined-applied-and-e-aware
+https://github.com/apachecn/econometrics-streamlined-applied-and-e-aware"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
