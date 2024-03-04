c91ad4991a7f8bd9fb6340fd5b06633d050cc3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:10 +0000

    Add package color-science/0.4.5/color-science-0.4.5.tar.gz

---
+++ packages/color-science/color-science-0.4.5.tar.gz/color-science-0.4.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+  name='color-science',
+  version='0.4.5',
+  py_modules=['colour'], 
+  description="Protective placeholder package for 'color-science'. Please use 'colour-science' instead.",
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  author='Jingyi Yang',
+  author_email='yangjingyi1210@gmail.com',
+  url="https://pypi.org/project/color-science",
+  license='MIT',
+  classifiers=[
+    "Programming Language :: Python",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
