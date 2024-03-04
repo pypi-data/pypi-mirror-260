81b24a3183135e9cca71eb8a5fa27e2780820c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:06:55 +0000

    Add package dai-ni-wanzhuan-visual-studio/2024.3.4.0/dai-ni-wanzhuan-visual-studio-2024.3.4.0.tar.gz

---
+++ packages/dai-ni-wanzhuan-visual-studio/dai-ni-wanzhuan-visual-studio-2024.3.4.0.tar.gz/dai-ni-wanzhuan-visual-studio-2024.3.4.0/DaiNiWanzhuanVisualStudio/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dai-ni-wanzhuan-visual-studio
+https://github.com/apachecn/dai-ni-wanzhuan-visual-studio"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
