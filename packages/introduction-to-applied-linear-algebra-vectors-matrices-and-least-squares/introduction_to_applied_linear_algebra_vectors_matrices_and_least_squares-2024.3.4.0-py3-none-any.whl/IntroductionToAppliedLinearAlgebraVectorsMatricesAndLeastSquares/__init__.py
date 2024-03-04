0fbf73568e05fa303bdafae0ed91bc4098ef8784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:13 +0000

    Add package introduction-to-applied-linear-algebra-vectors-matrices-and-least-squares/2024.3.4.0/introduction_to_applied_linear_algebra_vectors_matrices_and_least_squares-2024.3.4.0-py3-none-any.whl

---
+++ packages/introduction-to-applied-linear-algebra-vectors-matrices-and-least-squares/introduction_to_applied_linear_algebra_vectors_matrices_and_least_squares-2024.3.4.0-py3-none-any.whl/IntroductionToAppliedLinearAlgebraVectorsMatricesAndLeastSquares/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""introduction-to-applied-linear-algebra-vectors-matrices-and-least-squares
+https://github.com/apachecn/introduction-to-applied-linear-algebra-vectors-matrices-and-least-squares"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
