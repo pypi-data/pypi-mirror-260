f38c5ec8c4fb581b9a4ecd5950ac8910836ca86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:35 +0000

    Add package AndN/0.6.4/AndN-0.6.4.tar.gz

---
+++ packages/AndN/AndN-0.6.4.tar.gz/AndN-0.6.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+setuptools.setup(
+    name='AndN',
+    version='0.6.4',
+    url='https://github.com/grayrail000/AndroidQQ',
+    packages=setuptools.find_packages(),
+    license='',
+    author='1a',
+    author_email='',
+    description='',
+    install_requires=[
+        'AndroidTools',
+        'protobuf==4.23.4',
+        'cryptography',
+        'bs4',
+        'urllib3==1.26.16',
+        'pydantic',
+        'requests',
+        'python-box',
+        'PySocks',
+        'python-box'
+
+    ]
+
+)
