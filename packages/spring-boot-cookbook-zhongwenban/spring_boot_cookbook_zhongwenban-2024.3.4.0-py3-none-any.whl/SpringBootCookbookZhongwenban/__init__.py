76233425c8b82f973eb0e53f8a4609ffe4fdaa2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:12 +0000

    Add package spring-boot-cookbook-zhongwenban/2024.3.4.0/spring_boot_cookbook_zhongwenban-2024.3.4.0-py3-none-any.whl

---
+++ packages/spring-boot-cookbook-zhongwenban/spring_boot_cookbook_zhongwenban-2024.3.4.0-py3-none-any.whl/SpringBootCookbookZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""spring-boot-cookbook-zhongwenban
+https://github.com/apachecn/spring-boot-cookbook-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
