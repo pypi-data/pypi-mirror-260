370e8674d73e02b6ab9e807216b3d2c2221e32ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:10 +0000

    Add package fintekkers-devops-scripts/0.0.3/fintekkers-devops-scripts-0.0.3.tar.gz

---
+++ packages/fintekkers-devops-scripts/fintekkers-devops-scripts-0.0.3.tar.gz/fintekkers-devops-scripts-0.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = "0.0.0"
+DESCRIPTION = "Fintekkers Devops python package"
+LONG_DESCRIPTION = (
+    "Contains scripts to build and deploy Fintekkers services to the public cloud"
+)
+
+if "BUILD_VERSION" in os.environ:
+    print("******************************************")
+    print("************OVERRIDING VERSION FROM ENVIRONMENT******************")
+    print("******************************************")
+    VERSION = os.environ.get("BUILD_VERSION")
+
+setup(
+    name="fintekkers-devops-scripts",
+    license="MIT",
+    author="David Doherty",
+    author_email="dave@fintekkers.org",
+    include_package_data=True,
+    url="https://github.com/fintekkers/fintekkers-devops-scripts",
+    keywords="fintekkers devops",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
