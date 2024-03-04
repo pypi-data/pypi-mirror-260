926e8184f7b5c3bf3bebd0a8dfb6a9eeb7f05bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:18 +0000

    Add package forecasting-in-economics-business-finance-and-beyond/2024.3.3.0/forecasting-in-economics-business-finance-and-beyond-2024.3.3.0.tar.gz

---
+++ packages/forecasting-in-economics-business-finance-and-beyond/forecasting-in-economics-business-finance-and-beyond-2024.3.3.0.tar.gz/forecasting-in-economics-business-finance-and-beyond-2024.3.3.0/ForecastingInEconomicsBusinessFinanceAndBeyond/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""forecasting-in-economics-business-finance-and-beyond
+https://github.com/apachecn/forecasting-in-economics-business-finance-and-beyond"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
