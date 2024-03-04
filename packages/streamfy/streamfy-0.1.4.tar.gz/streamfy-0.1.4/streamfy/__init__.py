a8d3416307aa3d93656c8de8f981e248a97450e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:25 +0000

    Add package streamfy/0.1.4/streamfy-0.1.4.tar.gz

---
+++ packages/streamfy/streamfy-0.1.4.tar.gz/streamfy-0.1.4/streamfy/__init__.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+import os
+
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "streamfy",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/dist")
+    _component_func = components.declare_component(
+        "streamfy", path=build_dir)
+
+def taginput(data=[], placeholder="", label="", key=None):
+    component_value = _component_func(component="taginput", label=label, data=data, placeholder=placeholder, key=key)
+    return component_value
+
+def table(data=[], columns=[], label="", key=None):
+    component_value = _component_func(component="table", label=label, data=data, columns=columns)
+    return component_value
+
+if not _RELEASE:
+    st.subheader("Tags")
+    tags = taginput(data = ["A", "B", "C"], placeholder = "Choose letter")
+    st.write(tags)
+    st.subheader("Table")
+    columns = [
+        {
+            "field": 'id',
+            "label": 'ID',
+            "width": '40',
+            "numeric": True
+        },
+        {
+            "field": 'name',
+            "label": 'Name',
+        },
+    ]
+    data = [
+        { 'id': 1, 'name': 'Jesse' },
+        { 'id': 2, 'first_name': 'John' },
+    ]
+    table(data = data, columns = columns)
