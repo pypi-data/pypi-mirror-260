411f57e6652333ff8a8729355fe5645b8953f564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:35 +0000

    Add package pip-check-updates/0.24.0/pip-check-updates-0.24.0.tar.gz

---
+++ packages/pip-check-updates/pip-check-updates-0.24.0.tar.gz/pip-check-updates-0.24.0/src/pip_check_updates/style.py
@@ -0,0 +1,43 @@
+import ntpath
+import os
+import posixpath
+from pathlib import PurePosixPath, PureWindowsPath
+
+from colorama import Fore, Style
+
+
+def styled_text(text, category, no_color):
+    if no_color:
+        return text
+
+    mapping = {
+        "major": Fore.RED,
+        "minor": Fore.CYAN,
+        "patch": Fore.GREEN,
+        "other": Fore.MAGENTA,
+        "info": Fore.BLUE,
+        "cmd": Fore.YELLOW,
+        "warning": Fore.YELLOW,
+        "success": Fore.GREEN,
+        "error": Fore.RED,
+    }
+
+    color = mapping.get(category)
+
+    if color:
+        return color + text + Style.RESET_ALL
+
+    return text
+
+
+# https://stackoverflow.com/a/72064941
+def dot_path(pth):
+    """Return path str that may start with '.' if relative."""
+    if pth.is_absolute():
+        return os.fsdecode(pth)
+    if isinstance(pth, PureWindowsPath):
+        return ntpath.join(".", pth)
+    elif isinstance(pth, PurePosixPath):
+        return posixpath.join(".", pth)
+    else:
+        return os.path.join(".", pth)
