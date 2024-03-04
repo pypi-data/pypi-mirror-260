024e66c9078423c6f9690d67bd2c21799534953d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:04:05 +0000

    Add package itss/0.3/itss-0.3-py3-none-any.whl

---
+++ packages/itss/itss-0.3-py3-none-any.whl/itss/fun1.py
@@ -0,0 +1,10 @@
+def nithin():
+    a='hi nithin'
+    print(a)
+    return a
+
+def tamil():
+    b='hi tamil'
+    print(b)
+    return b
+
---
+++ packages/itss/itss-0.3-py3-none-any.whl/itss/fun2.py
@@ -0,0 +1,15 @@
+# create a Geeksforgeeks class
+class itssgroup:
+
+	def __init__(self):
+
+		self.topic = "ITSS EF"
+		self.place = "ITSS CBE"
+
+	def cbe(self):
+
+		print("Topic:", self.topic)
+		print("Place:", self.place)
+		
+
+
