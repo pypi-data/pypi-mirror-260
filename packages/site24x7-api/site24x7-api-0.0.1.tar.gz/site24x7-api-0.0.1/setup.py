6933f3c61d06ab9e9e17c07c83a088bcca17f9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:42 +0000

    Add package site24x7-api/0.0.1/site24x7-api-0.0.1.tar.gz

---
+++ packages/site24x7-api/site24x7-api-0.0.1.tar.gz/site24x7-api-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='site24x7-api',
+version='0.0.1',
+author='Subscription-pypi Creatoriq',
+author_email='subscription-pypi@creatoriq.com',
+description='Packageholdername site24x7-api  for square-math function',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.9',
+)
