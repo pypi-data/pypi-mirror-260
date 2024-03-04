011a5193c44be97b790121c815ab3eb8b92fbf30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:28 +0000

    Add package shengwu-xinxixue-zhaji-disanban/2024.3.3.0/shengwu_xinxixue_zhaji_disanban-2024.3.3.0-py3-none-any.whl

---
+++ packages/shengwu-xinxixue-zhaji-disanban/shengwu_xinxixue_zhaji_disanban-2024.3.3.0-py3-none-any.whl/ShengwuXinxixueZhajiDisanban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shengwu-xinxixue-zhaji-disanban
+https://github.com/apachecn/shengwu-xinxixue-zhaji-disanban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
