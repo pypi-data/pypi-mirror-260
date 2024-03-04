8ad57956368b6d3a9f71f94896391c1f9b3cac19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:15 +0000

    Add package anzhuo-yingyong-anquan-zhinan-zhongwenban/2024.3.3.0/anzhuo_yingyong_anquan_zhinan_zhongwenban-2024.3.3.0-py3-none-any.whl

---
+++ packages/anzhuo-yingyong-anquan-zhinan-zhongwenban/anzhuo_yingyong_anquan_zhinan_zhongwenban-2024.3.3.0-py3-none-any.whl/AnzhuoYingyongAnquanZhinanZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""anzhuo-yingyong-anquan-zhinan-zhongwenban
+https://github.com/apachecn/anzhuo-yingyong-anquan-zhinan-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
