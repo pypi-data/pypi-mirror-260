28078a44e25b7b8204336af37a13345c1aca6f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:26 +0000

    Add package myfirstmodelcsc/1.0/myfirstmodelcsc-1.0-py3-none-any.whl

---
+++ packages/myfirstmodelcsc/myfirstmodelcsc-1.0-py3-none-any.whl/myfirstmodelcsc/First_Test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/3/4 10:30
+# @Author  : Chen_Shi_Chao
+# @Email   : M202373504@hust.edu.cn
+# @File    : First_Test.py
+# @Software: PyCharm
+import numpy as np
+def CSC_First_Test(a,b):
+
+    distance = np.linalg.norm(a - b)  # 计算欧氏距离
+
+    return distance
+
+if __name__ == '__main__':
+     import MyModel_CSC
+
---
+++ packages/myfirstmodelcsc/myfirstmodelcsc-1.0-py3-none-any.whl/myfirstmodelcsc/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/3/4 12:59
+# @Author  : Chen_Shi_Chao
+# @Email   : M202373504@hust.edu.cn
+# @File    : __init__.py
+# @Software: PyCharm
