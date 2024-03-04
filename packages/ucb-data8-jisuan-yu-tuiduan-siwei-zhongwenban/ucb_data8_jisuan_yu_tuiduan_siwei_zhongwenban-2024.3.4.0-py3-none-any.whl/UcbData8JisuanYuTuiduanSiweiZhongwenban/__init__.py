1d2c73b8aa953dba9e05bb6b3044e6bdb0b043ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:10 +0000

    Add package ucb-data8-jisuan-yu-tuiduan-siwei-zhongwenban/2024.3.4.0/ucb_data8_jisuan_yu_tuiduan_siwei_zhongwenban-2024.3.4.0-py3-none-any.whl

---
+++ packages/ucb-data8-jisuan-yu-tuiduan-siwei-zhongwenban/ucb_data8_jisuan_yu_tuiduan_siwei_zhongwenban-2024.3.4.0-py3-none-any.whl/UcbData8JisuanYuTuiduanSiweiZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ucb-data8-jisuan-yu-tuiduan-siwei-zhongwenban
+https://github.com/apachecn/ucb-data8-jisuan-yu-tuiduan-siwei-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
