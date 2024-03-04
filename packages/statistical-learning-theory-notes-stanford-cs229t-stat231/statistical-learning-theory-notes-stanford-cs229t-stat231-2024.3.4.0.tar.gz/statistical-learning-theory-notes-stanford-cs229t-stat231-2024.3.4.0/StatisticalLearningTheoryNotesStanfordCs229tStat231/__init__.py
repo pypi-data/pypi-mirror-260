d98e90eca65cadcf7735a4e7edb148800fec3723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:07:31 +0000

    Add package statistical-learning-theory-notes-stanford-cs229t-stat231/2024.3.4.0/statistical-learning-theory-notes-stanford-cs229t-stat231-2024.3.4.0.tar.gz

---
+++ packages/statistical-learning-theory-notes-stanford-cs229t-stat231/statistical-learning-theory-notes-stanford-cs229t-stat231-2024.3.4.0.tar.gz/statistical-learning-theory-notes-stanford-cs229t-stat231-2024.3.4.0/StatisticalLearningTheoryNotesStanfordCs229tStat231/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""statistical-learning-theory-notes-stanford-cs229t-stat231
+https://github.com/apachecn/statistical-learning-theory-notes-stanford-cs229t-stat231"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
