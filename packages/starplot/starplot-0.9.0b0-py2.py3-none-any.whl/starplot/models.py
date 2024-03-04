eaa69bd1c848cc126b59d6d71df46787eb03076f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 14:03:16 +0000

    Add package starplot/0.9.0b0/starplot-0.9.0b0-py2.py3-none-any.whl

---
+++ packages/starplot/starplot-0.9.0b0-py2.py3-none-any.whl/starplot/models.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Star(BaseModel):
+    """
+    Star model. An instance of this model is passed to any [callables](/reference-callables) you define when plotting stars.
+    So, you can use any attributes of this model in your callables. Note that some may be null.
+    """
+
+    ra: float
+    """Right Ascension"""
+
+    dec: float
+    """Declination"""
+
+    magnitude: float
+    """Magnitude"""
+
+    bv: Optional[float] = None
+    """B-V Color Index, if available"""
+
+    hip: Optional[int] = None
+    """Hipparcos Catalog ID, if available"""
