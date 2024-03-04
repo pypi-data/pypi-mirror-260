1f3777fb63e78922ef3b88c1973278388c8753b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:49 +0000

    Add package lm-dw-deezer/0.0.1b600/lm_dw_deezer-0.0.1b600-py3-none-any.whl

---
+++ packages/lm-dw-deezer/lm_dw_deezer-0.0.1b600-py3-none-any.whl/lm_dw_deezer/generators/album.py
@@ -0,0 +1,32 @@
+# https://stackoverflow.com/questions/34073370/best-way-to-receive-the-return-value-from-a-python-generator
+
+from ..types import (
+	DW_Album, ITrack_Out
+)
+
+from ..types.aliases import (
+	G_Album, G_Track_Out
+)
+
+
+class Gen_Album:
+	def __init__(self, gen: G_Album) -> None:
+		self.__gen = gen
+		self.__first()
+
+
+	def __first(self) -> None:
+		self.album: DW_Album = next(self.__gen) #pyright: ignore [reportArgumentType]
+
+
+	def dw(self) -> ITrack_Out:
+		return next(self.__gen) #pyright: ignore [reportReturnType]
+
+
+	def __iter__(self) -> G_Track_Out:
+		yield from self.__gen #pyright: ignore [reportReturnType]
+
+
+	def wait(self) -> None:
+		for _ in self.__gen:
+			...
