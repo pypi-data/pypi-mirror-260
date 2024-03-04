8ebc6f9efce5353740983f4e4dcd6ef16893183a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 04:06:56 +0000

    Add package pkscreener/0.44.20240303.213/pkscreener-0.44.20240303.213-py3-none-any.whl

---
+++ packages/pkscreener/pkscreener-0.44.20240303.213-py3-none-any.whl/pkscreener/classes/Fetcher.py
@@ -0,0 +1,197 @@
+"""
+    The MIT License (MIT)
+
+    Copyright (c) 2023 pkjmesra
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+"""
+
+import os
+import sys
+import warnings
+
+warnings.simplefilter("ignore", DeprecationWarning)
+warnings.simplefilter("ignore", FutureWarning)
+import pandas as pd
+import yfinance as yf
+from PKDevTools.classes.ColorText import colorText
+from PKDevTools.classes.Fetcher import StockDataEmptyException
+from PKDevTools.classes.log import default_logger
+from PKDevTools.classes.SuppressOutput import SuppressOutput
+from PKNSETools.PKNSEStockDataFetcher import nseStockDataFetcher
+
+# This Class Handles Fetching of Stock Data over the internet
+
+
+class screenerStockDataFetcher(nseStockDataFetcher):
+    # Fetch stock price data from Yahoo finance
+    def fetchStockData(
+        self,
+        stockCode,
+        period,
+        duration,
+        proxyServer,
+        screenResultsCounter,
+        screenCounter,
+        totalSymbols,
+        printCounter=False,
+        start=None, 
+        end=None,
+    ):
+        with SuppressOutput(suppress_stdout=True, suppress_stderr=True):
+            data = yf.download(
+                tickers=stockCode + ".NS",
+                period=period,
+                interval=duration,
+                proxy=proxyServer,
+                progress=False,
+                timeout=self.configManager.longTimeout,
+                start=start,
+                end=end
+            )
+        if printCounter:
+            sys.stdout.write("\r\033[K")
+            try:
+                print(
+                    colorText.BOLD
+                    + colorText.GREEN
+                    + (
+                        "[%d%%] Screened %d, Found %d. Fetching data & Analyzing %s..."
+                        % (
+                            int((screenCounter.value / totalSymbols) * 100),
+                            screenCounter.value,
+                            screenResultsCounter.value,
+                            stockCode,
+                        )
+                    )
+                    + colorText.END,
+                    end="",
+                )
+            except ZeroDivisionError as e: # pragma: no cover
+                default_logger().debug(e, exc_info=True)
+                pass
+            except Exception as e:  # pragma: no cover
+                default_logger().debug(e, exc_info=True)
+                pass
+            if len(data) == 0:
+                print(
+                    colorText.BOLD
+                    + colorText.FAIL
+                    + "=> Failed to fetch!"
+                    + colorText.END,
+                    end="\r",
+                    flush=True,
+                )
+                raise StockDataEmptyException
+                return None
+            print(
+                colorText.BOLD + colorText.GREEN + "=> Done!" + colorText.END,
+                end="\r",
+                flush=True,
+            )
+        return data
+
+    # Get Daily Nifty 50 Index:
+    def fetchLatestNiftyDaily(self, proxyServer=None):
+        data = yf.download(
+            tickers="^NSEI",
+            period="5d",
+            interval="1d",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        return data
+
+    # Get Data for Five EMA strategy
+    def fetchFiveEmaData(self, proxyServer=None):
+        nifty_sell = yf.download(
+            tickers="^NSEI",
+            period="5d",
+            interval="5m",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        banknifty_sell = yf.download(
+            tickers="^NSEBANK",
+            period="5d",
+            interval="5m",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        nifty_buy = yf.download(
+            tickers="^NSEI",
+            period="5d",
+            interval="15m",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        banknifty_buy = yf.download(
+            tickers="^NSEBANK",
+            period="5d",
+            interval="15m",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        return nifty_buy, banknifty_buy, nifty_sell, banknifty_sell
+
+    # Load stockCodes from the watchlist.xlsx
+    def fetchWatchlist(self):
+        createTemplate = False
+        data = pd.DataFrame()
+        try:
+            data = pd.read_excel("watchlist.xlsx")
+        except FileNotFoundError as e:  # pragma: no cover
+            default_logger().debug(e, exc_info=True)
+            print(
+                colorText.BOLD
+                + colorText.FAIL
+                + f"[+] watchlist.xlsx not found in {os.getcwd()}"
+                + colorText.END
+            )
+            createTemplate = True
+        try:
+            if not createTemplate:
+                data = data["Stock Code"].values.tolist()
+        except KeyError as e: # pragma: no cover
+            default_logger().debug(e, exc_info=True)
+            print(
+                colorText.BOLD
+                + colorText.FAIL
+                + '[+] Bad Watchlist Format: First Column (A1) should have Header named "Stock Code"'
+                + colorText.END
+            )
+            createTemplate = True
+        if createTemplate:
+            sample = {"Stock Code": ["SBIN", "INFY", "TATAMOTORS", "ITC"]}
+            sample_data = pd.DataFrame(sample, columns=["Stock Code"])
+            sample_data.to_excel("watchlist_template.xlsx", index=False, header=True)
+            print(
+                colorText.BOLD
+                + colorText.BLUE
+                + f"[+] watchlist_template.xlsx created in {os.getcwd()} as a referance template."
+                + colorText.END
+            )
+            return None
+        return data
