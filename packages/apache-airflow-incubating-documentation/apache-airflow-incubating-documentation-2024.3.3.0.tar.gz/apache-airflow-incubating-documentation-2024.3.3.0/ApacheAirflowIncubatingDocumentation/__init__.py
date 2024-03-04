58d215a49628850c6b3657206394242f513e4156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:16 +0000

    Add package apache-airflow-incubating-documentation/2024.3.3.0/apache-airflow-incubating-documentation-2024.3.3.0.tar.gz

---
+++ packages/apache-airflow-incubating-documentation/apache-airflow-incubating-documentation-2024.3.3.0.tar.gz/apache-airflow-incubating-documentation-2024.3.3.0/ApacheAirflowIncubatingDocumentation/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apache-airflow-incubating-documentation
+https://github.com/apachecn/apache-airflow-incubating-documentation"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
