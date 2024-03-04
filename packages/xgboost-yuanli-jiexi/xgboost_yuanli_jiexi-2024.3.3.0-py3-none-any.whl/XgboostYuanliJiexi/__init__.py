b5e1ebbe23880e328561309c585d4b5ac26f573a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:14:35 +0000

    Add package xgboost-yuanli-jiexi/2024.3.3.0/xgboost_yuanli_jiexi-2024.3.3.0-py3-none-any.whl

---
+++ packages/xgboost-yuanli-jiexi/xgboost_yuanli_jiexi-2024.3.3.0-py3-none-any.whl/XgboostYuanliJiexi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xgboost-yuanli-jiexi
+https://github.com/apachecn/xgboost-yuanli-jiexi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
