98cf9e1bd746b7d9c33c06ce8dec4572c95c7313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 05:07:15 +0000

    Add package data-science-and-analytics-for-ordinary-people/2024.3.4.0/data-science-and-analytics-for-ordinary-people-2024.3.4.0.tar.gz

---
+++ packages/data-science-and-analytics-for-ordinary-people/data-science-and-analytics-for-ordinary-people-2024.3.4.0.tar.gz/data-science-and-analytics-for-ordinary-people-2024.3.4.0/DataScienceAndAnalyticsForOrdinaryPeople/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""data-science-and-analytics-for-ordinary-people
+https://github.com/apachecn/data-science-and-analytics-for-ordinary-people"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
