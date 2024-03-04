0e898d11434fa7744e69aef82aa3c4db7d5ce173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:19 +0000

    Add package cognite-neat/0.65.0/cognite_neat-0.65.0-py3-none-any.whl

---
+++ packages/cognite-neat/cognite_neat-0.65.0-py3-none-any.whl/cognite/neat/rules/exporters/__init__.py
@@ -0,0 +1,4 @@
+from ._rules2dms import CDFExporter, DMSExporter
+from ._rules2ontology import GraphExporter, OWLExporter, SemanticDataModelExporter, SHACLExporter
+
+__all__ = ["DMSExporter", "CDFExporter", "SemanticDataModelExporter", "OWLExporter", "GraphExporter", "SHACLExporter"]
---
+++ packages/cognite-neat/cognite_neat-0.65.0-py3-none-any.whl/cognite/neat/rules/importers/_base.py
@@ -0,0 +1,48 @@
+import getpass
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Literal, TypeAlias, overload
+
+from rdflib import Namespace
+
+from cognite.neat.rules.models._rules import DMSRules, DomainRules, InformationRules, RoleTypes
+from cognite.neat.rules.validation import IssueList
+
+Rule: TypeAlias = DomainRules | InformationRules | DMSRules
+
+
+class BaseImporter(ABC):
+    """
+    BaseImporter class which all importers inherit from.
+    """
+
+    @overload
+    def to_rules(self, errors: Literal["raise"], role: RoleTypes | None = None) -> Rule:
+        ...
+
+    @overload
+    def to_rules(
+        self, errors: Literal["continue"] = "continue", role: RoleTypes | None = None
+    ) -> tuple[Rule | None, IssueList]:
+        ...
+
+    @abstractmethod
+    def to_rules(
+        self, errors: Literal["raise", "continue"] = "continue", role: RoleTypes | None = None
+    ) -> tuple[Rule | None, IssueList] | Rule:
+        """
+        Creates `Rules` object from the data for target role.
+        """
+        ...
+
+    def _default_metadata(self):
+        return {
+            "prefix": "neat",
+            "namespace": Namespace("http://purl.org/cognite/neat/"),
+            "version": "0.1.0",
+            "title": "Neat Imported Data Model",
+            "created": datetime.now().replace(microsecond=0).isoformat(),
+            "updated": datetime.now().replace(microsecond=0).isoformat(),
+            "creator": getpass.getuser(),
+            "description": f"Imported using {type(self).__name__}",
+        }
