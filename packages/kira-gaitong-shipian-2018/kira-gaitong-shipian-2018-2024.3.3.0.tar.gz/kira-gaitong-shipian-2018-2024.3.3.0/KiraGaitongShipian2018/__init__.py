9bca370b4a656ad11650025b651f7c12cd9bd82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 03:13:48 +0000

    Add package kira-gaitong-shipian-2018/2024.3.3.0/kira-gaitong-shipian-2018-2024.3.3.0.tar.gz

---
+++ packages/kira-gaitong-shipian-2018/kira-gaitong-shipian-2018-2024.3.3.0.tar.gz/kira-gaitong-shipian-2018-2024.3.3.0/KiraGaitongShipian2018/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kira-gaitong-shipian-2018
+https://github.com/apachecn/kira-gaitong-shipian-2018"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.3.0"
---
+++ packages/kira-gaitong-shipian-2018/kira-gaitong-shipian-2018-2024.3.3.0.tar.gz/kira-gaitong-shipian-2018-2024.3.3.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import KiraGaitongShipian2018
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('KiraGaitongShipian2018'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="kira-gaitong-shipian-2018",
+    version=KiraGaitongShipian2018.__version__,
+    url="https://github.com/apachecn/kira-gaitong-shipian-2018",
+    author=KiraGaitongShipian2018.__author__,
+    author_email=KiraGaitongShipian2018.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="Kira 概统十篇 2018",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "kira-gaitong-shipian-2018=KiraGaitongShipian2018.__main__:main",
+            "KiraGaitongShipian2018=KiraGaitongShipian2018.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
