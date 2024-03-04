5fff58c391956248959851ef19e0f6bcc3505e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-04 15:03:37 +0000

    Add package launchable/1.82.5/launchable-1.82.5-py3-none-any.whl

---
+++ packages/launchable/launchable-1.82.5-py3-none-any.whl/tests/test_plugin.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+from unittest import mock
+
+import responses  # type: ignore
+
+from .cli_test_case import CliTestCase
+
+
+class PluginTest(CliTestCase):
+    @responses.activate
+    @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
+    def test_plugin_loading(self):
+        """
+        Load plugins/foo.py as a plugin and execute its code
+        """
+        plugin_dir = Path(__file__).parent.joinpath('plugins').resolve()
+        result = self.cli('--plugins', str(plugin_dir), 'record', 'tests',
+                          '--session', 'builds/dummy/test_sessions/123', 'foo', 'alpha', 'bravo', 'charlie')
+        self.assertTrue("foo:alpha" in result.stdout, result.stdout)
+        self.assertTrue("foo:bravo" in result.stdout, result.stdout)
+        self.assertTrue("foo:charlie" in result.stdout, result.stdout)
