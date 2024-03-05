2bcdec6f845252f181e4974093a1d286d56c679a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:08 +0000

    Add package myalias/0.2.2/myalias-0.2.2-py3-none-any.whl

---
+++ packages/myalias/myalias-0.2.2-py3-none-any.whl/myalias/cli.py
@@ -0,0 +1,101 @@
+import typer
+from rich.console import Console
+
+from myalias.commands.add_alias_command import AddAliasCommand
+from myalias.commands.import_aliases_command import ImportAliasesCommand
+from myalias.commands.list_alias_command import ListAliasCommand
+from myalias.commands.remove_alias_command import RemoveAliasCommand
+from myalias.core.commands.about_command import AboutCommand
+from myalias.core.commands.setup_command import SetupCommand
+from myalias.core.services.append_commands_cli_service import (
+    AppendCommandsCliService,
+)
+from myalias.core.services.update_version_app_service import (
+    UpdateVersionAppService,
+)
+
+console = Console()
+
+app = typer.Typer(
+    name='MyApp',
+    add_completion=False,
+)
+
+
+def versionCallback(showVersion: bool):
+    """Display information version the application."""
+    if showVersion:
+        AboutCommand().execute()
+        raise typer.Exit()
+
+
+version = typer.Option(
+    None,
+    '--version',
+    '-v',
+    callback=versionCallback,
+    is_eager=True,
+    help='Show the version and exit.',
+)
+
+
+@app.callback()
+def main(version: bool = version):
+    pass
+
+
+@app.command(rich_help_panel='Help and Others')
+def about():
+    """Display information about the application."""
+    AboutCommand().execute()
+
+
+@app.command(name='setup', rich_help_panel='Help and Others')
+def setup():
+    """Setup initial to configure application."""
+    SetupCommand().execute()
+
+
+@app.command(name='add-alias', rich_help_panel='Commands')
+def add_alias(name, description, command):
+    """
+    Add alias to the application.
+    Args:
+        name (str): Name of command.
+        description (str): Description of command.
+        command (str): Command to execute.
+
+    """
+    AddAliasCommand().execute(name, description, command)
+
+
+@app.command(name='import-aliases', rich_help_panel='Commands')
+def import_aliases(file_path):
+    """
+    Import list aliases to the application.
+    Args:
+        file_path (str): Path to aliases list file.
+
+    """
+    add_alias_command = AddAliasCommand()
+    ImportAliasesCommand(add_alias_command).execute(file_path)
+
+
+@app.command(name='remove-alias', rich_help_panel='Commands')
+def remove_alias(name):
+    """
+    Remove alias to the application.
+    Args:
+        name (str): Name of command.
+    """
+    RemoveAliasCommand().execute(name)
+
+
+@app.command(name='list-alias', rich_help_panel='Commands')
+def list_alias():
+    """List alias to the application."""
+    ListAliasCommand().execute()
+
+
+if __name__ == '__main__':
+    app()
