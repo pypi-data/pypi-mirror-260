190d361e29e75b869b9e6929090fa77449a93c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:45 +0000

    Add package nbval/0.11.0/nbval-0.11.0.tar.gz

---
+++ packages/nbval/nbval-0.11.0.tar.gz/nbval-0.11.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+from nbval._version import __version__
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name="nbval",
+    version=__version__,
+    author="Laslett, Cortes, Fauske, Kluyver, Pepper, Fangohr",
+    description='A py.test plugin to validate Jupyter notebooks',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages = ['nbval'],
+    url='https://github.com/computationalmodelling/nbval',
+    # the following makes a plugin available to pytest
+    entry_points = {
+        'pytest11': [
+            'nbval = nbval.plugin',
+        ]
+    },
+    install_requires = [
+        'pytest >= 7',
+        'jupyter_client',
+        'nbformat',
+        'ipykernel',
+        'coverage',
+    ],
+    python_requires='>=3.7, <4',
+    classifiers = [
+        'Framework :: IPython',
+        'Framework :: Pytest',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing',
+    ]
+)
