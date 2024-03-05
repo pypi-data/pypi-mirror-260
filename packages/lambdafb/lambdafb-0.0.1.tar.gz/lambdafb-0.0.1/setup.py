1f37c2bc91a060d85046624bbe6edc72360c08e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:34 +0000

    Add package lambdafb/0.0.1/lambdafb-0.0.1.tar.gz

---
+++ packages/lambdafb/lambdafb-0.0.1.tar.gz/lambdafb-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='lambdafb',
+version='0.0.1',
+author='Subscription-pypi Creatoriq',
+author_email='subscription-pypi@creatoriq.com',
+description='Packageholdername lambdafb  for modulo-math function',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.9',
+)
