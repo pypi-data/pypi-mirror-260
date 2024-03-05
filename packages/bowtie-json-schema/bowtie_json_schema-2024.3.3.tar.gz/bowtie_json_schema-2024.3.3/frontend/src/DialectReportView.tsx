1629caac95e7386bbcd8194c8fcbfd22a00cc49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:05:11 +0000

    Add package bowtie-json-schema/2024.3.3/bowtie_json_schema-2024.3.3.tar.gz

---
+++ packages/bowtie-json-schema/bowtie_json_schema-2024.3.3.tar.gz/bowtie_json_schema-2024.3.3/frontend/src/DialectReportView.tsx
@@ -0,0 +1,90 @@
+import CasesSection from "./components/Cases/CasesSection";
+import RunInfoSection from "./components/RunInfo/RunInfoSection";
+import SummarySection from "./components/Summary/SummarySection";
+import { useMemo } from "react";
+import {
+  Implementation,
+  ImplementationData,
+  ReportData,
+} from "./data/parseReportData.ts";
+import { FilterSection } from "./components/FilterSection.tsx";
+import { useSearchParams } from "./hooks/useSearchParams.ts";
+
+export const DialectReportView = ({
+  reportData,
+  topPageInfoSection,
+  allImplementationsData,
+}: {
+  reportData: ReportData;
+  topPageInfoSection?: React.ReactElement;
+  allImplementationsData: Record<string, Implementation>;
+}) => {
+  const params = useSearchParams();
+  const languages = useMemo(() => {
+    const langs = Array.from(reportData.implementations.values()).map(
+      (impl) => impl.metadata.language,
+    );
+    return Array.from(new Set(langs).values());
+  }, [reportData]);
+
+  const filteredReportData = useMemo(() => {
+    const filteredData = { ...reportData };
+    const selectedLanguages = params.getAll("language");
+
+    if (selectedLanguages.length > 0) {
+      const filteredReportArray = Array.from(
+        reportData.implementations.entries(),
+      ).filter(([, data]) =>
+        selectedLanguages.includes(data.metadata.language),
+      );
+      filteredData.implementations = new Map(filteredReportArray);
+    }
+
+    return filteredData;
+  }, [reportData, params]);
+
+  const filteredOtherImplementationsData = useMemo(() => {
+    const selectedLanguages = params.getAll("language");
+    const availableLanguages =
+      selectedLanguages.length > 0 ? selectedLanguages : languages;
+
+    return filterOtherImplementations(
+      allImplementationsData,
+      availableLanguages,
+      filteredReportData.implementations,
+    );
+  }, [
+    params,
+    allImplementationsData,
+    languages,
+    filteredReportData.implementations,
+  ]);
+
+  return (
+    <main className="container-lg">
+      <div className="col col-lg-8 mx-auto">
+        {topPageInfoSection}
+        <RunInfoSection runInfo={filteredReportData.runInfo} />
+        <FilterSection languages={languages} />
+        <SummarySection
+          reportData={filteredReportData}
+          otherImplementationsData={filteredOtherImplementationsData}
+        />
+        <CasesSection reportData={filteredReportData} />
+      </div>
+    </main>
+  );
+};
+
+const filterOtherImplementations = (
+  allImplementationsData: Record<string, Implementation>,
+  langs: string[],
+  filteredReportImplementationsMap: Map<string, ImplementationData>,
+): Record<string, Implementation> => {
+  const filteredOtherImplementationsArray = Object.entries(
+    allImplementationsData,
+  )
+    .filter(([, impl]) => langs.includes(impl.language))
+    .filter(([key]) => !filteredReportImplementationsMap.has(key));
+  return Object.fromEntries(filteredOtherImplementationsArray);
+};
