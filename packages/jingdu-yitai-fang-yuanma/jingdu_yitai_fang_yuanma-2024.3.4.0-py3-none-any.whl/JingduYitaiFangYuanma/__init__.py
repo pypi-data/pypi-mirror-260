d0b7c2d0410a8382dd8a7ece3514f8bf7b837ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:27 +0000

    Add package jingdu-yitai-fang-yuanma/2024.3.4.0/jingdu_yitai_fang_yuanma-2024.3.4.0-py3-none-any.whl

---
+++ packages/jingdu-yitai-fang-yuanma/jingdu_yitai_fang_yuanma-2024.3.4.0-py3-none-any.whl/JingduYitaiFangYuanma/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingdu-yitai-fang-yuanma
+https://github.com/apachecn/jingdu-yitai-fang-yuanma"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
