fe78aaf164a3a304a39603d5d0e18656322e1b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:52 +0000

    Add package spring-data-redis-2-1-5-release-zhongwen-wendang/2024.3.5.0/spring-data-redis-2-1-5-release-zhongwen-wendang-2024.3.5.0.tar.gz

---
+++ packages/spring-data-redis-2-1-5-release-zhongwen-wendang/spring-data-redis-2-1-5-release-zhongwen-wendang-2024.3.5.0.tar.gz/spring-data-redis-2-1-5-release-zhongwen-wendang-2024.3.5.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import SpringDataRedis215ReleaseZhongwenWendang
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('SpringDataRedis215ReleaseZhongwenWendang'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="spring-data-redis-2-1-5-release-zhongwen-wendang",
+    version=SpringDataRedis215ReleaseZhongwenWendang.__version__,
+    url="https://github.com/apachecn/spring-data-redis-2-1-5-release-zhongwen-wendang",
+    author=SpringDataRedis215ReleaseZhongwenWendang.__author__,
+    author_email=SpringDataRedis215ReleaseZhongwenWendang.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="Spring Data Redis 2.1.5.RELEASE 中文文档",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "spring-data-redis-2-1-5-release-zhongwen-wendang=SpringDataRedis215ReleaseZhongwenWendang.__main__:main",
+            "SpringDataRedis215ReleaseZhongwenWendang=SpringDataRedis215ReleaseZhongwenWendang.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
