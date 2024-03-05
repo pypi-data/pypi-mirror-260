6597000bb497ecb42ecc973ac6d4509950cf21ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:04:56 +0000

    Add package cong-lingdao-yi-shenru-xuexi-qukuai-lian-mimaxue/2024.3.5.0/cong-lingdao-yi-shenru-xuexi-qukuai-lian-mimaxue-2024.3.5.0.tar.gz

---
+++ packages/cong-lingdao-yi-shenru-xuexi-qukuai-lian-mimaxue/cong-lingdao-yi-shenru-xuexi-qukuai-lian-mimaxue-2024.3.5.0.tar.gz/cong-lingdao-yi-shenru-xuexi-qukuai-lian-mimaxue-2024.3.5.0/CongLingdaoYiShenruXuexiQukuaiLianMimaxue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""cong-lingdao-yi-shenru-xuexi-qukuai-lian-mimaxue
+https://github.com/apachecn/cong-lingdao-yi-shenru-xuexi-qukuai-lian-mimaxue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
