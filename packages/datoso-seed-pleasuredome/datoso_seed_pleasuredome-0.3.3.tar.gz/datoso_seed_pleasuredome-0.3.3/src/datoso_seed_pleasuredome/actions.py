14e65ffae0956a23f2f1abde2d561c0710580975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:14 +0000

    Add package datoso-seed-pleasuredome/0.3.3/datoso_seed_pleasuredome-0.3.3.tar.gz

---
+++ packages/datoso-seed-pleasuredome/datoso_seed_pleasuredome-0.3.3.tar.gz/datoso_seed_pleasuredome-0.3.3/src/datoso_seed_pleasuredome/actions.py
@@ -0,0 +1,71 @@
+from datoso_seed_pleasuredome.dats import HomeBrewMameDat, mame_dat_factory, fruit_machine_factory
+
+actions = {
+    '{dat_origin}/FruitMachines': [
+        {
+            'action': 'LoadDatFile',
+            '_factory': fruit_machine_factory
+        },
+        {
+            'action': 'DeleteOld'
+        },
+        {
+            'action': 'Copy',
+            'folder': '{dat_destination}'
+        },
+        {
+            'action': 'SaveToDatabase'
+        }
+    ],
+    '{dat_origin}/HBMAME': [
+        {
+            'action': 'LoadDatFile',
+            '_class': HomeBrewMameDat
+        },
+        {
+            'action': 'DeleteOld'
+        },
+        {
+            'action': 'Copy',
+            'folder': '{dat_destination}'
+        },
+        {
+            'action': 'SaveToDatabase'
+        }
+    ],
+    '{dat_origin}/MAME': [
+        {
+            'action': 'LoadDatFile',
+            '_factory': mame_dat_factory
+        },
+        {
+            'action': 'DeleteOld'
+        },
+        {
+            'action': 'Copy',
+            'folder': '{dat_destination}'
+        },
+        {
+            'action': 'SaveToDatabase'
+        }
+    ],
+    # '{dat_origin}/Reference': [
+    #     {
+    #         'action': 'LoadDatFile',
+    #         '_factory': mame_dat_factory
+    #     },
+    #     {
+    #         'action': 'DeleteOld'
+    #     },
+    #     {
+    #         'action': 'Copy',
+    #         'folder': '{dat_destination}'
+    #     },
+    #     {
+    #         'action': 'SaveToDatabase'
+    #     }
+    # ]
+}
+
+def get_actions():
+    return actions
