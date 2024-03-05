2cb38979f47aa2ced75d492fb5a321eb540bd2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:04:01 +0000

    Add package jiyu-ethereum-de-wuzhong-lian-huanjing-dajian-fangshi/2024.3.4.0/jiyu-ethereum-de-wuzhong-lian-huanjing-dajian-fangshi-2024.3.4.0.tar.gz

---
+++ packages/jiyu-ethereum-de-wuzhong-lian-huanjing-dajian-fangshi/jiyu-ethereum-de-wuzhong-lian-huanjing-dajian-fangshi-2024.3.4.0.tar.gz/jiyu-ethereum-de-wuzhong-lian-huanjing-dajian-fangshi-2024.3.4.0/JiyuEthereumDeWuzhongLianHuanjingDajianFangshi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiyu-ethereum-de-wuzhong-lian-huanjing-dajian-fangshi
+https://github.com/apachecn/jiyu-ethereum-de-wuzhong-lian-huanjing-dajian-fangshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
