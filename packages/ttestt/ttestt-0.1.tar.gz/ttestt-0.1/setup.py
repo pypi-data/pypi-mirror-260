3499d4335640d33296d917852f7bc7ec1325f4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:19 +0000

    Add package ttestt/0.1/ttestt-0.1.tar.gz

---
+++ packages/ttestt/ttestt-0.1.tar.gz/ttestt-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ttestt",
+    version='0.1',
+    packages=find_packages(),
+    # package_data={
+    #     'AbdoELBALFF': ['GG.wav', 'abdo.png'],
+    # },
+    # install_requires=[
+    #     'Pillow>=7.0.0',
+    #     'playsound~=1.2.2'
+    # ],
+    install_requires=[
+    ],
+)
