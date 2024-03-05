b0ef81815b2bd29c145cb1146649f7be787e9426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:05:13 +0000

    Add package da-nei-java-hexin-api-shang-saomiao-ban-3-05m/2024.3.5.0/da_nei_java_hexin_api_shang_saomiao_ban_3_05m-2024.3.5.0-py3-none-any.whl

---
+++ packages/da-nei-java-hexin-api-shang-saomiao-ban-3-05m/da_nei_java_hexin_api_shang_saomiao_ban_3_05m-2024.3.5.0-py3-none-any.whl/DaNeiJavaHexinApiShangSaomiaoBan305m/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""da-nei-java-hexin-api-shang-saomiao-ban-3-05m
+https://github.com/apachecn/da-nei-java-hexin-api-shang-saomiao-ban-3-05m"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
