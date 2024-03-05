3d90b10f86d2398ec862090bb44f6f3d71ee4658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:29 +0000

    Add package vistutils/0.1.16/vistutils-0.1.16-py3-none-any.whl

---
+++ packages/vistutils/vistutils-0.1.16-py3-none-any.whl/vistutils/fields/__init__.py
@@ -0,0 +1,12 @@
+"""The fields module provide a collection of descriptor classes. """
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._unparse_args import unParseArgs
+from ._core_descriptor import CoreDescriptor
+from ._typed_field import TypedField
+from ._flag import Flag
+from ._text_field import TextField
+from ._int_field import IntField
+from ._field import Field
