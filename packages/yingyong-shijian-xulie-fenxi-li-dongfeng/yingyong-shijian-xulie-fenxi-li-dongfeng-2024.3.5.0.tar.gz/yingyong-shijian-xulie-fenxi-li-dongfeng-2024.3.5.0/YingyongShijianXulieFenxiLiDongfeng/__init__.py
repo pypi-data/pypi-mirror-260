75e5abf439a757bf974c6ce3d0e821b3db17bc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:47 +0000

    Add package yingyong-shijian-xulie-fenxi-li-dongfeng/2024.3.5.0/yingyong-shijian-xulie-fenxi-li-dongfeng-2024.3.5.0.tar.gz

---
+++ packages/yingyong-shijian-xulie-fenxi-li-dongfeng/yingyong-shijian-xulie-fenxi-li-dongfeng-2024.3.5.0.tar.gz/yingyong-shijian-xulie-fenxi-li-dongfeng-2024.3.5.0/YingyongShijianXulieFenxiLiDongfeng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyong-shijian-xulie-fenxi-li-dongfeng
+https://github.com/apachecn/yingyong-shijian-xulie-fenxi-li-dongfeng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
