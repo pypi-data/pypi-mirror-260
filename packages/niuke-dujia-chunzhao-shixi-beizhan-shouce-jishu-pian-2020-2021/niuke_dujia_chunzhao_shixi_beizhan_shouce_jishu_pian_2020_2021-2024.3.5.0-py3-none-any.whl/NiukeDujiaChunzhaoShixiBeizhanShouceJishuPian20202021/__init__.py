0fcb3796ee91525d405b998379931f8b9762db90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:49 +0000

    Add package niuke-dujia-chunzhao-shixi-beizhan-shouce-jishu-pian-2020-2021/2024.3.5.0/niuke_dujia_chunzhao_shixi_beizhan_shouce_jishu_pian_2020_2021-2024.3.5.0-py3-none-any.whl

---
+++ packages/niuke-dujia-chunzhao-shixi-beizhan-shouce-jishu-pian-2020-2021/niuke_dujia_chunzhao_shixi_beizhan_shouce_jishu_pian_2020_2021-2024.3.5.0-py3-none-any.whl/NiukeDujiaChunzhaoShixiBeizhanShouceJishuPian20202021/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""niuke-dujia-chunzhao-shixi-beizhan-shouce-jishu-pian-2020-2021
+https://github.com/apachecn/niuke-dujia-chunzhao-shixi-beizhan-shouce-jishu-pian-2020-2021"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
