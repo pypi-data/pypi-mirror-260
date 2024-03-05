3790a840a80ae462d1e290709db330aa7bd45d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:44 +0000

    Add package netkiller-radio/2024.3.5.0/netkiller_radio-2024.3.5.0-py3-none-any.whl

---
+++ packages/netkiller-radio/netkiller_radio-2024.3.5.0-py3-none-any.whl/NetkillerRadio/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""netkiller-radio
+https://github.com/apachecn/netkiller-radio"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
