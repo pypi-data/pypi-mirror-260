edf802d84fa2467ef94076dffa9eb9a02fe3242d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:05:51 +0000

    Add package quool/5.0.0/quool-5.0.0.tar.gz

---
+++ packages/quool/quool-5.0.0.tar.gz/quool-5.0.0/quool/__init__.py
@@ -0,0 +1,33 @@
+from .core import Table
+
+from .table import (
+    ItemTable,
+    DatetimeTable,
+    PanelTable,
+)
+
+from .spider import (
+    ProxyManager,
+    get_spot_data,
+    wechat_login,
+    ewechat_notify,
+)
+
+from .trade import (
+    TradeRecorder
+)
+
+from .util import (
+    Logger,
+    parse_commastr,
+    reduce_mem_usage,
+)
+
+
+DEBUG = 10
+INFO = 20
+WARNING = 30
+CRITICAL = 40
+
+
+__version__ = "5.0.0"
