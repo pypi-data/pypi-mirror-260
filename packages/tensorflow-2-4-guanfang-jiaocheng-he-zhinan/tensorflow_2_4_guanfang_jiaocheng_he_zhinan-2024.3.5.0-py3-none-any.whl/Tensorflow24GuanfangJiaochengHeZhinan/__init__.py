76bf8e0d805890802941f81274e4029a0ce62f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:04:46 +0000

    Add package tensorflow-2-4-guanfang-jiaocheng-he-zhinan/2024.3.5.0/tensorflow_2_4_guanfang_jiaocheng_he_zhinan-2024.3.5.0-py3-none-any.whl

---
+++ packages/tensorflow-2-4-guanfang-jiaocheng-he-zhinan/tensorflow_2_4_guanfang_jiaocheng_he_zhinan-2024.3.5.0-py3-none-any.whl/Tensorflow24GuanfangJiaochengHeZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tensorflow-2-4-guanfang-jiaocheng-he-zhinan
+https://github.com/apachecn/tensorflow-2-4-guanfang-jiaocheng-he-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
