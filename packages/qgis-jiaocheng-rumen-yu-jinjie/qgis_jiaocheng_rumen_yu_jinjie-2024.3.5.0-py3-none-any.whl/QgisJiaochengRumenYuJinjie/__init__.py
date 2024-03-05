6d50eca7131ac02515a45e194b63120bad2e8351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:56 +0000

    Add package qgis-jiaocheng-rumen-yu-jinjie/2024.3.5.0/qgis_jiaocheng_rumen_yu_jinjie-2024.3.5.0-py3-none-any.whl

---
+++ packages/qgis-jiaocheng-rumen-yu-jinjie/qgis_jiaocheng_rumen_yu_jinjie-2024.3.5.0-py3-none-any.whl/QgisJiaochengRumenYuJinjie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qgis-jiaocheng-rumen-yu-jinjie
+https://github.com/apachecn/qgis-jiaocheng-rumen-yu-jinjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
