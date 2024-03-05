4d573131e9b739b38dd777e7a871ff3fdf26af43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:14 +0000

    Add package latex-paiban-xinde-li-dongfeng/2024.3.5.0/latex_paiban_xinde_li_dongfeng-2024.3.5.0-py3-none-any.whl

---
+++ packages/latex-paiban-xinde-li-dongfeng/latex_paiban_xinde_li_dongfeng-2024.3.5.0-py3-none-any.whl/LatexPaibanXindeLiDongfeng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""latex-paiban-xinde-li-dongfeng
+https://github.com/apachecn/latex-paiban-xinde-li-dongfeng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
