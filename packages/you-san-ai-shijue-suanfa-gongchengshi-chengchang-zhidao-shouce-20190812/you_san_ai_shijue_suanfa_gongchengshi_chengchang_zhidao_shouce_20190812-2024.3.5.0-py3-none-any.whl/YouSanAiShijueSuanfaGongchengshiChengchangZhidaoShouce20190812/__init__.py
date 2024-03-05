42c6a9b118e3e85eb4a892f79a1bd1020b503acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:53 +0000

    Add package you-san-ai-shijue-suanfa-gongchengshi-chengchang-zhidao-shouce-20190812/2024.3.5.0/you_san_ai_shijue_suanfa_gongchengshi_chengchang_zhidao_shouce_20190812-2024.3.5.0-py3-none-any.whl

---
+++ packages/you-san-ai-shijue-suanfa-gongchengshi-chengchang-zhidao-shouce-20190812/you_san_ai_shijue_suanfa_gongchengshi_chengchang_zhidao_shouce_20190812-2024.3.5.0-py3-none-any.whl/YouSanAiShijueSuanfaGongchengshiChengchangZhidaoShouce20190812/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""you-san-ai-shijue-suanfa-gongchengshi-chengchang-zhidao-shouce-20190812
+https://github.com/apachecn/you-san-ai-shijue-suanfa-gongchengshi-chengchang-zhidao-shouce-20190812"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
