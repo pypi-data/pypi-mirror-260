4cb1ba75783a85a6a4ec9ea016100418b0e514cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:09 +0000

    Add package Osobisty-asystent/1.0/Osobisty_asystent-1.0.tar.gz

---
+++ packages/Osobisty-asystent/Osobisty_asystent-1.0.tar.gz/Osobisty_asystent-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='Osobisty_asystent',
+    version='1.0',
+    packages=['address_book'],
+    entry_points={
+        'console_scripts': [
+            'personal-assistant = address_book.assistant:main'
+        ]
+    }
+)
