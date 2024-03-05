d319b71f5adc25a4e76a8674f26a6e74600943e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:55 +0000

    Add package matlab-rumenjiaocheng-c-yuyan-biancheng-wang/2024.3.5.0/matlab_rumenjiaocheng_c_yuyan_biancheng_wang-2024.3.5.0-py3-none-any.whl

---
+++ packages/matlab-rumenjiaocheng-c-yuyan-biancheng-wang/matlab_rumenjiaocheng_c_yuyan_biancheng_wang-2024.3.5.0-py3-none-any.whl/MatlabRumenjiaochengCYuyanBianchengWang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""matlab-rumenjiaocheng-c-yuyan-biancheng-wang
+https://github.com/apachecn/matlab-rumenjiaocheng-c-yuyan-biancheng-wang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
