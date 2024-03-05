7ecc1adbd4636ab0f28acd429ef20a28f2c859cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:05:52 +0000

    Add package meerkatio/1.0/meerkatio-1.0.tar.gz

---
+++ packages/meerkatio/meerkatio-1.0.tar.gz/meerkatio-1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meerkatio',
+    version='1.0',
+    packages=find_packages(),
+    package_data={'': ['*.mp3']},
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "click",
+        "playsound",
+        "PyObjC"
+    ],
+    entry_points='''
+        [console_scripts]
+        meerkat=meerkat.cli:meerkat
+    ''',
+    author="meerkat.io",
+    description="Simple notification tool for multi-tasking developers",
+    long_description=""
+    # Add other metadata such as author, author_email, description, etc.
+)
