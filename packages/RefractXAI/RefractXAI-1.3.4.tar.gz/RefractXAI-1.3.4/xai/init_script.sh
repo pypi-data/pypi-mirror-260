d0a6b34ccb1e25cc7c057e3f19933d2ff01d8602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 17:06:16 +0000

    Add package RefractXAI/1.3.4/RefractXAI-1.3.4.tar.gz

---
+++ packages/RefractXAI/RefractXAI-1.3.4.tar.gz/RefractXAI-1.3.4/xai/init_script.sh
@@ -0,0 +1,4 @@
+echo "Installing packaeges";
+# pip install bs4==0.0.1 -i https://artifactory.global.standardchartered.com/artifactory/api/pypi/pypi/simple
+pip install bs4==0.0.1;
+echo "Done with dependency installtion!";
