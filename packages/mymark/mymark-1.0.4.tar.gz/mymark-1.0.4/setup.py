4b979502327d0d6e99be95b97afb51f0ed54fd43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:43 +0000

    Add package mymark/1.0.4/mymark-1.0.4.tar.gz

---
+++ packages/mymark/mymark-1.0.4.tar.gz/mymark-1.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mymark',
+    version='1.0.4',
+    packages=find_packages(),
+    scripts=['src/marker/mymark/mymark.py'],
+    install_requires=[
+        'aiohttp',
+        'pytest',
+        'pylint',
+        'mypy',
+    ]
+)
