320ff226986e04953dbc3420914037adaedc8a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:09 +0000

    Add package sklearn-xuexi-shouce-chuyi/2024.3.5.0/sklearn_xuexi_shouce_chuyi-2024.3.5.0-py3-none-any.whl

---
+++ packages/sklearn-xuexi-shouce-chuyi/sklearn_xuexi_shouce_chuyi-2024.3.5.0-py3-none-any.whl/SklearnXuexiShouceChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sklearn-xuexi-shouce-chuyi
+https://github.com/apachecn/sklearn-xuexi-shouce-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
