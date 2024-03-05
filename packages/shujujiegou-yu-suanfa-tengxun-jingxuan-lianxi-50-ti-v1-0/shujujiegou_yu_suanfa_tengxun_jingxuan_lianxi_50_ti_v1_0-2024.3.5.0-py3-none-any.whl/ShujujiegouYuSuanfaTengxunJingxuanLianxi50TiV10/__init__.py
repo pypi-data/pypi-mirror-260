5c36ed73463803debede8ee9f6bd4e3698a3ed26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:09 +0000

    Add package shujujiegou-yu-suanfa-tengxun-jingxuan-lianxi-50-ti-v1-0/2024.3.5.0/shujujiegou_yu_suanfa_tengxun_jingxuan_lianxi_50_ti_v1_0-2024.3.5.0-py3-none-any.whl

---
+++ packages/shujujiegou-yu-suanfa-tengxun-jingxuan-lianxi-50-ti-v1-0/shujujiegou_yu_suanfa_tengxun_jingxuan_lianxi_50_ti_v1_0-2024.3.5.0-py3-none-any.whl/ShujujiegouYuSuanfaTengxunJingxuanLianxi50TiV10/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shujujiegou-yu-suanfa-tengxun-jingxuan-lianxi-50-ti-v1-0
+https://github.com/apachecn/shujujiegou-yu-suanfa-tengxun-jingxuan-lianxi-50-ti-v1-0"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
