8998acfd65bae36187772234f1a3627ec1fe4696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:39 +0000

    Add package gcc-bianyiqi-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang/2024.3.5.0/gcc-bianyiqi-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang-2024.3.5.0.tar.gz

---
+++ packages/gcc-bianyiqi-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang/gcc-bianyiqi-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang-2024.3.5.0.tar.gz/gcc-bianyiqi-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang-2024.3.5.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import GccBianyiqi30FenzhongRumenjiaochengCYuyanZhongwenwang
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('GccBianyiqi30FenzhongRumenjiaochengCYuyanZhongwenwang'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="gcc-bianyiqi-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang",
+    version=GccBianyiqi30FenzhongRumenjiaochengCYuyanZhongwenwang.__version__,
+    url="https://github.com/apachecn/gcc-bianyiqi-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang",
+    author=GccBianyiqi30FenzhongRumenjiaochengCYuyanZhongwenwang.__author__,
+    author_email=GccBianyiqi30FenzhongRumenjiaochengCYuyanZhongwenwang.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="GCC 编译器 30 分钟入门教程（C 语言中文网）",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "gcc-bianyiqi-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang=GccBianyiqi30FenzhongRumenjiaochengCYuyanZhongwenwang.__main__:main",
+            "GccBianyiqi30FenzhongRumenjiaochengCYuyanZhongwenwang=GccBianyiqi30FenzhongRumenjiaochengCYuyanZhongwenwang.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
