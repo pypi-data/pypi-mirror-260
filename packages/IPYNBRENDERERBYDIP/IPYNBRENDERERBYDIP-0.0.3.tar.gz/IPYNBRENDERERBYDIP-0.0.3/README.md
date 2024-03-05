eb29afd20ac2797942895aa938c53009aa7988fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:14 +0000

    Add package IPYNBRENDERERBYDIP/0.0.3/IPYNBRENDERERBYDIP-0.0.3.tar.gz

---
+++ packages/IPYNBRENDERERBYDIP/IPYNBRENDERERBYDIP-0.0.3.tar.gz/IPYNBRENDERERBYDIP-0.0.3/README.md
@@ -0,0 +1,1 @@
+# IPYNBRENDERERBYDIP Python PYPI Package
---
+++ packages/IPYNBRENDERERBYDIP/IPYNBRENDERERBYDIP-0.0.3.tar.gz/IPYNBRENDERERBYDIP-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.3"
+
+REPO_NAME = "IPYNBRENDERERBYDIP"
+AUTHOR_USER_NAME = "dipdaiict"
+SRC_REPO = "IPYNBRENDERERBYDIP" 
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email="dippatel256@gmail.com",
+    description="This is a Small Python Package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{SRC_REPO}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{SRC_REPO}/issues"
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
