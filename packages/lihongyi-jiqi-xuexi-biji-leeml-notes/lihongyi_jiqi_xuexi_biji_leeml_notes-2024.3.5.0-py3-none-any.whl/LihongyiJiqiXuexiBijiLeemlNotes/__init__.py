32289022d38ddc6f5d7a8872848cf9e79d28ef5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:47 +0000

    Add package lihongyi-jiqi-xuexi-biji-leeml-notes/2024.3.5.0/lihongyi_jiqi_xuexi_biji_leeml_notes-2024.3.5.0-py3-none-any.whl

---
+++ packages/lihongyi-jiqi-xuexi-biji-leeml-notes/lihongyi_jiqi_xuexi_biji_leeml_notes-2024.3.5.0-py3-none-any.whl/LihongyiJiqiXuexiBijiLeemlNotes/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lihongyi-jiqi-xuexi-biji-leeml-notes
+https://github.com/apachecn/lihongyi-jiqi-xuexi-biji-leeml-notes"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
