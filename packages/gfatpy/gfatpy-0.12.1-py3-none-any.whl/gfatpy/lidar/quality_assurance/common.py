0f5760cf27e69af92013cae34e23c9fffc1c5fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:33 +0000

    Add package gfatpy/0.12.1/gfatpy-0.12.1-py3-none-any.whl

---
+++ packages/gfatpy/gfatpy-0.12.1-py3-none-any.whl/gfatpy/lidar/quality_assurance/common.py
@@ -0,0 +1,29 @@
+from loguru import logger
+
+DARK_SUB = {0: "not-dark-subtracted", 1: "dark-subtracted"}
+
+
+def select_dc(lidar_ds, channel):
+    """Read Associated Dark Current Measurements, if wanted"""
+    ilu_dict = {True: "day", False: "ngt"}
+    if channel[-1] == "a":
+        if lidar_ds:
+            dark_subtracted = lidar_ds[f"signal_{channel}"].dark_corrected
+            if dark_subtracted:
+                # day/ngt DC measurements
+                ilu_flag = (
+                    lidar_ds.cosine_sza.mean("time").values > 0.0
+                )  # True: day, False: night
+                dc = lidar_ds[f"dc_{channel}"].sel(ilu=ilu_dict[ilu_flag])
+                dark_subtracted = DARK_SUB[1]
+            else:
+                dc = None
+                dark_subtracted = DARK_SUB[0]
+                logger.warning("dark measurement not subtracted.")
+        else:
+            dc = None
+            dark_subtracted = ""
+            logger.warning("lidar " "xarray.Dataset" " not provided.")
+    else:
+        dc, dark_subtracted = None, ""
+    return dc, dark_subtracted
