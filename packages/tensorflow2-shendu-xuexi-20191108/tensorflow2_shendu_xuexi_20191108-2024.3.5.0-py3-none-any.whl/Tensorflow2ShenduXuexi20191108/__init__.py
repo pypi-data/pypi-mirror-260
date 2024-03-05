af158aaff106b5656ab7d7c5e0a9b6c325f117e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:44 +0000

    Add package tensorflow2-shendu-xuexi-20191108/2024.3.5.0/tensorflow2_shendu_xuexi_20191108-2024.3.5.0-py3-none-any.whl

---
+++ packages/tensorflow2-shendu-xuexi-20191108/tensorflow2_shendu_xuexi_20191108-2024.3.5.0-py3-none-any.whl/Tensorflow2ShenduXuexi20191108/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tensorflow2-shendu-xuexi-20191108
+https://github.com/apachecn/tensorflow2-shendu-xuexi-20191108"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
