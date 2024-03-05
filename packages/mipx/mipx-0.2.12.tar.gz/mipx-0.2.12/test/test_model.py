970119594ded7d759f39a0f24b7498b3fb9d5d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:06 +0000

    Add package mipx/0.2.12/mipx-0.2.12.tar.gz

---
+++ packages/mipx/mipx-0.2.12.tar.gz/mipx-0.2.12/test/test_model.py
@@ -0,0 +1,14 @@
+import mipx
+if __name__ == '__main__':
+    model = mipx.Model(solver_id='CP')
+    x = model.addVars(2, name='x', vtype=mipx.Vtype.BINARY)
+    z = model.addVar(name='z')
+    m = model.addVars(1, (3, 4), name='hello')
+    y = model.addVar(name='x', ub=3000)
+    model.addConstrMultiply(z, (x[0], y))
+    model.setObjective(z)
+
+    status = model.optimize()
+    if status == mipx.OptimizationStatus.OPTIMAL:
+        print("success")
+        mipx.debugVar(z)
