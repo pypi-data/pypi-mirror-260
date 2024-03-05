4e3bab531a9080cb2a040afb994a31dd4078c37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:04:45 +0000

    Add package bauplan/0.0.3a64/bauplan-0.0.3a64-py3-none-macosx_11_0_arm64.whl

---
+++ packages/bauplan/bauplan-0.0.3a64-py3-none-macosx_11_0_arm64.whl/bauplan/store.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+
+def load_obj(_: str) -> Any:
+    raise Exception('this function is a stub to be replaced at runtime. This should not execute')
+
+
+def save_obj(_: str, obj: Any) -> None:
+    raise Exception('this function is a stub to be replaced at runtime. This should not execute')
