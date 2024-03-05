914fcfcb4f534d3d4329f8dd83d43f2e165a9790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:57 +0000

    Add package consolemenu.py/0.2.2/consolemenu.py-0.2.2-py3-none-any.whl

---
+++ packages/consolemenu.py/consolemenu.py-0.2.2-py3-none-any.whl/consolemenu_py/__init__.py
@@ -0,0 +1,5 @@
+#  SPDX-FileCopyrightText: 2024 Pirulax <patrikjankovics7@gmail.com>
+#  SPDX-License-Identifier: MIT
+
+from .menu import Menu, MainMenu
+from .menuitem import MenuItem, FunctionMenuItem
