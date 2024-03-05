05eaa1839fa1dc5fcf330ebaaffbd6cf5efda5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:15 +0000

    Add package zetcode-ruby-jiaocheng-chuyi/2024.3.5.0/zetcode-ruby-jiaocheng-chuyi-2024.3.5.0.tar.gz

---
+++ packages/zetcode-ruby-jiaocheng-chuyi/zetcode-ruby-jiaocheng-chuyi-2024.3.5.0.tar.gz/zetcode-ruby-jiaocheng-chuyi-2024.3.5.0/ZetcodeRubyJiaochengChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zetcode-ruby-jiaocheng-chuyi
+https://github.com/apachecn/zetcode-ruby-jiaocheng-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
