278def29b21f5812656d30b8d605ca8fffa38ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:06 +0000

    Add package kaiyuan-ruanjian-zhinan/2024.3.5.0/kaiyuan_ruanjian_zhinan-2024.3.5.0-py3-none-any.whl

---
+++ packages/kaiyuan-ruanjian-zhinan/kaiyuan_ruanjian_zhinan-2024.3.5.0-py3-none-any.whl/KaiyuanRuanjianZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kaiyuan-ruanjian-zhinan
+https://github.com/apachecn/kaiyuan-ruanjian-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
