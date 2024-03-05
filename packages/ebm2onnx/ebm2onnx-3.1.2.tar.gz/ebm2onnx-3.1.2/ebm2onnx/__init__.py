4cdcf2767edbbe5c3935c9123f50e5773b8a72c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:37 +0000

    Add package ebm2onnx/3.1.2/ebm2onnx-3.1.2.tar.gz

---
+++ packages/ebm2onnx/ebm2onnx-3.1.2.tar.gz/ebm2onnx-3.1.2/ebm2onnx/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for ebm2onnx."""
+
+__author__ = """Romain Picard"""
+__email__ = 'romain.picard@softathome.com'
+__version__ = '3.1.2'
+
+from .convert import to_onnx, get_dtype_from_pandas
