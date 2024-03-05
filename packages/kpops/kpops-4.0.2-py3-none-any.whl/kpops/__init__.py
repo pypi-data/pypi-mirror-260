e049461c6d15e4404d4f17088bf8260fc686cec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:04:05 +0000

    Add package kpops/4.0.2/kpops-4.0.2-py3-none-any.whl

---
+++ packages/kpops/kpops-4.0.2-py3-none-any.whl/kpops/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "4.0.2"
+
+# export public API functions
+from kpops.cli.main import clean, deploy, destroy, generate, manifest, reset
+
+__all__ = (
+    "generate",
+    "manifest",
+    "deploy",
+    "destroy",
+    "reset",
+    "clean",
+)
