e353454e729e9c928a68e3015134ef3cf110801c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:41 +0000

    Add package yc-chuangye-ke-2018-zhongwen-biji/2024.3.5.0/yc_chuangye_ke_2018_zhongwen_biji-2024.3.5.0-py3-none-any.whl

---
+++ packages/yc-chuangye-ke-2018-zhongwen-biji/yc_chuangye_ke_2018_zhongwen_biji-2024.3.5.0-py3-none-any.whl/YcChuangyeKe2018ZhongwenBiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yc-chuangye-ke-2018-zhongwen-biji
+https://github.com/apachecn/yc-chuangye-ke-2018-zhongwen-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
