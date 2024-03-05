82e8c413bd4cfefb4a09a07be93b33ff97367a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:43 +0000

    Add package shuju-kexue-he-rengongzhineng-jishu-biji/2024.3.5.0/shuju-kexue-he-rengongzhineng-jishu-biji-2024.3.5.0.tar.gz

---
+++ packages/shuju-kexue-he-rengongzhineng-jishu-biji/shuju-kexue-he-rengongzhineng-jishu-biji-2024.3.5.0.tar.gz/shuju-kexue-he-rengongzhineng-jishu-biji-2024.3.5.0/ShujuKexueHeRengongzhinengJishuBiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuju-kexue-he-rengongzhineng-jishu-biji
+https://github.com/apachecn/shuju-kexue-he-rengongzhineng-jishu-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
