0ac1f6e6089699de406da53a8db0aca889fb23e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:28 +0000

    Add package javaguide-mianshi-tuji-ban/2024.3.4.0/javaguide-mianshi-tuji-ban-2024.3.4.0.tar.gz

---
+++ packages/javaguide-mianshi-tuji-ban/javaguide-mianshi-tuji-ban-2024.3.4.0.tar.gz/javaguide-mianshi-tuji-ban-2024.3.4.0/JavaguideMianshiTujiBan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""javaguide-mianshi-tuji-ban
+https://github.com/apachecn/javaguide-mianshi-tuji-ban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
