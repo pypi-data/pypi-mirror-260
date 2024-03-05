2787aead5a44e70de4e48e5f1ed8d0fb34b0c180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:11 +0000

    Add package yuanmeng-xiangmu-biji-wangzhuan-juejin-shicao-xiangmu-dianzishu-180710/2024.3.5.0/yuanmeng-xiangmu-biji-wangzhuan-juejin-shicao-xiangmu-dianzishu-180710-2024.3.5.0.tar.gz

---
+++ packages/yuanmeng-xiangmu-biji-wangzhuan-juejin-shicao-xiangmu-dianzishu-180710/yuanmeng-xiangmu-biji-wangzhuan-juejin-shicao-xiangmu-dianzishu-180710-2024.3.5.0.tar.gz/yuanmeng-xiangmu-biji-wangzhuan-juejin-shicao-xiangmu-dianzishu-180710-2024.3.5.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import YuanmengXiangmuBijiWangzhuanJuejinShicaoXiangmuDianzishu180710
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('YuanmengXiangmuBijiWangzhuanJuejinShicaoXiangmuDianzishu180710'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="yuanmeng-xiangmu-biji-wangzhuan-juejin-shicao-xiangmu-dianzishu-180710",
+    version=YuanmengXiangmuBijiWangzhuanJuejinShicaoXiangmuDianzishu180710.__version__,
+    url="https://github.com/apachecn/yuanmeng-xiangmu-biji-wangzhuan-juejin-shicao-xiangmu-dianzishu-180710",
+    author=YuanmengXiangmuBijiWangzhuanJuejinShicaoXiangmuDianzishu180710.__author__,
+    author_email=YuanmengXiangmuBijiWangzhuanJuejinShicaoXiangmuDianzishu180710.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="缘梦项目笔记——网赚掘金实操项目电子书180710",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "yuanmeng-xiangmu-biji-wangzhuan-juejin-shicao-xiangmu-dianzishu-180710=YuanmengXiangmuBijiWangzhuanJuejinShicaoXiangmuDianzishu180710.__main__:main",
+            "YuanmengXiangmuBijiWangzhuanJuejinShicaoXiangmuDianzishu180710=YuanmengXiangmuBijiWangzhuanJuejinShicaoXiangmuDianzishu180710.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
