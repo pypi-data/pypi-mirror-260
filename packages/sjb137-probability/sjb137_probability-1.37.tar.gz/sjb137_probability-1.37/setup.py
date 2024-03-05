60a381a12e112dca64c9be6f352fcc8444cf8d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 17:07:14 +0000

    Add package sjb137-probability/1.37/sjb137_probability-1.37.tar.gz

---
+++ packages/sjb137-probability/sjb137_probability-1.37.tar.gz/sjb137_probability-1.37/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sjb137_probability',
+      version='1.37',
+      description='Gaussian and Binomial distributions',
+      packages=['sjb137_probability'],
+      author = 'Stephen Barrie',
+      zip_safe=False)
