175a65155276dffce6ce1bcb1d6f2f76f5b20bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:05:19 +0000

    Add package cofutils/1.1.3/cofutils-1.1.3-py3-none-any.whl

---
+++ packages/cofutils/cofutils-1.1.3-py3-none-any.whl/cofutils/package_info.py
@@ -0,0 +1,7 @@
+__version__="1.1.3"
+__contact_names__="whq1516@mail.ustc.edu.cn"
+__url__="https://gitee.com/haiqwa/cofutils.git"
+__keywords__ = 'Python tools'
+__license__ = 'See https://gitee.com/haiqwa/cofutils/blob/main/LICENSE'
+__description__ = 'Experiment toolkits'
+__package_name__ = 'cofutils'
