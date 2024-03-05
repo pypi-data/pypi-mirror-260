3047a22ebd51ef5a223b443502e68195537bb3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:06:39 +0000

    Add package x30-tian-chidiao-na-zhi-tensorflow2-0/2024.3.5.0/x30_tian_chidiao_na_zhi_tensorflow2_0-2024.3.5.0-py3-none-any.whl

---
+++ packages/x30-tian-chidiao-na-zhi-tensorflow2-0/x30_tian_chidiao_na_zhi_tensorflow2_0-2024.3.5.0-py3-none-any.whl/X30TianChidiaoNaZhiTensorflow20/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x30-tian-chidiao-na-zhi-tensorflow2-0
+https://github.com/apachecn/x30-tian-chidiao-na-zhi-tensorflow2-0"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
