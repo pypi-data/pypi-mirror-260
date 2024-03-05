8c8f568ea49c4d0e0d1fe03e713c82d90aca9609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:45 +0000

    Add package nyctibius/0.0.6/nyctibius-0.0.6-py3-none-any.whl

---
+++ packages/nyctibius/nyctibius-0.0.6-py3-none-any.whl/nyctibius/dto/data_info.py
@@ -0,0 +1,78 @@
+import pandas as pd
+
+
+class DataInfo:
+    def __init__(self, file_path=None, name=None, description=None, url=None, data=None):
+        self._file_path = file_path
+        self._name = name
+        self._description = description
+        self._url = url
+        self._data = data
+    def __str__(self):
+        DataInfo_dict = {"name":self._name, "file_path":self._file_path, "url":self._url, "description":self._description, "data":self._data}
+        return str(DataInfo_dict)
+
+    @property
+    def file_path(self):
+        return self._file_path
+
+    @file_path.setter
+    def file_path(self, file_path):
+        if file_path is not None and not isinstance(file_path, str):
+            raise TypeError('file_path must be a String')
+        try:
+            self._file_path = file_path
+        except Exception as e:
+            raise Exception(f'Error setting file_path: {e}')
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if name is not None and not isinstance(name, str):
+            raise TypeError('name must be a String')
+        try:
+            self._file_path = name
+        except Exception as e:
+            raise Exception(f'Error setting name: {e}')
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, description):
+        if description is not None and not isinstance(description, str):
+            raise TypeError('description must be a string')
+        try:
+            self._description = description
+        except Exception as e:
+            raise Exception(f'Error setting description: {e}')
+
+    @property
+    def url(self):
+        return self._url
+
+    @url.setter
+    def url(self, url):
+        if url is not None and not isinstance(url, str):
+            raise TypeError('url must be a string')
+        try:
+            self._url = url
+        except Exception as e:
+            raise Exception(f'Error setting url: {e}')
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, data):
+        if data is not None and not isinstance(data, pd.DataFrame):
+            raise TypeError('data must be a pandas DataFrame')
+        try:
+            self._data = data
+        except Exception as e:
+            raise Exception(f'Error setting data: {e}')
---
+++ packages/nyctibius/nyctibius-0.0.6-py3-none-any.whl/nyctibius/etl/transformer.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+from pandas import DataFrame
+import pyreadstat
+
+
+class Transformer:
+    """
+    A class used to transform data into a Python Dataframe.
+
+    Attributes:
+        file_path (str): The path to the source file.
+        db_file (str): The path to the SQLite database file.
+    """
+
+    def __init__(self, file_path, db_file):
+        self.file_path = file_path
+        self.db_file = db_file
+
+    def transform_data(self, headers=None) -> DataFrame:
+        """
+       Reads a file (CSV, TXT, or XLSX), selects specified columns, and writes the transformed data
+       to a SQLite database.
+
+       Args:
+           headers (List[str]): Path to the file (CSV, TXT, or XLSX)
+
+       Returns:
+           Dataframe: Dataframe of the transformed file
+       """
+        # Get file extension
+        _, file_extension = os.path.splitext(self.file_path)
+        # TODO delete file when saved df
+        # Depending on the file extension, read the file using the appropriate pandas function
+        if file_extension.lower() == '.csv':
+            df = pd.read_csv(self.file_path, sep='[,|;]', engine='python', usecols=headers, header=0 if headers is None else None)
+        elif file_extension.lower() == '.txt':
+            df = pd.read_table(self.file_path, usecols=headers, header=0 if headers is None else None)
+        elif file_extension.lower() == '.xlsx' or file_extension.lower() == '.xls':
+            df = pd.read_excel(self.file_path, usecols=headers, header=0 if headers is None else None)
+        elif file_extension.lower() == '.sav':
+            df, meta = pyreadstat.read_sav(self.file_path, usecols=headers)
+        else:
+            raise ValueError(f'Unsupported file type: {file_extension}')
+        return df
