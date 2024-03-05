41d1239c208d4d9c3dcc05cacec8ea215fdf0ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:04:35 +0000

    Add package pyramid-1-10-zhongwen-wendang/2024.3.5.0/pyramid_1_10_zhongwen_wendang-2024.3.5.0-py3-none-any.whl

---
+++ packages/pyramid-1-10-zhongwen-wendang/pyramid_1_10_zhongwen_wendang-2024.3.5.0-py3-none-any.whl/Pyramid110ZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pyramid-1-10-zhongwen-wendang
+https://github.com/apachecn/pyramid-1-10-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
