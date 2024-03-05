ac096c6de7445ca0bffe6031d3e55b80178667ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:43 +0000

    Add package pydase/0.7.2/pydase-0.7.2.tar.gz

---
+++ packages/pydase/pydase-0.7.2.tar.gz/pydase-0.7.2/src/pydase/data_service/data_service_cache.py
@@ -0,0 +1,39 @@
+import logging
+from typing import TYPE_CHECKING, Any
+
+from pydase.utils.serializer import (
+    SerializationPathError,
+    SerializationValueError,
+    get_nested_dict_by_path,
+    set_nested_value_by_path,
+)
+
+if TYPE_CHECKING:
+    from pydase import DataService
+
+logger = logging.getLogger(__name__)
+
+
+class DataServiceCache:
+    def __init__(self, service: "DataService") -> None:
+        self._cache: dict[str, Any] = {}
+        self.service = service
+        self._initialize_cache()
+
+    @property
+    def cache(self) -> dict[str, Any]:
+        return self._cache
+
+    def _initialize_cache(self) -> None:
+        """Initializes the cache and sets up the callback."""
+        logger.debug("Initializing cache.")
+        self._cache = self.service.serialize()
+
+    def update_cache(self, full_access_path: str, value: Any) -> None:
+        set_nested_value_by_path(self._cache["value"], full_access_path, value)
+
+    def get_value_dict_from_cache(self, full_access_path: str) -> dict[str, Any]:
+        try:
+            return get_nested_dict_by_path(self._cache["value"], full_access_path)
+        except (SerializationPathError, SerializationValueError, KeyError):
+            return {}
