242ab246363e15158d92cde69b8faf24eee64b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 17:07:31 +0000

    Add package ruptura/1.0.1/ruptura-1.0.1.tar.gz

---
+++ packages/ruptura/ruptura-1.0.1.tar.gz/ruptura-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from glob import glob
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+ext_modules = [
+    Pybind11Extension(
+    "_ruptura",
+    sources=glob("src/*.cpp"),
+    extra_compile_args=["-std=c++17", "-DPYBUILD=1"]
+    )
+]
+
+setup(
+    ext_modules=ext_modules,
+    zip_safe=False,
+    packages=["ruptura"],
+    package_dir={"ruptura":"src"}
+)
