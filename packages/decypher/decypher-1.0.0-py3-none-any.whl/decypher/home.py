e2f0814d1827a5a469571fdce40094959d35a489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:47 +0000

    Add package decypher/1.0.0/decypher-1.0.0-py3-none-any.whl

---
+++ packages/decypher/decypher-1.0.0-py3-none-any.whl/decypher/home.py
@@ -0,0 +1,15 @@
+from termcolor import colored
+version= '1.0.0'
+def screen():
+    banner="""
+    
+    ███████████████████████████████████████████████
+    █▄─▄▄▀█▄─▄▄─█─▄▄▄─█▄─█─▄█▄─▄▄─█─█─█▄─▄▄─█▄─▄▄▀█                      DECRYPT CIPHERS / ENCRYPT TEXT
+    ██─██─██─▄█▀█─███▀██▄─▄███─▄▄▄█─▄─██─▄█▀██─▄─▄█                   https://github.com/Aswinr24/Decypher
+    ▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▀▀▄▀▄▀▄▄▄▄▄▀▄▄▀▄▄▀                   ------------version 1.0.0----------- 
+    
+    """
+    print(colored(banner,'yellow'))
+    
+    
+    
