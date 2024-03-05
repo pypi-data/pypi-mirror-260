344ce1e5323ac47bcc7a8b0941aa4578d890c1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:44 +0000

    Add package dstack/0.16.1rc2/dstack-0.16.1rc2-py3-none-any.whl

---
+++ packages/dstack/dstack-0.16.1rc2-py3-none-any.whl/dstack/_internal/core/models/backends/base.py
@@ -0,0 +1,47 @@
+import enum
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class BackendType(str, enum.Enum):
+    """
+    Attributes:
+        AWS (BackendType): Amazon Web Services
+        AZURE (BackendType): Microsoft Azure
+        DSTACK (BackendType): dstack Sky
+        GCP (BackendType): Google Cloud Platform
+        DATACRUNCH (BackendType): DataCrunch
+        KUBERNETES (BackendType): Kubernetes
+        LAMBDA (BackendType): Lambda Cloud
+        TENSORDOCK (BackendType): TensorDock Marketplace
+        VASTAI (BackendType): Vast.ai Marketplace
+    """
+
+    AWS = "aws"
+    AZURE = "azure"
+    DATACRUNCH = "datacrunch"
+    DSTACK = "dstack"
+    GCP = "gcp"
+    KUBERNETES = "kubernetes"
+    LAMBDA = "lambda"
+    LOCAL = "local"
+    REMOTE = "remote"  # TODO: replace for LOCAL
+    NEBIUS = "nebius"
+    TENSORDOCK = "tensordock"
+    VASTAI = "vastai"
+
+
+class ConfigElementValue(BaseModel):
+    value: str
+    label: str
+
+
+class ConfigElement(BaseModel):
+    selected: Optional[str] = None
+    values: List[ConfigElementValue] = []
+
+
+class ConfigMultiElement(BaseModel):
+    selected: List[str] = []
+    values: List[ConfigElementValue] = []
