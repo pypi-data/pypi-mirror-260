a43fa3835b8fd08a935ec7743fef674e024e6ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:04:54 +0000

    Add package programiz-swift-jiaocheng-chuyi/2024.3.5.0/programiz_swift_jiaocheng_chuyi-2024.3.5.0-py3-none-any.whl

---
+++ packages/programiz-swift-jiaocheng-chuyi/programiz_swift_jiaocheng_chuyi-2024.3.5.0-py3-none-any.whl/ProgramizSwiftJiaochengChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""programiz-swift-jiaocheng-chuyi
+https://github.com/apachecn/programiz-swift-jiaocheng-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
