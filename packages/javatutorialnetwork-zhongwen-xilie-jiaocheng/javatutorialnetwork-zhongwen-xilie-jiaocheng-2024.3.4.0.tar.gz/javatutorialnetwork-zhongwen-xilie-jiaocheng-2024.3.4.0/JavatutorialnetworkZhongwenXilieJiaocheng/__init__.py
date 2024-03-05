27b9b4ff1680dc9fc11ec4a6916c009f48226705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:25 +0000

    Add package javatutorialnetwork-zhongwen-xilie-jiaocheng/2024.3.4.0/javatutorialnetwork-zhongwen-xilie-jiaocheng-2024.3.4.0.tar.gz

---
+++ packages/javatutorialnetwork-zhongwen-xilie-jiaocheng/javatutorialnetwork-zhongwen-xilie-jiaocheng-2024.3.4.0.tar.gz/javatutorialnetwork-zhongwen-xilie-jiaocheng-2024.3.4.0/JavatutorialnetworkZhongwenXilieJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""javatutorialnetwork-zhongwen-xilie-jiaocheng
+https://github.com/apachecn/javatutorialnetwork-zhongwen-xilie-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
