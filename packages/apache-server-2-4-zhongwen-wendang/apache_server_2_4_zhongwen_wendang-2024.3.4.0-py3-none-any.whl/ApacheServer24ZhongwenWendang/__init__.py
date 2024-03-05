f9385a2df5265cc533c2f72496241e6a0719111d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:05:51 +0000

    Add package apache-server-2-4-zhongwen-wendang/2024.3.4.0/apache_server_2_4_zhongwen_wendang-2024.3.4.0-py3-none-any.whl

---
+++ packages/apache-server-2-4-zhongwen-wendang/apache_server_2_4_zhongwen_wendang-2024.3.4.0-py3-none-any.whl/ApacheServer24ZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apache-server-2-4-zhongwen-wendang
+https://github.com/apachecn/apache-server-2-4-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
