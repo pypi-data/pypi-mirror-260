2037e890ee4eccd5ab76c094329490827fed487f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:23 +0000

    Add package java-9-mokuaihua-kaifa/2024.3.4.0/java_9_mokuaihua_kaifa-2024.3.4.0-py3-none-any.whl

---
+++ packages/java-9-mokuaihua-kaifa/java_9_mokuaihua_kaifa-2024.3.4.0-py3-none-any.whl/Java9MokuaihuaKaifa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-9-mokuaihua-kaifa
+https://github.com/apachecn/java-9-mokuaihua-kaifa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
