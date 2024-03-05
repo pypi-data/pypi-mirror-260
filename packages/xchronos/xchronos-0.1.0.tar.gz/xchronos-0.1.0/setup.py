676825c0d7112b36898f38b474447e6ba6dd3e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:05:00 +0000

    Add package xchronos/0.1.0/xchronos-0.1.0.tar.gz

---
+++ packages/xchronos/xchronos-0.1.0.tar.gz/xchronos-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    details = f.read()
+
+
+setup(
+    name="xchronos",
+    version="0.1.0",
+    description="An innovated periodic cron time utility using similar but extended expression with cron, and optimized algorithm implementation for large number of steps search.",
+    long_description=details,
+    long_description_content_type="text/markdown",
+    author="RobbieL-nlp",
+    license_files="LICENSE",
+    url="https://github.com/RobbieL-nlp/xchronos",
+    python_requires=">=3.8",
+    keywords="cron, chronos, period, time, date, datetime, cronos",
+    package_dir={"xchronos": "xchronos"},
+    packages=find_packages("xchronos")
+)
+
