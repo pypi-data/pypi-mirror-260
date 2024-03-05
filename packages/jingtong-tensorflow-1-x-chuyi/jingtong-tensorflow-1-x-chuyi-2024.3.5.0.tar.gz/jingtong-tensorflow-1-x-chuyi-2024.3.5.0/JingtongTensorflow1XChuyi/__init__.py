d261670061340414106bc04d2571b545d09eb999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:16 +0000

    Add package jingtong-tensorflow-1-x-chuyi/2024.3.5.0/jingtong-tensorflow-1-x-chuyi-2024.3.5.0.tar.gz

---
+++ packages/jingtong-tensorflow-1-x-chuyi/jingtong-tensorflow-1-x-chuyi-2024.3.5.0.tar.gz/jingtong-tensorflow-1-x-chuyi-2024.3.5.0/JingtongTensorflow1XChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingtong-tensorflow-1-x-chuyi
+https://github.com/apachecn/jingtong-tensorflow-1-x-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
