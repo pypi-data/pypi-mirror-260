920352db98687ae47b62e59e7b176bfa2de0cc2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:05 +0000

    Add package qi-anxin-zidui-shijiao-xia-de-shizhan-gongfang-yanxi-zuzhi/2024.3.5.0/qi_anxin_zidui_shijiao_xia_de_shizhan_gongfang_yanxi_zuzhi-2024.3.5.0-py3-none-any.whl

---
+++ packages/qi-anxin-zidui-shijiao-xia-de-shizhan-gongfang-yanxi-zuzhi/qi_anxin_zidui_shijiao_xia_de_shizhan_gongfang_yanxi_zuzhi-2024.3.5.0-py3-none-any.whl/QiAnxinZiduiShijiaoXiaDeShizhanGongfangYanxiZuzhi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qi-anxin-zidui-shijiao-xia-de-shizhan-gongfang-yanxi-zuzhi
+https://github.com/apachecn/qi-anxin-zidui-shijiao-xia-de-shizhan-gongfang-yanxi-zuzhi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
