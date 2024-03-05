8c7196b3c47fe1010fc4c5aad6c645123e6b5ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:48 +0000

    Add package youta-zhoulidaxue-python-gdal-kecheng-biji/2024.3.5.0/youta_zhoulidaxue_python_gdal_kecheng_biji-2024.3.5.0-py3-none-any.whl

---
+++ packages/youta-zhoulidaxue-python-gdal-kecheng-biji/youta_zhoulidaxue_python_gdal_kecheng_biji-2024.3.5.0-py3-none-any.whl/YoutaZhoulidaxuePythonGdalKechengBiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""youta-zhoulidaxue-python-gdal-kecheng-biji
+https://github.com/apachecn/youta-zhoulidaxue-python-gdal-kecheng-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
