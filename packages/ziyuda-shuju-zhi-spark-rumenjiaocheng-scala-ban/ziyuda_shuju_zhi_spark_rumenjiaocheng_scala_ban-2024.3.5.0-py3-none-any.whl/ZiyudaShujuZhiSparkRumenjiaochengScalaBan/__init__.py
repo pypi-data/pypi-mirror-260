add918e919f6bfd4aba18e8912e32236ca6a288d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:17 +0000

    Add package ziyuda-shuju-zhi-spark-rumenjiaocheng-scala-ban/2024.3.5.0/ziyuda_shuju_zhi_spark_rumenjiaocheng_scala_ban-2024.3.5.0-py3-none-any.whl

---
+++ packages/ziyuda-shuju-zhi-spark-rumenjiaocheng-scala-ban/ziyuda_shuju_zhi_spark_rumenjiaocheng_scala_ban-2024.3.5.0-py3-none-any.whl/ZiyudaShujuZhiSparkRumenjiaochengScalaBan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziyuda-shuju-zhi-spark-rumenjiaocheng-scala-ban
+https://github.com/apachecn/ziyuda-shuju-zhi-spark-rumenjiaocheng-scala-ban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
