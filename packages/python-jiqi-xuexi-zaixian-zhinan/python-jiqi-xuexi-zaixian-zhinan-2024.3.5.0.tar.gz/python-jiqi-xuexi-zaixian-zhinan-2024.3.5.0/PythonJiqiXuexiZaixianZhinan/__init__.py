4c28c13b08dd7736569d38106544eb27928d4fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:52 +0000

    Add package python-jiqi-xuexi-zaixian-zhinan/2024.3.5.0/python-jiqi-xuexi-zaixian-zhinan-2024.3.5.0.tar.gz

---
+++ packages/python-jiqi-xuexi-zaixian-zhinan/python-jiqi-xuexi-zaixian-zhinan-2024.3.5.0.tar.gz/python-jiqi-xuexi-zaixian-zhinan-2024.3.5.0/PythonJiqiXuexiZaixianZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""python-jiqi-xuexi-zaixian-zhinan
+https://github.com/apachecn/python-jiqi-xuexi-zaixian-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
