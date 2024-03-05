daee46e0daaa080801624bc8dc50fbb6851312cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:49 +0000

    Add package ringcentral/0.8.4/ringcentral-0.8.4-py3-none-any.whl

---
+++ packages/ringcentral/ringcentral-0.8.4-py3-none-any.whl/ringcentral/platform/events.py
@@ -0,0 +1,11 @@
+
+class Events:
+    refreshSuccess = 'refreshSuccess'
+    refreshError = 'refreshError'
+    loginSuccess = 'loginSuccess'
+    loginError = 'loginError'
+    logoutSuccess = 'logoutSuccess'
+    logoutError = 'logoutError'
+
+    def __init__(self):
+        pass
