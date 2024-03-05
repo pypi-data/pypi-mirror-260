3ada8b0598e8df19ae4bfd20df761f68b86e2f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:04:57 +0000

    Add package findallprimes/1.0.0/findallprimes-1.0.0.tar.gz

---
+++ packages/findallprimes/findallprimes-1.0.0.tar.gz/findallprimes-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = 'findallprimes',
+	version = '1.0.0',
+	py_modules = ['findallprimes'],
+	author = 'Arman',
+	author_email = 'armanashraf015@gmail.com',
+	description = 'This package finds all prime numbers from 2 to that number'
+)
+
+ 
+ 
