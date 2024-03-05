3973061295ef4741c40dd14b734ec80f94cb73cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 17:06:27 +0000

    Add package KeyboardPaster/0.1.10/KeyboardPaster-0.1.10.tar.gz

---
+++ packages/KeyboardPaster/KeyboardPaster-0.1.10.tar.gz/KeyboardPaster-0.1.10/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="KeyboardPaster",
+    version="0.1.10",
+    author="Brian Knutsson",
+    author_email="development@knutsson.it",
+    description="A Python package to type text using the keyboard module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KnutssonDevelopment/KeyboardPaster",
+    packages=find_packages(),
+    package_data={
+        "keyboardpaster": ["keyboardpaster_app.kv"],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        "pynput",
+        "kivy",
+        "kivymd",
+    ],
+    extras_require={
+        "windows": ["pywin32"],
+        "linux": ["setuptools"],
+        "macos": ["setuptools"],
+    },
+    entry_points={
+        "console_scripts": [
+            "keyboardpaster=keyboardpaster.app:main",
+        ],
+    },
+)
