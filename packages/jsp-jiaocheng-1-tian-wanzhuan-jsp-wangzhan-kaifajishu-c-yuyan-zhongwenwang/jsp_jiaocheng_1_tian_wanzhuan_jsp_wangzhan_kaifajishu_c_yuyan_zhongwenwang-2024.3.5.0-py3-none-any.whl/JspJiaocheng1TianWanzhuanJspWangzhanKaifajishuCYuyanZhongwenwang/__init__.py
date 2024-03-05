f1efa79f620bea66aeb1baee7773366aeb93ae8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:13 +0000

    Add package jsp-jiaocheng-1-tian-wanzhuan-jsp-wangzhan-kaifajishu-c-yuyan-zhongwenwang/2024.3.5.0/jsp_jiaocheng_1_tian_wanzhuan_jsp_wangzhan_kaifajishu_c_yuyan_zhongwenwang-2024.3.5.0-py3-none-any.whl

---
+++ packages/jsp-jiaocheng-1-tian-wanzhuan-jsp-wangzhan-kaifajishu-c-yuyan-zhongwenwang/jsp_jiaocheng_1_tian_wanzhuan_jsp_wangzhan_kaifajishu_c_yuyan_zhongwenwang-2024.3.5.0-py3-none-any.whl/JspJiaocheng1TianWanzhuanJspWangzhanKaifajishuCYuyanZhongwenwang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jsp-jiaocheng-1-tian-wanzhuan-jsp-wangzhan-kaifajishu-c-yuyan-zhongwenwang
+https://github.com/apachecn/jsp-jiaocheng-1-tian-wanzhuan-jsp-wangzhan-kaifajishu-c-yuyan-zhongwenwang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
