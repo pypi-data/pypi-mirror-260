888b3c8df59457cc404c655259b46102de15144c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:04:06 +0000

    Add package app-yingxiao-shizhan-shouji-heji-niaoge-biji/2024.3.4.0/app_yingxiao_shizhan_shouji_heji_niaoge_biji-2024.3.4.0-py3-none-any.whl

---
+++ packages/app-yingxiao-shizhan-shouji-heji-niaoge-biji/app_yingxiao_shizhan_shouji_heji_niaoge_biji-2024.3.4.0-py3-none-any.whl/AppYingxiaoShizhanShoujiHejiNiaogeBiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""app-yingxiao-shizhan-shouji-heji-niaoge-biji
+https://github.com/apachecn/app-yingxiao-shizhan-shouji-heji-niaoge-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
