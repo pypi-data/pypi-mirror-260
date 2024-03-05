71ea032a0e7dc979636ca5d15b221fe3a1bfb2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:10 +0000

    Add package cloudcix/0.13.6/cloudcix-0.13.6.tar.gz

---
+++ packages/cloudcix/cloudcix-0.13.6.tar.gz/cloudcix-0.13.6/cloudcix/api/pat.py
@@ -0,0 +1,12 @@
+from cloudcix.client import Client
+
+
+class PAT:
+    """
+    The Pat Application exposes a REST API capable of managing Pod records
+    """
+    _application_name = 'pat'
+    pod = Client(
+        _application_name,
+        'pod/',
+    )
---
+++ packages/cloudcix/cloudcix-0.13.6.tar.gz/cloudcix-0.13.6/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requires = f.read().split('\n')
+
+setup(
+    name='cloudcix',
+    version='0.13.6',
+    description='Python3 bindings and utils for CloudCIX API.',
+    long_description=readme,
+    author='CloudCIX',
+    author_email='developers@cloudcix.com',
+    url='https://python-cloudcix.readthedocs.io/en/latest/',
+    packages=[
+        'cloudcix',
+        'cloudcix.api',
+        'cloudcix.rcc',
+    ],
+    keywords=['cix', 'cloudcix', 'bindings', 'client', 'api'],
+    install_requires=requires,
+    package_data={
+        '': ['LICENSE', 'README.rst'],
+    },
+    package_dir={'cloudcix': 'cloudcix'},
+    include_package_data=True,
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ])
