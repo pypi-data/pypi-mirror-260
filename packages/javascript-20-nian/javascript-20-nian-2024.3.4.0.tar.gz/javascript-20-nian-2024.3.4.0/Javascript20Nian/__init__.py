bacdbca000b5e8b65a9f0959b21818c957732aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:28 +0000

    Add package javascript-20-nian/2024.3.4.0/javascript-20-nian-2024.3.4.0.tar.gz

---
+++ packages/javascript-20-nian/javascript-20-nian-2024.3.4.0.tar.gz/javascript-20-nian-2024.3.4.0/Javascript20Nian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""javascript-20-nian
+https://github.com/apachecn/javascript-20-nian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
