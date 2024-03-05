8bf616881d68ba8c1bfd43bf2e61e6d0bad38fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:05:00 +0000

    Add package txdprogbar/1.0.0/txdprogbar-1.0.0.tar.gz

---
+++ packages/txdprogbar/txdprogbar-1.0.0.tar.gz/txdprogbar-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import setuptools
+packages = ['txdprogbar']
+setup(name='txdprogbar',
+    version='1.0.0',
+    author='唐旭东',
+    packages=packages,
+    package_dir={'requests': 'requests'})
