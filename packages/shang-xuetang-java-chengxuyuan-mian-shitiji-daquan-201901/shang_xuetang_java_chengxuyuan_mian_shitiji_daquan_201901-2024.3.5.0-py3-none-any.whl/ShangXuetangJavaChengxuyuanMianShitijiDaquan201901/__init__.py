2810396b18d2406d8ceaf578f7a86219124bd55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:26 +0000

    Add package shang-xuetang-java-chengxuyuan-mian-shitiji-daquan-201901/2024.3.5.0/shang_xuetang_java_chengxuyuan_mian_shitiji_daquan_201901-2024.3.5.0-py3-none-any.whl

---
+++ packages/shang-xuetang-java-chengxuyuan-mian-shitiji-daquan-201901/shang_xuetang_java_chengxuyuan_mian_shitiji_daquan_201901-2024.3.5.0-py3-none-any.whl/ShangXuetangJavaChengxuyuanMianShitijiDaquan201901/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shang-xuetang-java-chengxuyuan-mian-shitiji-daquan-201901
+https://github.com/apachecn/shang-xuetang-java-chengxuyuan-mian-shitiji-daquan-201901"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
