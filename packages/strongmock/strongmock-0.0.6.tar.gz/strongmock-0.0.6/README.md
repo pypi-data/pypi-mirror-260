8c2831f81dfc414e74c146355625e8b00b06fc20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 17:07:36 +0000

    Add package strongmock/0.0.6/strongmock-0.0.6.tar.gz

---
+++ packages/strongmock/strongmock-0.0.6.tar.gz/strongmock-0.0.6/README.md
@@ -0,0 +1,82 @@
+# StrongMock
+
+Ever wanted **Unlimited Power** while patching things ? 
+
+StrongMock is a powerful mocking library for Python that leverages low-level ctypes functionality to provide extensive mocking capabilities. Some care may be needed while using this.
+
+# Functionality/Usage
+
+## Basic 
+instead of `unittest.mock.patch` use `strongmock.strongpatch`
+
+## More advanced features
+
+### `strongpatch.equal_basic_objects`
+Can make floats / ints / True False and other basic objects equal to each other. \
+This customization of the equality behavior of basic objects can be useful when you have to test very very specific edge cases, maybe something governing program flow etc.
+
+### `strongpatch.mock_imports`
+Replaces imports with Magicmock, takes in a tuple of strings, optional parameter override which decides if we override existing imports as well (True by default).
+
+### `strongpatch.object`
+Same as `patch.object`, but does the same advanced replacement as `strongpatch`
+
+### `strongpatch.multiple`
+Same as `patch.multiple`, but does the same advanced replacement as `strongpatch`
+
+# Async
+
+Also works on async functions, with similar stronger patching
+
+# Safety
+
+Some care has been taken to avoid crashes and breakage, but the user does have full control. One word answer - no.
+
+# Working and usage
+
+## strongpatch
+
+This applies when the target of a `strongmock.strongpatch` is a function defined in python with a `__code__` attribute. For other cases (methods in a class, classes, lbrary functions in c, etc.), the behaviour is the same as `unittest.patch`.\
+This will patch the `__code__` attribute of the function to call the mock, meaning that references will also have the functionality of mock. \
+This can be extremely convenient in some cases.
+
+## strongpatch.mock_imports
+
+We can pass testcases that need imports inside them
+
+```python
+class TestImportPatch(unittest.TestCase):
+    @strongpatch.mock_imports(("somelib_abcd",))
+    def test_import_somelib_abcd(self):
+        import somelib_abcd
+        self.assertIsInstance(somelib_abcd.somefn(), MagicMock)
+```
+
+## strongpatch.equal_basic_objects
+
+We dump the bytes from objsrc to objdst
+
+### huh?
+
+Yes, you can now pass these testcases.
+
+```python
+import unittest
+from strongmock import strongpatch
+class StrongMockDemoTest(unittest.TestCase):
+    @strongpatch.equal_basic_objects(False, True)
+    def test_truefalse_patch_0(self):
+        if True != False:
+            raise RuntimeError("TRUEFALSE PATCH FAILED")
+```
+
+# Links
+[PyPi](https://pypi.org/project/strongmock) \
+[GitHub](https://github.com/arrmansa/strongmock)
+
+# License
+StrongMock is licensed under the Unlicense. See the LICENSE file for details.
+
+# Official theme music
+When using these methods, it is recommended that you listen to this for better code. \
+[Kai's Theme Epic Version (Slowed + BassBoosted)](https://www.youtube.com/watch?v=uMvNQRSKccg)
---
+++ packages/strongmock/strongmock-0.0.6.tar.gz/strongmock-0.0.6/tests/test_full.py
@@ -0,0 +1,175 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+import tests.fnsource as fnsource
+from src import strongpatch
+from tests.fnsource import asyncplain, generic, pair, pairgeneric, plain
+
+
+def plainmock():
+    return "MOCKED"
+
+
+async def asyncplainmock():
+    return "MOCKED"
+
+
+def genericmock(a, *args, b="d", c="c", **kwargs):
+    return a + "MOCKED" + b + c + str((args, kwargs))
+
+
+pairgenericmock = pair(genericmock)
+
+
+class TestFunctionPatch(unittest.TestCase):
+
+    @patch("tests.fnsource.plain", plainmock)
+    def test_plain_0_mockfail(self):
+        self.assertEqual(plain(), "ORIGINAL")
+
+    @strongpatch("tests.fnsource.plain", plainmock)
+    def test_plain_1_mockwork(self):
+        self.assertEqual(plain(), "MOCKED")
+
+    @strongpatch("tests.fnsource.plain")
+    def test_plain_2_mockwork(self, _):
+        self.assertIsInstance(plain(), MagicMock)
+
+    @strongpatch.object(fnsource, "plain")
+    def test_plain_3_mockobjectwork(self, _):
+        self.assertIsInstance(plain(), MagicMock)
+
+    def test_plain_4_after(self):
+        self.assertEqual(plain(), "ORIGINAL")
+
+
+class TestImportPatch(unittest.TestCase):
+
+    @strongpatch.mock_imports(("somelib_abcd", "somelib_abcef", "tests.fnsource"))
+    def test_plain_1_mockwork(self):
+        import somelib_abcd
+        from somelib_abcef.test1 import test2
+        from tests.fnsource import plain
+        self.assertIsInstance(somelib_abcd.somefn(), MagicMock)
+        self.assertIsInstance(plain, MagicMock)
+
+    @strongpatch.mock_imports(("tests.fnsource",), override=False)
+    def test_plain_2_after(self):
+        from tests.fnsource import plain
+        self.assertNotIsInstance(plain, MagicMock)
+
+    def test_plain_2_after(self):
+        with self.assertRaises(ImportError):
+            import somelib_abcd
+        with self.assertRaises(ImportError):
+            import somelib_abcef
+        from tests.fnsource import plain
+        self.assertNotIsInstance(plain, MagicMock)
+
+class TestBasicObjectPatching(unittest.TestCase):
+
+    # Can't use self.assertEqual for these because assertEqual can be patched and will not show that we can patch == of floats/ints/truefalse
+
+    @strongpatch.equal_basic_objects(100.0, 101.0)
+    def test_float_patch_0(self):
+        if 100.0 != 101.0:
+            raise RuntimeError("FLOAT PATCH FAILED")
+
+    def test_float_patch_1_after(self):
+        if 100.0 == 101.0:
+            raise RuntimeError("FLOAT PATCH REMOVAL FAILED")
+
+    @strongpatch.equal_basic_objects(False, True)
+    def test_truefalse_patch_0(self):
+        if True != False:
+            raise RuntimeError("TRUEFALSE PATCH FAILED")
+
+    def test_truefalse_patch_1_after(self):
+        if True == False:
+            raise RuntimeError("TRUEFALSE PATCH REMOVAL FAILED")
+
+    @strongpatch.equal_basic_objects(37838, 2673229)
+    def test_int_patch_0(self):
+        if 37838 != 2673229:
+            raise RuntimeError("INT PATCH FAILED")
+
+    def test_int_patch_1_after(self):
+        if 37838 == 2673229:
+            raise RuntimeError("INT PATCH REMOVAL FAILED")
+
+
+class TestArgsKwargs(unittest.TestCase):
+
+    def test_generic_0_before(self):
+        self.assertEqual(generic("a"), "aORIGINALb((), {})")
+        self.assertEqual(generic("a", b="c"), "aORIGINALc((), {})")
+        self.assertEqual(generic("a", "x", b="c", y="z"), "aORIGINALc(('x',), {'y': 'z'})")
+
+    @strongpatch("tests.fnsource.generic", lambda: "MOCKED")
+    def test_generic_1_0_mocked(self):
+        self.assertEqual(generic(), "MOCKED")
+
+    @strongpatch("tests.fnsource.generic", lambda a="a": a + "MOCKED")
+    def test_generic_1_1_mocked(self):
+        self.assertEqual(generic(), "aMOCKED")
+        self.assertEqual(generic(a="b"), "bMOCKED")
+
+    @strongpatch("tests.fnsource.generic", genericmock)
+    def test_generic_1_2_mocked(self):
+        self.assertEqual(generic("a"), "aMOCKEDdc((), {})")
+        self.assertEqual(generic("a", b="c"), "aMOCKEDcc((), {})")
+        self.assertEqual(generic("a", "x", b="e", y="z"), "aMOCKEDec(('x',), {'y': 'z'})")
+
+    def test_generic_2_after(self):
+        self.assertEqual(generic("a"), "aORIGINALb((), {})")
+        self.assertEqual(generic("a", b="c"), "aORIGINALc((), {})")
+        self.assertEqual(generic("a", "x", b="c", y="z"), "aORIGINALc(('x',), {'y': 'z'})")
+
+
+class TestClosurePair(unittest.TestCase):
+
+    def test_generic_0_before(self):
+        self.assertEqual(pairgeneric("a"), "aORIGINALb((), {})" * 2)
+        self.assertEqual(pairgeneric("a", b="c"), "aORIGINALc((), {})" * 2)
+        self.assertEqual(pairgeneric("a", "x", b="c", y="z"), "aORIGINALc(('x',), {'y': 'z'})" * 2)
+
+    @strongpatch("tests.fnsource.pairgeneric", lambda: "MOCKED")
+    def test_generic_1_0_mocked(self):
+        self.assertEqual(pairgeneric(), "MOCKED")
+
+    @strongpatch("tests.fnsource.pairgeneric", lambda a="a": a + "MOCKED")
+    def test_generic_1_1_mocked(self):
+        self.assertEqual(pairgeneric(), "aMOCKED")
+        self.assertEqual(pairgeneric(a="b"), "bMOCKED")
+
+    @strongpatch("tests.fnsource.pairgeneric", genericmock)
+    def test_generic_1_2_mocked(self):
+        self.assertEqual(pairgeneric("a"), "aMOCKEDdc((), {})")
+        self.assertEqual(pairgeneric("a", b="c"), "aMOCKEDcc((), {})")
+        self.assertEqual(pairgeneric("a", "x", b="e", y="z"), "aMOCKEDec(('x',), {'y': 'z'})")
+
+    def test_generic_2_after(self):
+        self.assertEqual(pairgeneric("a"), "aORIGINALb((), {})" * 2)
+        self.assertEqual(pairgeneric("a", b="c"), "aORIGINALc((), {})" * 2)
+        self.assertEqual(pairgeneric("a", "x", b="c", y="z"), "aORIGINALc(('x',), {'y': 'z'})" * 2)
+
+
+class TestAsyncPlain(unittest.IsolatedAsyncioTestCase):
+
+    async def test_async_plain_0_before(self):
+        self.assertEqual(await asyncplain(), "ORIGINAL")
+
+    @patch("tests.fnsource.asyncplain", asyncplainmock)
+    async def test_async_plain_1_normal(self):
+        self.assertEqual(await asyncplain(), "ORIGINAL")
+
+    @strongpatch("tests.fnsource.asyncplain", asyncplainmock)
+    async def test_async_plain_2_strong(self):
+        self.assertEqual(await asyncplain(), "MOCKED")
+
+    async def test_async_plain_4_after(self):
+        self.assertEqual(await asyncplain(), "ORIGINAL")
+
+
+if __name__ == "__main__":
+    unittest.main()
