3d910a0688b214819e3b19b58080ad53566e794e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:05:00 +0000

    Add package psqlpy/0.2.3/psqlpy-0.2.3.tar.gz

---
+++ packages/psqlpy/psqlpy-0.2.3.tar.gz/psqlpy-0.2.3/src/driver/transaction_options.rs
@@ -0,0 +1,41 @@
+use pyo3::pyclass;
+
+#[pyclass]
+#[derive(Clone, Copy)]
+pub enum IsolationLevel {
+    ReadUncommitted,
+    ReadCommitted,
+    RepeatableRead,
+    Serializable,
+}
+
+impl IsolationLevel {
+    /// Return isolation level as String literal.
+    #[must_use]
+    pub fn to_str_level(&self) -> String {
+        match self {
+            IsolationLevel::ReadUncommitted => "READ UNCOMMITTED".into(),
+            IsolationLevel::ReadCommitted => "READ COMMITTED".into(),
+            IsolationLevel::RepeatableRead => "REPEATABLE READ".into(),
+            IsolationLevel::Serializable => "SERIALIZABLE".into(),
+        }
+    }
+}
+
+#[pyclass]
+#[derive(Clone, Copy)]
+pub enum ReadVariant {
+    ReadOnly,
+    ReadWrite,
+}
+
+impl ReadVariant {
+    /// Return Read variant as String literal.
+    #[must_use]
+    pub fn to_str_option(&self) -> String {
+        match self {
+            ReadVariant::ReadOnly => "READ ONLY".into(),
+            ReadVariant::ReadWrite => "READ WRITE".into(),
+        }
+    }
+}
