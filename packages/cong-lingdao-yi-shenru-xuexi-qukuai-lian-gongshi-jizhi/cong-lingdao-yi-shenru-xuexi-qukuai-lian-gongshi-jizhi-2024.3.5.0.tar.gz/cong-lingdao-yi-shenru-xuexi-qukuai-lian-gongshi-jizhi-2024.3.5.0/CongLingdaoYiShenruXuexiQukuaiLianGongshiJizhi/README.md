b1b41bea162cfce7e2074687b13c9961676925b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:05:02 +0000

    Add package cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi/2024.3.5.0/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi-2024.3.5.0.tar.gz

---
+++ packages/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi-2024.3.5.0.tar.gz/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi-2024.3.5.0/CongLingdaoYiShenruXuexiQukuaiLianGongshiJizhi/README.md
@@ -0,0 +1,27 @@
+# 从零到壹深入学习区块链共识机制
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi
+docker run -tid -p <port>:80 apachecn0/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi
+cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi
+cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi-2024.3.5.0.tar.gz/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi-2024.3.5.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import CongLingdaoYiShenruXuexiQukuaiLianGongshiJizhi
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('CongLingdaoYiShenruXuexiQukuaiLianGongshiJizhi'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi",
+    version=CongLingdaoYiShenruXuexiQukuaiLianGongshiJizhi.__version__,
+    url="https://github.com/apachecn/cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi",
+    author=CongLingdaoYiShenruXuexiQukuaiLianGongshiJizhi.__author__,
+    author_email=CongLingdaoYiShenruXuexiQukuaiLianGongshiJizhi.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="从零到壹深入学习区块链共识机制",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "cong-lingdao-yi-shenru-xuexi-qukuai-lian-gongshi-jizhi=CongLingdaoYiShenruXuexiQukuaiLianGongshiJizhi.__main__:main",
+            "CongLingdaoYiShenruXuexiQukuaiLianGongshiJizhi=CongLingdaoYiShenruXuexiQukuaiLianGongshiJizhi.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
