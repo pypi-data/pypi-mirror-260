ce1d3c0ea20f24d31871129a4acecd19bf54be23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:05:56 +0000

    Add package rad-tools/0.8.13/rad_tools-0.8.13-py3-none-any.whl

---
+++ packages/rad-tools/rad_tools-0.8.13-py3-none-any.whl/rad_tools-0.8.13.data/scripts/rad-extract-tb2j.py
@@ -0,0 +1,10 @@
+#!python
+
+from radtools._osfix import _winwait
+from radtools.score.extract_tb2j import create_parser, manager
+
+if __name__ == "__main__":
+    parser = create_parser()
+    args = parser.parse_args()
+    manager(**vars(args))
+    _winwait()
---
+++ packages/rad-tools/rad_tools-0.8.13-py3-none-any.whl/rad_tools-0.8.13.data/scripts/rad-plot-fatbands.py
@@ -0,0 +1,10 @@
+#!python
+
+from radtools._osfix import _winwait
+from radtools.score.plot_fatbands import create_parser, manager
+
+if __name__ == "__main__":
+    parser = create_parser()
+    args = parser.parse_args()
+    manager(**vars(args))
+    _winwait()
