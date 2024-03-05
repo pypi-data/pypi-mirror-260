f56a1b56f1fe46f16e85b350071840cbe0d40531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:05:29 +0000

    Add package webwithpy/0.6.3.1/webwithpy-0.6.3.1-py3-none-any.whl

---
+++ packages/webwithpy/webwithpy-0.6.3.1-py3-none-any.whl/webwithpy/html/pyhtml.py
@@ -0,0 +1,159 @@
+from typing import Type
+
+
+class HtmlTag:
+    @classmethod
+    def set_dict_attrs(cls, kwargs):
+        return " ".join([f'{k.replace("_", "")}="{v}"' for k, v in kwargs.items()])
+
+    @classmethod
+    def set_tag(cls, text, field):
+        return text if field else ""
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Form(HtmlTag):
+    def __init__(
+        self, *tags: str | HtmlTag, _name: str = "", _class: str = "", **attrs
+    ):
+        self.attributes = {"name": _name, "class": _class}
+        self.attributes.update(attrs)
+
+        for attribute, value in self.attributes.items():
+            self.attributes[attribute] = self.set_tag(f"{attribute}={value}", value)
+
+        self.tags = tags
+
+    def __str__(self):
+        return f"<form {' '.join([attr for attr in self.attributes.values()])}>{''.join([tag.__str__() for tag in self.tags])}</form>"
+
+
+class Label(HtmlTag):
+    def __init__(self, _for: str = "", _form: str = "", *tags: str | HtmlTag):
+        self._for = self.set_tag(f"for='{_for}'", _for)
+        self._form = self.set_tag(f"form={_form}", _form)
+        self.tags = tags
+
+    def __str__(self):
+        return (
+            f"<label {self._for} {self._form}> {''.join(self.tags.__str__())} </label>"
+        )
+
+
+class Input(HtmlTag):
+    def __init__(
+        self,
+        disabled: bool = False,
+        _name: str = "",
+        _type: str = "",
+        _class: str = "",
+        **attrs: str,
+    ):
+        self._name = self.set_tag(f"name='{_name}'", _name)
+        self._type = self.set_tag(f"type='{_type}'", _type)
+        self._class = self.set_tag(f"class='{_class}'", _class)
+        self.disabled = self.set_tag("disabled", disabled)
+        self.attrs = self.set_dict_attrs(attrs)
+
+    def __str__(self):
+        return f"<input {self._name} {self._type} {self._class} {self.attrs} {self.disabled}/>"
+
+
+class Span(HtmlTag):
+    def __init__(self, *tags: str | HtmlTag, **attrs: str):
+        self.tags = tags
+        self.attrs = attrs
+
+    def __str__(self):
+        return f"<span {self.set_dict_attrs(self.attrs)}>{''.join([tag for tag in self.tags])}</span>"
+
+
+class Div(HtmlTag):
+    def __init__(self, *tags: str | HtmlTag, _class: str = "", **attrs: str):
+        self._class = _class
+        self.tags = tags
+        self.attrs = attrs
+
+    def __str__(self):
+        return f"<div class={self._class} {self.set_dict_attrs(self.attrs)}>{''.join([tag.__str__() for tag in self.tags])}</div>"
+
+
+class H1(HtmlTag):
+    def __init__(self, text: str = "", **attrs):
+        self.text = text
+        self.attrs = self.set_dict_attrs(attrs)
+
+    def __str__(self):
+        return f"<h1 {self.attrs}>{''.join(self.text)}</h1>"
+
+
+class H2(HtmlTag):
+    def __init__(self, text: str = "", **attrs):
+        self.text = text
+        self.attrs = self.set_dict_attrs(attrs)
+
+    def __str__(self):
+        return f"<h2 {self.attrs}>{''.join(self.text)}</h2>"
+
+
+class H3(HtmlTag):
+    def __init__(self, text: str = "", **attrs):
+        self.text = text
+        self.attrs = self.set_dict_attrs(attrs)
+
+    def __str__(self):
+        return f"<h3 {self.attrs}>{''.join(self.text)}</h3>"
+
+
+class H4(HtmlTag):
+    def __init__(self, _class: str = "", text: str = "", **attrs):
+        self._class = _class
+        self.text = text
+        self.attrs = self.set_dict_attrs(attrs)
+
+    def __str__(self):
+        return f"<h4 class={self._class} {self.attrs}>{''.join(self.text)}</h4>"
+
+
+class A(HtmlTag):
+    """
+    Html <a> </a> tag in python
+    """
+
+    def __init__(
+        self,
+        *tags: str | HtmlTag,
+        _class: str = "",
+        _href: str = "",
+        **attrs: str,
+    ):
+        self._class = self.set_tag(f"class='{_class}'", _class)
+        self._href = self.set_tag(f'href="{_href}"', _href)
+        self.tags = tags
+        self.attrs = self.set_dict_attrs(attrs)
+
+    def __str__(self):
+        return f"<a {self._class} {self._href} {self.attrs}> {''.join([tag.__str__() for tag in self.tags])} </a>"
+
+
+class P(HtmlTag):
+    """
+    Html <p> </p> tag in python
+    """
+
+    def __init__(
+        self,
+        text: str = "",
+        _class: str = "",
+        _href: str = "",
+        **attrs: str,
+    ):
+        self.text = text
+        self._class = self.set_tag(f"class='{_class}'", _class)
+        self._href = self.set_tag(f'href="{_href}"', _href)
+        self.attrs = self.set_dict_attrs(attrs)
+
+    def __str__(self):
+        return f"<a {self._class} {self._href} {self.attrs}> {self.text} </a>"
---
+++ packages/webwithpy/webwithpy-0.6.3.1-py3-none-any.whl/webwithpy/http/handler.py
@@ -0,0 +1,136 @@
+from ..exeptions.RouteExceptions import RouteNotFound
+from ..routing.router import Router
+from ..app import App
+from .redirect import Redirect
+from .request import Request
+from .response import Response
+from asyncio import AbstractEventLoop, StreamWriter
+import socket
+import asyncio
+import traceback
+import uuid
+
+
+class HTTPHandler:
+    def __init__(self):
+        self.server = None
+        self.client = None
+        self.writer = None
+        App.request = None
+        self.resp = None
+
+    async def handle_client(
+        self,
+        server: AbstractEventLoop,
+        client: socket.socket,
+        writer: StreamWriter,
+        client_request: str,
+    ):
+        """
+        Handles the client request by setting cookies, finding the function by the clients path and sending a request
+        back
+        """
+
+        self.server = server
+        self.client = client
+        self.writer = writer
+        App.request = Request(client_request)
+
+        session = (
+            self.generate_session()
+            if "session" not in App.request.cookies
+            else App.request.cookies["session"]
+        )
+
+        # make a response object with a new session or the current session given by the request
+        self.resp: Response = Response(session)
+        App.response = self.resp
+
+        #  try to find the function by the request path
+        try:
+            func_out, html_template, content_type = await self.call_func_by_route(
+                App.request.path, App.request.method
+            )
+
+            self.resp.content_type = content_type
+
+            # make sure we never send any data with the response is 404(DON'T REMOVE CAN LEAK DATA IF REMOVED)
+            if isinstance(func_out, RouteNotFound):
+                return
+            elif App.redirect is not None:
+                # function is only protected for people who use the framework, the framework itself can always call
+                # protected functions
+                await self.send_response(App.redirect._to_http())
+                return
+            self.resp._add_content(func_out, html_template)
+        except Exception as e:
+            # it is possible that a response
+            await self.send_response(self.resp._generate_error(500))
+            traceback.print_exception(e)
+            return
+
+        # only send response after try catch bc if something goes wrong while sending the response it will give
+        # a completely different exception
+        await self.send_response(self.resp._encode())
+
+    async def call_func_by_route(self, route, method):
+        """
+        Runs a function based on the request method and route
+        :return: function output and html template
+        """
+        routed_data = Router.get_data_by_route(route, method)
+
+        if routed_data is None:
+            print(
+                f"User tried to go to this path: {App.request.path} with this method: {App.request.method},"
+                f" However it was not found or it could not be accessed with this request method!"
+            )
+
+            # send a 404 not found back because the path->method->function was not found!
+            await self.send_response(self.resp._generate_error(404))
+            return RouteNotFound(route, method), None, None
+
+        # we will need to awaut the routed function if it is async!
+        if self.async_func(routed_data.func):
+            return (
+                await routed_data.func(*routed_data.args, **routed_data.kwargs),
+                routed_data.html_template,
+                routed_data.content_type,
+            )
+
+        # run routed function no async
+        return (
+            routed_data.func(*routed_data.args, **routed_data.kwargs),
+            routed_data.html_template,
+            routed_data.content_type,
+        )
+
+    async def send_response(self, resp: bytes | str = b""):
+        """
+        Writes the response back to the user
+        """
+        if isinstance(resp, str):
+            resp = resp.encode()
+
+        self.writer.write(resp)
+
+    async def close_server(self):
+        """
+        Closes the connection
+        """
+        self.client.close()
+
+    @classmethod
+    def async_func(cls, func) -> bool:
+        """
+        tests if the function is async or not
+        """
+        return asyncio.iscoroutinefunction(func)
+
+    @classmethod
+    def generate_session(cls):
+        """
+        Generate a session key for any user connecting to the server
+        """
+        key = f"{uuid.uuid4()}-{uuid.uuid4()}"
+        return key.replace("\r", "")
