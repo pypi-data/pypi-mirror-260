5e46714e67e52e686717be812e8e7f97e7d732ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:04:40 +0000

    Add package taichu-storage/1.0.19/taichu_storage-1.0.19-py3-none-any.whl

---
+++ packages/taichu-storage/taichu_storage-1.0.19-py3-none-any.whl/taichu_storage/__init__.py
@@ -0,0 +1,71 @@
+from abc import abstractmethod, ABC
+from enum import Enum
+
+
+class Protocol(Enum):
+    OBS = 1
+    BOTO3 = 2
+
+
+class StorageInterface(ABC):
+
+    @abstractmethod
+    def list_objects(self, key, delimiter=''):
+        pass
+
+    @abstractmethod
+    def get_object(self, key):
+        pass
+
+    @abstractmethod
+    def put_object(self, key, content):
+        pass
+
+    @abstractmethod
+    def upload_file(self, local_filename, key):
+        pass
+
+    @abstractmethod
+    def upload_dir(self, local_dir, key):
+        pass
+
+    @abstractmethod
+    def download_file(self, key, local_filename):
+        pass
+
+    @abstractmethod
+    def download_dir(self, key, local_dir):
+        pass
+
+    @abstractmethod
+    def copy_object(self, source_key, target_key):
+        pass
+
+    @abstractmethod
+    def copy_dir(self, source_dir, dest_dir):
+        pass
+
+    @abstractmethod
+    def create_dir(self, dirname):
+        pass
+
+    @abstractmethod
+    def generate_signed_url(self, key, expiration=3600, use_external_host=True):
+        pass
+
+    @abstractmethod
+    def generate_upload_credentials(self, key, expiration=3600, use_external_host=True):
+        pass
+
+
+def create_storage(protocol: Protocol, cfgs=None):
+    if cfgs is None:
+        cfgs = {}
+    if protocol == Protocol.OBS:
+        from taichu_storage.obs_client import StorageObs
+        return StorageObs(cfgs)
+    if protocol == Protocol.BOTO3:
+        from taichu_storage.boto3_client import StorageBoto3
+        return StorageBoto3(cfgs)
+    return None
+
