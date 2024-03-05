5a7c9de5e20a9c86acd6e5c1d9c0ab7c467999cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:41 +0000

    Add package zhihu-wenda-xiang-ba-ziji-de-shuji-saomiao-cheng-pdf-shenmeyang-de-saomiaoyi-bijiao-heshi/2024.3.5.0/zhihu-wenda-xiang-ba-ziji-de-shuji-saomiao-cheng-pdf-shenmeyang-de-saomiaoyi-bijiao-heshi-2024.3.5.0.tar.gz

---
+++ packages/zhihu-wenda-xiang-ba-ziji-de-shuji-saomiao-cheng-pdf-shenmeyang-de-saomiaoyi-bijiao-heshi/zhihu-wenda-xiang-ba-ziji-de-shuji-saomiao-cheng-pdf-shenmeyang-de-saomiaoyi-bijiao-heshi-2024.3.5.0.tar.gz/zhihu-wenda-xiang-ba-ziji-de-shuji-saomiao-cheng-pdf-shenmeyang-de-saomiaoyi-bijiao-heshi-2024.3.5.0/ZhihuWendaXiangBaZijiDeShujiSaomiaoChengPdfShenmeyangDeSaomiaoyiBijiaoHeshi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhihu-wenda-xiang-ba-ziji-de-shuji-saomiao-cheng-pdf-shenmeyang-de-saomiaoyi-bijiao-heshi
+https://github.com/apachecn/zhihu-wenda-xiang-ba-ziji-de-shuji-saomiao-cheng-pdf-shenmeyang-de-saomiaoyi-bijiao-heshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
