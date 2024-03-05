af7b2934db1bfe7fc6e4bf5c33da4e998a20cc2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:51 +0000

    Add package handong-li-de-tanhuo-jiqi-zhixin-2018-gaofen-jiaocheng-heji/2024.3.4.0/handong_li_de_tanhuo_jiqi_zhixin_2018_gaofen_jiaocheng_heji-2024.3.4.0-py3-none-any.whl

---
+++ packages/handong-li-de-tanhuo-jiqi-zhixin-2018-gaofen-jiaocheng-heji/handong_li_de_tanhuo_jiqi_zhixin_2018_gaofen_jiaocheng_heji-2024.3.4.0-py3-none-any.whl/HandongLiDeTanhuoJiqiZhixin2018GaofenJiaochengHeji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""handong-li-de-tanhuo-jiqi-zhixin-2018-gaofen-jiaocheng-heji
+https://github.com/apachecn/handong-li-de-tanhuo-jiqi-zhixin-2018-gaofen-jiaocheng-heji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
