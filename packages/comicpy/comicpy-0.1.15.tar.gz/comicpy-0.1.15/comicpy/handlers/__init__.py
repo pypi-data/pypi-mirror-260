47fabb13ba595033ecb4001e9dc4fcbd059c3b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 15:04:25 +0000

    Add package comicpy/0.1.15/comicpy-0.1.15.tar.gz

---
+++ packages/comicpy/comicpy-0.1.15.tar.gz/comicpy-0.1.15/comicpy/handlers/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+handlers
+"""
+
+from comicpy.handlers.pdfhandler import PdfHandler
+from comicpy.handlers.ziphandler import ZipHandler
+from comicpy.handlers.rarhandler import RarHandler
+from comicpy.handlers.imageshandler import ImagesHandler
