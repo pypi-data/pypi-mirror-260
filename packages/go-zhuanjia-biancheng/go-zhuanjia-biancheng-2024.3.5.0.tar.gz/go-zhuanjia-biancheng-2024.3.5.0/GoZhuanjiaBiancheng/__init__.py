7bab4730c96560879b0a05ef663f89d889f4d341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:06:28 +0000

    Add package go-zhuanjia-biancheng/2024.3.5.0/go-zhuanjia-biancheng-2024.3.5.0.tar.gz

---
+++ packages/go-zhuanjia-biancheng/go-zhuanjia-biancheng-2024.3.5.0.tar.gz/go-zhuanjia-biancheng-2024.3.5.0/GoZhuanjiaBiancheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""go-zhuanjia-biancheng
+https://github.com/apachecn/go-zhuanjia-biancheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
