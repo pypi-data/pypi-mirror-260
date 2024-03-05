e5a442f870ee3ca9bb35023c9ca2513398a234ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:50 +0000

    Add package spring-data-jpa-2-1-5-release-zhongwen-wendang/2024.3.5.0/spring_data_jpa_2_1_5_release_zhongwen_wendang-2024.3.5.0-py3-none-any.whl

---
+++ packages/spring-data-jpa-2-1-5-release-zhongwen-wendang/spring_data_jpa_2_1_5_release_zhongwen_wendang-2024.3.5.0-py3-none-any.whl/SpringDataJpa215ReleaseZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""spring-data-jpa-2-1-5-release-zhongwen-wendang
+https://github.com/apachecn/spring-data-jpa-2-1-5-release-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
