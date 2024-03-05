808ff67f4eae149255c9ccf6634f330ef7a7d55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:50 +0000

    Add package niuke-wang-mingqi-bishi-mianshi-qiuzhi-gonglve-feijishu-pian-2019/2024.3.5.0/niuke-wang-mingqi-bishi-mianshi-qiuzhi-gonglve-feijishu-pian-2019-2024.3.5.0.tar.gz

---
+++ packages/niuke-wang-mingqi-bishi-mianshi-qiuzhi-gonglve-feijishu-pian-2019/niuke-wang-mingqi-bishi-mianshi-qiuzhi-gonglve-feijishu-pian-2019-2024.3.5.0.tar.gz/niuke-wang-mingqi-bishi-mianshi-qiuzhi-gonglve-feijishu-pian-2019-2024.3.5.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import NiukeWangMingqiBishiMianshiQiuzhiGonglveFeijishuPian2019
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('NiukeWangMingqiBishiMianshiQiuzhiGonglveFeijishuPian2019'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="niuke-wang-mingqi-bishi-mianshi-qiuzhi-gonglve-feijishu-pian-2019",
+    version=NiukeWangMingqiBishiMianshiQiuzhiGonglveFeijishuPian2019.__version__,
+    url="https://github.com/apachecn/niuke-wang-mingqi-bishi-mianshi-qiuzhi-gonglve-feijishu-pian-2019",
+    author=NiukeWangMingqiBishiMianshiQiuzhiGonglveFeijishuPian2019.__author__,
+    author_email=NiukeWangMingqiBishiMianshiQiuzhiGonglveFeijishuPian2019.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="牛客网名企笔试面试求职攻略非技术篇 2019",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "niuke-wang-mingqi-bishi-mianshi-qiuzhi-gonglve-feijishu-pian-2019=NiukeWangMingqiBishiMianshiQiuzhiGonglveFeijishuPian2019.__main__:main",
+            "NiukeWangMingqiBishiMianshiQiuzhiGonglveFeijishuPian2019=NiukeWangMingqiBishiMianshiQiuzhiGonglveFeijishuPian2019.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
