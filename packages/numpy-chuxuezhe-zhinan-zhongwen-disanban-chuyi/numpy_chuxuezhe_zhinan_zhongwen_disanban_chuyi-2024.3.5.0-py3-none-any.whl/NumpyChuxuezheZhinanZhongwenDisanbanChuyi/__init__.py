9d506573a7777180f30b49989bb92cc48b55fbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 15:04:27 +0000

    Add package numpy-chuxuezhe-zhinan-zhongwen-disanban-chuyi/2024.3.5.0/numpy_chuxuezhe_zhinan_zhongwen_disanban_chuyi-2024.3.5.0-py3-none-any.whl

---
+++ packages/numpy-chuxuezhe-zhinan-zhongwen-disanban-chuyi/numpy_chuxuezhe_zhinan_zhongwen_disanban_chuyi-2024.3.5.0-py3-none-any.whl/NumpyChuxuezheZhinanZhongwenDisanbanChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""numpy-chuxuezhe-zhinan-zhongwen-disanban-chuyi
+https://github.com/apachecn/numpy-chuxuezhe-zhinan-zhongwen-disanban-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
