be14becdff4cf258f3ad3fd9391c61de493d7a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:04:11 +0000

    Add package french-color-to-hex/0.1.1/french_color_to_hex-0.1.1.tar.gz

---
+++ packages/french-color-to-hex/french_color_to_hex-0.1.1.tar.gz/french_color_to_hex-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='french_color_to_hex',
+    version='0.1.1',
+    packages=find_packages(),
+    package_data={'french_color_to_hex': ['colors.json']},
+    install_requires=[
+        'unidecode'
+    ],
+)
