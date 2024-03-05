3620c27c2d61e958371d7ec10a5dda32da077b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:57 +0000

    Add package pyproject-maker/0.9.5/pyproject_maker-0.9.5.tar.gz

---
+++ packages/pyproject-maker/pyproject_maker-0.9.5.tar.gz/pyproject_maker-0.9.5/py_maker/commands/config.py
@@ -0,0 +1,36 @@
+"""Show or change the Configuration."""
+
+import typer
+
+from py_maker.config import settings
+from py_maker.helpers import header, show_table
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def show() -> None:
+    """Show the current settings from the Configuration file."""
+    header()
+    show_table(settings.get_attrs())
+
+
+@app.command()
+def change() -> None:
+    """Change the current configuration."""
+    header()
+    settings.change_settings()
+
+
+@app.command()
+def token() -> None:
+    """Change the current token."""
+    header()
+    settings.change_token()
+
+
+@app.command(name="edit")
+def edit_config() -> None:
+    """Open the Configuration file in the default editor."""
+    header()
+    settings.edit_config()
