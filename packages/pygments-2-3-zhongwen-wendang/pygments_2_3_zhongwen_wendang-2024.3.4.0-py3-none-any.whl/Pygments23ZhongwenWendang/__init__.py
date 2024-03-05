14f6b65b9930f868f781c541fca54c79d248afce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:04:05 +0000

    Add package pygments-2-3-zhongwen-wendang/2024.3.4.0/pygments_2_3_zhongwen_wendang-2024.3.4.0-py3-none-any.whl

---
+++ packages/pygments-2-3-zhongwen-wendang/pygments_2_3_zhongwen_wendang-2024.3.4.0-py3-none-any.whl/Pygments23ZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pygments-2-3-zhongwen-wendang
+https://github.com/apachecn/pygments-2-3-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
