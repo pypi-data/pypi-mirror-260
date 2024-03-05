f88974279c6a02355e448e81de8693c6b10be98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:18 +0000

    Add package maxima-jieshao-li-dongfeng/2024.3.5.0/maxima_jieshao_li_dongfeng-2024.3.5.0-py3-none-any.whl

---
+++ packages/maxima-jieshao-li-dongfeng/maxima_jieshao_li_dongfeng-2024.3.5.0-py3-none-any.whl/MaximaJieshaoLiDongfeng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""maxima-jieshao-li-dongfeng
+https://github.com/apachecn/maxima-jieshao-li-dongfeng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
