f07b1406ba2220158cffc2128f6afb0fc90a2cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:05:48 +0000

    Add package aaai-2019-notes/2024.3.4.0/aaai_2019_notes-2024.3.4.0-py3-none-any.whl

---
+++ packages/aaai-2019-notes/aaai_2019_notes-2024.3.4.0-py3-none-any.whl/Aaai2019Notes/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aaai-2019-notes
+https://github.com/apachecn/aaai-2019-notes"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
