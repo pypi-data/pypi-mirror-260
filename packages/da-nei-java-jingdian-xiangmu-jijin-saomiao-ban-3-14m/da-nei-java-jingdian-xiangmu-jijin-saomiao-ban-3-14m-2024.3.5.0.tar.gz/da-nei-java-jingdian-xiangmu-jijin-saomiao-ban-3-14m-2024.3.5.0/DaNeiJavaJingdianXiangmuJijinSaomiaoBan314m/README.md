a31b5d0fca05098f4da82fa2bff7c9731cff0dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:05:13 +0000

    Add package da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m/2024.3.5.0/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m-2024.3.5.0.tar.gz

---
+++ packages/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m-2024.3.5.0.tar.gz/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m-2024.3.5.0/DaNeiJavaJingdianXiangmuJijinSaomiaoBan314m/README.md
@@ -0,0 +1,27 @@
+# 达内 Java 经典项目集锦_扫描版_3.14M
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m
+docker run -tid -p <port>:80 apachecn0/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m
+da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-yisim
+da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-yisim <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m-2024.3.5.0.tar.gz/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m-2024.3.5.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import DaNeiJavaJingdianXiangmuJijinSaomiaoBan314m
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('DaNeiJavaJingdianXiangmuJijinSaomiaoBan314m'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m",
+    version=DaNeiJavaJingdianXiangmuJijinSaomiaoBan314m.__version__,
+    url="https://github.com/apachecn/da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m",
+    author=DaNeiJavaJingdianXiangmuJijinSaomiaoBan314m.__author__,
+    author_email=DaNeiJavaJingdianXiangmuJijinSaomiaoBan314m.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="达内 Java 经典项目集锦_扫描版_3.14M",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "da-nei-java-jingdian-xiangmu-jijin-saomiao-ban-3-14m=DaNeiJavaJingdianXiangmuJijinSaomiaoBan314m.__main__:main",
+            "DaNeiJavaJingdianXiangmuJijinSaomiaoBan314m=DaNeiJavaJingdianXiangmuJijinSaomiaoBan314m.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
