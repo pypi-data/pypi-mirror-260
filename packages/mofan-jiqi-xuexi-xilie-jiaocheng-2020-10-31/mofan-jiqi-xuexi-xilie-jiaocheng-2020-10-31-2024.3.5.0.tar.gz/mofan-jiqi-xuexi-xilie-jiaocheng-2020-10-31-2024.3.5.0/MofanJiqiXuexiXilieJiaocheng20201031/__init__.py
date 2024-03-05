207a7bd90f604666354249a8c7a6fa93aa3867f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:36 +0000

    Add package mofan-jiqi-xuexi-xilie-jiaocheng-2020-10-31/2024.3.5.0/mofan-jiqi-xuexi-xilie-jiaocheng-2020-10-31-2024.3.5.0.tar.gz

---
+++ packages/mofan-jiqi-xuexi-xilie-jiaocheng-2020-10-31/mofan-jiqi-xuexi-xilie-jiaocheng-2020-10-31-2024.3.5.0.tar.gz/mofan-jiqi-xuexi-xilie-jiaocheng-2020-10-31-2024.3.5.0/MofanJiqiXuexiXilieJiaocheng20201031/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mofan-jiqi-xuexi-xilie-jiaocheng-2020-10-31
+https://github.com/apachecn/mofan-jiqi-xuexi-xilie-jiaocheng-2020-10-31"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
