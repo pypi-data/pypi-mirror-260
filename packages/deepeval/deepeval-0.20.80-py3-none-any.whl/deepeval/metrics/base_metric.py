22d3441797857cd76de30e3332265e2313d25ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:10 +0000

    Add package deepeval/0.20.80/deepeval-0.20.80-py3-none-any.whl

---
+++ packages/deepeval/deepeval-0.20.80-py3-none-any.whl/deepeval/metrics/base_metric.py
@@ -0,0 +1,60 @@
+from abc import abstractmethod
+
+from deepeval.test_case import LLMTestCase, ConversationalTestCase
+from typing import Optional, Dict
+
+
+class BaseMetric:
+    score: float = 0
+    score_breakdown: Dict = None
+    reason: Optional[str] = None
+    evaluation_model: Optional[str] = None
+
+    @property
+    def threshold(self) -> float:
+        return self._threshold
+
+    @threshold.setter
+    def threshold(self, value: float):
+        self._threshold = value
+
+    @abstractmethod
+    def measure(self, test_case: LLMTestCase, *args, **kwargs) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_successful(self) -> bool:
+        raise NotImplementedError
+
+    @property
+    def __name__(self):
+        return "Base Metric"
+
+
+class BaseConversationalMetric:
+    score: float = 0
+    score_breakdown: Dict = None
+    reason: Optional[str] = None
+    evaluation_model: Optional[str] = None
+
+    @property
+    def threshold(self) -> float:
+        return self._threshold
+
+    @threshold.setter
+    def threshold(self, value: float):
+        self._threshold = value
+
+    @abstractmethod
+    def measure(
+        self, test_case: ConversationalTestCase, *args, **kwargs
+    ) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_successful(self) -> bool:
+        raise NotImplementedError
+
+    @property
+    def __name__(self):
+        return "Base Conversational Metric"
