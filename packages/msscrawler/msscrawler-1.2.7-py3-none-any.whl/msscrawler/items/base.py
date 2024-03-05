db119918353d25ec0646f15392a01b81117b9cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:05:10 +0000

    Add package msscrawler/1.2.7/msscrawler-1.2.7-py3-none-any.whl

---
+++ packages/msscrawler/msscrawler-1.2.7-py3-none-any.whl/msscrawler/items/base.py
@@ -0,0 +1,9 @@
+import scrapy
+
+
+class BaseItem(scrapy.Item):
+    url = scrapy.Field()
+    brand = scrapy.Field()
+    status = scrapy.Field()
+    status_code = scrapy.Field()
+    last_crawl = scrapy.Field()
