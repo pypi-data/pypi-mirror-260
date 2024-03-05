4cdbd3edfd70607413a3c28cb8c047662754d5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:06:12 +0000

    Add package pydantic2avro/0.2.0/pydantic2avro-0.2.0.tar.gz

---
+++ packages/pydantic2avro/pydantic2avro-0.2.0.tar.gz/pydantic2avro-0.2.0/src/pydantic2avro/exceptions.py
@@ -0,0 +1,14 @@
+class NotAPydanticModelException(Exception):
+    pass
+
+class NotAnAvroPrimitiveDataTypeException(Exception):
+    pass
+
+class NotAnAvroLogicalDataTypeException(Exception):
+    pass
+
+class UnsupportedTypeException(Exception):
+    pass
+
+class InvalidEnumMemeberException(Exception):
+    pass
