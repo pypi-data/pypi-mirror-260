b3b6526aa54dbb28b6d1eeb0290d461ad056b28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:50 +0000

    Add package usf-msds501-jisuan-shuju-kexue-zhongwen-jiangyi/2024.3.5.0/usf_msds501_jisuan_shuju_kexue_zhongwen_jiangyi-2024.3.5.0-py3-none-any.whl

---
+++ packages/usf-msds501-jisuan-shuju-kexue-zhongwen-jiangyi/usf_msds501_jisuan_shuju_kexue_zhongwen_jiangyi-2024.3.5.0-py3-none-any.whl/UsfMsds501JisuanShujuKexueZhongwenJiangyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""usf-msds501-jisuan-shuju-kexue-zhongwen-jiangyi
+https://github.com/apachecn/usf-msds501-jisuan-shuju-kexue-zhongwen-jiangyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
