e67d330f25b5ba67c6bd47a2c772b7a2ae0f55bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:04:21 +0000

    Add package lgtmeow/0.4.0/lgtmeow-0.4.0.tar.gz

---
+++ packages/lgtmeow/lgtmeow-0.4.0.tar.gz/lgtmeow-0.4.0/src/kitchen/recommands.rs
@@ -0,0 +1,35 @@
+/// A list of recommended combinations of 🐾
+#[cfg(feature = "emoji-paw-prints")]
+pub static RECOMMAND_EMOJI_CODEPOINTS_COMBINATE_WITH_PAW_PRINTS: &[&str] = &[
+    "2601-fe0f",  // ☁️
+    "1f495",      // ❤️
+    "2b50",       // ⭐
+    "1f30a",      // 🌊
+    "1f31f",      // 🌟
+    "1f4ae",      // 🌸
+    "1f381",      // 🎁
+    "1f386",      // 🎆
+    "1f38a",      // 🎊
+    "1f3b0",      // 🎰
+    "1f44d",      // 👍
+    "1f495",      // 💕
+    "1f493",      // 💓
+    "1f498",      // 💘
+    "1f4a1",      // 💡
+    "1f4a5",      // 💥
+    "1f4df",      // 📟
+    "1f4f0",      // 📰
+    "1f525",      // 🔥
+    "1f52e",      // 🔮
+    "1f5ef-fe0f", // 🗯️
+    "1f6f8",      // 🛸
+    "1f947",      // 🥇
+    "1f9ca",      // 🧊
+    "1faa4",      // 🪤
+];
+
+/// A list of recommended combinations of 🐱
+#[cfg(feature = "emoji-cat")]
+pub static RECOMMAND_EMOJI_CODEPOINTS_COMBINATE_WITH_CAT: &[&str] = &[
+    "1f4df", // 📟
+];
