765b7bb06941fe815a5e37ef79c9f480d116d585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:52 +0000

    Add package ershisi-xiaoshi-wanzhuan-bite-bi-qianbao-kaifa/2024.3.4.0/ershisi_xiaoshi_wanzhuan_bite_bi_qianbao_kaifa-2024.3.4.0-py3-none-any.whl

---
+++ packages/ershisi-xiaoshi-wanzhuan-bite-bi-qianbao-kaifa/ershisi_xiaoshi_wanzhuan_bite_bi_qianbao_kaifa-2024.3.4.0-py3-none-any.whl/ErshisiXiaoshiWanzhuanBiteBiQianbaoKaifa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ershisi-xiaoshi-wanzhuan-bite-bi-qianbao-kaifa
+https://github.com/apachecn/ershisi-xiaoshi-wanzhuan-bite-bi-qianbao-kaifa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
