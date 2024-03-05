7faa89ee9f09a439a0662d1dd5663c78e402055d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:05:07 +0000

    Add package vocaloid3-chuji-shiyong-jiaocheng-acgmiku/2024.3.5.0/vocaloid3-chuji-shiyong-jiaocheng-acgmiku-2024.3.5.0.tar.gz

---
+++ packages/vocaloid3-chuji-shiyong-jiaocheng-acgmiku/vocaloid3-chuji-shiyong-jiaocheng-acgmiku-2024.3.5.0.tar.gz/vocaloid3-chuji-shiyong-jiaocheng-acgmiku-2024.3.5.0/Vocaloid3ChujiShiyongJiaochengAcgmiku/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""vocaloid3-chuji-shiyong-jiaocheng-acgmiku
+https://github.com/apachecn/vocaloid3-chuji-shiyong-jiaocheng-acgmiku"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
