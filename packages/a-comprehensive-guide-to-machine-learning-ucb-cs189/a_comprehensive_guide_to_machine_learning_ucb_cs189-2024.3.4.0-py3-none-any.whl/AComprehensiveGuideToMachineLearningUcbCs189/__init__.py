7b35f1af7b4c837a81c3b38941b20f702159c80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:05:46 +0000

    Add package a-comprehensive-guide-to-machine-learning-ucb-cs189/2024.3.4.0/a_comprehensive_guide_to_machine_learning_ucb_cs189-2024.3.4.0-py3-none-any.whl

---
+++ packages/a-comprehensive-guide-to-machine-learning-ucb-cs189/a_comprehensive_guide_to_machine_learning_ucb_cs189-2024.3.4.0-py3-none-any.whl/AComprehensiveGuideToMachineLearningUcbCs189/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""a-comprehensive-guide-to-machine-learning-ucb-cs189
+https://github.com/apachecn/a-comprehensive-guide-to-machine-learning-ucb-cs189"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
