bd93b3d73a23c8504ef6b0dea6a117afe5fe48e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:05:52 +0000

    Add package apache-freemarker-2-3-28-zhongwen-wendang/2024.3.4.0/apache_freemarker_2_3_28_zhongwen_wendang-2024.3.4.0-py3-none-any.whl

---
+++ packages/apache-freemarker-2-3-28-zhongwen-wendang/apache_freemarker_2_3_28_zhongwen_wendang-2024.3.4.0-py3-none-any.whl/ApacheFreemarker2328ZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apache-freemarker-2-3-28-zhongwen-wendang
+https://github.com/apachecn/apache-freemarker-2-3-28-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
