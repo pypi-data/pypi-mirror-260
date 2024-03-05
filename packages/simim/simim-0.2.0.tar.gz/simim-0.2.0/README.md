20fff7a0daa5883eb71a42f124a0d2e6c22feaf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:05:07 +0000

    Add package simim/0.2.0/simim-0.2.0.tar.gz

---
+++ packages/simim/simim-0.2.0.tar.gz/simim-0.2.0/README.md
@@ -0,0 +1,15 @@
+SimIM
+=====
+
+SimIM is a tool for simulating the extra-galactic sky in radio, sub-mm and
+far-infrared. Full documentation is available via [Read the
+Docs](https://simim.readthedocs.io). Source code is avialable via
+[GitHub](https://github.com/rpkeenan/simim_public).
+
+This is Version 0.2, which includes utilities for downloading and formatting
+simulation data, adding new properties such as spectral line emission to
+simulations, visualizing basic data products, constructing light cones, and
+making pixelized maps or data cubes. A future release will add more detailed
+documentation with examples of all functionality and relevant scientific
+applications. A module for creating mock timestreams is currently in
+development.
---
+++ packages/simim/simim-0.2.0.tar.gz/simim-0.2.0/tests/test_imports.py
@@ -0,0 +1,44 @@
+def test_import_simim():
+    """Make sure the expected files are imported - simim main"""
+
+    import simim
+
+    resources = dir(simim)
+    for check_resource in ['setupsimim','siminterface','galprops','map']:
+        assert check_resource in resources
+
+def test_import_siminterface():
+    """Make sure the expected files are imported - simim.siminterface"""
+
+    import simim.siminterface
+
+    resources = dir(simim.siminterface)
+    for check_resource in ['IllustrisCatalogs','UniversemachineCatalogs','SnapHandler','SimHandler']:
+        assert check_resource in resources
+
+def test_import_galprops():
+    """Make sure the expected files are imported - simim.galprops"""
+
+    import simim.galprops
+
+    resources = dir(simim.galprops)
+    for check_resource in ['Prop','MultiProp','prop_behroozi_sfr','prop_li_co']:
+        assert check_resource in resources
+
+def test_import_map():
+    """Make sure the expected files are imported - simim.map"""
+
+    import simim.map
+
+    resources = dir(simim.map)
+    for check_resource in ['Grid','Gridder','LoadGrid','GridFromAxes','GridFromAxesAndFunction','PSF','SpectralPSF','gridder_function',]:
+        assert check_resource in resources
+
+def test_import_map():
+    """Make sure the expected files are imported - simim.lightcone"""
+
+    import simim.lightcone
+
+    resources = dir(simim.lightcone)
+    for check_resource in ['LCMaker','LCHandler']:
+        assert check_resource in resources
