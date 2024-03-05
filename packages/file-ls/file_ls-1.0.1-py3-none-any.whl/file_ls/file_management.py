a5be16bbd94dd26acbc24f545a0cb462e9296d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 15:04:21 +0000

    Add package file-ls/1.0.1/file_ls-1.0.1-py3-none-any.whl

---
+++ packages/file-ls/file_ls-1.0.1-py3-none-any.whl/file_ls/file_management.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from natsort import ns, natsorted
+import os
+
+#查找文件夹下所有文件路径及文件名称
+def ls(path,isrfp=False):
+    """
+    :param path:文件夹路径
+    :param isrfp:是否返回文件名，默认为False,只返回文件路径，True返回文件名称
+    """
+    file_list = os.listdir(path)
+    file_list = natsorted(file_list, alg=ns.PATH)
+    if isrfp:
+        return [(file, os.path.join(path, file)) for file in file_list]
+    return [(os.path.join(path, file)) for file in file_list]
