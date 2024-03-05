16dafb89e9680c7ad91a4215e898af2db7d90697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:28 +0000

    Add package iiyo/1.0.6/iiyo-1.0.6-py3-none-any.whl

---
+++ packages/iiyo/iiyo-1.0.6-py3-none-any.whl/iiyo/random_names/home_address.py
@@ -0,0 +1,29 @@
+sadasiri Niwasa 12/A
+word Place 401 2/A
+Amal Niwasa 1st Lane
+27 Z/1 New Road
+Wewa Rauma para 
+Lake Road 4/5
+Main Road
+Cross Road
+Temple Road
+28/1 Pansala Para
+School Road
+Star Apatment
+Sir Chitthampalam A Gardiner Mawatha
+711-2880 Nulla St.
+606-3727 Ullamcorper. Street
+Ap 867-859 Sit Rd.
+San Antonio MI 47096
+Ap 651-8679 Sodales Av.
+P.O. Box 929 4189 Nunc Road
+Erie Rhode Island 24975
+778-9383 Suspendisse Av.
+P.O. Box 147 2546 Sociosqu Rd.
+
+
+
+
+
+
+
