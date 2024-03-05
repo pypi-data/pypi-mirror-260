bfb5ae3b72406c2d8b37e17bb0ee8ee54f0dd336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:11 +0000

    Add package datawhale-ling-jichu-rumen-shujuwajue/2024.3.5.0/datawhale_ling_jichu_rumen_shujuwajue-2024.3.5.0-py3-none-any.whl

---
+++ packages/datawhale-ling-jichu-rumen-shujuwajue/datawhale_ling_jichu_rumen_shujuwajue-2024.3.5.0-py3-none-any.whl/DatawhaleLingJichuRumenShujuwajue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""datawhale-ling-jichu-rumen-shujuwajue
+https://github.com/apachecn/datawhale-ling-jichu-rumen-shujuwajue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
