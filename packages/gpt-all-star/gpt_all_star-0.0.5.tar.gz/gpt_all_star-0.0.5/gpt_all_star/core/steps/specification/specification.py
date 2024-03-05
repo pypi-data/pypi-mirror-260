2d2bf91814d5c634d0f379c4e4afcd114a29cec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:10 +0000

    Add package gpt-all-star/0.0.5/gpt_all_star-0.0.5.tar.gz

---
+++ packages/gpt-all-star/gpt_all_star-0.0.5.tar.gz/gpt_all_star-0.0.5/gpt_all_star/core/steps/specification/specification.py
@@ -0,0 +1,40 @@
+from gpt_all_star.core.agents.copilot import Copilot
+from gpt_all_star.core.steps.specification.additional_tasks import (
+    create_additional_tasks,
+)
+from gpt_all_star.core.steps.step import Step
+
+
+class Specification(Step):
+    def __init__(
+        self,
+        copilot: Copilot,
+    ) -> None:
+        super().__init__(copilot)
+        self.working_directory = self.copilot.storages.docs.path.absolute()
+
+    def planning_prompt(self) -> str:
+        return ""
+
+    def additional_tasks(self) -> list:
+        instructions = self.copilot.get_instructions()
+        app_type = self.copilot.get_app_type()
+        self.copilot.state("Ok, we have a instruction and app type now!")
+        self.copilot.state(
+            f"""
+---
+instruction:
+{instructions}
+app_type:
+{app_type}
+---
+""",
+        )
+        return create_additional_tasks(app_type, instructions)
+
+    def callback(self) -> bool:
+        specifications = self.copilot.storages.docs.get("specifications.md")
+        has_specifications = bool(specifications)
+        if has_specifications:
+            self.copilot.output_md(specifications)
+        return has_specifications
---
+++ packages/gpt-all-star/gpt_all_star-0.0.5.tar.gz/gpt_all_star-0.0.5/gpt_all_star/core/steps/step.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+from gpt_all_star.core.agents.copilot import Copilot
+
+
+class Step(ABC):
+    def __init__(
+        self,
+        copilot: Copilot,
+    ) -> None:
+        self.copilot = copilot
+        self.copilot.console.section(f"STEP: {self.__class__.__name__}")
+        self.working_directory = self.copilot.storages.root.path.absolute()
+        self.plan_and_solve = False
+        self.exclude_dirs = [".archive", "node_modules", "build"]
+
+    @abstractmethod
+    def planning_prompt(self) -> str:
+        pass
+
+    @abstractmethod
+    def additional_tasks(self) -> list:
+        pass
+
+    @abstractmethod
+    def callback(self) -> bool:
+        pass
