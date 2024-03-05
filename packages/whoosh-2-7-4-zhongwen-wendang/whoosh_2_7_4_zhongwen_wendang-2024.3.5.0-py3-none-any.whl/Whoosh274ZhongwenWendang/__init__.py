e2f26c856d2618991ad7e627ba2f5d500d60694a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:08 +0000

    Add package whoosh-2-7-4-zhongwen-wendang/2024.3.5.0/whoosh_2_7_4_zhongwen_wendang-2024.3.5.0-py3-none-any.whl

---
+++ packages/whoosh-2-7-4-zhongwen-wendang/whoosh_2_7_4_zhongwen_wendang-2024.3.5.0-py3-none-any.whl/Whoosh274ZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""whoosh-2-7-4-zhongwen-wendang
+https://github.com/apachecn/whoosh-2-7-4-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
