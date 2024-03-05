861c00adc5cedb0eef9f0c343a31886ab978a7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:04:12 +0000

    Add package html-rumenjiaocheng-c-yuyan-biancheng-wang/2024.3.4.0/html_rumenjiaocheng_c_yuyan_biancheng_wang-2024.3.4.0-py3-none-any.whl

---
+++ packages/html-rumenjiaocheng-c-yuyan-biancheng-wang/html_rumenjiaocheng_c_yuyan_biancheng_wang-2024.3.4.0-py3-none-any.whl/HtmlRumenjiaochengCYuyanBianchengWang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""html-rumenjiaocheng-c-yuyan-biancheng-wang
+https://github.com/apachecn/html-rumenjiaocheng-c-yuyan-biancheng-wang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
