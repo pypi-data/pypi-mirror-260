7bc898dbac25c2c7b385833a8f907cdd281a40c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:44 +0000

    Add package bu-cha-dian-de-jisuanjikexue/2024.3.5.0/bu_cha_dian_de_jisuanjikexue-2024.3.5.0-py3-none-any.whl

---
+++ packages/bu-cha-dian-de-jisuanjikexue/bu_cha_dian_de_jisuanjikexue-2024.3.5.0-py3-none-any.whl/BuChaDianDeJisuanjikexue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bu-cha-dian-de-jisuanjikexue
+https://github.com/apachecn/bu-cha-dian-de-jisuanjikexue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
