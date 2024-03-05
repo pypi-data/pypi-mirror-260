9e596abcc911fad8b77d296112359782fcd7192d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:19 +0000

    Add package jingtong-jiqi-xuexi-matlab-fenbu-shishi-zhinan/2024.3.4.0/jingtong-jiqi-xuexi-matlab-fenbu-shishi-zhinan-2024.3.4.0.tar.gz

---
+++ packages/jingtong-jiqi-xuexi-matlab-fenbu-shishi-zhinan/jingtong-jiqi-xuexi-matlab-fenbu-shishi-zhinan-2024.3.4.0.tar.gz/jingtong-jiqi-xuexi-matlab-fenbu-shishi-zhinan-2024.3.4.0/JingtongJiqiXuexiMatlabFenbuShishiZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingtong-jiqi-xuexi-matlab-fenbu-shishi-zhinan
+https://github.com/apachecn/jingtong-jiqi-xuexi-matlab-fenbu-shishi-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
