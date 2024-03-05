d6c07dbb901a736d08718d777986f7bcaf61df81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:29 +0000

    Add package jiagou-zhengji-zhidao/2024.3.4.0/jiagou-zhengji-zhidao-2024.3.4.0.tar.gz

---
+++ packages/jiagou-zhengji-zhidao/jiagou-zhengji-zhidao-2024.3.4.0.tar.gz/jiagou-zhengji-zhidao-2024.3.4.0/JiagouZhengjiZhidao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiagou-zhengji-zhidao
+https://github.com/apachecn/jiagou-zhengji-zhidao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
