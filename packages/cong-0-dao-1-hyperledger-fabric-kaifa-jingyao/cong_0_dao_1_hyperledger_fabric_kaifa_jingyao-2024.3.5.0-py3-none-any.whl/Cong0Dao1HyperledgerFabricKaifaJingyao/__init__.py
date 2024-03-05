3bf3a434ada5fe07d78a69981b71baa1e436b7c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:50 +0000

    Add package cong-0-dao-1-hyperledger-fabric-kaifa-jingyao/2024.3.5.0/cong_0_dao_1_hyperledger_fabric_kaifa_jingyao-2024.3.5.0-py3-none-any.whl

---
+++ packages/cong-0-dao-1-hyperledger-fabric-kaifa-jingyao/cong_0_dao_1_hyperledger_fabric_kaifa_jingyao-2024.3.5.0-py3-none-any.whl/Cong0Dao1HyperledgerFabricKaifaJingyao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""cong-0-dao-1-hyperledger-fabric-kaifa-jingyao
+https://github.com/apachecn/cong-0-dao-1-hyperledger-fabric-kaifa-jingyao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
