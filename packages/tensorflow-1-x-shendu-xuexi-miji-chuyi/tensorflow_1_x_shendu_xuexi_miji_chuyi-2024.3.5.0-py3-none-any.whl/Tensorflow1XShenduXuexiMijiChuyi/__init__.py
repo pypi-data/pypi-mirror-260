6612be3dabc598c2a720ef7c0947952b40a950ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:04:32 +0000

    Add package tensorflow-1-x-shendu-xuexi-miji-chuyi/2024.3.5.0/tensorflow_1_x_shendu_xuexi_miji_chuyi-2024.3.5.0-py3-none-any.whl

---
+++ packages/tensorflow-1-x-shendu-xuexi-miji-chuyi/tensorflow_1_x_shendu_xuexi_miji_chuyi-2024.3.5.0-py3-none-any.whl/Tensorflow1XShenduXuexiMijiChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tensorflow-1-x-shendu-xuexi-miji-chuyi
+https://github.com/apachecn/tensorflow-1-x-shendu-xuexi-miji-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
