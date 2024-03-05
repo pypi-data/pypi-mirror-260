02c4f0df689e981355823b96289444fbc14d2b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:48 +0000

    Add package tessarinified/0.1.3/tessarinified-0.1.3.tar.gz

---
+++ packages/tessarinified/tessarinified-0.1.3.tar.gz/tessarinified-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+import os
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+version = '0.1.3'
+
+test = False
+
+setup(
+    name='tessarinified',
+    packages=find_packages(include=['tessarinified']),
+    version=version,
+    description='n-complex numbers (Tessarines) in Python',
+    author='corruptconverter, slycedf, goblinovermind, jerridium',
+    install_requires=['numpy'],
+    license='MIT',
+    long_description=read('README.md'),
+    classifiers=["Development Status :: 3 - Alpha"]
+)
+
+token = open(f'D:/slycefolder/ins/tsr/{ {True: "tt", False: "tr"}[test] }', 'r').read()
+
+os.system(f'twine upload --repository { {True: "testpypi", False: "pypi"}[test] } dist/*{version}* -u __token__ -p {token} --verbose')
---
+++ packages/tessarinified/tessarinified-0.1.3.tar.gz/tessarinified-0.1.3/test/test.py
@@ -0,0 +1,5 @@
+from tessarinified import *
+
+a = 41
+
+print(Tsr(5+3j).native())
