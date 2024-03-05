d23f5b3dcb95e70adf97ea369d201d0db75553be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:17 +0000

    Add package keras-tutorials-tgjeon/2024.3.5.0/keras_tutorials_tgjeon-2024.3.5.0-py3-none-any.whl

---
+++ packages/keras-tutorials-tgjeon/keras_tutorials_tgjeon-2024.3.5.0-py3-none-any.whl/KerasTutorialsTgjeon/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""keras-tutorials-tgjeon
+https://github.com/apachecn/keras-tutorials-tgjeon"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
