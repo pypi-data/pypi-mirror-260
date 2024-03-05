13f6148592c71899b118e2f8c4d49eb37f41630c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:06:46 +0000

    Add package da-nei-ajax-he-jquery-saomiao-ban-2-11m/2024.3.5.0/da_nei_ajax_he_jquery_saomiao_ban_2_11m-2024.3.5.0-py3-none-any.whl

---
+++ packages/da-nei-ajax-he-jquery-saomiao-ban-2-11m/da_nei_ajax_he_jquery_saomiao_ban_2_11m-2024.3.5.0-py3-none-any.whl/DaNeiAjaxHeJquerySaomiaoBan211m/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""da-nei-ajax-he-jquery-saomiao-ban-2-11m
+https://github.com/apachecn/da-nei-ajax-he-jquery-saomiao-ban-2-11m"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
