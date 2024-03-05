8b7ff14ba8d372cf4332196e696ddd9fb1bed631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:17 +0000

    Add package jiqi-zhixin-2019-gaofen-jiaocheng-rumen-jiehuo/2024.3.5.0/jiqi-zhixin-2019-gaofen-jiaocheng-rumen-jiehuo-2024.3.5.0.tar.gz

---
+++ packages/jiqi-zhixin-2019-gaofen-jiaocheng-rumen-jiehuo/jiqi-zhixin-2019-gaofen-jiaocheng-rumen-jiehuo-2024.3.5.0.tar.gz/jiqi-zhixin-2019-gaofen-jiaocheng-rumen-jiehuo-2024.3.5.0/JiqiZhixin2019GaofenJiaochengRumenJiehuo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiqi-zhixin-2019-gaofen-jiaocheng-rumen-jiehuo
+https://github.com/apachecn/jiqi-zhixin-2019-gaofen-jiaocheng-rumen-jiehuo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
