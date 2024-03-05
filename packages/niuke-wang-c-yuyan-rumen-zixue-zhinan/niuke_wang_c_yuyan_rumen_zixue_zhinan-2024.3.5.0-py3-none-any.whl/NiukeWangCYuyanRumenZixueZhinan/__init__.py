38287496fbb4e3c52da3d5463eb23625be0e878f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:45 +0000

    Add package niuke-wang-c-yuyan-rumen-zixue-zhinan/2024.3.5.0/niuke_wang_c_yuyan_rumen_zixue_zhinan-2024.3.5.0-py3-none-any.whl

---
+++ packages/niuke-wang-c-yuyan-rumen-zixue-zhinan/niuke_wang_c_yuyan_rumen_zixue_zhinan-2024.3.5.0-py3-none-any.whl/NiukeWangCYuyanRumenZixueZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""niuke-wang-c-yuyan-rumen-zixue-zhinan
+https://github.com/apachecn/niuke-wang-c-yuyan-rumen-zixue-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
