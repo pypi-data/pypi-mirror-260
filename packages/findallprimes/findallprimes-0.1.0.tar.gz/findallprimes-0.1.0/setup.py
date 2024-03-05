0407563739c98171589ce143a7bcc23dfeeec121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:04:41 +0000

    Add package findallprimes/0.1.0/findallprimes-0.1.0.tar.gz

---
+++ packages/findallprimes/findallprimes-0.1.0.tar.gz/findallprimes-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+name='findallprimes',
+version='0.1.0',
+author='ARMAN',
+description='This package finds all prime numbers from 2 to that number ',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
