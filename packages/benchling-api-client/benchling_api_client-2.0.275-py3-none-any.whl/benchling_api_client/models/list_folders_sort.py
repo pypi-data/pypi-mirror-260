3926961996f7a3e7d5a8ceed5c45e34955320717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:04:44 +0000

    Add package benchling-api-client/2.0.275/benchling_api_client-2.0.275-py3-none-any.whl

---
+++ packages/benchling-api-client/benchling_api_client-2.0.275-py3-none-any.whl/benchling_api_client/models/list_folders_sort.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from functools import lru_cache
+from typing import cast
+
+from ..extensions import Enums
+
+
+class ListFoldersSort(Enums.KnownString):
+    MODIFIEDAT = "modifiedAt"
+    NAME = "name"
+    MODIFIEDATASC = "modifiedAt:asc"
+    NAMEASC = "name:asc"
+    MODIFIEDATDESC = "modifiedAt:desc"
+    NAMEDESC = "name:desc"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def of_unknown(val: str) -> "ListFoldersSort":
+        if not isinstance(val, str):
+            raise ValueError(f"Value of ListFoldersSort must be a string (encountered: {val})")
+        newcls = Enum("ListFoldersSort", {"_UNKNOWN": val}, type=Enums.UnknownString)  # type: ignore
+        return cast(ListFoldersSort, getattr(newcls, "_UNKNOWN"))
