518a25626b47e1ce40a1cbdc41e19647069240c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:26 +0000

    Add package zhihu-wenda-qinshou-ba-yijia-gongsi-nongkua-shi-zenyang-de-ganjue/2024.3.5.0/zhihu_wenda_qinshou_ba_yijia_gongsi_nongkua_shi_zenyang_de_ganjue-2024.3.5.0-py3-none-any.whl

---
+++ packages/zhihu-wenda-qinshou-ba-yijia-gongsi-nongkua-shi-zenyang-de-ganjue/zhihu_wenda_qinshou_ba_yijia_gongsi_nongkua_shi_zenyang_de_ganjue-2024.3.5.0-py3-none-any.whl/ZhihuWendaQinshouBaYijiaGongsiNongkuaShiZenyangDeGanjue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhihu-wenda-qinshou-ba-yijia-gongsi-nongkua-shi-zenyang-de-ganjue
+https://github.com/apachecn/zhihu-wenda-qinshou-ba-yijia-gongsi-nongkua-shi-zenyang-de-ganjue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
