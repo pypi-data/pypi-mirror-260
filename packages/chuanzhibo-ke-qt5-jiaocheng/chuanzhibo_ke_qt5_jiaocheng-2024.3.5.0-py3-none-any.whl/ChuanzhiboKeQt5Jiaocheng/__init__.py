4c3e6542015fd07a375ff2a7fcdb96f20c7d8f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:51 +0000

    Add package chuanzhibo-ke-qt5-jiaocheng/2024.3.5.0/chuanzhibo_ke_qt5_jiaocheng-2024.3.5.0-py3-none-any.whl

---
+++ packages/chuanzhibo-ke-qt5-jiaocheng/chuanzhibo_ke_qt5_jiaocheng-2024.3.5.0-py3-none-any.whl/ChuanzhiboKeQt5Jiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chuanzhibo-ke-qt5-jiaocheng
+https://github.com/apachecn/chuanzhibo-ke-qt5-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
