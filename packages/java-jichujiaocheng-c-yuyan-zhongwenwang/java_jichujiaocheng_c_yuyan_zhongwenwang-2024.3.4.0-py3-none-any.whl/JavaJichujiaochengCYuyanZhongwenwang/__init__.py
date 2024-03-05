6216cb69b44b6e4bf50135280dbb9c1f46594f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:05:14 +0000

    Add package java-jichujiaocheng-c-yuyan-zhongwenwang/2024.3.4.0/java_jichujiaocheng_c_yuyan_zhongwenwang-2024.3.4.0-py3-none-any.whl

---
+++ packages/java-jichujiaocheng-c-yuyan-zhongwenwang/java_jichujiaocheng_c_yuyan_zhongwenwang-2024.3.4.0-py3-none-any.whl/JavaJichujiaochengCYuyanZhongwenwang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-jichujiaocheng-c-yuyan-zhongwenwang
+https://github.com/apachecn/java-jichujiaocheng-c-yuyan-zhongwenwang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
