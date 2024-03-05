0d959b16adaf73021eb2c2ddfee94572a52f73bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 07:05:03 +0000

    Add package css-quanwei-zhinan-zhongwen-disiban/2024.3.5.0/css_quanwei_zhinan_zhongwen_disiban-2024.3.5.0-py3-none-any.whl

---
+++ packages/css-quanwei-zhinan-zhongwen-disiban/css_quanwei_zhinan_zhongwen_disiban-2024.3.5.0-py3-none-any.whl/CssQuanweiZhinanZhongwenDisiban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""css-quanwei-zhinan-zhongwen-disiban
+https://github.com/apachecn/css-quanwei-zhinan-zhongwen-disiban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
