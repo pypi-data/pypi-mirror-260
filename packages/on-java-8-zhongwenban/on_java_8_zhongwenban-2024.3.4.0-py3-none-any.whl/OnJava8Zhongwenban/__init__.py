f9d7959bfe1d4daf10579744ab094461b1ce6cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:57 +0000

    Add package on-java-8-zhongwenban/2024.3.4.0/on_java_8_zhongwenban-2024.3.4.0-py3-none-any.whl

---
+++ packages/on-java-8-zhongwenban/on_java_8_zhongwenban-2024.3.4.0-py3-none-any.whl/OnJava8Zhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""on-java-8-zhongwenban
+https://github.com/apachecn/on-java-8-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
