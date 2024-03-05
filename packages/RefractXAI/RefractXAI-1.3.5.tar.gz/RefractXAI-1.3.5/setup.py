fa8b266e9a000cc80e04539adb80e99be8d127e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 17:05:56 +0000

    Add package RefractXAI/1.3.5/RefractXAI-1.3.5.tar.gz

---
+++ packages/RefractXAI/RefractXAI-1.3.5.tar.gz/RefractXAI-1.3.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "RefractXAI",
+    version = "1.3.5",
+    packages = find_packages(),
+    install_requires = [],
+    include_package_data=True
+)
