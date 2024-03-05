b382ba42758e40f6e29a9ec176b2934864ca4682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:42 +0000

    Add package qbraid-qir/0.1.1/qbraid-qir-0.1.1.tar.gz

---
+++ packages/qbraid-qir/qbraid-qir-0.1.1.tar.gz/qbraid-qir-0.1.1/qbraid_qir/__init__.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2023 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+This top level module contains the main qBraid QIR functionality.
+
+.. currentmodule:: qbraid_qir
+
+Functions
+-----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   dumps
+
+Exceptions
+-----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   QbraidQirError
+   QirConversionError
+
+"""
+from ._version import __version__
+from .cirq import cirq_to_qir
+from .exceptions import QbraidQirError, QirConversionError
+from .serialization import dumps
