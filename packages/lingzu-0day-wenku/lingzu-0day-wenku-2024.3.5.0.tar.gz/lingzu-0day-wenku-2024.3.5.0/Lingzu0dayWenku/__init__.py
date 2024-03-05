82aaad6c7c1deab9c4a08b5018a0836f27f69d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:49 +0000

    Add package lingzu-0day-wenku/2024.3.5.0/lingzu-0day-wenku-2024.3.5.0.tar.gz

---
+++ packages/lingzu-0day-wenku/lingzu-0day-wenku-2024.3.5.0.tar.gz/lingzu-0day-wenku-2024.3.5.0/Lingzu0dayWenku/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingzu-0day-wenku
+https://github.com/apachecn/lingzu-0day-wenku"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
