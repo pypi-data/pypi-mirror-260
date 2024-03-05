d45683d443d1ff0ba7e8824b8f4f8e2814f09a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:54 +0000

    Add package niuke-wang-mingqi-chunzhao-shixi-beizhan-gonglve-jishu-pian-2019/2024.3.4.0/niuke-wang-mingqi-chunzhao-shixi-beizhan-gonglve-jishu-pian-2019-2024.3.4.0.tar.gz

---
+++ packages/niuke-wang-mingqi-chunzhao-shixi-beizhan-gonglve-jishu-pian-2019/niuke-wang-mingqi-chunzhao-shixi-beizhan-gonglve-jishu-pian-2019-2024.3.4.0.tar.gz/niuke-wang-mingqi-chunzhao-shixi-beizhan-gonglve-jishu-pian-2019-2024.3.4.0/NiukeWangMingqiChunzhaoShixiBeizhanGonglveJishuPian2019/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""niuke-wang-mingqi-chunzhao-shixi-beizhan-gonglve-jishu-pian-2019
+https://github.com/apachecn/niuke-wang-mingqi-chunzhao-shixi-beizhan-gonglve-jishu-pian-2019"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
