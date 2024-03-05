8365fc2bb26f609b1972e5254bee0c2f73be745f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:50 +0000

    Add package php-jiaocheng-c-yuyan-biancheng-wang/2024.3.5.0/php-jiaocheng-c-yuyan-biancheng-wang-2024.3.5.0.tar.gz

---
+++ packages/php-jiaocheng-c-yuyan-biancheng-wang/php-jiaocheng-c-yuyan-biancheng-wang-2024.3.5.0.tar.gz/php-jiaocheng-c-yuyan-biancheng-wang-2024.3.5.0/PhpJiaochengCYuyanBianchengWang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""php-jiaocheng-c-yuyan-biancheng-wang
+https://github.com/apachecn/php-jiaocheng-c-yuyan-biancheng-wang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
