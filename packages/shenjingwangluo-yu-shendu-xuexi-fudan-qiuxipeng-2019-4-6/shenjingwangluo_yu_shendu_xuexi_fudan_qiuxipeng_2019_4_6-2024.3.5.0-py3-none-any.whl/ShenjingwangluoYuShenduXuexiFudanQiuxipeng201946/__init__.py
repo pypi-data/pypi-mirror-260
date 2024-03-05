729aab8d80846ff6e71873c52daf676ddbb0a938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:26 +0000

    Add package shenjingwangluo-yu-shendu-xuexi-fudan-qiuxipeng-2019-4-6/2024.3.5.0/shenjingwangluo_yu_shendu_xuexi_fudan_qiuxipeng_2019_4_6-2024.3.5.0-py3-none-any.whl

---
+++ packages/shenjingwangluo-yu-shendu-xuexi-fudan-qiuxipeng-2019-4-6/shenjingwangluo_yu_shendu_xuexi_fudan_qiuxipeng_2019_4_6-2024.3.5.0-py3-none-any.whl/ShenjingwangluoYuShenduXuexiFudanQiuxipeng201946/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenjingwangluo-yu-shendu-xuexi-fudan-qiuxipeng-2019-4-6
+https://github.com/apachecn/shenjingwangluo-yu-shendu-xuexi-fudan-qiuxipeng-2019-4-6"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
