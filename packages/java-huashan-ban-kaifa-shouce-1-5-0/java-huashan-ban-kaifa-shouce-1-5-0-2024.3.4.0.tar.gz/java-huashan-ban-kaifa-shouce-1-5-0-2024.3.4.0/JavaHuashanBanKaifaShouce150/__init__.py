b7758eba77695523bcf421ad47feda77b4c1a92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:23 +0000

    Add package java-huashan-ban-kaifa-shouce-1-5-0/2024.3.4.0/java-huashan-ban-kaifa-shouce-1-5-0-2024.3.4.0.tar.gz

---
+++ packages/java-huashan-ban-kaifa-shouce-1-5-0/java-huashan-ban-kaifa-shouce-1-5-0-2024.3.4.0.tar.gz/java-huashan-ban-kaifa-shouce-1-5-0-2024.3.4.0/JavaHuashanBanKaifaShouce150/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-huashan-ban-kaifa-shouce-1-5-0
+https://github.com/apachecn/java-huashan-ban-kaifa-shouce-1-5-0"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
