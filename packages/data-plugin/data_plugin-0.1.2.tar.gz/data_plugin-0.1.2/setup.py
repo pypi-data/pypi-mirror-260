f04fcd76c66910897f8998b75b02603dfa9da4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:04:18 +0000

    Add package data-plugin/0.1.2/data_plugin-0.1.2.tar.gz

---
+++ packages/data-plugin/data_plugin-0.1.2.tar.gz/data_plugin-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_plugin',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'typer',
+        # Add any other dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'plug=data_plugin:app',
+        ],
+    },
+)
