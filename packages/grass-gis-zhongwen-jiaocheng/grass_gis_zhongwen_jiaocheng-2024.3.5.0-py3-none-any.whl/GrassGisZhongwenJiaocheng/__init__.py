7580e7e4dd13726a0c069ef50c20b83ebc33d1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:53 +0000

    Add package grass-gis-zhongwen-jiaocheng/2024.3.5.0/grass_gis_zhongwen_jiaocheng-2024.3.5.0-py3-none-any.whl

---
+++ packages/grass-gis-zhongwen-jiaocheng/grass_gis_zhongwen_jiaocheng-2024.3.5.0-py3-none-any.whl/GrassGisZhongwenJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""grass-gis-zhongwen-jiaocheng
+https://github.com/apachecn/grass-gis-zhongwen-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
