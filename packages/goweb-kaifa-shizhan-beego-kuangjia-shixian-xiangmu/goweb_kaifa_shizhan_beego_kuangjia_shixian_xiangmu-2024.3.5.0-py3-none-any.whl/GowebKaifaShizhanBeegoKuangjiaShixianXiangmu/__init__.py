d2a99442c15df0dba5ac8bd45bd5b18f8a7919a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:47 +0000

    Add package goweb-kaifa-shizhan-beego-kuangjia-shixian-xiangmu/2024.3.5.0/goweb_kaifa_shizhan_beego_kuangjia_shixian_xiangmu-2024.3.5.0-py3-none-any.whl

---
+++ packages/goweb-kaifa-shizhan-beego-kuangjia-shixian-xiangmu/goweb_kaifa_shizhan_beego_kuangjia_shixian_xiangmu-2024.3.5.0-py3-none-any.whl/GowebKaifaShizhanBeegoKuangjiaShixianXiangmu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""goweb-kaifa-shizhan-beego-kuangjia-shixian-xiangmu
+https://github.com/apachecn/goweb-kaifa-shizhan-beego-kuangjia-shixian-xiangmu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
