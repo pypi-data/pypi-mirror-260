5c93f4655caebde06c95d7a08178212e2bd1ee8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:04:53 +0000

    Add package energy-base/1.4.2/energy_base-1.4.2.tar.gz

---
+++ packages/energy-base/energy_base-1.4.2.tar.gz/energy_base-1.4.2/src/energy_base/translation/utils.py
@@ -0,0 +1,11 @@
+import os
+
+import requests
+from django.utils.translation import get_language
+
+
+def translate(key: str) -> str:
+    base_url = os.environ.get('TRANSLATION_SERVICE_URL')
+    return requests.get(f'{base_url}/api/translations/{key}', params={
+        'lang': get_language()
+    }).text
