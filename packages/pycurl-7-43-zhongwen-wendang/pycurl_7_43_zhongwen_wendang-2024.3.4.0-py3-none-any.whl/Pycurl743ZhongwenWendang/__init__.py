cc25e97b4ff4c3d6f16945e54c99f6ad001e9e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:04:05 +0000

    Add package pycurl-7-43-zhongwen-wendang/2024.3.4.0/pycurl_7_43_zhongwen_wendang-2024.3.4.0-py3-none-any.whl

---
+++ packages/pycurl-7-43-zhongwen-wendang/pycurl_7_43_zhongwen_wendang-2024.3.4.0-py3-none-any.whl/Pycurl743ZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pycurl-7-43-zhongwen-wendang
+https://github.com/apachecn/pycurl-7-43-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
