6d8d6ef3f81d2fc87ba66772e0118a74f078cde0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:48 +0000

    Add package xin-gainian-moni-dianlu-2-fufankui-he-yunsuan-fangdaqi-jichu/2024.3.5.0/xin_gainian_moni_dianlu_2_fufankui_he_yunsuan_fangdaqi_jichu-2024.3.5.0-py3-none-any.whl

---
+++ packages/xin-gainian-moni-dianlu-2-fufankui-he-yunsuan-fangdaqi-jichu/xin_gainian_moni_dianlu_2_fufankui_he_yunsuan_fangdaqi_jichu-2024.3.5.0-py3-none-any.whl/XinGainianMoniDianlu2FufankuiHeYunsuanFangdaqiJichu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xin-gainian-moni-dianlu-2-fufankui-he-yunsuan-fangdaqi-jichu
+https://github.com/apachecn/xin-gainian-moni-dianlu-2-fufankui-he-yunsuan-fangdaqi-jichu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
