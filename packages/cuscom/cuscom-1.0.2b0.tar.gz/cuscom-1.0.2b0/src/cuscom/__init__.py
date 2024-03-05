a74736d10388cda885c8cd39e1ab3a98b4ca3b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:20 +0000

    Add package cuscom/1.0.2b0/cuscom-1.0.2b0.tar.gz

---
+++ packages/cuscom/cuscom-1.0.2b0.tar.gz/cuscom-1.0.2b0/src/cuscom/__init__.py
@@ -0,0 +1,6 @@
+# pylint: disable=wildcard-import, unused-wildcard-import, unused-import
+from cuscom.clients import *
+from cuscom.hooks import *
+from cuscom.exceptions import *
+from cuscom.operators import *
+from cuscom.utils import trigger_dag
---
+++ packages/cuscom/cuscom-1.0.2b0.tar.gz/cuscom-1.0.2b0/src/cuscom/hooks/__init__.py
@@ -0,0 +1,3 @@
+# pylint: disable=unused-import
+from cuscom.hooks.slack import on_failure
+from cuscom.hooks.ms_teams import on_failure
