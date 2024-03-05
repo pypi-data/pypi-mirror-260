c39eb95f2cb3a3627a9eccf48ec8db80f59d1b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:16 +0000

    Add package imagify/1.0.4/imagify-1.0.4.tar.gz

---
+++ packages/imagify/imagify-1.0.4.tar.gz/imagify-1.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='imagify',
+    packages=find_packages(include=['imagify']),
+    version='1.0.4',
+    description='Revolutionize your text into stunning visual art with our Python library! Convert plain text into captivating images effortlessly',
+    author='Muhammad Asif Ali',
+    install_requires=['selenium','toml','requests']
+)
+
