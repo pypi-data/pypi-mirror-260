2fedd546700442b31fc9bc03d8ebeff3b05cf6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:32 +0000

    Add package shuju-toushi-biao-zhongjiban/2024.3.5.0/shuju-toushi-biao-zhongjiban-2024.3.5.0.tar.gz

---
+++ packages/shuju-toushi-biao-zhongjiban/shuju-toushi-biao-zhongjiban-2024.3.5.0.tar.gz/shuju-toushi-biao-zhongjiban-2024.3.5.0/ShujuToushiBiaoZhongjiban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuju-toushi-biao-zhongjiban
+https://github.com/apachecn/shuju-toushi-biao-zhongjiban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
