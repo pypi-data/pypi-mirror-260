a64604e1310d62efc408b14db935bb6fc57be91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:05:11 +0000

    Add package fileformats/0.11.1/fileformats-0.11.1-py3-none-any.whl

---
+++ packages/fileformats/fileformats-0.11.1-py3-none-any.whl/fileformats/core/exceptions.py
@@ -0,0 +1,33 @@
+class FileFormatsError(RuntimeError):
+    "Base exception class"
+
+
+class FormatMismatchError(FileFormatsError):
+    "File formats don't match"
+
+
+class FormatConversionError(FileFormatsError):
+    "No converters exist between formats"
+
+
+class FormatRecognitionError(KeyError, FileFormatsError):
+    "Did not find a format class corresponding to a MIME, or MIME-like, type string"
+
+
+class UnconstrainedExtensionException(FileFormatsError):
+    "Did not find a format class corresponding to a MIME, or MIME-like, type string"
+
+
+class FileFormatsExtrasError(FileFormatsError):
+    """If there is an "extras hook" in the datatype class but no methods have been
+    registered on it"""
+
+
+class FileFormatsExtrasPkgUninstalledError(FileFormatsExtrasError):
+    """If there is an "extras hook" in the datatype class and a extras package on PyPI
+    but it hasn't been installed"""
+
+
+class FileFormatsExtrasPkgNotCheckedError(FileFormatsExtrasError):
+    """If there is an "extras hook" in the datatype class and a extras package on PyPI
+    but it hasn't been installed"""
