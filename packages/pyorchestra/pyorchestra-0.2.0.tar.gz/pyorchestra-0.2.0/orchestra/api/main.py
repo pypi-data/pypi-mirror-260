d89329d2ef14343c92bf501fd59217d4675a51ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:30 +0000

    Add package pyorchestra/0.2.0/pyorchestra-0.2.0.tar.gz

---
+++ packages/pyorchestra/pyorchestra-0.2.0.tar.gz/pyorchestra-0.2.0/orchestra/api/main.py
@@ -0,0 +1,23 @@
+import logging
+import os
+import signal
+
+from fastapi import FastAPI
+from rich.logging import RichHandler
+
+from orchestra.api.routers import jobs, tasks
+from orchestra.core import instance
+
+logging.basicConfig(
+    level="INFO", format="%(message)s", datefmt="[%X]", handlers=[RichHandler(rich_tracebacks=True, tracebacks_show_locals=True)]
+)
+
+logger = logging.getLogger("orchestra.api")
+
+app = FastAPI(title="Orchestra")
+app.include_router(jobs.router)
+app.include_router(tasks.router)
+
+if instance is None:
+    logger.error("Orchestra is not started, can't start API server")
+    os.kill(os.getpid(), signal.SIGTERM)
