322ea9ff65d3bde8b1d828b5f1772fc8613820ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:44 +0000

    Add package niuke-jiqi-xuexi-suanfa-gongchengshi-mianshi-tiku/2024.3.5.0/niuke_jiqi_xuexi_suanfa_gongchengshi_mianshi_tiku-2024.3.5.0-py3-none-any.whl

---
+++ packages/niuke-jiqi-xuexi-suanfa-gongchengshi-mianshi-tiku/niuke_jiqi_xuexi_suanfa_gongchengshi_mianshi_tiku-2024.3.5.0-py3-none-any.whl/NiukeJiqiXuexiSuanfaGongchengshiMianshiTiku/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""niuke-jiqi-xuexi-suanfa-gongchengshi-mianshi-tiku
+https://github.com/apachecn/niuke-jiqi-xuexi-suanfa-gongchengshi-mianshi-tiku"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
