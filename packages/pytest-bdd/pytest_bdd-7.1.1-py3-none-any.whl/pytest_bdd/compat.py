2b74f981c9bb31ea8744e241a5f98b46a33ebc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:06:23 +0000

    Add package pytest-bdd/7.1.1/pytest_bdd-7.1.1-py3-none-any.whl

---
+++ packages/pytest-bdd/pytest_bdd-7.1.1-py3-none-any.whl/pytest_bdd/compat.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from importlib.metadata import version
+
+from _pytest.fixtures import FixtureDef, FixtureManager
+from _pytest.nodes import Node
+from packaging.version import parse as parse_version
+
+pytest_version = parse_version(version("pytest"))
+
+
+if pytest_version.release >= (8, 1):
+
+    def getfixturedefs(fixturemanager: FixtureManager, fixturename: str, node: Node) -> Sequence[FixtureDef] | None:
+        return fixturemanager.getfixturedefs(fixturename, node)
+
+else:
+
+    def getfixturedefs(fixturemanager: FixtureManager, fixturename: str, node: Node) -> Sequence[FixtureDef] | None:
+        return fixturemanager.getfixturedefs(fixturename, node.nodeid)
