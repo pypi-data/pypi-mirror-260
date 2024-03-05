e12b7fe4cc5dba180d61b31f8f6450d3c62c63ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:00 +0000

    Add package fdnubank/0.1.5/fdnubank-0.1.5.tar.gz

---
+++ packages/fdnubank/fdnubank-0.1.5.tar.gz/fdnubank-0.1.5/README.md
@@ -0,0 +1,10 @@
+# Модуль для выгрузки данных счётов NuBank
+На основе - https://github.com/andreroggeri/pynubank/tree/main
+
+# Генерация сертификата для авторизации
+> poetry run python -m fdnubank.cli
+- ввести CPF
+- ввести пароль
+- ввести код, присланный на email
+В корневой папке будет создан файл cert.p12
+
---
+++ packages/fdnubank/fdnubank-0.1.5.tar.gz/fdnubank-0.1.5/fdnubank/auth_mode.py
@@ -0,0 +1,28 @@
+# https://github.com/andreroggeri/pynubank/blob/main/pynubank/auth_mode.py
+from enum import Enum
+from functools import wraps
+
+from fdnubank.exception import NuInvalidAuthenticationMethod
+
+
+class AuthMode(Enum):
+    UNAUTHENTICATED = 0
+    WEB = 1
+    APP = 2
+
+
+def requires_auth_mode(*required_auth_mode: AuthMode):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            instance = args[0]
+            if instance._auth_mode not in required_auth_mode:
+                raise NuInvalidAuthenticationMethod(
+                    'The authentication method used doest not allow access to this resource'
+                )
+
+            return function(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
