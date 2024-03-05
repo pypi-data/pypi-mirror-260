608e9148c7bbd7f2fba4302f749c3a079d52666b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 16:05:29 +0000

    Add package checks-is-prime/1.0.0/checks_is_prime-1.0.0.tar.gz

---
+++ packages/checks-is-prime/checks_is_prime-1.0.0.tar.gz/checks_is_prime-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'checks_is_prime',
+	version = '1.0.0',
+	py_modules = ['checks_is_prime'],
+	author = 'Muhammad Usman',
+	author_email = 'msusman97@gmail.com',
+	description = 'Module to find whether a given number is prime number and its nearest prime. A new method game() included in version 1.0.1.'
+)
