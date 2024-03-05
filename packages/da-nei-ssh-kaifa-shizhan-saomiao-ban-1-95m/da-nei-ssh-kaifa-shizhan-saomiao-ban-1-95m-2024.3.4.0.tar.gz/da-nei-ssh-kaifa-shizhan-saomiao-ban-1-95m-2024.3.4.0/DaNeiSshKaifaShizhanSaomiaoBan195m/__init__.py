4ab48294ea84de69c3996c9bf189b8e730cfdf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:17 +0000

    Add package da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m/2024.3.4.0/da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m-2024.3.4.0.tar.gz

---
+++ packages/da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m/da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m-2024.3.4.0.tar.gz/da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m-2024.3.4.0/DaNeiSshKaifaShizhanSaomiaoBan195m/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m
+https://github.com/apachecn/da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
---
+++ packages/da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m/da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m-2024.3.4.0.tar.gz/da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m-2024.3.4.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import DaNeiSshKaifaShizhanSaomiaoBan195m
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('DaNeiSshKaifaShizhanSaomiaoBan195m'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m",
+    version=DaNeiSshKaifaShizhanSaomiaoBan195m.__version__,
+    url="https://github.com/apachecn/da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m",
+    author=DaNeiSshKaifaShizhanSaomiaoBan195m.__author__,
+    author_email=DaNeiSshKaifaShizhanSaomiaoBan195m.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="达内 SSH 开发实战_扫描版_1.95M",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "da-nei-ssh-kaifa-shizhan-saomiao-ban-1-95m=DaNeiSshKaifaShizhanSaomiaoBan195m.__main__:main",
+            "DaNeiSshKaifaShizhanSaomiaoBan195m=DaNeiSshKaifaShizhanSaomiaoBan195m.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
