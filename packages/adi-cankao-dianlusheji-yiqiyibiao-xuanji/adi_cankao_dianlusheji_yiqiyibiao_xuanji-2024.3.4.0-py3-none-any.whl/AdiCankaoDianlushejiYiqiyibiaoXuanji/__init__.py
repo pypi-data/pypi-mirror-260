2cc30582931b9a6f520dc0459605b83989e350d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:28 +0000

    Add package adi-cankao-dianlusheji-yiqiyibiao-xuanji/2024.3.4.0/adi_cankao_dianlusheji_yiqiyibiao_xuanji-2024.3.4.0-py3-none-any.whl

---
+++ packages/adi-cankao-dianlusheji-yiqiyibiao-xuanji/adi_cankao_dianlusheji_yiqiyibiao_xuanji-2024.3.4.0-py3-none-any.whl/AdiCankaoDianlushejiYiqiyibiaoXuanji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""adi-cankao-dianlusheji-yiqiyibiao-xuanji
+https://github.com/apachecn/adi-cankao-dianlusheji-yiqiyibiao-xuanji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
