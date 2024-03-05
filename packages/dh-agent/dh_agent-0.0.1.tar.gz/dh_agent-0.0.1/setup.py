2b3880da1454f0eef9f19283f5ec5597c5105365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:05 +0000

    Add package dh-agent/0.0.1/dh_agent-0.0.1.tar.gz

---
+++ packages/dh-agent/dh_agent-0.0.1.tar.gz/dh_agent-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dh_agent',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.explayn.me',
+    author='me',
+    author_email='francois.lagunas@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
