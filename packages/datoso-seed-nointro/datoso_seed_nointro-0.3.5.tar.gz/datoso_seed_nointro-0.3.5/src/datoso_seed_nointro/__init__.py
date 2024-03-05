c787347d12a69e37609b9a0d99c4a57827ef2f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:04:12 +0000

    Add package datoso-seed-nointro/0.3.5/datoso_seed_nointro-0.3.5.tar.gz

---
+++ packages/datoso-seed-nointro/datoso_seed_nointro-0.3.5.tar.gz/datoso_seed_nointro-0.3.5/src/datoso_seed_nointro/__init__.py
@@ -0,0 +1,13 @@
+"""
+__init__.py
+"""
+__all__ = ["__version__", "__author__", "__description__"]
+__version__ = "0.3.5"
+__author__ = "Lacides Miranda"
+__description__ = "No-Intro is a collection of DATs aimed for video game preservation especialized in cartridge-based and digital systems."
+__preffix__ = "nointro"
+__requirements__ = {
+    'executables': [
+        'geckodriver',
+    ]
+}
