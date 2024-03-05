912be76ecd6e674c49a98ad963ef4f82ae063c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:16 +0000

    Add package lemon-tictactoe/0.1.0/lemon_tictactoe-0.1.0.tar.gz

---
+++ packages/lemon-tictactoe/lemon_tictactoe-0.1.0.tar.gz/lemon_tictactoe-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lemon_tictactoe",
+    version="0.1.0",
+    packages=find_packages(),
+    license="GNU General Public License v3.0",
+    description="A library simplifying the process of embedding a TicTacToe game in your python project.",
+    author="Zitronenjoghurt",
+    extras_require={'dev': ['pytest']},
+    url="https://github.com/Zitronenjoghurt/Lemon-TicTacToe"
+)
