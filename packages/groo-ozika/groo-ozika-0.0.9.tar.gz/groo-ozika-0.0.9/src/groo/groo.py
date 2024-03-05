53b61ef8b0d38e1c5267a985f35e6f5bba43f5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:45 +0000

    Add package groo-ozika/0.0.9/groo-ozika-0.0.9.tar.gz

---
+++ packages/groo-ozika/groo-ozika-0.0.9.tar.gz/groo-ozika-0.0.9/src/groo/groo.py
@@ -0,0 +1,33 @@
+def get_root(rootfile, timelim=10):
+    import os 
+    from pathlib import Path
+    import time
+    start = time.time()
+    d = Path(os.getcwd())
+    found = 0   
+    while (found == 0) &  (time.time()-start)<float(3):
+        if os.path.isfile(os.path.join(d, rootfile)):
+            found = 1
+        else:
+            d=d.parent
+
+    if found==0:
+        while (found != 1) & ((time.time()-start) < float(timelim)):
+            if os.path.isfile(os.path.join(d, rootfile)):
+                found=1
+            else:
+                for root, dir, files in os.walk(d):
+                    #print(root)
+                    if rootfile in files:
+                        found = 1 
+                        d = root
+                if found == 0:
+                    d=d.parent
+        if found == 0:
+            print("Root file not found in the time limit ("+str(timelim)+"s)\nIncrease time lim by providing timelim=XX argument")
+    return d
+
+
+    
+
+
