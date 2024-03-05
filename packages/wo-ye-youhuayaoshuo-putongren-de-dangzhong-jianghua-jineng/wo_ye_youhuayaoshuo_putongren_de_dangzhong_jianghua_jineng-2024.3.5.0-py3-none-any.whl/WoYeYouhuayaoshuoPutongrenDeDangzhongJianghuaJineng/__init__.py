f1cb89677b100cf57596007029c15dab703082d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 05:04:09 +0000

    Add package wo-ye-youhuayaoshuo-putongren-de-dangzhong-jianghua-jineng/2024.3.5.0/wo_ye_youhuayaoshuo_putongren_de_dangzhong_jianghua_jineng-2024.3.5.0-py3-none-any.whl

---
+++ packages/wo-ye-youhuayaoshuo-putongren-de-dangzhong-jianghua-jineng/wo_ye_youhuayaoshuo_putongren_de_dangzhong_jianghua_jineng-2024.3.5.0-py3-none-any.whl/WoYeYouhuayaoshuoPutongrenDeDangzhongJianghuaJineng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wo-ye-youhuayaoshuo-putongren-de-dangzhong-jianghua-jineng
+https://github.com/apachecn/wo-ye-youhuayaoshuo-putongren-de-dangzhong-jianghua-jineng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
