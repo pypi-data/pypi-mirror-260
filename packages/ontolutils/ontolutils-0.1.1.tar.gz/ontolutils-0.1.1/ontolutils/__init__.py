32df437aa1a1ef657bece2ebcba1721a6b1a4a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 17:06:09 +0000

    Add package ontolutils/0.1.1/ontolutils-0.1.1.tar.gz

---
+++ packages/ontolutils/ontolutils-0.1.1.tar.gz/ontolutils-0.1.1/ontolutils/__init__.py
@@ -0,0 +1,7 @@
+from ._version import __version__
+from .classes import Thing
+from .classes import namespaces, urirefs
+from .classes import query
+from .namespacelib import *
+
+__all__ = ['Thing', '__version__', 'namespaces', 'urirefs', 'query']
