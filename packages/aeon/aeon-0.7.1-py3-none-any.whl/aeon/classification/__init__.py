f239934d1693613d05c74ba7540e3cc008763866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:04:04 +0000

    Add package aeon/0.7.1/aeon-0.7.1-py3-none-any.whl

---
+++ packages/aeon/aeon-0.7.1-py3-none-any.whl/aeon/classification/__init__.py
@@ -0,0 +1,9 @@
+"""Classifier Base."""
+
+__all__ = [
+    "BaseClassifier",
+    "DummyClassifier",
+]
+
+from aeon.classification._dummy import DummyClassifier
+from aeon.classification.base import BaseClassifier
---
+++ packages/aeon/aeon-0.7.1-py3-none-any.whl/aeon/regression/deep_learning/__init__.py
@@ -0,0 +1,21 @@
+"""Deep learning based regressors."""
+
+__all__ = [
+    "CNNRegressor",
+    "FCNRegressor",
+    "InceptionTimeRegressor",
+    "IndividualInceptionRegressor",
+    "ResNetRegressor",
+    "TapNetRegressor",
+    "MLPRegressor",
+]
+
+from aeon.regression.deep_learning._cnn import CNNRegressor
+from aeon.regression.deep_learning._fcn import FCNRegressor
+from aeon.regression.deep_learning._inception_time import (
+    InceptionTimeRegressor,
+    IndividualInceptionRegressor,
+)
+from aeon.regression.deep_learning._mlp import MLPRegressor
+from aeon.regression.deep_learning._resnet import ResNetRegressor
+from aeon.regression.deep_learning._tapnet import TapNetRegressor
