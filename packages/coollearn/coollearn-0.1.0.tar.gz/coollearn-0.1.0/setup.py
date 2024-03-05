a713d6224d3fd4f7263836a2410d211424f68937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 04:03:36 +0000

    Add package coollearn/0.1.0/coollearn-0.1.0.tar.gz

---
+++ packages/coollearn/coollearn-0.1.0.tar.gz/coollearn-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coollearn",
+    version="0.1.0",
+    author="boyjiangboyu",
+    author_email="boyjiangboyu@outlook.com",
+    description="Personalized Learning Assistant",
+    long_description=open("Readme.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=open("requirements.txt").read().splitlines(),
+    python_requires='>=3.10',
+)
