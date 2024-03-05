ae8f0ba99d54a6f9fbc0e67d1e8fe06c52d2600e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 14:03:40 +0000

    Add package linux-vi-mingling-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang/2024.3.5.0/linux_vi_mingling_30_fenzhong_rumenjiaocheng_c_yuyan_zhongwenwang-2024.3.5.0-py3-none-any.whl

---
+++ packages/linux-vi-mingling-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang/linux_vi_mingling_30_fenzhong_rumenjiaocheng_c_yuyan_zhongwenwang-2024.3.5.0-py3-none-any.whl/LinuxViMingling30FenzhongRumenjiaochengCYuyanZhongwenwang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""linux-vi-mingling-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang
+https://github.com/apachecn/linux-vi-mingling-30-fenzhong-rumenjiaocheng-c-yuyan-zhongwenwang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
