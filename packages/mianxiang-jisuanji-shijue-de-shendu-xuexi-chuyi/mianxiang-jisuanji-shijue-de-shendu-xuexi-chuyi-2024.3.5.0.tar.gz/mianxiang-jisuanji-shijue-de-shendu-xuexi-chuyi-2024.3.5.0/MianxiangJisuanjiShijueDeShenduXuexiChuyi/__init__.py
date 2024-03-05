173673089f774fb53b8d2f766cff162a116aa8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:35 +0000

    Add package mianxiang-jisuanji-shijue-de-shendu-xuexi-chuyi/2024.3.5.0/mianxiang-jisuanji-shijue-de-shendu-xuexi-chuyi-2024.3.5.0.tar.gz

---
+++ packages/mianxiang-jisuanji-shijue-de-shendu-xuexi-chuyi/mianxiang-jisuanji-shijue-de-shendu-xuexi-chuyi-2024.3.5.0.tar.gz/mianxiang-jisuanji-shijue-de-shendu-xuexi-chuyi-2024.3.5.0/MianxiangJisuanjiShijueDeShenduXuexiChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mianxiang-jisuanji-shijue-de-shendu-xuexi-chuyi
+https://github.com/apachecn/mianxiang-jisuanji-shijue-de-shendu-xuexi-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
