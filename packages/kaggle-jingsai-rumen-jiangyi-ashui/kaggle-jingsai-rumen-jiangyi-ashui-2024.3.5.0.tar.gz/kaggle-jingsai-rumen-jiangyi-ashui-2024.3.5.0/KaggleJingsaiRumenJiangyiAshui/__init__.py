6bf337df16c91be34ad3ca7826bca30aee7591c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:34 +0000

    Add package kaggle-jingsai-rumen-jiangyi-ashui/2024.3.5.0/kaggle-jingsai-rumen-jiangyi-ashui-2024.3.5.0.tar.gz

---
+++ packages/kaggle-jingsai-rumen-jiangyi-ashui/kaggle-jingsai-rumen-jiangyi-ashui-2024.3.5.0.tar.gz/kaggle-jingsai-rumen-jiangyi-ashui-2024.3.5.0/KaggleJingsaiRumenJiangyiAshui/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kaggle-jingsai-rumen-jiangyi-ashui
+https://github.com/apachecn/kaggle-jingsai-rumen-jiangyi-ashui"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
