dce604c034cd6ceabd987e15379f48a18bf5a6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:12 +0000

    Add package flink-1-7-snapshot-zhongwen-wendang/2024.3.4.0/flink_1_7_snapshot_zhongwen_wendang-2024.3.4.0-py3-none-any.whl

---
+++ packages/flink-1-7-snapshot-zhongwen-wendang/flink_1_7_snapshot_zhongwen_wendang-2024.3.4.0-py3-none-any.whl/Flink17SnapshotZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""flink-1-7-snapshot-zhongwen-wendang
+https://github.com/apachecn/flink-1-7-snapshot-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
