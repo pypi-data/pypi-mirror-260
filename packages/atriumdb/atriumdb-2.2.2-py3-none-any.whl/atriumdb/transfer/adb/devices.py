8bf131c264f23f4d587d0f47110b623f57db7ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 06:03:58 +0000

    Add package atriumdb/2.2.2/atriumdb-2.2.2-py3-none-any.whl

---
+++ packages/atriumdb/atriumdb-2.2.2-py3-none-any.whl/atriumdb/transfer/adb/devices.py
@@ -0,0 +1,31 @@
+# AtriumDB is a timeseries database software designed to best handle the unique features and
+# challenges that arise from clinical waveform data.
+#     Copyright (C) 2023  The Hospital for Sick Children
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+def transfer_devices(from_sdk, to_sdk, device_id_list=None):
+    from_devices = from_sdk.get_all_devices()
+
+    device_map = {}
+    for from_device_id, device_info in from_devices.items():
+        if device_id_list is None or from_device_id in device_id_list:
+            device_tag = device_info['tag']
+            device_name = device_info['name']
+            to_device_id = to_sdk.insert_device(device_tag=device_tag, device_name=device_name, device_id=from_device_id)
+
+            device_map[from_device_id] = to_device_id
+
+    return device_map
