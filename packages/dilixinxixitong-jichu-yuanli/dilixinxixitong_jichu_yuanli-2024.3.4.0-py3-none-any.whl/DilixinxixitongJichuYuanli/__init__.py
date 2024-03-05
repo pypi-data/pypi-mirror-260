453ef3ef8e62c738d69ec52350fde84df77b412a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-05 03:04:35 +0000

    Add package dilixinxixitong-jichu-yuanli/2024.3.4.0/dilixinxixitong_jichu_yuanli-2024.3.4.0-py3-none-any.whl

---
+++ packages/dilixinxixitong-jichu-yuanli/dilixinxixitong_jichu_yuanli-2024.3.4.0-py3-none-any.whl/DilixinxixitongJichuYuanli/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dilixinxixitong-jichu-yuanli
+https://github.com/apachecn/dilixinxixitong-jichu-yuanli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.4.0"
