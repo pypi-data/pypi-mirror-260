8e48928b684a00443d1c7158abdc39361f402f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:44 +0000

    Add package jian-zhi-offer-tijie-itcharge/2024.3.5.0/jian-zhi-offer-tijie-itcharge-2024.3.5.0.tar.gz

---
+++ packages/jian-zhi-offer-tijie-itcharge/jian-zhi-offer-tijie-itcharge-2024.3.5.0.tar.gz/jian-zhi-offer-tijie-itcharge-2024.3.5.0/JianZhiOfferTijieItcharge/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jian-zhi-offer-tijie-itcharge
+https://github.com/apachecn/jian-zhi-offer-tijie-itcharge"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
