114910ad778b83e30753c63fed8fb78cc39239ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:27 +0000

    Add package java-xuniji-diceng-yuanli-zhishi-zongjie/2024.3.5.0/java-xuniji-diceng-yuanli-zhishi-zongjie-2024.3.5.0.tar.gz

---
+++ packages/java-xuniji-diceng-yuanli-zhishi-zongjie/java-xuniji-diceng-yuanli-zhishi-zongjie-2024.3.5.0.tar.gz/java-xuniji-diceng-yuanli-zhishi-zongjie-2024.3.5.0/JavaXunijiDicengYuanliZhishiZongjie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-xuniji-diceng-yuanli-zhishi-zongjie
+https://github.com/apachecn/java-xuniji-diceng-yuanli-zhishi-zongjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
