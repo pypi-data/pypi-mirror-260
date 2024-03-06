5cb928e138e23ea09be1e318ad7ffe3ed122217b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:04 +0000

    Add package pysorters/0.5.6/pysorters-0.5.6-py3-none-any.whl

---
+++ packages/pysorters/pysorters-0.5.6-py3-none-any.whl/pysorters/__init__.py
@@ -0,0 +1,7 @@
+"""PyPi package with sort func."""
+
+from .functions import methods
+from pysorters.functions.methods import Sorter, gran_array_int, gran_array_float
+from pysorters.automations import filepy
+from pysorters.automations.filepy import Pyxplorer, Sweepy
+from .automations import filepy
---
+++ packages/pysorters/pysorters-0.5.6-py3-none-any.whl/pysorters/automations/__init__.py
@@ -0,0 +1,6 @@
+"""
+This module contains the automations for the pysorters_project package.
+"""
+
+from pysorters.automations import filepy
+from pysorters.automations.filepy import Pyxplorer, Sweepy
