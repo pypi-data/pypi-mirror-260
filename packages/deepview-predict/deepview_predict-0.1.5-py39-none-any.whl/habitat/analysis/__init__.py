ddc2bb0b4895ed41caaec2b95ad0acb346814dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:05:40 +0000

    Add package deepview-predict/0.1.5/deepview_predict-0.1.5-py39-none-any.whl

---
+++ packages/deepview-predict/deepview_predict-0.1.5-py39-none-any.whl/habitat/analysis/__init__.py
@@ -0,0 +1,19 @@
+from habitat.analysis.device import _Device
+
+Device = _Device()
+
+SPECIAL_OPERATIONS = {
+    # Convolution
+    'conv2d',
+    'conv_transpose2d',
+
+    # Matrix multiply operations
+    'linear',
+    'bmm',
+
+    # Recurrent operations
+    'lstm',
+    'gru',
+    'rnn_tanh',
+    'rnn_relu',
+}
