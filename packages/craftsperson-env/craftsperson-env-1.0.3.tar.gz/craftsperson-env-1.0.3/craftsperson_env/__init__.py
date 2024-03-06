5a4519a4402f7dda6e8bc94ea8ff44b8e8770b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 17:04:41 +0000

    Add package craftsperson-env/1.0.3/craftsperson-env-1.0.3.tar.gz

---
+++ packages/craftsperson-env/craftsperson-env-1.0.3.tar.gz/craftsperson-env-1.0.3/craftsperson_env/__init__.py
@@ -0,0 +1,4 @@
+"""
+This file imports classes and functions.
+"""
+from .__main__ import CraftsEnvConfig
---
+++ packages/craftsperson-env/craftsperson-env-1.0.3.tar.gz/craftsperson-env-1.0.3/setup.py
@@ -0,0 +1,50 @@
+"""
+This file is setuptools.
+"""
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt', encoding='utf-8') as requirements_file:
+    requirements_list = requirements_file.read().strip().split("\n")
+
+setup(
+    name='craftsperson-env',
+    version='1.0.3',
+    packages=find_packages(),
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Celal Akçelik',
+    author_email='celalakcelikk@gmail.com',
+    url='https://github.com/celalakcelikk/craftsperson-env',
+    python_requires='>=3.8',
+    install_requires=requirements_list,
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python",
+    ],
+    keywords=[
+        "environment",
+        "PyYAML",
+        "yaml",
+        "toml",
+        "env",
+        "json",
+        "xml",
+        "xmltodict",
+        "Python",
+        "projects",
+        "Config",
+        "package",
+        "packaging"]
+
+)
