b2243380af0496b586023a5fccde94ce505b0eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:45 +0000

    Add package elasticsearch-bada-jingdian-yingyong/2024.3.5.0/elasticsearch-bada-jingdian-yingyong-2024.3.5.0.tar.gz

---
+++ packages/elasticsearch-bada-jingdian-yingyong/elasticsearch-bada-jingdian-yingyong-2024.3.5.0.tar.gz/elasticsearch-bada-jingdian-yingyong-2024.3.5.0/ElasticsearchBadaJingdianYingyong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""elasticsearch-bada-jingdian-yingyong
+https://github.com/apachecn/elasticsearch-bada-jingdian-yingyong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
