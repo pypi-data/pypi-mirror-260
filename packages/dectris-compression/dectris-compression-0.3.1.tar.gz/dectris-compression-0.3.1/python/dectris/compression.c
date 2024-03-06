366d0e9ce759d0463d1f4e0ed37c1c08fadc1da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:25 +0000

    Add package dectris-compression/0.3.1/dectris-compression-0.3.1.tar.gz

---
+++ packages/dectris-compression/dectris-compression-0.3.1.tar.gz/dectris-compression-0.3.1/python/dectris/compression.c
@@ -0,0 +1,101 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include "src/compression.h"
+
+static int parse_algorithm(const char* identifier,
+                           CompressionAlgorithm* algorithm) {
+    if (strcmp(identifier, "bslz4") == 0) {
+        *algorithm = COMPRESSION_BSLZ4;
+        return 1;
+    } else if (strcmp(identifier, "lz4") == 0) {
+        *algorithm = COMPRESSION_LZ4;
+        return 1;
+    }
+    return 0;
+}
+
+PyDoc_STRVAR(decompress__doc__, "\
+Decompress, returning a bytes object of the uncompressed data.");
+
+static PyObject* decompress(PyObject* module, PyObject* args, PyObject* kw) {
+    static char* keywords[] = {"data", "algorithm", "elem_size", NULL};
+    Py_buffer input;
+    const char* algorithm_str;
+    Py_ssize_t algorithm_len;
+    Py_ssize_t elem_size = 0;
+    CompressionAlgorithm algorithm;
+    size_t n;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kw, "y*s#|$n", keywords, &input,
+                                     &algorithm_str, &algorithm_len,
+                                     &elem_size))
+        return NULL;
+
+    if (!parse_algorithm(algorithm_str, &algorithm)) {
+        PyErr_Format(PyExc_ValueError, "unknown algorithm: '%s'",
+                     algorithm_str);
+        goto error;
+    }
+
+    if (elem_size < 0) {
+        PyErr_SetString(PyExc_ValueError, "'elem_size' must be positive");
+        goto error;
+    }
+
+    if (algorithm == COMPRESSION_BSLZ4) {
+        if (elem_size == 0) {
+            PyErr_SetString(PyExc_ValueError,
+                            "algorithm requires argument 'elem_size'");
+            goto error;
+        }
+    }
+
+    n = compression_decompress_buffer(algorithm, NULL, 0, input.buf, input.len,
+                                      elem_size);
+    if (n != COMPRESSION_ERROR) {
+        const size_t output_len = n;
+        PyObject* buffer = PyBytes_FromStringAndSize(NULL, output_len);
+        if (!buffer)
+            goto error;
+
+        Py_BEGIN_ALLOW_THREADS
+        n = compression_decompress_buffer(algorithm, PyBytes_AS_STRING(buffer),
+                                          output_len, input.buf, input.len,
+                                          elem_size);
+        Py_END_ALLOW_THREADS
+
+        if (n == output_len) {
+            PyBuffer_Release(&input);
+            return buffer;
+        }
+
+        Py_DECREF(buffer);
+    }
+
+    PyErr_SetString(PyExc_RuntimeError, "error decompressing stream");
+error:
+    PyBuffer_Release(&input);
+    return NULL;
+}
+
+static PyMethodDef compression_methods[] = {
+    {"decompress", (PyCFunction)&decompress, METH_VARARGS | METH_KEYWORDS,
+     decompress__doc__},
+    {NULL, NULL, 0, NULL},
+};
+
+PyDoc_STRVAR(compression__doc__, "\
+This module provides an interface to compression algorithms used by DECTRIS.");
+
+static PyModuleDef compression_module = {
+    PyModuleDef_HEAD_INIT,
+    .m_name = "compression",
+    .m_doc = compression__doc__,
+    .m_size = -1,
+    .m_methods = compression_methods,
+};
+
+PyMODINIT_FUNC PyInit_compression(void) {
+    return PyModule_Create(&compression_module);
+}
---
+++ packages/dectris-compression/dectris-compression-0.3.1.tar.gz/dectris-compression-0.3.1/setup.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+
+class BuildExt(build_ext):
+    def build_extensions(self):
+        if self.compiler.compiler_type != "msvc":
+            for ext in self.extensions:
+                ext.extra_compile_args.append("-std=c99")
+        build_ext.build_extensions(self)
+
+
+setup(
+    name="dectris-compression",
+    version="0.3.1",
+    description="DECTRIS compression algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Kal Conley",
+    author_email="kal.conley@dectris.com",
+    url="https://github.com/dectris/compression",
+    package_dir={"": "python"},
+    packages=["dectris"],
+    ext_modules=[
+        Extension(
+            name="dectris.compression",
+            sources=[
+                "python/dectris/compression.c",
+                "src/compression.c",
+                "third_party/bitshuffle/src/bitshuffle.c",
+                "third_party/lz4/lib/lz4.c",
+            ],
+            include_dirs=[
+                ".",
+                "third_party",
+                "third_party/lz4/lib",
+            ],
+        )
+    ],
+    cmdclass={"build_ext": BuildExt},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: C",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    license="MIT",
+    keywords=["compression", "dectris"],
+)
