cd60a2b26c1d019aba228814f58ba9411d74444d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:41 +0000

    Add package IBITNormalizer/1.0.1/IBITNormalizer-1.0.1.tar.gz

---
+++ packages/IBITNormalizer/IBITNormalizer-1.0.1.tar.gz/IBITNormalizer-1.0.1/tests/test_normalizer.py
@@ -0,0 +1,12 @@
+import unittest
+import IBITNormalizer
+
+
+class TestNormalizer(unittest.TestCase):
+
+    def test_normalize_text(self):
+        self.assertEqual(IBITNormalizer.forLm.normalize('سلام'), 'سلام')
+
+
+if __name__ == '__main__':
+    unittest.main()
