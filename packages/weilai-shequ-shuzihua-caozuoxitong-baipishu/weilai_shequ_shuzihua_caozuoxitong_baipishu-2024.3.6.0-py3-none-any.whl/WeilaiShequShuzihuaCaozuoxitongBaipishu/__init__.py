baf584e838323301c1c18bb77b02c27a776aaa17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:31 +0000

    Add package weilai-shequ-shuzihua-caozuoxitong-baipishu/2024.3.6.0/weilai_shequ_shuzihua_caozuoxitong_baipishu-2024.3.6.0-py3-none-any.whl

---
+++ packages/weilai-shequ-shuzihua-caozuoxitong-baipishu/weilai_shequ_shuzihua_caozuoxitong_baipishu-2024.3.6.0-py3-none-any.whl/WeilaiShequShuzihuaCaozuoxitongBaipishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""weilai-shequ-shuzihua-caozuoxitong-baipishu
+https://github.com/apachecn/weilai-shequ-shuzihua-caozuoxitong-baipishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
