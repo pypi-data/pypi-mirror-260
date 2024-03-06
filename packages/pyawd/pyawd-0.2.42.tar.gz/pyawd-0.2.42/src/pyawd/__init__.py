45308ecb7bd773a5811149cf29bb3159fee6d074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:48 +0000

    Add package pyawd/0.2.42/pyawd-0.2.42.tar.gz

---
+++ packages/pyawd/pyawd-0.2.42.tar.gz/pyawd-0.2.42/src/pyawd/__init__.py
@@ -0,0 +1,5 @@
+# pyawd
+# Tribel Pascal - pascal.tribel@ulb.be
+from pyawd.ScalarAcousticWaveDataset import ScalarAcousticWaveDataset
+from pyawd.VectorialAcousticWaveDataset import VectorialAcousticWaveDataset
+from pyawd.Marmousi import Marmousi
