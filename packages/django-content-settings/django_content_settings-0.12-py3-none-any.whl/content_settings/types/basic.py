9d5c7c6469944f333fedf5f48961b10920faa09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:41 +0000

    Add package django-content-settings/0.12/django_content_settings-0.12-py3-none-any.whl

---
+++ packages/django-content-settings/django_content_settings-0.12-py3-none-any.whl/content_settings/types/basic.py
@@ -0,0 +1,368 @@
+from inspect import isclass
+from functools import cached_property
+from pprint import pformat
+from typing import Optional, Set, Tuple, Union, Any, Callable
+from collections.abc import Iterable
+from json import dumps
+
+from django import forms
+from django.core.serializers.json import DjangoJSONEncoder
+from django.core.exceptions import ValidationError
+
+from content_settings.context_managers import context_defaults_kwargs
+from content_settings.settings import CACHE_SPLITER
+from content_settings.types.lazy import LazyObject
+from content_settings.types import (
+    PREVIEW,
+    required,
+    optional,
+    pre,
+)
+from content_settings.types.mixins import HTMLMixin
+from content_settings.permissions import none, staff
+
+
+class BaseSetting:
+    pass
+
+
+class SimpleString(BaseSetting):
+    """
+     Attributes:
+    - constant (bool): Whether the setting is constant (can not be changed).
+    - cls_field (forms.CharField): The form field class to use for the setting.
+    - widget (forms.Widget): The form widget to use for the cls_field.
+    - widget_attrs (Optional[dict]): Optional attributes for the widget initiation.
+    - fetch_permission (Callable): Optional permission required to fetch the setting.
+    - update_permission (Callable): Optional permission required to update the setting.
+    - help_format (Optional[str]): Optional format string for the help text for the format (align to the type).
+    - help (Optional[str]): Optional help text for the setting.
+    - value_required (bool): Whether a value is required for the setting.
+    - version (str): The version of the setting (using for caching).
+    - tags (Optional[Iterable[str]]): Optional tags associated with the setting.
+    - validators (Tuple[Callable]): Validators to apply to the setting value.
+    - validators_raw (Tuple[Callable]): Validators to apply to the text value of the setting.
+    - admin_preview_as (PREVIEW): The format to use for the admin preview.
+    - suffixes (Tuple[str]): Suffixes that can be appended to the setting value.
+    - user_defined_slug (str): it contains a slug from db If the setting is defined in DB only (should not be set in content_settings)
+    - overwrite_user_defined (bool): Whether the setting can overwrite a user defined setting.
+    - default (str): The default value for the setting.
+    - on_change: Tuple[Callable] - list of functions to call when the setting is changed
+    - on_change_commited: Tuple[Callable] - list of functions to call when the setting is changed and commited
+    """
+
+    constant: bool = False
+    cls_field: forms.CharField = forms.CharField
+    widget: forms.Widget = forms.TextInput
+    widget_attrs: Optional[dict] = None
+    fetch_permission: Callable = staticmethod(none)
+    update_permission: Callable = staticmethod(staff)
+    view_permission: Callable = staticmethod(staff)
+    view_history_permission: Optional[Callable] = None
+    help_format: str = "string"
+    help: str = ""
+    value_required: bool = False
+    version: str = ""
+    tags: Optional[Iterable[str]] = None
+    validators: Tuple[Callable] = ()
+    validators_raw: Tuple[Callable] = ()
+    admin_preview_as: PREVIEW = PREVIEW.NONE
+    suffixes: Tuple[str] = ()
+    user_defined_slug: Optional[str] = None
+    overwrite_user_defined: bool = False
+    default: Union[str, required, optional] = ""
+    json_encoder = DjangoJSONEncoder
+    on_change: Tuple[Callable] = ()
+    on_change_commited: Tuple[Callable] = ()
+
+    def __init__(
+        self, default: Optional[Union[str, required, optional]] = None, **kwargs
+    ):
+        # optional support of help_text instead of help
+        if "help" not in kwargs and "help_text" in kwargs:
+            kwargs["help"] = kwargs.pop("help_text")
+
+        for k in kwargs.keys():
+            assert self.can_assign(k), "Attribute {} not found".format(k)
+
+        if default is not None:
+            self.default = default
+        assert self.default in (required, optional) or isinstance(
+            self.default, str
+        ), "Default should be str (or required or optional for special cases)"
+
+        kwargs = self.update_defaults_context(kwargs)
+        self.init_assign_kwargs(kwargs)
+
+        assert isinstance(self.version, str), "Version should be str"
+        assert (
+            CACHE_SPLITER not in self.version
+        ), f"Version should not contain CACHE_SPLITER:{CACHE_SPLITER}"
+        assert (
+            self.get_admin_preview_as() in PREVIEW
+        ), f"admin_preview_as should be one of {PREVIEW}"
+
+    def can_view(self, user):
+        return self.view_permission(user)
+
+    def can_view_history(self, user):
+        return (
+            self.can_view(user)
+            if self.view_history_permission is None
+            else self.view_history_permission(user)
+        )
+
+    def can_update(self, user):
+        return self.update_permission(user)
+
+    def can_fetch(self, user):
+        return self.fetch_permission(user)
+
+    def get_admin_preview_as(self) -> str:
+        return self.admin_preview_as
+
+    def get_on_change(self) -> Tuple:
+        return self.on_change
+
+    def get_on_change_commited(self) -> Tuple:
+        return self.on_change_commited
+
+    def init_assign_kwargs(self, kwargs):
+        for k, v in kwargs.items():
+            if not self.can_assign(k):
+                raise ValueError("Attribute {} not found".format(k))
+            if hasattr(self, "init__{}".format(k)):
+                getattr(self, "init__{}".format(k))(v)
+            else:
+                setattr(self, k, v)
+
+    def init__tags(self, tags: Union[None, str, Iterable[str]]) -> None:
+        if not tags:
+            return
+
+        if isinstance(tags, str):
+            tags = {tags}
+        else:
+            tags = set(tags)
+
+        self.tags = tags if self.tags is None else self.tags | tags
+
+    @cached_property
+    def field(self) -> forms.Field:
+        return self.get_field()
+
+    def get_suffixes(self) -> Tuple:
+        return self.suffixes
+
+    def can_suffix(self, suffix: Optional[str]) -> bool:
+        return suffix is None or suffix in self.get_suffixes()
+
+    def can_assign(self, name: str) -> bool:
+        return hasattr(self, name)
+
+    def update_defaults_context(self, kwargs: dict) -> dict:
+        # initiate default values
+        context_defaults = {
+            name: value
+            for name, value in context_defaults_kwargs().items()
+            if self.can_assign(name)
+        }
+
+        # update actual values
+        return {
+            name: value
+            for name, value in context_defaults_kwargs(
+                {
+                    **context_defaults,
+                    **kwargs,
+                }
+            ).items()
+            if self.can_assign(name)
+        }
+
+    def get_help_format(self) -> Iterable[str]:
+        yield self.help_format
+
+    def get_help(self) -> str:
+        help = self.help
+        help_format = "".join(self.get_help_format())
+
+        if help and help_format:
+            return f"{help}<br><br>{help_format}"
+
+        return help or help_format
+
+    def get_tags(self) -> Set[str]:
+        tags = self.tags
+        if tags is None:
+            tags = set()
+        return set(tags)
+
+    def get_content_tags(self, value: str) -> Set[str]:
+        from content_settings.conf import gen_tags
+
+        return gen_tags(self, value)
+
+    def get_validators(self) -> Tuple[Callable]:
+        return tuple(self.validators) + tuple(self.cls_field.default_validators)
+
+    def get_validators_raw(self) -> Tuple[Callable]:
+        return tuple(self.validators_raw)
+
+    def get_field(self) -> forms.Field:
+        return self.cls_field(
+            widget=self.get_widget(),
+            validators=(self.validate_value,),
+            required=self.value_required,
+        )
+
+    def get_widget(self) -> forms.Widget:
+        if isclass(self.widget) and self.widget_attrs is not None:
+            return self.widget(attrs=self.widget_attrs)
+        else:
+            return self.widget
+
+    def validate_raw_value(self, value: str) -> None:
+        for validator in self.get_validators_raw():
+            validator(value)
+
+    def validate_value(self, value: str) -> Any:
+        if self.value_required and not value:
+            raise ValidationError("This field is required")
+        self.validate_raw_value(value)
+        val = self.to_python(value)
+        self.validate(val)
+
+    def validate(self, value):
+        for validator in self.get_validators():
+            validator(value)
+
+    def to_python(self, value: str) -> Any:
+        return self.field.to_python(value)
+
+    def json_view_value(self, value: Any, **kwargs) -> Any:
+        return dumps(value, cls=self.json_encoder)
+
+    def give_python_to_admin(self, value: str, name: str, **kwargs) -> Any:
+        return self.to_python(value)
+
+    def get_admin_preview_html(self, value: Any, name: str, **kwargs) -> Any:
+        return value
+
+    def get_admin_preview_text(self, value: Any, name: str, **kwargs) -> Any:
+        return value
+
+    def get_admin_preview_python(self, value: Any, name: str, **kwargs) -> Any:
+        return value
+
+    def get_admin_preview_value(self, value: str, *args, **kwargs) -> str:
+        if self.get_admin_preview_as() == PREVIEW.NONE:
+            return ""
+
+        value = self.give_python_to_admin(value, *args, **kwargs)
+
+        return str(self.get_admin_preview_object(value, *args, **kwargs))
+
+    def get_full_admin_preview_value(self, value: str, *args, **kwargs) -> str:
+        try:
+            self.validate_value(value)
+        except Exception as e:
+            return {
+                "error": str(e),
+            }
+
+        return {
+            "html": self.get_admin_preview_value(value, *args, **kwargs),
+        }
+
+    def get_admin_preview_object(self, value: str, name: str, **kwargs) -> str:
+        if self.get_admin_preview_as() == PREVIEW.HTML:
+            return str(self.get_admin_preview_html(value, name, **kwargs))
+
+        if self.get_admin_preview_as() == PREVIEW.TEXT:
+            value = str(self.get_admin_preview_text(value, name, **kwargs))
+        else:
+            value = pformat(self.get_admin_preview_python(value, name, **kwargs))
+
+        return pre(value)
+
+    def lazy_give(self, l_func: Callable, suffix=None) -> LazyObject:
+        return LazyObject(l_func)
+
+    def give(self, value, suffix: Optional[str] = None):
+        return value
+
+    def give_python(self, value: str, suffix=None) -> Any:
+        return self.give(self.to_python(value), suffix)
+
+
+class SimpleText(SimpleString):
+    widget: forms.Widget = forms.Textarea
+    widget_attrs: dict = {"rows": 10, "cols": 80}
+
+
+class SimpleTextPreview(SimpleText):
+    admin_preview_as: PREVIEW = PREVIEW.TEXT
+
+
+class SimpleHTML(HTMLMixin, SimpleText):
+    pass
+
+
+class URLString(SimpleString):
+    cls_field: forms.Field = forms.URLField
+    widget: forms.Widget = forms.URLInput
+    help_format: str = "URL"
+    widget_attrs: dict = {"style": "max-width: 600px; width: 100%"}
+
+
+class EmailString(SimpleString):
+    cls_field: forms.Field = forms.EmailField
+    widget: forms.Widget = forms.EmailInput
+    help_format: str = "Email"
+    widget_attrs: dict = {"style": "max-width: 600px; width: 100%"}
+
+
+class SimpleInt(SimpleString):
+    admin_preview_as: PREVIEW = PREVIEW.PYTHON
+    cls_field: forms.Field = forms.IntegerField
+    help_format: str = "Any number"
+
+
+class SimpleBool(SimpleString):
+    admin_preview_as: PREVIEW = PREVIEW.PYTHON
+    yeses: Tuple[str] = ("yes", "true", "1", "+", "ok")
+    noes: Tuple[str] = ("no", "not", "false", "0", "-", "")
+
+    def to_python(self, value) -> bool:
+        value = value.lower().strip()
+        if value in self.yeses:
+            return True
+        if value in self.noes:
+            return False
+        return None
+
+    @property
+    def explained_accepted_values(self):
+        return [f"'{v}'" if v else "empty" for v in self.yeses + self.noes]
+
+    def validate(self, value):
+        if value is None:
+            raise ValidationError(
+                f"Value cannot be {', '.join(self.explained_accepted_values)} only"
+            )
+
+    def get_help_format(self):
+        yield "boolean (True/False) value. "
+        yield f"Accepted values: {', '.join(self.explained_accepted_values)}"
+
+
+class SimpleDecimal(SimpleString):
+    admin_preview_as: PREVIEW = PREVIEW.PYTHON
+    cls_field: forms.Field = forms.DecimalField
+    help_format: str = "Decimal number with floating point"
+
+
+class SimplePassword(SimpleString):
+    admin_preview_as: PREVIEW = PREVIEW.NONE
+    widget_attrs: dict = {"type": "password"}
---
+++ packages/django-content-settings/django_content_settings-0.12-py3-none-any.whl/content_settings/types/mixins.py
@@ -0,0 +1,336 @@
+from typing import Any, Optional
+from pprint import pformat
+
+from django.core.exceptions import ValidationError
+from django.utils.safestring import mark_safe
+
+from .validators import call_validator
+from . import PREVIEW, pre
+
+
+def mix(*cls):
+    return type("", cls, {})
+
+
+class MinMaxValidationMixin:
+    min_value = None
+    max_value = None
+
+    def validate(self, value):
+        super().validate(value)
+
+        is_none_valid = self.min_value is None and self.max_value is None
+        if not is_none_valid and value is None:
+            raise ValidationError("Value cannot be None")
+
+        if self.min_value is not None and value < self.min_value:
+            raise ValidationError(f"Value cannot be less than {self.min_value}")
+
+        if self.max_value is not None and value > self.max_value:
+            raise ValidationError(f"Value cannot be greater than {self.max_value}")
+
+    def get_help_format(self):
+        yield from super().get_help_format()
+
+        if self.min_value is not None:
+            yield f" from {self.min_value}"
+
+        if self.max_value is not None:
+            yield f" to {self.max_value}"
+
+
+class EmptyNoneMixin:
+    value_required = False
+
+    def to_python(self, value):
+        if value.strip() == "":
+            return None
+        return super().to_python(value)
+
+
+class HTMLMixin:
+    admin_preview_as: PREVIEW = PREVIEW.HTML
+
+    def get_help_format(self):
+        yield from super().get_help_format()
+        yield " in HTML format"
+
+    def give(self, value, suffix=None):
+        return mark_safe(super().give(value, suffix))
+
+
+class PositiveValidationMixin(MinMaxValidationMixin):
+    min_value = 0
+
+
+class CallToPythonMixin:
+    call_func = None
+
+    def prepare_python_call(self, value):
+        return {"prepared": value}
+
+    def get_preview_validators(self):
+        return [
+            validator
+            for validator in self.get_validators()
+            if isinstance(validator, call_validator)
+        ]
+
+    def get_call_func(self):
+        return self.call_func
+
+    def python_call(self, *args, **kwargs):
+        return self.call_func(*args, **kwargs)
+
+    def to_python(self, value):
+        value = super().to_python(value)
+        prepared_kwargs = self.prepare_python_call(value)
+
+        def _(*args, **kwargs):
+            return self.python_call(*args, **prepared_kwargs, **kwargs)
+
+        return _
+
+    def give_python_to_admin(self, value, name, **kwargs):
+        try:
+            value = self.to_python(value)
+        except Exception as e:
+            return [(None, e)]
+        ret = []
+        for validator in self.get_preview_validators():
+            str_validator = f"{name}({str(validator)})"
+            try:
+                ret.append((str_validator, validator(value)))
+            except Exception as e:
+                ret.append((str_validator, e))
+        return ret
+
+    def get_admin_preview_object(self, value, name, **kwargs):
+        admin_preview_as = self.get_admin_preview_as()
+        if admin_preview_as == PREVIEW.NONE:
+            return ""
+
+        if not value:
+            return "No preview (add at least one call_validator in validators)"
+
+        if len(value) == 1 and admin_preview_as in (PREVIEW.TEXT, PREVIEW.HTML):
+            value = value[0][1]
+            if isinstance(value, Exception):
+                return f"ERROR!!! {value}"
+            if admin_preview_as == PREVIEW.TEXT:
+                return pre(value)
+            else:
+                return str(value)
+
+        def _():
+            for validator, val in value:
+                yield pre(f">>> {validator}")
+
+                if isinstance(val, Exception):
+                    yield f"ERROR!!! {val}"
+                else:
+                    if admin_preview_as == PREVIEW.TEXT:
+                        yield pre(val)
+                    elif admin_preview_as == PREVIEW.HTML:
+                        yield str(val)
+                    else:
+                        yield pre("<<< " + pformat(val))
+
+        return "\n".join(_())
+
+
+class GiveCallMixin:
+    """
+    for Template-like classes, when to_python should return a function,
+    but as value you want to return not a function, but call it and return result
+    """
+
+    def get_suffixes(self):
+        return ("call",) + super().get_suffixes()
+
+    def get_validators(self):
+        return (call_validator(),) + super().get_validators()
+
+    def get_validators(self):
+        validators = super().get_validators()
+        if not validators:
+            return (call_validator(),)
+        return validators
+
+    def give(self, value, suffix=None):
+        if suffix is None:
+            return value()
+        if suffix == "call":
+            return value
+        return super().give(value, suffix)
+
+
+class MakeCallMixin:
+    def get_suffixes(self):
+        return ("call",) + super().get_suffixes()
+
+    def give_python_to_admin(self, value, name, **kwargs):
+        return self.give_python(value)()
+
+    def give(self, value, suffix=None):
+        return lambda *args, **kwargs: value
+
+
+class DictSuffixesMixin:
+    suffixes = {}
+
+    def give(self, value, suffix=None):
+        if suffix is None:
+            return value
+        return self.suffixes[suffix](value)
+
+
+class AdminPreviewMenuMixin:
+    def gen_admin_preview_html_menu_items(self, value: Any, name: str, **kwargs) -> str:
+        return
+        yield
+
+    def get_admin_preview_html_menu(self, value: Any, name: str, **kwargs) -> str:
+        html_items = "".join(
+            self.gen_admin_preview_html_menu_items(value, name, **kwargs)
+        )
+        if not html_items:
+            return ""
+        return "<div>" + html_items + "</div>"
+
+    def get_admin_preview_object(self, value: str, name: str, **kwargs) -> str:
+
+        return self.get_admin_preview_html_menu(
+            value, name, **kwargs
+        ) + self.get_admin_preview_under_menu_object(value, name, **kwargs)
+
+    def get_admin_preview_under_menu_object(
+        self, value: Any, name: str, **kwargs
+    ) -> str:
+        return super().get_admin_preview_object(value, name, **kwargs)
+
+
+class AdminSuffixesMixinPreview:
+    admin_preview_suffixes = ()
+    admin_preview_suffixes_default = "default"
+
+    def get_admin_preview_suffixes_default(self):
+        return self.admin_preview_suffixes_default
+
+    def get_admin_preview_suffixes(self, value: str, name: str, **kwargs):
+        return self.admin_preview_suffixes
+
+    def gen_admin_preview_html_menu_items(
+        self, value: Any, name: str, suffix: Optional[str] = None, **kwargs
+    ) -> str:
+        suffixes = self.get_admin_preview_suffixes(value, name, **kwargs)
+        if not len(suffixes):
+            return
+
+        for suf in (None, *suffixes):
+            if suf == suffix:
+                yield f" <b>{suf or self.get_admin_preview_suffixes_default()}</b> "
+            elif suf is None:
+                yield f' <a class="cs_set_params">{self.get_admin_preview_suffixes_default()}</a> '
+            else:
+                yield f' <a class="cs_set_params" data-param-suffix="{suf}">{suf}</a> '
+
+        yield from super().gen_admin_preview_html_menu_items(
+            value, name, suffix=suffix, **kwargs
+        )
+
+    def get_admin_preview_under_menu_object(
+        self, value: Any, name: str, **kwargs
+    ) -> str:
+        suffix = kwargs.get("suffix", None)
+        return super().get_admin_preview_under_menu_object(
+            self.give(value, suffix), name, **kwargs
+        )
+
+
+class AdminPreviewSuffixesMixin(AdminSuffixesMixinPreview, AdminPreviewMenuMixin):
+    pass
+
+
+class ActionResponse(dict):
+    def error(self, message):
+        self["error"] = message
+        return self
+
+    def html(self, html):
+        self["html"] = html
+        return self
+
+    def alert(self, message):
+        self["alert"] = message
+        return self
+
+    def value(self, value):
+        self["value"] = value
+        return self
+
+    def before_html(self, html):
+        self["before_html"] = html
+        return self
+
+
+class AdminActionsMixinPreview:
+    # list of tuples (name, function)
+    admin_preview_actions = None
+
+    def gen_admin_preview_html_menu_items(
+        self, value: Any, name: str, suffix: Optional[str] = None, **kwargs
+    ) -> str:
+        if self.admin_preview_actions is None:
+            return
+
+        for i, (action_name, _) in enumerate(self.admin_preview_actions):
+            yield f' <a class="cs_set_params cs_set_params_once" data-param-action="{i}">{action_name}</a> '
+
+    def get_admin_preview_function(self, *args, **kwargs):
+        if self.admin_preview_actions is None:
+            return
+
+        action_name = kwargs.get("action", None)
+        if action_name is None:
+            return
+
+        try:
+            return self.admin_preview_actions[int(action_name)][1]
+        except (IndexError, ValueError):
+            return
+
+    def process_action(self, *args, **kwargs):
+        func = self.get_admin_preview_function(*args, **kwargs)
+        if func is None:
+            return
+
+        response = ActionResponse()
+        func(response, *args, _cs_type=self, **kwargs)
+        return response
+
+    def get_full_admin_preview_value(self, *args, **kwargs) -> str:
+        response = self.process_action(*args, **kwargs)
+        if not response:
+            return super().get_full_admin_preview_value(*args, **kwargs)
+
+        response = dict(response)
+
+        before_html = response.pop("before_html", "")
+        if before_html:
+            response = {
+                **response,
+                **super().get_full_admin_preview_value(*args, **kwargs),
+            }
+            response["html"] = before_html + response["html"]
+
+        return response
+
+
+class AdminPreviewActionsMixin(AdminActionsMixinPreview, AdminPreviewMenuMixin):
+    pass
+
+
+class DictSuffixesPreviewMixin(DictSuffixesMixin, AdminPreviewSuffixesMixin):
+    def get_admin_preview_suffixes(self, value: str, name: str, **kwargs):
+        return tuple(self.suffixes.keys())
