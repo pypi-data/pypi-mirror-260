aeb62167813346271bf2367a836511bba239df5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:45 +0000

    Add package ob-metaflow-stubs/2.11.4.9/ob-metaflow-stubs-2.11.4.9.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-2.11.4.9.tar.gz/ob-metaflow-stubs-2.11.4.9/metaflow-stubs/plugins/aws/step_functions/production_token.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.4.9                                                           #
+# Generated on 2024-03-05T22:22:04.933039                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def new_token(token_prefix, prev_token = None):
+    ...
+
+def load_token(token_prefix):
+    ...
+
+def store_token(token_prefix, token):
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-2.11.4.9.tar.gz/ob-metaflow-stubs-2.11.4.9/metaflow-stubs/plugins/pypi/__init__.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.4.9                                                           #
+# Generated on 2024-03-05T22:22:04.873491                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+class MetaflowException(Exception, metaclass=type):
+    def __init__(self, msg = "", lineno = None):
+        ...
+    def __str__(self):
+        ...
+    ...
+
+MAGIC_FILE: str
+
