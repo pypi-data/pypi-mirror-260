384cfcf793e04dc62f62ec360191160df2d79f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:45 +0000

    Add package hulianwang-java-gongchengshi-jinjie-zhishi-wanquan-saomang/2024.3.6.0/hulianwang_java_gongchengshi_jinjie_zhishi_wanquan_saomang-2024.3.6.0-py3-none-any.whl

---
+++ packages/hulianwang-java-gongchengshi-jinjie-zhishi-wanquan-saomang/hulianwang_java_gongchengshi_jinjie_zhishi_wanquan_saomang-2024.3.6.0-py3-none-any.whl/HulianwangJavaGongchengshiJinjieZhishiWanquanSaomang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hulianwang-java-gongchengshi-jinjie-zhishi-wanquan-saomang
+https://github.com/apachecn/hulianwang-java-gongchengshi-jinjie-zhishi-wanquan-saomang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
