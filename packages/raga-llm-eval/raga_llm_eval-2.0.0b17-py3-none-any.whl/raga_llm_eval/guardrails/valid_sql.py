10f4b62665d86943b2f9164f1904aa4d05a15890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:31 +0000

    Add package raga-llm-eval/2.0.0b17/raga_llm_eval-2.0.0b17-py3-none-any.whl

---
+++ packages/raga-llm-eval/raga_llm_eval-2.0.0b17-py3-none-any.whl/raga_llm_eval/guardrails/valid_sql.py
@@ -0,0 +1,35 @@
+import sqlvalidator
+
+class ValidSql:
+    """
+    Checks whether provided sql query is correct or not.
+    """
+
+    def __init__(
+        self,
+        prompt,
+    ):
+        """
+        Initializes the ValidSql class.
+
+        Args:
+        - prompt: str, the text to be evaluated for nsfw.
+        """
+        self.prompt = prompt
+
+
+    def run(self):
+        sql_query = sqlvalidator.parse(self.prompt)
+        res = ""
+        if not sql_query.is_valid():
+            res = "Failed"
+        else:
+            res = "Passed"
+            
+       
+        result = {
+            "prompt": self.prompt,
+            "is_passed": res
+        }
+
+        return result
