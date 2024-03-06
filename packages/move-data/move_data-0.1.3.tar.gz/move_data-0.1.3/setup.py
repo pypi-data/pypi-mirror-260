9043f17ad49bdf20df9bd3dbacaa22a0b54cacef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:39 +0000

    Add package move-data/0.1.3/move_data-0.1.3.tar.gz

---
+++ packages/move-data/move_data-0.1.3.tar.gz/move_data-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='move_data',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your package requires to run
+        'snowflake-connector-python[pandas]',
+        'python-dotenv',
+        'pandas',
+        'pygsheets',
+        'google-cloud-storage',
+    ],
+)
+
