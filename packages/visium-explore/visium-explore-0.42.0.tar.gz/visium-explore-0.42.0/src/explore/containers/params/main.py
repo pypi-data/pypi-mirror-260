c1d378bed2c58157d87ab3a9b68fc8b6834ad459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:05:39 +0000

    Add package visium-explore/0.42.0/visium-explore-0.42.0.tar.gz

---
+++ packages/visium-explore/visium-explore-0.42.0.tar.gz/visium-explore-0.42.0/src/explore/containers/params/main.py
@@ -0,0 +1,11 @@
+"""Params container."""
+import dvc.api
+import pandas as pd
+import streamlit as st
+
+
+def params_container():
+    st.subheader("Params")
+    params = dvc.api.params_show()
+    df_params = pd.DataFrame([params], index=["params"])
+    st.write(df_params)
