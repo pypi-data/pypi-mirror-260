47d81a7fb13e5b0acad90f521ca278a6f7cd42d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:06:17 +0000

    Add package AndTools/0.2.6/AndTools-0.2.6.tar.gz

---
+++ packages/AndTools/AndTools-0.2.6.tar.gz/AndTools-0.2.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+# with open("README.md", "r", encoding='utf-8') as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="AndTools",
+    version="0.2.6",
+    author="1x",
+    author_email="111111@gmail.com",
+    description="Android Tools for Python",
+    long_description='Android Tools for Python',
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
