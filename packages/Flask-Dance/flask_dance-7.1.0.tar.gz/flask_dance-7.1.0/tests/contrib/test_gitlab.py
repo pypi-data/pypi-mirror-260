14d92696e461fdfbfc20f74520ee004f01e1a7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:05:25 +0000

    Add package Flask-Dance/7.1.0/flask_dance-7.1.0.tar.gz

---
+++ packages/Flask-Dance/flask_dance-7.1.0.tar.gz/flask_dance-7.1.0/tests/contrib/test_gitlab.py
@@ -0,0 +1,164 @@
+import pytest
+import responses
+from flask import Flask
+from urlobject import URLObject
+
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+from flask_dance.consumer.storage import MemoryStorage
+from flask_dance.contrib.gitlab import gitlab, make_gitlab_blueprint
+
+
+@pytest.fixture
+def make_app():
+    "A callable to create a Flask app with the GitLab provider"
+
+    def _make_app(*args, **kwargs):
+        app = Flask(__name__)
+        app.config["SERVER_NAME"] = "a.b.c"
+        app.secret_key = "whatever"
+        blueprint = make_gitlab_blueprint(*args, **kwargs)
+        app.register_blueprint(blueprint)
+        return app
+
+    return _make_app
+
+
+def test_blueprint_factory_default():
+    # Test with gitlab.com
+    glbp = make_gitlab_blueprint(
+        client_id="foo", client_secret="bar", scope="read_user", redirect_to="index"
+    )
+    assert isinstance(glbp, OAuth2ConsumerBlueprint)
+    assert glbp.session.scope == "read_user"
+    assert glbp.session.base_url == "https://gitlab.com/api/v4/"
+    assert glbp.session.client_id == "foo"
+    assert glbp.client_secret == "bar"
+    assert glbp.authorization_url == "https://gitlab.com/oauth/authorize"
+    assert glbp.token_url == "https://gitlab.com/oauth/token"
+
+
+def test_blueprint_factory_custom():
+    glbp = make_gitlab_blueprint(
+        client_id="foo",
+        client_secret="bar",
+        scope="read_user",
+        redirect_to="index",
+        hostname="git.example.com",
+    )
+    assert isinstance(glbp, OAuth2ConsumerBlueprint)
+    assert glbp.session.scope == "read_user"
+    assert glbp.session.base_url == "https://git.example.com/api/v4/"
+    assert glbp.session.client_id == "foo"
+    assert glbp.client_secret == "bar"
+    assert glbp.authorization_url == "https://git.example.com/oauth/authorize"
+    assert glbp.token_url == "https://git.example.com/oauth/token"
+
+
+def test_load_from_config(make_app):
+    app = make_app()
+    app.config["GITLAB_OAUTH_CLIENT_ID"] = "foo"
+    app.config["GITLAB_OAUTH_CLIENT_SECRET"] = "bar"
+
+    resp = app.test_client().get("/gitlab")
+    url = resp.headers["Location"]
+    client_id = URLObject(url).query.dict.get("client_id")
+    assert client_id == "foo"
+
+
+@responses.activate
+def test_context_local(make_app):
+    responses.add(responses.GET, "https://google.com")
+
+    # set up two apps with two different set of auth tokens
+    app1 = make_app(
+        "foo1",
+        "bar1",
+        redirect_to="url1",
+        storage=MemoryStorage({"access_token": "app1"}),
+    )
+    app2 = make_app(
+        "foo2",
+        "bar2",
+        redirect_to="url2",
+        storage=MemoryStorage({"access_token": "app2"}),
+    )
+
+    # outside of a request context, referencing functions on the `gitlab` object
+    # will raise an exception
+    with pytest.raises(RuntimeError):
+        gitlab.get("https://google.com")
+
+    # inside of a request context, `gitlab` should be a proxy to the correct
+    # blueprint session
+    with app1.test_request_context("/"):
+        app1.preprocess_request()
+        gitlab.get("https://google.com")
+        request = responses.calls[0].request
+        assert request.headers["Authorization"] == "Bearer app1"
+
+    with app2.test_request_context("/"):
+        app2.preprocess_request()
+        gitlab.get("https://google.com")
+        request = responses.calls[1].request
+        assert request.headers["Authorization"] == "Bearer app2"
+
+
+def test_no_verify_token(make_app, mocker):
+    app = make_app(
+        "foo",
+        "bar",
+        hostname="my-insecure-gitlab.com",
+        storage=MemoryStorage(),
+        verify_tls_certificates=False,
+    )
+
+    with responses.RequestsMock() as rsps:
+        mock_on_request = mocker.patch.object(
+            rsps, "_on_request", wraps=rsps._on_request
+        )
+        rsps.add(
+            responses.POST,
+            "https://my-insecure-gitlab.com/oauth/token",
+            body='{"access_token":"foobar","token_type":"bearer","scope":"admin"}',
+        )
+        with app.test_client() as client:
+            # reset the session before the request
+            with client.session_transaction() as sess:
+                sess["gitlab_oauth_state"] = "random-string"
+            # make the request
+            resp = client.get(
+                "/gitlab/authorized?code=secret-code&state=random-string",
+                base_url="https://a.b.c",
+            )
+            # check that `verify=False` in the token request
+            mock_on_request.assert_called()
+            call_verify = mock_on_request.call_args[1].get("verify", True)
+            assert call_verify is False
+
+
+def test_no_verify_api_call(make_app, mocker):
+    app = make_app(
+        "foo",
+        "bar",
+        hostname="my-insecure-gitlab.com",
+        storage=MemoryStorage({"access_token": "fake-token"}),
+        verify_tls_certificates=False,
+    )
+
+    with responses.RequestsMock() as rsps:
+        mock_on_request = mocker.patch.object(
+            rsps, "_on_request", wraps=rsps._on_request
+        )
+        rsps.add(
+            method=responses.GET,
+            url="https://my-insecure-gitlab.com",
+            body="insecure but OK",
+        )
+        with app.test_request_context("/"):
+            app.preprocess_request()
+            gitlab.get("https://my-insecure-gitlab.com")
+
+        # check that `verify=False` in the API call
+        mock_on_request.assert_called()
+        call_verify = mock_on_request.call_args[1].get("verify", True)
+        assert call_verify is False
---
+++ packages/Flask-Dance/flask_dance-7.1.0.tar.gz/flask_dance-7.1.0/tests/contrib/test_jira.py
@@ -0,0 +1,203 @@
+import os
+import tempfile
+from unittest import mock
+
+import pytest
+import responses
+from flask import Flask
+from oauthlib.oauth1.rfc5849.utils import parse_authorization_header
+
+from flask_dance.consumer import OAuth1ConsumerBlueprint
+from flask_dance.consumer.storage import MemoryStorage
+from flask_dance.contrib.jira import jira, make_jira_blueprint
+
+
+@pytest.fixture
+def make_app():
+    "A callable to create a Flask app with the JIRA provider"
+
+    def _make_app(*args, **kwargs):
+        app = Flask(__name__)
+        app.secret_key = "whatever"
+        blueprint = make_jira_blueprint(*args, **kwargs)
+        app.register_blueprint(blueprint)
+        return app
+
+    return _make_app
+
+
+def test_blueprint_factory():
+    jira_bp = make_jira_blueprint(
+        consumer_key="foobar",
+        rsa_key="supersecret",
+        base_url="https://flask.atlassian.net",
+        redirect_to="index",
+    )
+    assert isinstance(jira_bp, OAuth1ConsumerBlueprint)
+    assert jira_bp.session.base_url == "https://flask.atlassian.net"
+    assert jira_bp.session.auth.client.client_key == "foobar"
+    assert jira_bp.session.auth.client.rsa_key == "supersecret"
+    assert (
+        jira_bp.request_token_url
+        == "https://flask.atlassian.net/plugins/servlet/oauth/request-token"
+    )
+    assert (
+        jira_bp.access_token_url
+        == "https://flask.atlassian.net/plugins/servlet/oauth/access-token"
+    )
+    assert (
+        jira_bp.authorization_url
+        == "https://flask.atlassian.net/plugins/servlet/oauth/authorize"
+    )
+
+
+def test_blueprint_factory_rule_kwargs(make_app):
+    app = make_app(
+        "https://flask.atlassian.net",
+        redirect_to="index",
+        rule_kwargs={"host": "example2.com"},
+    )
+    rules = [
+        rule for rule in app.url_map.iter_rules() if rule.endpoint.startswith("jira.")
+    ]
+    assert all(rule.host == "example2.com" for rule in rules)
+    assert len(rules) == 2
+
+
+def test_blueprint_factory_no_rule_kwargs(make_app):
+    app = make_app(
+        "https://flask.atlassian.net",
+        redirect_to="index",
+    )
+    rules = [
+        rule for rule in app.url_map.iter_rules() if rule.endpoint.startswith("jira.")
+    ]
+    assert all(rule.host is None for rule in rules)
+    assert len(rules) == 2
+
+
+def test_rsa_key_file(tmp_path):
+    rsa_key = tmp_path / "fake.key"
+    rsa_key.write_text("my-fake-key")
+
+    jira_bp = make_jira_blueprint(
+        rsa_key=str(rsa_key), base_url="https://flask.atlassian.net"
+    )
+    assert jira_bp.rsa_key == "my-fake-key"
+
+
+@responses.activate
+@mock.patch(
+    "oauthlib.oauth1.rfc5849.Client.get_oauth_signature", return_value="fakesig"
+)
+def test_load_from_config(sign_func, make_app):
+    responses.add(
+        responses.POST,
+        "https://flask.atlassian.net/plugins/servlet/oauth/request-token",
+        body="oauth_token=faketoken&oauth_token_secret=fakesecret",
+    )
+    app = make_app("https://flask.atlassian.net", redirect_to="index")
+    app.config["JIRA_OAUTH_CONSUMER_KEY"] = "foo"
+    app.config["JIRA_OAUTH_RSA_KEY"] = "bar"
+
+    resp = app.test_client().get("/jira")
+    assert len(responses.calls) > 0
+    auth_header = dict(
+        parse_authorization_header(
+            responses.calls[0].request.headers["Authorization"].decode("utf-8")
+        )
+    )
+    assert auth_header["oauth_consumer_key"] == "foo"
+    sign_func.assert_called()
+    request = sign_func.call_args[0][0]
+    assert dict(request.oauth_params)["oauth_signature"] == "fakesig"
+
+
+@responses.activate
+@mock.patch(
+    "oauthlib.oauth1.rfc5849.Client.get_oauth_signature", return_value="fakesig"
+)
+def test_content_type(sign_func, make_app):
+    responses.add(responses.GET, "https://flask.atlassian.net/")
+
+    storage = MemoryStorage(
+        {
+            "oauth_token": "faketoken",
+            "oauth_token_secret": "fakesecret",
+            "oauth_session_handle": "fakehandle",
+            "oauth_expires_in": "157680000",
+            "oauth_authorization_expires_in": "160272000",
+        }
+    )
+    app = make_app(
+        "https://flask.atlassian.net",
+        rsa_key="fakersa",
+        consumer_key="fakekey",
+        storage=storage,
+    )
+
+    @app.route("/test")
+    def api_request():
+        jira.get("/")
+        return "success"
+
+    resp = app.test_client().get("/test")
+    assert len(responses.calls) > 0
+    headers = responses.calls[0].request.headers
+    assert "Content-Type" in headers
+    assert headers["Content-Type"] == b"application/json"
+
+
+@responses.activate
+def test_context_local():
+    responses.add(responses.GET, "https://google.com")
+
+    # set up two apps with two different set of auth tokens
+    app1 = Flask(__name__)
+    jbp1 = make_jira_blueprint(
+        "https://t1.atlassian.com", "foo1", "bar1", redirect_to="url1"
+    )
+    app1.register_blueprint(jbp1)
+
+    app2 = Flask(__name__)
+    jbp2 = make_jira_blueprint(
+        "https://t2.atlassian.com", "foo2", "bar2", redirect_to="url2"
+    )
+    app2.register_blueprint(jbp2)
+
+    # outside of a request context, referencing functions on the `jira` object
+    # will raise an exception
+    with pytest.raises(RuntimeError):
+        jira.get("https://google.com")
+
+    # inside of a request context, `jira` should be a proxy to the correct
+    # blueprint session
+    with app1.test_request_context("/"):
+        jbp1.session.auth.client.get_oauth_signature = mock.Mock(return_value="sig1")
+        jbp2.session.auth.client.get_oauth_signature = mock.Mock(return_value="sig2")
+
+        app1.preprocess_request()
+        jira.get("https://google.com")
+        assert len(responses.calls) > 0
+        auth_header = dict(
+            parse_authorization_header(
+                responses.calls[0].request.headers["Authorization"].decode("utf-8")
+            )
+        )
+        assert auth_header["oauth_consumer_key"] == "foo1"
+        assert auth_header["oauth_signature"] == "sig1"
+
+    with app2.test_request_context("/"):
+        jbp1.session.auth.client.get_oauth_signature = mock.Mock(return_value="sig1")
+        jbp2.session.auth.client.get_oauth_signature = mock.Mock(return_value="sig2")
+
+        app2.preprocess_request()
+        jira.get("https://google.com")
+        assert len(responses.calls) > 0
+        auth_header = dict(
+            parse_authorization_header(
+                responses.calls[1].request.headers["Authorization"].decode("utf-8")
+            )
+        )
+        assert auth_header["oauth_consumer_key"] == "foo2"
+        assert auth_header["oauth_signature"] == "sig2"
