650981e38815dd61382685d38c3bc5207275ff3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:13 +0000

    Add package xiao-chengxu-da-shijie/2024.3.6.0/xiao_chengxu_da_shijie-2024.3.6.0-py3-none-any.whl

---
+++ packages/xiao-chengxu-da-shijie/xiao_chengxu_da_shijie-2024.3.6.0-py3-none-any.whl/XiaoChengxuDaShijie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiao-chengxu-da-shijie
+https://github.com/apachecn/xiao-chengxu-da-shijie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
