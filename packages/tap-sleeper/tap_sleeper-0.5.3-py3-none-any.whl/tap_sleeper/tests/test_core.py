add1bbdbe626724c26ea0ed35e46fda362eb7291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:04:19 +0000

    Add package tap-sleeper/0.5.3/tap_sleeper-0.5.3-py3-none-any.whl

---
+++ packages/tap-sleeper/tap_sleeper-0.5.3-py3-none-any.whl/tap_sleeper/tests/test_core.py
@@ -0,0 +1,19 @@
+"""Tests standard tap features using the built-in SDK tests library."""
+
+import os
+
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
+
+from tap_sleeper.tap import TapSleeper
+
+SAMPLE_CONFIG = {
+    "sport": "nfl",
+    "league_id": os.environ.get("SAMPLE_CONFIG_LEAGUE_ID", ""),
+}
+
+# Run standard built-in tap tests from the SDK:
+TestTapSleeper = get_tap_test_class(
+    tap_class=TapSleeper,
+    config=SAMPLE_CONFIG,
+    suite_config=SuiteConfig(max_records_limit=10, ignore_no_records=True),
+)
