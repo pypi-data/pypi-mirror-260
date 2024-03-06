10ae9ae17c082bb4d46233b1614d18aba2bf79aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:04:52 +0000

    Add package algorithhmicas/3.2/algorithhmicas-3.2.tar.gz

---
+++ packages/algorithhmicas/algorithhmicas-3.2.tar.gz/algorithhmicas-3.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='algorithhmicas',
+    version='3.2',
+    author='Your Name',
+    author_email='cinnabonwithcinnamon@email.com',
+    description='Description of library',
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 'requests'
+    ]
+)
