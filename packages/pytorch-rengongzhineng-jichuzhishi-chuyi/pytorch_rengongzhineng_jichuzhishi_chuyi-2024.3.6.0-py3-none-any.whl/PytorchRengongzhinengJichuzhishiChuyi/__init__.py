faa8b97841ba9150fc5b802751d477d0a4eb46cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:04:48 +0000

    Add package pytorch-rengongzhineng-jichuzhishi-chuyi/2024.3.6.0/pytorch_rengongzhineng_jichuzhishi_chuyi-2024.3.6.0-py3-none-any.whl

---
+++ packages/pytorch-rengongzhineng-jichuzhishi-chuyi/pytorch_rengongzhineng_jichuzhishi_chuyi-2024.3.6.0-py3-none-any.whl/PytorchRengongzhinengJichuzhishiChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pytorch-rengongzhineng-jichuzhishi-chuyi
+https://github.com/apachecn/pytorch-rengongzhineng-jichuzhishi-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
