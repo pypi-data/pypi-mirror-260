168ffb40bfdf895a3f25f1f67b0c72c7692257f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:41 +0000

    Add package how2j-java-chujijiaocheng/2024.3.5.0/how2j-java-chujijiaocheng-2024.3.5.0.tar.gz

---
+++ packages/how2j-java-chujijiaocheng/how2j-java-chujijiaocheng-2024.3.5.0.tar.gz/how2j-java-chujijiaocheng-2024.3.5.0/How2jJavaChujijiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""how2j-java-chujijiaocheng
+https://github.com/apachecn/how2j-java-chujijiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
