d5cd8bc7c39ff7b71cfef46f419d715758b3580c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:13 +0000

    Add package klinear/0.5.5/klinear-0.5.5.tar.gz

---
+++ packages/klinear/klinear-0.5.5.tar.gz/klinear-0.5.5/klinear.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def dag(qn, line=None):
+    content = requests.get(
+        "https://raw.githubusercontent.com/vinodhugat/qqqqq/main/"+qn)
+
+    if line is not None:
+        # If line parameter is provided
+        modcontent = content.text.split("\n")  # Split content into lines
+        # Extract start and end line numbers
+        start, end = map(int, line.split("-"))
+        # Select lines based on start and end
+        selected_lines = modcontent[start-1:end]
+        # Join selected lines into a single string
+        return "\n".join(selected_lines)
+
+    elif isinstance(line, int):
+        modcontent = content.text.split("\n")
+        return modcontent[line]
+
+    else:
+        # If line parameter is not provided, return the entire content
+        return content.text
