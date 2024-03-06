706d4b027b5c81bbb8c44c5331d348dc67d35b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:05:07 +0000

    Add package algorithhmicas/4.2/algorithhmicas-4.2.tar.gz

---
+++ packages/algorithhmicas/algorithhmicas-4.2.tar.gz/algorithhmicas-4.2/algorithhmicas/module1.py
@@ -0,0 +1,13 @@
+def main_function():
+    print("Hello my friend")
+
+def factorial(n):
+    """Вычисляет факториал числа n."""
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+if __name__ == "__main__":
+    print("Пример использования модуля module1.py:")
+    print("Факториал числа 5:", factorial(5))
---
+++ packages/algorithhmicas/algorithhmicas-4.2.tar.gz/algorithhmicas-4.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command - runs a script after installing the module."""
+    def run(self):
+        install.run(self)
+        print("hello world")  # Этот код выполнится при установке пакета
+
+setup(
+    name='algorithhmicas',
+    version='4.2',
+    author='Your Name',
+    author_email='cinnabonwithcinnamon@email.com',
+    description='Description of library',
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 'requests'
+    ],
+    cmdclass={
+        'install': CustomInstallCommand,
+    }
+)
