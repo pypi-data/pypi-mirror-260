e3cdcc67ba75377196849466706bc49f6bbde8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:28 +0000

    Add package shell-jiaoben-sucha-shouce/2024.3.6.0/shell-jiaoben-sucha-shouce-2024.3.6.0.tar.gz

---
+++ packages/shell-jiaoben-sucha-shouce/shell-jiaoben-sucha-shouce-2024.3.6.0.tar.gz/shell-jiaoben-sucha-shouce-2024.3.6.0/ShellJiaobenSuchaShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shell-jiaoben-sucha-shouce
+https://github.com/apachecn/shell-jiaoben-sucha-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
