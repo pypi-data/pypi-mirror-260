4ba25ddc9dec543288314881e211e3aa4a45e365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:04:57 +0000

    Add package ccna-shiyan-shouce-xuzhou-sibo-jiaoyu/2024.3.5.0/ccna-shiyan-shouce-xuzhou-sibo-jiaoyu-2024.3.5.0.tar.gz

---
+++ packages/ccna-shiyan-shouce-xuzhou-sibo-jiaoyu/ccna-shiyan-shouce-xuzhou-sibo-jiaoyu-2024.3.5.0.tar.gz/ccna-shiyan-shouce-xuzhou-sibo-jiaoyu-2024.3.5.0/CcnaShiyanShouceXuzhouSiboJiaoyu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ccna-shiyan-shouce-xuzhou-sibo-jiaoyu
+https://github.com/apachecn/ccna-shiyan-shouce-xuzhou-sibo-jiaoyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
