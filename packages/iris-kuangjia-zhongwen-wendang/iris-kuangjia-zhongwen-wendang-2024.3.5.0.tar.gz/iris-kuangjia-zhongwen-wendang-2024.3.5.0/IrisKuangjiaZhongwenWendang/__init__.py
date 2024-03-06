8599c44ae64608bc89c079f118e9b0dab883316f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:14 +0000

    Add package iris-kuangjia-zhongwen-wendang/2024.3.5.0/iris-kuangjia-zhongwen-wendang-2024.3.5.0.tar.gz

---
+++ packages/iris-kuangjia-zhongwen-wendang/iris-kuangjia-zhongwen-wendang-2024.3.5.0.tar.gz/iris-kuangjia-zhongwen-wendang-2024.3.5.0/IrisKuangjiaZhongwenWendang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""iris-kuangjia-zhongwen-wendang
+https://github.com/apachecn/iris-kuangjia-zhongwen-wendang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
