8881fa95911b3106f34596afbbd356c8f0738479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:04:46 +0000

    Add package views-tensor-utilities/0.6.1/views_tensor_utilities-0.6.1-py3-none-any.whl

---
+++ packages/views-tensor-utilities/views_tensor_utilities-0.6.1-py3-none-any.whl/views_tensor_utilities/defaults.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+pg_stride = 720
+
+splits = ['number', 'object']
+fmissing = np.nan
+fdne = -np.inf
+smissing = 'null'
+sdne = '-'
+string_type = np.str_
+float_type = np.float32
+allowed_float_types = [np.float32, np.float64]
+allowed_string_types = [np.str_, 'object']
+allowed_dtypes = allowed_float_types + allowed_string_types
