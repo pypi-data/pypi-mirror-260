2ded84a2a32b3ba0280d650e0efe677119859485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:21 +0000

    Add package kivera-sdk/1.7.0/kivera-sdk-1.7.0.tar.gz

---
+++ packages/kivera-sdk/kivera-sdk-1.7.0.tar.gz/kivera-sdk-1.7.0/kivera/managedrules/list.py
@@ -0,0 +1,48 @@
+from gql import gql
+from typing import Sequence
+
+class listMethods:
+
+    _ListManagedRulesQuery = """
+    query ListManagedRules {
+    ManagedRules{
+        id
+        type_id
+        description
+        policy
+        config
+        tags
+        version
+        risk_rating
+        compliance_mappings
+        Service {
+            id
+            GlobalService {
+                id
+                name
+                Provider {
+                    id
+                    name
+                }
+                Services {
+                    id
+                    inspection
+                }
+            }
+        }
+    }
+}
+    """
+
+    def ListManagedRules(self):
+        query = gql(self._ListManagedRulesQuery)
+        variables = {
+        }
+        operation_name = "ListManagedRules"
+        operation_type = "read"
+        return self.execute(
+            query,
+            variable_values=variables,
+            operation_name=operation_name,
+            operation_type=operation_type,
+        )
