1b24e39795280e024f4ad9a16c7cbe4f8a311e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:05:19 +0000

    Add package labelbox/3.65.0/labelbox-3.65.0-py3-none-any.whl

---
+++ packages/labelbox/labelbox-3.65.0-py3-none-any.whl/labelbox/schema/asset_attachment.py
@@ -0,0 +1,67 @@
+import warnings
+from enum import Enum
+from typing import Dict
+
+from labelbox.orm.db_object import DbObject
+from labelbox.orm.model import Field
+
+
+class AssetAttachment(DbObject):
+    """Asset attachment provides extra context about an asset while labeling.
+
+    Attributes:
+        attachment_type (str): IMAGE, VIDEO, IMAGE_OVERLAY, HTML, RAW_TEXT, TEXT_URL, or PDF_URL. TEXT attachment type is deprecated.
+        attachment_value (str): URL to an external file or a string of text
+    """
+
+    class AttachmentType(Enum):
+
+        @classmethod
+        def __missing__(cls, value: object):
+            if str(value) == "TEXT":
+                warnings.warn(
+                    "The TEXT attachment type is deprecated. Use RAW_TEXT instead."
+                )
+                return cls.RAW_TEXT
+            return value
+
+        VIDEO = "VIDEO"
+        IMAGE = "IMAGE"
+        # TEXT = "TEXT"  # Deprecated
+        IMAGE_OVERLAY = "IMAGE_OVERLAY"
+        HTML = "HTML"
+        RAW_TEXT = "RAW_TEXT"
+        TEXT_URL = "TEXT_URL"
+        PDF_URL = "PDF_URL"
+        CAMERA_IMAGE = "CAMERA_IMAGE"  # Used by experimental point-cloud editor
+
+    for topic in AttachmentType:
+        vars()[topic.name] = topic.value
+
+    attachment_type = Field.String("attachment_type", "type")
+    attachment_value = Field.String("attachment_value", "value")
+
+    @classmethod
+    def validate_attachment_json(cls, attachment_json: Dict[str, str]) -> None:
+        for required_key in ['type', 'value']:
+            if required_key not in attachment_json:
+                raise ValueError(
+                    f"Must provide a `{required_key}` key for each attachment. Found {attachment_json}."
+                )
+            cls.validate_attachment_type(attachment_json['type'])
+
+    @classmethod
+    def validate_attachment_type(cls, attachment_type: str) -> None:
+        valid_types = set(cls.AttachmentType.__members__)
+        if attachment_type not in valid_types:
+            raise ValueError(
+                f"meta_type must be one of {valid_types}. Found {attachment_type}"
+            )
+
+    def delete(self) -> None:
+        """Deletes an attachment on the data row."""
+        query_str = """mutation deleteDataRowAttachmentPyApi($attachment_id: ID!) {
+            deleteDataRowAttachment(where: {id: $attachment_id}) {
+                    id}
+            }"""
+        self.client.execute(query_str, {"attachment_id": self.uid})
