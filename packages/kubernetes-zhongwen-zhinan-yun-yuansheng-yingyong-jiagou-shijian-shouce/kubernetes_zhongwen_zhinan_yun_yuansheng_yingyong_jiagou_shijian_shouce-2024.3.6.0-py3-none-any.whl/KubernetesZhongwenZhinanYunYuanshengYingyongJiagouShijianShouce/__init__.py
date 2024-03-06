4aa169e3b641b0339879235c3c468a55c0204641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:05:28 +0000

    Add package kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce/2024.3.6.0/kubernetes_zhongwen_zhinan_yun_yuansheng_yingyong_jiagou_shijian_shouce-2024.3.6.0-py3-none-any.whl

---
+++ packages/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce/kubernetes_zhongwen_zhinan_yun_yuansheng_yingyong_jiagou_shijian_shouce-2024.3.6.0-py3-none-any.whl/KubernetesZhongwenZhinanYunYuanshengYingyongJiagouShijianShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce
+https://github.com/apachecn/kubernetes-zhongwen-zhinan-yun-yuansheng-yingyong-jiagou-shijian-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
