59c34301fc2d15d782823f6faa4a6a1ff45e5181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:04:46 +0000

    Add package saas-moshi-yun-shujucangku-shijian-shouce/2024.3.6.0/saas_moshi_yun_shujucangku_shijian_shouce-2024.3.6.0-py3-none-any.whl

---
+++ packages/saas-moshi-yun-shujucangku-shijian-shouce/saas_moshi_yun_shujucangku_shijian_shouce-2024.3.6.0-py3-none-any.whl/SaasMoshiYunShujucangkuShijianShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""saas-moshi-yun-shujucangku-shijian-shouce
+https://github.com/apachecn/saas-moshi-yun-shujucangku-shijian-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
