b1038c1916cccf7b71704663f288eedb6964836b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:48 +0000

    Add package how2j-disanfang-kaifa-jiaocheng-shijian-xiangmu-mianshiti/2024.3.5.0/how2j_disanfang_kaifa_jiaocheng_shijian_xiangmu_mianshiti-2024.3.5.0-py3-none-any.whl

---
+++ packages/how2j-disanfang-kaifa-jiaocheng-shijian-xiangmu-mianshiti/how2j_disanfang_kaifa_jiaocheng_shijian_xiangmu_mianshiti-2024.3.5.0-py3-none-any.whl/How2jDisanfangKaifaJiaochengShijianXiangmuMianshiti/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""how2j-disanfang-kaifa-jiaocheng-shijian-xiangmu-mianshiti
+https://github.com/apachecn/how2j-disanfang-kaifa-jiaocheng-shijian-xiangmu-mianshiti"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
