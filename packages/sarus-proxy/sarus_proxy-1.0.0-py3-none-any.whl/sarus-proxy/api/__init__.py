b06a896408b30267fa088353a25a91fd449b6171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:39 +0000

    Add package sarus-proxy/1.0.0/sarus_proxy-1.0.0-py3-none-any.whl

---
+++ packages/sarus-proxy/sarus_proxy-1.0.0-py3-none-any.whl/sarus-proxy/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from sarus-proxy.api.default_api import DefaultApi
+from sarus-proxy.api.events_api import EventsApi
+from sarus-proxy.api.login_api import LoginApi
+from sarus-proxy.api.mistral_api import MistralApi
+from sarus-proxy.api.openai_api import OpenaiApi
+from sarus-proxy.api.settings_api import SettingsApi
+from sarus-proxy.api.users_api import UsersApi
+from sarus-proxy.api.utils_api import UtilsApi
+
