fe82691211bedd512cfe428befa198aa630bd60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:04:44 +0000

    Add package conkeyscan/0.1.0/conkeyscan-0.1.0.tar.gz

---
+++ packages/conkeyscan/conkeyscan-0.1.0.tar.gz/conkeyscan-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="conkeyscan",
+    version="0.1.0",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    py_modules=["conkeyscan"],
+    install_requires=[
+        "loguru",
+        "atlassian-python-api>=3",
+        "beautifulsoup4>=4",
+        "requests-ratelimiter",
+        "clize>=5",
+        "random-user-agent>=1",
+        "readchar",
+        "PySocks>=1",
+    ],
+    entry_points={"console_scripts": ["conkeyscan = conkeyscan:entry_point"]},
+)
