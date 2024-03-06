a5078367f57385a8d6a5155965281b644771d1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:04:53 +0000

    Add package klowd-server-ext/0.3.0/klowd_server_ext-0.3.0.tar.gz

---
+++ packages/klowd-server-ext/klowd_server_ext-0.3.0.tar.gz/klowd_server_ext-0.3.0/src/index.ts
@@ -0,0 +1,32 @@
+import {
+  JupyterLiteServer,
+  JupyterLiteServerPlugin
+} from '@jupyterlite/server';
+
+/**
+ * Initialization data for the klowd_server_ext extension.
+ */
+const plugin: JupyterLiteServerPlugin<void> = {
+  id: 'klowd_server_ext:plugin',
+  autoStart: true,
+  activate: (app: JupyterLiteServer) => {
+    console.log('JupyterLite server extension klowd_server_ext is activated!');
+
+    global.fetch = (function (origFetch) {
+      return function myFetch(resource: any, options = {}) {
+          console.log('fetch intercepted for', resource);
+          const headers = new Headers((options as any).headers);
+          headers.append('Authorization', `Bearer MYTOKEN`);
+
+          const modifiedOptions = {
+            ...options,
+            headers: headers,
+          };
+
+          return origFetch(resource, modifiedOptions);
+      };
+    })(global.fetch);
+  }
+};
+
+export default plugin;
