e6ce0afad76501bbaa3db83c65678c26734018f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:40 +0000

    Add package palletjack/2.0.0rc1/palletjack-2.0.0rc1-cp312-cp312-win_amd64.whl

---
+++ packages/palletjack/palletjack-2.0.0rc1-cp312-cp312-win_amd64.whl/palletjack/palletjack.h
@@ -0,0 +1,6 @@
+#include "parquet/arrow/reader.h"
+#include "parquet/arrow/writer.h"
+#include "parquet/arrow/schema.h"
+
+void GenerateMetadataIndex(const char *parquet_path, const char *index_file_path);
+std::shared_ptr<parquet::FileMetaData> ReadMetadata(const char *index_file_path, const std::vector<uint32_t>& row_groups, const std::vector<uint32_t>& columns);
