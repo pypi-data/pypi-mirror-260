ad2599746c83ebcad4edab54ec3533423b0b9daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:05:09 +0000

    Add package shang-guigu-qianduan-jishu-zhi-e-le-ma-waimai-xiangmu/2024.3.6.0/shang-guigu-qianduan-jishu-zhi-e-le-ma-waimai-xiangmu-2024.3.6.0.tar.gz

---
+++ packages/shang-guigu-qianduan-jishu-zhi-e-le-ma-waimai-xiangmu/shang-guigu-qianduan-jishu-zhi-e-le-ma-waimai-xiangmu-2024.3.6.0.tar.gz/shang-guigu-qianduan-jishu-zhi-e-le-ma-waimai-xiangmu-2024.3.6.0/ShangGuiguQianduanJishuZhiELeMaWaimaiXiangmu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shang-guigu-qianduan-jishu-zhi-e-le-ma-waimai-xiangmu
+https://github.com/apachecn/shang-guigu-qianduan-jishu-zhi-e-le-ma-waimai-xiangmu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
