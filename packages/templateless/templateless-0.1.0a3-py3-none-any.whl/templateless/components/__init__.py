fa09c506ec0ab0ecac97c0f10db238529c11c195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:10 +0000

    Add package templateless/0.1.0a3/templateless-0.1.0a3-py3-none-any.whl

---
+++ packages/templateless/templateless-0.1.0a3-py3-none-any.whl/templateless/components/__init__.py
@@ -0,0 +1,7 @@
+from .button import Button  # noqa
+from .image import Image  # noqa
+from .link import Link  # noqa
+from .otp import Otp  # noqa
+from .socials import Socials, Item as SocialItem, Service  # noqa
+from .text import Text  # noqa
+from .view_in_browser import ViewInBrowser  # noqa
