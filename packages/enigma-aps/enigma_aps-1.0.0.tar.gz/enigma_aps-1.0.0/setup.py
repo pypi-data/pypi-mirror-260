cde491ca178db7a570335fe2abe1589177cab482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:00 +0000

    Add package enigma-aps/1.0.0/enigma_aps-1.0.0.tar.gz

---
+++ packages/enigma-aps/enigma_aps-1.0.0.tar.gz/enigma_aps-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='enigma_aps',
+    version='1.0.0',
+    py_modules=['funções'],
+    url='https://github.com/gabrielfmendes/enigma_aps',
+    license='MIT',
+    author='Gabriel Mendes',
+    author_email='gabrielfmm@al.insper.edu.br',
+    description='Uma biblioteca que contém funções para cifrar e decifrar mensagens usando uma simulação da máquina enigma.',
+)
