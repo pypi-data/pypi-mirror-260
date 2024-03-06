aba2b040c89f27503e9e7ea16656730d5e88cb65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:21 +0000

    Add package project-config/0.9.5/project_config-0.9.5.tar.gz

---
+++ packages/project-config/project_config-0.9.5.tar.gz/project_config-0.9.5/src/project_config/__init__.py
@@ -0,0 +1,31 @@
+"""Reproducible configuration across projects."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from project_config import tree
+from project_config.constants import Error, InterruptingError, ResultValue
+from project_config.types import ActionsContext
+
+
+__all__ = [
+    "tree",
+    "Rule",
+    "Error",
+    "InterruptingError",
+    "ResultValue",
+    "ActionsContext",
+]
+
+
+if TYPE_CHECKING:
+    # TYPE_CHECKING guards have been added to the source code to avoid
+    # runtime errors when using future annotations styles in TypeAlias(es)
+    #
+    # TODO: When the minimum Python version is 3.10, drop these
+    # TYPE_CHECKING branches
+
+    from project_config.types import Results, Rule  # noqa: F401
+
+    __all__.extend(["Results", "Rule"])
