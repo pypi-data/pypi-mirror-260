e8db21c13002707d37302531bebd9472b7296810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:08 +0000

    Add package ddtrace/2.7.1/ddtrace-2.7.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ddtrace/ddtrace-2.7.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ddtrace/internal/datadog/profiling/CMakeFiles/cppcheck_dd_wrapper.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppcheck_dd_wrapper"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cppcheck_dd_wrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ packages/ddtrace/ddtrace-2.7.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ddtrace/internal/datadog/profiling/CMakeFiles/cppcheck_dd_wrapper.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for cppcheck_dd_wrapper.
+# This may be replaced when dependencies are built.
