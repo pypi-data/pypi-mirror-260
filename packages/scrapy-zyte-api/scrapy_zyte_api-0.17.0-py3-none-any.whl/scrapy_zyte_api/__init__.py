9d988d128900783f23f17af6364953d74d774389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:13 +0000

    Add package scrapy-zyte-api/0.17.0/scrapy_zyte_api-0.17.0-py3-none-any.whl

---
+++ packages/scrapy-zyte-api/scrapy_zyte_api-0.17.0-py3-none-any.whl/scrapy_zyte_api/__init__.py
@@ -0,0 +1,15 @@
+from .utils import _NEEDS_EARLY_REACTOR
+
+if _NEEDS_EARLY_REACTOR:
+    from scrapy.utils.reactor import install_reactor
+
+    install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
+
+from ._annotations import ExtractFrom, Geolocation
+from ._middlewares import (
+    ScrapyZyteAPIDownloaderMiddleware,
+    ScrapyZyteAPISpiderMiddleware,
+)
+from ._request_fingerprinter import ScrapyZyteAPIRequestFingerprinter
+from .addon import Addon
+from .handler import ScrapyZyteAPIDownloadHandler
