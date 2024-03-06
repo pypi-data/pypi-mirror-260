93bc6d2aea65f4f11885c984d211d79dcc2dc360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:05:44 +0000

    Add package loggingpython/1.0.0/loggingpython-1.0.0.tar.gz

---
+++ packages/loggingpython/loggingpython-1.0.0.tar.gz/loggingpython-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name='loggingpython',
+    version='1.0.0',
+    description='Loggingpython is a Python package that provides a simple and\
+ extensible way to integrate logging into your applications. The package\
+ starts with a simple logger and can be extended with additional functions to\
+ meet the requirements of your application.',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author='mrmajor.programmer',
+    author_email='mrmajork.programmer@gmail.com',
+    url='https://github.com/loggingpython-Community/loggingpython',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=['colorama'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
