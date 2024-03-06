68a650f9299b0fddeef6c7e17087ddf4c024a4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:10 +0000

    Add package enigma-lipe-caio/0.2/enigma_lipe_caio-0.2.tar.gz

---
+++ packages/enigma-lipe-caio/enigma_lipe_caio-0.2.tar.gz/enigma_lipe_caio-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="enigma_lipe_caio",
+    version="0.2",
+    description="",
+    packages = find_packages(),
+    author= "Lipe Barretto",
+    author_email="lfjanot87@gmail.com",
+    install_requires=[
+        'numpy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
