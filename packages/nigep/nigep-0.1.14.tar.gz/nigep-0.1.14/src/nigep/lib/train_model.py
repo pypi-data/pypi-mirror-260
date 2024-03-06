875ac46c5bd8e30fc98e26376a13373229140289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:07 +0000

    Add package nigep/0.1.14/nigep-0.1.14.tar.gz

---
+++ packages/nigep/nigep-0.1.14.tar.gz/nigep-0.1.14/src/nigep/lib/train_model.py
@@ -0,0 +1,14 @@
+from keras.models import Sequential
+
+
+def train_model(model: Sequential, epochs, callbacks, train_data):
+    x_train, y_train = train_data
+
+    model.fit(
+        x_train,
+        y_train,
+        callbacks=callbacks,
+        epochs=epochs,
+        validation_split=.1,
+    )
+
