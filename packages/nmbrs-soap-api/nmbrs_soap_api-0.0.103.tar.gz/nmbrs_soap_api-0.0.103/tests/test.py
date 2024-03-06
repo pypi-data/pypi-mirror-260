64039408a5f794b550366f4b4b51a920a0ec656c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:44 +0000

    Add package nmbrs-soap-api/0.0.103/nmbrs_soap_api-0.0.103.tar.gz

---
+++ packages/nmbrs-soap-api/nmbrs_soap_api-0.0.103.tar.gz/nmbrs_soap_api-0.0.103/tests/test.py
@@ -0,0 +1,5 @@
+from nmbrs_soap_api.nmbrs_soap_api import NmbrsSoapAPI
+
+soap_api = NmbrsSoapAPI({"Username": "lk@incomme.nl", "Token": "739daa8a334d4918a9ffef0d0ba984b5"})
+
+print(soap_api.auth_header)
