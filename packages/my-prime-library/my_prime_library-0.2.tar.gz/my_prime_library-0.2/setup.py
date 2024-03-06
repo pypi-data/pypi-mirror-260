130035d0c69cf6bf87fcd573aee1dd83378d99ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:27 +0000

    Add package my-prime-library/0.2/my_prime_library-0.2.tar.gz

---
+++ packages/my-prime-library/my_prime_library-0.2.tar.gz/my_prime_library-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='my_prime_library',
+    version='0.02',  
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'my_prime_library=my_prime_library.sieve:main',
+        ],
+    },
+    description='A Python library implementing the Sieve of Eratosthenes algorithm for prime number generation.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
