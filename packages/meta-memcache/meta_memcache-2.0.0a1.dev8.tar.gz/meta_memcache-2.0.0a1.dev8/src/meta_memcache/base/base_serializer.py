5209660c9f0c572aa5e3fffe8fe3439b8ce7be85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:46 +0000

    Add package meta-memcache/2.0.0a1.dev8/meta_memcache-2.0.0a1.dev8.tar.gz

---
+++ packages/meta-memcache/meta_memcache-2.0.0a1.dev8.tar.gz/meta_memcache-2.0.0a1.dev8/src/meta_memcache/base/base_serializer.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from typing import Any, NamedTuple
+
+from meta_memcache.protocol import Blob
+
+
+class EncodedValue(NamedTuple):
+    data: bytes
+    encoding_id: int
+
+
+class BaseSerializer(ABC):
+    @abstractmethod
+    def serialize(
+        self,
+        value: Any,
+    ) -> EncodedValue: ...
+
+    @abstractmethod
+    def unserialize(self, data: Blob, encoding_id: int) -> Any: ...
