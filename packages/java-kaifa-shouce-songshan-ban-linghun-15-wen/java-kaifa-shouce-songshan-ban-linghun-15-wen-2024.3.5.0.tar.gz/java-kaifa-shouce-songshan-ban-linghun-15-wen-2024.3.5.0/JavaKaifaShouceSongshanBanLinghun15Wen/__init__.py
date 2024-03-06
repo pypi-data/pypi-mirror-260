4716750f90e55f4c94384c62a6041a44d3a1cdd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:26 +0000

    Add package java-kaifa-shouce-songshan-ban-linghun-15-wen/2024.3.5.0/java-kaifa-shouce-songshan-ban-linghun-15-wen-2024.3.5.0.tar.gz

---
+++ packages/java-kaifa-shouce-songshan-ban-linghun-15-wen/java-kaifa-shouce-songshan-ban-linghun-15-wen-2024.3.5.0.tar.gz/java-kaifa-shouce-songshan-ban-linghun-15-wen-2024.3.5.0/JavaKaifaShouceSongshanBanLinghun15Wen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-kaifa-shouce-songshan-ban-linghun-15-wen
+https://github.com/apachecn/java-kaifa-shouce-songshan-ban-linghun-15-wen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
