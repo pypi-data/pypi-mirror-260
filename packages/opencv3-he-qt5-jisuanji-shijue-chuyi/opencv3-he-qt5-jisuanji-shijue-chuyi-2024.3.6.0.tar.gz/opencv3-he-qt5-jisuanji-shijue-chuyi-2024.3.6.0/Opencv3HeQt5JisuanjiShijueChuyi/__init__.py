3f47f1ccbfa2fca6f215a946c70fd9293e267601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:06:07 +0000

    Add package opencv3-he-qt5-jisuanji-shijue-chuyi/2024.3.6.0/opencv3-he-qt5-jisuanji-shijue-chuyi-2024.3.6.0.tar.gz

---
+++ packages/opencv3-he-qt5-jisuanji-shijue-chuyi/opencv3-he-qt5-jisuanji-shijue-chuyi-2024.3.6.0.tar.gz/opencv3-he-qt5-jisuanji-shijue-chuyi-2024.3.6.0/Opencv3HeQt5JisuanjiShijueChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""opencv3-he-qt5-jisuanji-shijue-chuyi
+https://github.com/apachecn/opencv3-he-qt5-jisuanji-shijue-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
