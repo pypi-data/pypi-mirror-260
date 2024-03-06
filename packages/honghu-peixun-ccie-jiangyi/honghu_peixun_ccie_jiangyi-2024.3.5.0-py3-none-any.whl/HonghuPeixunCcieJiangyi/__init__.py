d4dfebb125c628be7a6f060f350d349eaaab95f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:43 +0000

    Add package honghu-peixun-ccie-jiangyi/2024.3.5.0/honghu_peixun_ccie_jiangyi-2024.3.5.0-py3-none-any.whl

---
+++ packages/honghu-peixun-ccie-jiangyi/honghu_peixun_ccie_jiangyi-2024.3.5.0-py3-none-any.whl/HonghuPeixunCcieJiangyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""honghu-peixun-ccie-jiangyi
+https://github.com/apachecn/honghu-peixun-ccie-jiangyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
