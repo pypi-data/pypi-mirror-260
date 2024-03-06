7ffce0dea68d842655e0dad3dd12e71070cca45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:05:31 +0000

    Add package latex-lunwen-xiezuo-jiaocheng/2024.3.6.0/latex-lunwen-xiezuo-jiaocheng-2024.3.6.0.tar.gz

---
+++ packages/latex-lunwen-xiezuo-jiaocheng/latex-lunwen-xiezuo-jiaocheng-2024.3.6.0.tar.gz/latex-lunwen-xiezuo-jiaocheng-2024.3.6.0/LatexLunwenXiezuoJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""latex-lunwen-xiezuo-jiaocheng
+https://github.com/apachecn/latex-lunwen-xiezuo-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
