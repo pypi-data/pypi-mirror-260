24553ae17c0e5fa3c87c43383329e1b3c37eeee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:05:38 +0000

    Add package jwcrypto/1.5.5/jwcrypto-1.5.5.tar.gz

---
+++ packages/jwcrypto/jwcrypto-1.5.5.tar.gz/jwcrypto-1.5.5/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2015  JWCrypto Project Contributors, see  LICENSE file
+
+import os
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+version = None
+with open(os.path.join('jwcrypto', 'VERSION')) as verfile:
+    version = verfile.read().strip()
+
+setup(
+    name = 'jwcrypto',
+    version = version,
+    license = 'LGPLv3+',
+    maintainer = 'JWCrypto Project Contributors',
+    maintainer_email = 'simo@redhat.com',
+    url='https://github.com/latchset/jwcrypto',
+    packages = ['jwcrypto'],
+    description = 'Implementation of JOSE Web standards',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers = [
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Intended Audience :: Developers',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    data_files = [('share/doc/jwcrypto', ['LICENSE', 'README.md'])],
+    install_requires = [
+        'cryptography >= 3.4',
+        'typing_extensions >= 4.5.0',
+    ],
+    python_requires = '>= 3.8',
+)
