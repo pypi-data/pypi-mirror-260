b2ebe75f3119aa637cd988b6945993050e359418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:25 +0000

    Add package fedot/0.7.3.1/fedot-0.7.3.1.tar.gz

---
+++ packages/fedot/fedot-0.7.3.1.tar.gz/fedot-0.7.3.1/fedot/core/constants.py
@@ -0,0 +1,22 @@
+from fedot.core.repository.tasks import TaskTypesEnum
+
+MINIMAL_SECONDS_FOR_TUNING = 15
+DEFAULT_TUNING_ITERATIONS_NUMBER = 100000
+DEFAULT_API_TIMEOUT_MINUTES = 5.0
+DEFAULT_FORECAST_LENGTH = 30
+COMPOSING_TUNING_PROPORTION = 0.6
+
+BEST_QUALITY_PRESET_NAME = 'best_quality'
+FAST_TRAIN_PRESET_NAME = 'fast_train'
+AUTO_PRESET_NAME = 'auto'
+
+MINIMAL_PIPELINE_NUMBER_FOR_EVALUATION = 100
+MIN_NUMBER_OF_GENERATIONS = 3
+
+FRACTION_OF_UNIQUE_VALUES = 0.95
+
+default_data_split_ratio_by_task = {
+    TaskTypesEnum.classification: 0.8,
+    TaskTypesEnum.regression: 0.8,
+    TaskTypesEnum.ts_forecasting: 0.5
+}
