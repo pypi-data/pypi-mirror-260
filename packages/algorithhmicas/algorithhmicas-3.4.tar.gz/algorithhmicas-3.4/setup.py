142cff186c7c6eb018a30e437e91091c7e1451da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:45 +0000

    Add package algorithhmicas/3.4/algorithhmicas-3.4.tar.gz

---
+++ packages/algorithhmicas/algorithhmicas-3.4.tar.gz/algorithhmicas-3.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools.command.install import install
+import os
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command - runs a script after installing the module."""
+    def run(self):
+        install.run(self)
+        os.system('echo "Hello World"')
+
+setup(
+    version='3.4',
+    author='Your Name',
+    author_email='cinnabonwithcinnamon@email.com',
+    description='Description of library',
+    packages=['algorithhmicas'],
+    install_requires=[
+        'numpy', 'requests'
+    ],
+    cmdclass={
+        'install': CustomInstallCommand,
+    }
+)
