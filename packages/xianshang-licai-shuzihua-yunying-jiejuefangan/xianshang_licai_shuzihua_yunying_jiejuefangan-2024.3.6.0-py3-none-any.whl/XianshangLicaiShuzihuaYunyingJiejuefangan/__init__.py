96e2fb7e530759f4cc632b27d371aca9621af91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:12 +0000

    Add package xianshang-licai-shuzihua-yunying-jiejuefangan/2024.3.6.0/xianshang_licai_shuzihua_yunying_jiejuefangan-2024.3.6.0-py3-none-any.whl

---
+++ packages/xianshang-licai-shuzihua-yunying-jiejuefangan/xianshang_licai_shuzihua_yunying_jiejuefangan-2024.3.6.0-py3-none-any.whl/XianshangLicaiShuzihuaYunyingJiejuefangan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xianshang-licai-shuzihua-yunying-jiejuefangan
+https://github.com/apachecn/xianshang-licai-shuzihua-yunying-jiejuefangan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
