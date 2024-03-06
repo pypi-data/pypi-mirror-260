a3c18a82d5b299003b4bb7e87cf4c5c1fecbcda5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:13 +0000

    Add package AKBAgeCalculator/0.0.1/AKBAgeCalculator-0.0.1.tar.gz

---
+++ packages/AKBAgeCalculator/AKBAgeCalculator-0.0.1.tar.gz/AKBAgeCalculator-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+# 若Discription.md中有中文 須加上 encoding="utf-8"
+with open("Discription.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="AKBAgeCalculator",
+    version="0.0.1",
+    author="AK",
+    author_email="ak8893893@yahoo.com.tw",
+    description="Age_calculator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ak8893893/python-upload-pip", packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
