96ddee9dcdc7d8770f2a5c54f448987c3ab0dc16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:05:28 +0000

    Add package edustudio/1.1.1/edustudio-1.1.1-py3-none-any.whl

---
+++ packages/edustudio/edustudio-1.1.1-py3-none-any.whl/edustudio/evaltpl/__init__.py
@@ -0,0 +1,4 @@
+from .base_evaltpl import BaseEvalTPL
+from .prediction_evaltpl import PredictionEvalTPL
+from .interpretability_evaltpl import InterpretabilityEvalTPL
+from .fairness_evaltpl import FairnessEvalTPL
