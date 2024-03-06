5128fb60f972de34034b1926846199224fa2c01a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:54 +0000

    Add package activity-calendar-widget/0.1/activity_calendar_widget-0.1.tar.gz

---
+++ packages/activity-calendar-widget/activity_calendar_widget-0.1.tar.gz/activity_calendar_widget-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='activity_calendar_widget',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'PySide6',
+        # Other dependencies if any
+    ],
+)
