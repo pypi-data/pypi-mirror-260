be263555884041aed6a2cfb93097f85aa7572a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:06:01 +0000

    Add package wem-saas/0.1/wem-saas-0.1.tar.gz

---
+++ packages/wem-saas/wem-saas-0.1.tar.gz/wem-saas-0.1/swagger_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.apontamentos_api import ApontamentosApi
+from swagger_client.api.converso_de_fatores_de_emisso_api import ConversoDeFatoresDeEmissoApi
+from swagger_client.api.metas_api import MetasApi
+from swagger_client.api.relatrio_api import RelatrioApi
