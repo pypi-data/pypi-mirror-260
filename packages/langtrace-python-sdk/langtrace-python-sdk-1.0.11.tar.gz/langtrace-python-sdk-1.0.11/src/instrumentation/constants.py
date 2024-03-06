027966e06e5b00a0e8d7e29ab5c461c92d4a0147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:45 +0000

    Add package langtrace-python-sdk/1.0.11/langtrace-python-sdk-1.0.11.tar.gz

---
+++ packages/langtrace-python-sdk/langtrace-python-sdk-1.0.11.tar.gz/langtrace-python-sdk-1.0.11/src/instrumentation/constants.py
@@ -0,0 +1,13 @@
+"""
+This file contains the constants used in the project.
+"""
+SERVICE_PROVIDERS = {
+    "OPENAI": "OpenAI",
+    "AZURE": "Azure",
+    "LANGCHAIN": "Langchain",
+    "LANGCHAIN_CORE": "Langchain Core",
+    "LANGCHAIN_COMMUNITY": "Langchain Community",
+    "PINECONE": "Pinecone",
+    "LLAMAINDEX": "LlamaIndex",
+    "CHROMA": "Chroma",
+}
