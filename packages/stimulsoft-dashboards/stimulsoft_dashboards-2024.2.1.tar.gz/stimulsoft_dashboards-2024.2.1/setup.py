890462b646701a0af633047ec613595457c166ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:04:45 +0000

    Add package stimulsoft-dashboards/2024.2.1/stimulsoft_dashboards-2024.2.1.tar.gz

---
+++ packages/stimulsoft-dashboards/stimulsoft_dashboards-2024.2.1.tar.gz/stimulsoft_dashboards-2024.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as file:
+    readmeFile = file.read()
+
+setup(
+    name = 'stimulsoft_dashboards',
+    version = '2024.2.1',
+    author = 'Stimulsoft',
+    author_email = 'info@stimulsoft.com',
+    description = 'Data visualization in Python applications.',
+    long_description = readmeFile,
+    long_description_content_type = 'text/markdown',
+    url = 'https://www.stimulsoft.com/en/products/dashboards-python',
+    license = 'https://www.stimulsoft.com/en/licensing/developers',
+    classifiers=[
+        'License :: Other/Proprietary License',
+        'Framework :: Django',
+        'Framework :: Flask',
+        'Programming Language :: Python',
+        'Topic :: Office/Business',
+        'Topic :: Software Development'
+    ],
+    install_requires = ['stimulsoft-reports==2024.2.1'],
+    packages = find_packages(include=['stimulsoft_dashboards', 'stimulsoft_dashboards.*']),
+    package_data = {'stimulsoft_dashboards': ['**/scripts/*.js']},
+    python_requires = '>=3.7'
+)
