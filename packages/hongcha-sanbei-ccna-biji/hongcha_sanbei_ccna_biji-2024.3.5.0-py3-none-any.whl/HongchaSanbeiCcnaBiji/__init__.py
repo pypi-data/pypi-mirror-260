f28ac54a92765db1d0ce96d66d10102c3945c543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:40 +0000

    Add package hongcha-sanbei-ccna-biji/2024.3.5.0/hongcha_sanbei_ccna_biji-2024.3.5.0-py3-none-any.whl

---
+++ packages/hongcha-sanbei-ccna-biji/hongcha_sanbei_ccna_biji-2024.3.5.0-py3-none-any.whl/HongchaSanbeiCcnaBiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hongcha-sanbei-ccna-biji
+https://github.com/apachecn/hongcha-sanbei-ccna-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
