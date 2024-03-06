5318aded0a750e795b3478501eabb9830c0941b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:33 +0000

    Add package Darwinian/0.0.5.4/Darwinian-0.0.5.4-py3-none-any.whl

---
+++ packages/Darwinian/Darwinian-0.0.5.4-py3-none-any.whl/examples/maximum_and_minimum.py
@@ -0,0 +1,23 @@
+# импорт библиотек
+from darwinian import Darwinian_GA
+import random
+
+# Во первых мы создадим экземпляр maxGA
+maxGA = Darwinian_GA()
+
+# Функция приспособленности
+def fitness(*individual):
+    return sum(individual[0])
+
+# инициализируем 
+maxGA.setup(10, 10, 0.01, 0.8, 100, fitness, lambda: random.randint(0, 1))
+
+# запускаем
+max_individual = maxGA.constructor(MaxOrMin=True)
+min_individual = maxGA.constructor(MaxOrMin=False)
+
+print(max_individual[0])
+print(min_individual[0])
+
+# >>> ([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 10)
+# >>> ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0)
