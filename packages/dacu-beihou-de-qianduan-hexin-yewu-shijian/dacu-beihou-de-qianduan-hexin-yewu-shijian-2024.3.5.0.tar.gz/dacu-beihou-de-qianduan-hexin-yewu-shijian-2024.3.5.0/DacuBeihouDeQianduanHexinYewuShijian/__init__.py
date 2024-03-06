361355d4b44174605fabacb43cf7773d21643704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:35 +0000

    Add package dacu-beihou-de-qianduan-hexin-yewu-shijian/2024.3.5.0/dacu-beihou-de-qianduan-hexin-yewu-shijian-2024.3.5.0.tar.gz

---
+++ packages/dacu-beihou-de-qianduan-hexin-yewu-shijian/dacu-beihou-de-qianduan-hexin-yewu-shijian-2024.3.5.0.tar.gz/dacu-beihou-de-qianduan-hexin-yewu-shijian-2024.3.5.0/DacuBeihouDeQianduanHexinYewuShijian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dacu-beihou-de-qianduan-hexin-yewu-shijian
+https://github.com/apachecn/dacu-beihou-de-qianduan-hexin-yewu-shijian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
