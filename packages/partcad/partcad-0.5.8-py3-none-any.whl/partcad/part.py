a7820086d4e06c7f78ad55a329bb98045cb6853a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:05:58 +0000

    Add package partcad/0.5.8/partcad-0.5.8-py3-none-any.whl

---
+++ packages/partcad/partcad-0.5.8-py3-none-any.whl/partcad/part.py
@@ -0,0 +1,97 @@
+#
+# OpenVMP, 2023
+#
+# Author: Roman Kuzmenko
+# Created: 2023-08-19
+#
+# Licensed under Apache License, Version 2.0.
+#
+
+import asyncio
+import math
+import typing
+
+from . import shape
+from . import sync_threads as pc_thread
+
+
+class Part(shape.Shape):
+    path: typing.Optional[str] = None
+
+    def __init__(self, config: object = {}, shape=None):
+        super().__init__(config)
+
+        self.shape = shape
+        self.lock = asyncio.Lock()
+
+        self.desc = None
+        if "desc" in config:
+            self.desc = config["desc"]
+        self.vendor = None
+        if "vendor" in config:
+            self.vendor = config["vendor"]
+        self.sku = None
+        if "sku" in config:
+            self.sku = config["sku"]
+        self.url = None
+        if "url" in config:
+            self.url = config["url"]
+        if "count_per_sku" in config:
+            self.count_per_sku = config["count_per_sku"]
+        else:
+            self.count_per_sku = 1
+        self.count = 0
+
+    async def get_shape(self):
+        async with self.lock:
+            if self.shape is None:
+                self.shape = await pc_thread.run(self.instantiate, self)
+            return self.shape
+
+    def ref_inc(self):
+        # TODO(clairbee): add a thread lock here
+        self.count += 1
+
+    def clone(self):
+        cloned = Part(self.name, self.config, self.shape)
+        cloned.count = self.count
+        return cloned
+
+    def _render_txt_real(self, file):
+        file.write(self.name + ": " + self.count + "\n")
+
+    def _render_markdown_real(self, file):
+        name = self.name
+        vendor = ""
+        sku = ""
+        if not self.desc is None:
+            name = self.desc
+        if not self.vendor is None:
+            vendor = self.vendor
+        if not self.sku is None:
+            sku = self.sku
+        if self.url is None:
+            label = name
+        else:
+            label = "[" + name + "](" + self.url + ")"
+            sku = "[" + sku + "](" + self.url + ")"
+        count = str(math.ceil(self.count / self.count_per_sku))
+        img_url = self._get_svg_url()
+
+        file.write(
+            "| "
+            + label
+            + " | "
+            + count
+            + " |"
+            + vendor
+            + " |"
+            + sku
+            + " |"
+            + " !["
+            + name
+            + "]("
+            + img_url
+            + ")"
+            + " |\n"
+        )
