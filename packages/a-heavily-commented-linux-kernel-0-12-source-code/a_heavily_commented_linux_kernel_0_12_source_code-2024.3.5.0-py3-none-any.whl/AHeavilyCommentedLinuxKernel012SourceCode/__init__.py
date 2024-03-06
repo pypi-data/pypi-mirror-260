264b6bb0f23bc05e7968e79fddd773ff6f036546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:05:06 +0000

    Add package a-heavily-commented-linux-kernel-0-12-source-code/2024.3.5.0/a_heavily_commented_linux_kernel_0_12_source_code-2024.3.5.0-py3-none-any.whl

---
+++ packages/a-heavily-commented-linux-kernel-0-12-source-code/a_heavily_commented_linux_kernel_0_12_source_code-2024.3.5.0-py3-none-any.whl/AHeavilyCommentedLinuxKernel012SourceCode/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""a-heavily-commented-linux-kernel-0-12-source-code
+https://github.com/apachecn/a-heavily-commented-linux-kernel-0-12-source-code"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
