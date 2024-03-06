7e48e89ae6008970932cd9925d1c5a1678c539e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:05:11 +0000

    Add package mingrentang-ii/2024.3.6.0/mingrentang-ii-2024.3.6.0.tar.gz

---
+++ packages/mingrentang-ii/mingrentang-ii-2024.3.6.0.tar.gz/mingrentang-ii-2024.3.6.0/MingrentangIi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mingrentang-ii
+https://github.com/apachecn/mingrentang-ii"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
