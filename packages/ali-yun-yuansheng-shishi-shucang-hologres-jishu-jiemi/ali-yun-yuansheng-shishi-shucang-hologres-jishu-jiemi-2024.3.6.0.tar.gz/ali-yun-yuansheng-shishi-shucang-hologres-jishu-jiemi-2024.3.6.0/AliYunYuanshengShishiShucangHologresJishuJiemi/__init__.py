f6572eec2152a9a612f29b9fd6ddfc77f0e3da3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:56 +0000

    Add package ali-yun-yuansheng-shishi-shucang-hologres-jishu-jiemi/2024.3.6.0/ali-yun-yuansheng-shishi-shucang-hologres-jishu-jiemi-2024.3.6.0.tar.gz

---
+++ packages/ali-yun-yuansheng-shishi-shucang-hologres-jishu-jiemi/ali-yun-yuansheng-shishi-shucang-hologres-jishu-jiemi-2024.3.6.0.tar.gz/ali-yun-yuansheng-shishi-shucang-hologres-jishu-jiemi-2024.3.6.0/AliYunYuanshengShishiShucangHologresJishuJiemi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ali-yun-yuansheng-shishi-shucang-hologres-jishu-jiemi
+https://github.com/apachecn/ali-yun-yuansheng-shishi-shucang-hologres-jishu-jiemi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
