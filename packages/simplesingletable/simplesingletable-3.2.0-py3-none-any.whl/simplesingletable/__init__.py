d15b1fe3c1e2f4d64d4605731ce5de0e8cafa47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:37 +0000

    Add package simplesingletable/3.2.0/simplesingletable-3.2.0-py3-none-any.whl

---
+++ packages/simplesingletable/simplesingletable-3.2.0-py3-none-any.whl/simplesingletable/__init__.py
@@ -0,0 +1,7 @@
+from .dynamodb_memory import DynamoDbMemory, DynamoDbResource, DynamoDbVersionedResource
+
+package_version = "3.2.0"
+
+_ = DynamoDbMemory
+_ = DynamoDbResource
+_ = DynamoDbVersionedResource
