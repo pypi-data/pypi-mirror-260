59836ead5e6e427d4cd010a4ef0cccfced6c7725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:40 +0000

    Add package django-dsfr/0.18.0/django_dsfr-0.18.0-py3-none-any.whl

---
+++ packages/django-dsfr/django_dsfr-0.18.0-py3-none-any.whl/dsfr/checksums.py
@@ -0,0 +1,49 @@
+# Integrity checks for the css/js/favicon files
+# Do not update manually!
+# Generated with the following command:
+# python manage.py integrity_checksums
+
+# dsfr/static/dsfr/dist/dsfr/dsfr.module.min.js
+INTEGRITY_JS_MODULE = (
+    "sha384-ToZ2KPANcYOYrPshKd8bJxk+utYf7qGe3ob4+NkCjeda5DRDtM9tp5GTyMGXakre"
+)
+
+# dsfr/static/dsfr/dist/dsfr/dsfr.nomodule.min.js
+INTEGRITY_JS_NOMODULE = (
+    "sha384-OJq7jonDJ6QWsuRqnCWE3Gep7eKu0DEaasALVb40NLARi4cGCIjgVHHhnGJuByNq"
+)
+
+# dsfr/static/dsfr/dist/dsfr/dsfr.min.css
+INTEGRITY_CSS = (
+    "sha384-dPQgYpB1TxmQr6BYJVjZno5onyq5FTc2Y1VkxUIteXfWQjORdOWc/Zh0lODQsnx+"
+)
+
+# dsfr/static/dsfr/dist/utility/utility.min.css
+INTEGRITY_UTILITY_CSS = (
+    "sha384-zyJ07edvc/zdLxAnpNVT3jkcNOeweHoBlOrSwGeWsxRPByiIJaeEKdeV0FgNwW3K"
+)
+
+# dsfr/static/dsfr/dist/utility/icons/icons.min.css
+INTEGRITY_CSS_ICONS = (
+    "sha384-hA1C/qFm8PMx34t5U8FWKqgBEpwdfdZNZFnwJjfMVEaRo8ug9qk78uDwa+3CPByj"
+)
+
+# dsfr/static/dsfr/dist/favicon/apple-touch-icon.png
+INTEGRITY_FAVICON_APPLE = (
+    "sha384-bE/sjT09LYXMMjd/7ovUY40XBU2WQoLkIRw4/eBcHdBVsJOhoomJuSSa+qEFGku/"
+)
+
+# dsfr/static/dsfr/dist/favicon/favicon.svg
+INTEGRITY_FAVICON_SVG = (
+    "sha384-z4gMU2V+U4skVvrlEwyCe+lUVDhliQY6Tg0az69xNkmyu4H84cy+uJZnWVkHfMrN"
+)
+
+# dsfr/static/dsfr/dist/favicon/favicon.ico
+INTEGRITY_FAVICON_ICO = (
+    "sha384-esZ1Wf54Wvb66vtrLBKzz2QEZciEIV9R+wc+w7C3e5mID8A1SgJpXXIc+3lY9YqP"
+)
+
+# dsfr/static/dsfr/dist/favicon/manifest.webmanifest
+INTEGRITY_FAVICON_MANIFEST = (
+    "sha384-9rQ8jBh6MztX7GlSBVwj2yJEUaxKTCy3pYRLq1Qu57zkdJ3P5ftf6GyfYJwFuR7j"
+)
