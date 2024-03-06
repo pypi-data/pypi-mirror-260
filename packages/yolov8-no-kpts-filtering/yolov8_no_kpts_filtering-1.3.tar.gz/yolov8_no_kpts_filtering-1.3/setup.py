c3c60ce48ec9ce12582c35620d4ade08a7f6c3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:05:05 +0000

    Add package yolov8-no-kpts-filtering/1.3/yolov8_no_kpts_filtering-1.3.tar.gz

---
+++ packages/yolov8-no-kpts-filtering/yolov8_no_kpts_filtering-1.3.tar.gz/yolov8_no_kpts_filtering-1.3/setup.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+import setuptools
+
+
+
+def setup() -> None:
+
+
+
+    setuptools.setup(
+        packages=setuptools.find_packages(),
+        python_requires='>=3.10.0',
+        include_package_data=True,
+        author='ErnisMeshi',
+        version=1.3,
+        name='yolov8_no_kpts_filtering',
+    )
+
+
+
+if __name__ == '__main__':
+    setup()
