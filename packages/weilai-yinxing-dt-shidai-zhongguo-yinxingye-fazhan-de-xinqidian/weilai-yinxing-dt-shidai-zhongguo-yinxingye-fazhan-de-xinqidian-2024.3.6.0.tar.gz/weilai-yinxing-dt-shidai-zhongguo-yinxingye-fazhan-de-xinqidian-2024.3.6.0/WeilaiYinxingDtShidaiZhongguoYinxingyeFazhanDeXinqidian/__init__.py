d64eb53c050898596a0dad3acd3a4b3e5f2484a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:31 +0000

    Add package weilai-yinxing-dt-shidai-zhongguo-yinxingye-fazhan-de-xinqidian/2024.3.6.0/weilai-yinxing-dt-shidai-zhongguo-yinxingye-fazhan-de-xinqidian-2024.3.6.0.tar.gz

---
+++ packages/weilai-yinxing-dt-shidai-zhongguo-yinxingye-fazhan-de-xinqidian/weilai-yinxing-dt-shidai-zhongguo-yinxingye-fazhan-de-xinqidian-2024.3.6.0.tar.gz/weilai-yinxing-dt-shidai-zhongguo-yinxingye-fazhan-de-xinqidian-2024.3.6.0/WeilaiYinxingDtShidaiZhongguoYinxingyeFazhanDeXinqidian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""weilai-yinxing-dt-shidai-zhongguo-yinxingye-fazhan-de-xinqidian
+https://github.com/apachecn/weilai-yinxing-dt-shidai-zhongguo-yinxingye-fazhan-de-xinqidian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
