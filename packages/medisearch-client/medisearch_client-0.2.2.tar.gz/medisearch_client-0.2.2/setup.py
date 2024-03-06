a661e18b35b0bb035eaac3a08024811b8f36a970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:30 +0000

    Add package medisearch-client/0.2.2/medisearch_client-0.2.2.tar.gz

---
+++ packages/medisearch-client/medisearch_client-0.2.2.tar.gz/medisearch_client-0.2.2/setup.py
@@ -0,0 +1,18 @@
+"""Setup file for the medisearch_client package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="medisearch_client",
+    version="0.2.2",
+    packages=find_packages(),
+    install_requires=[
+        "websocket-client", "pyasn1", "pyopenssl", "ndg-httpsclient"
+    ],
+    author="Michal Pandy",
+    author_email="founders@medisearch.io",
+    description="A simple client for the MediSearch API",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/MediSearch/medisearch_client_python",
+)
