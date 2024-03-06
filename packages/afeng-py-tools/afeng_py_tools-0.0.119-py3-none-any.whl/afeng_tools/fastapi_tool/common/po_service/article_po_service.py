44c56813b1b40010853cc8d894adc336084c4fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:40 +0000

    Add package afeng-py-tools/0.0.119/afeng_py_tools-0.0.119-py3-none-any.whl

---
+++ packages/afeng-py-tools/afeng_py_tools-0.0.119-py3-none-any.whl/afeng_tools/fastapi_tool/common/po_service/article_po_service.py
@@ -0,0 +1,9 @@
+from afeng_tools.fastapi_tool.common.po_service.po_service import PoService
+
+
+class ArticlePoService(PoService):
+    """
+    使用示例：article_po_service = ArticlePoService(app_info.db_code, ArticleInfoPo)
+    """
+
+    pass
