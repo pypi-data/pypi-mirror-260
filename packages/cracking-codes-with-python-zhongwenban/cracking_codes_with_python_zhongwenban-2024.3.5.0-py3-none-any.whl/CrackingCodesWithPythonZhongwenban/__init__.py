ec5ea2b67d4df1707b3284ea619efb357ddc88ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:05:37 +0000

    Add package cracking-codes-with-python-zhongwenban/2024.3.5.0/cracking_codes_with_python_zhongwenban-2024.3.5.0-py3-none-any.whl

---
+++ packages/cracking-codes-with-python-zhongwenban/cracking_codes_with_python_zhongwenban-2024.3.5.0-py3-none-any.whl/CrackingCodesWithPythonZhongwenban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""cracking-codes-with-python-zhongwenban
+https://github.com/apachecn/cracking-codes-with-python-zhongwenban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
