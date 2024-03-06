84d21d0f0225c4bd6208998add88e964b64ab7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:05:48 +0000

    Add package dalhelper/0.4/dalhelper-0.4.tar.gz

---
+++ packages/dalhelper/dalhelper-0.4.tar.gz/dalhelper-0.4/dalhelper/dalhelper.py
@@ -0,0 +1,4 @@
+# dalhelper.py
+
+def write_dataframe_to_datacloud(table_name, dataframe):
+    print("invoke DAL API from sidecar container")
---
+++ packages/dalhelper/dalhelper-0.4.tar.gz/dalhelper-0.4/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dalhelper',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=[],
+)
