675f279947e25ddf746b4d861bf66923d478cec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:04:43 +0000

    Add package yolov8-no-kpts-filtering/1.6/yolov8_no_kpts_filtering-1.6.tar.gz

---
+++ packages/yolov8-no-kpts-filtering/yolov8_no_kpts_filtering-1.6.tar.gz/yolov8_no_kpts_filtering-1.6/setup.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+import setuptools
+
+
+
+def setup() -> None:
+
+    cfg_files = []
+    for root, dirs, files in os.walk("ultralytics/cfg"):
+        for file in files:
+            cfg_files.append(os.path.join(root, file))
+
+    setuptools.setup(
+        packages=setuptools.find_packages(),
+        python_requires='>=3.10.0',
+        include_package_data=True,
+        author='ErnisMeshi',
+        version=1.6,
+        name='yolov8_no_kpts_filtering',
+                package_data={"ultralytics": cfg_files},  # Include cfg files
+    )
+
+
+
+if __name__ == '__main__':
+    setup()
