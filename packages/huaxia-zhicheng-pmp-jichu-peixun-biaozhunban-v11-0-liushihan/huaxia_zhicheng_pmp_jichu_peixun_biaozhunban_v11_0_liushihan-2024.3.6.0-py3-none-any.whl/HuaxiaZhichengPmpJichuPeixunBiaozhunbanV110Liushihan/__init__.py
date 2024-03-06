2741a28d36f47f993ab4d824e418f2321ded9795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:31 +0000

    Add package huaxia-zhicheng-pmp-jichu-peixun-biaozhunban-v11-0-liushihan/2024.3.6.0/huaxia_zhicheng_pmp_jichu_peixun_biaozhunban_v11_0_liushihan-2024.3.6.0-py3-none-any.whl

---
+++ packages/huaxia-zhicheng-pmp-jichu-peixun-biaozhunban-v11-0-liushihan/huaxia_zhicheng_pmp_jichu_peixun_biaozhunban_v11_0_liushihan-2024.3.6.0-py3-none-any.whl/HuaxiaZhichengPmpJichuPeixunBiaozhunbanV110Liushihan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huaxia-zhicheng-pmp-jichu-peixun-biaozhunban-v11-0-liushihan
+https://github.com/apachecn/huaxia-zhicheng-pmp-jichu-peixun-biaozhunban-v11-0-liushihan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
