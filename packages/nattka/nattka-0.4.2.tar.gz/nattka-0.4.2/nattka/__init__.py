2b5c246557d01abda7992be7a80dab1c6ed6e580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:05:39 +0000

    Add package nattka/0.4.2/nattka-0.4.2.tar.gz

---
+++ packages/nattka/nattka-0.4.2.tar.gz/nattka-0.4.2/nattka/__init__.py
@@ -0,0 +1,6 @@
+# (c) 2020-2024 Michał Górny
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+"""A New Arch Tester Toolkit"""
+
+__version__ = "0.4.2"
---
+++ packages/nattka/nattka-0.4.2.tar.gz/nattka-0.4.2/test/__init__.py
@@ -0,0 +1,4 @@
+# (c) 2020-2024 Michał Górny
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+"""NATTkA test suite"""
