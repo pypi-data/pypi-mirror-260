8ecee6100758787835fdad93332d2ee1ebbe2d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:48 +0000

    Add package mmpy-bot-mk2/2.1.4/mmpy_bot_mk2-2.1.4-py3-none-any.whl

---
+++ packages/mmpy-bot-mk2/mmpy_bot_mk2-2.1.4-py3-none-any.whl/mmpy_bot_mk2/plugins/__init__.py
@@ -0,0 +1,6 @@
+from mmpy_bot_mk2.plugins.base import Plugin, PluginManager
+from mmpy_bot_mk2.plugins.example import ExamplePlugin
+from mmpy_bot_mk2.plugins.help_example import HelpPlugin
+from mmpy_bot_mk2.plugins.webhook_example import WebHookExample
+
+__all__ = ["Plugin", "PluginManager", "HelpPlugin", "ExamplePlugin", "WebHookExample"]
