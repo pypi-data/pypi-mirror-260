068d7ae6898b318899541cbd21cf8b7103798b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:34 +0000

    Add package apache-flink-shida-jishu-nandian-shizhan/2024.3.6.0/apache_flink_shida_jishu_nandian_shizhan-2024.3.6.0-py3-none-any.whl

---
+++ packages/apache-flink-shida-jishu-nandian-shizhan/apache_flink_shida_jishu_nandian_shizhan-2024.3.6.0-py3-none-any.whl/ApacheFlinkShidaJishuNandianShizhan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apache-flink-shida-jishu-nandian-shizhan
+https://github.com/apachecn/apache-flink-shida-jishu-nandian-shizhan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
