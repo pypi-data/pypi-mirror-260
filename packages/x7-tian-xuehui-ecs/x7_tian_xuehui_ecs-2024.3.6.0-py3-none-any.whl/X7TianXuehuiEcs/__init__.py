f34efae8c9e310a46b337f4f72ce4d96dd2c3d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:11 +0000

    Add package x7-tian-xuehui-ecs/2024.3.6.0/x7_tian_xuehui_ecs-2024.3.6.0-py3-none-any.whl

---
+++ packages/x7-tian-xuehui-ecs/x7_tian_xuehui_ecs-2024.3.6.0-py3-none-any.whl/X7TianXuehuiEcs/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x7-tian-xuehui-ecs
+https://github.com/apachecn/x7-tian-xuehui-ecs"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
