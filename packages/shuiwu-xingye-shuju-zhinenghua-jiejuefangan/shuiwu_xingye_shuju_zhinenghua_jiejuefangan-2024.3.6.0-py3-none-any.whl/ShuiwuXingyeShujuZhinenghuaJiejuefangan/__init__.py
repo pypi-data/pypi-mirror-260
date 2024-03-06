0fd947f571a33f810299bf404b4da28241a7aed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:18 +0000

    Add package shuiwu-xingye-shuju-zhinenghua-jiejuefangan/2024.3.6.0/shuiwu_xingye_shuju_zhinenghua_jiejuefangan-2024.3.6.0-py3-none-any.whl

---
+++ packages/shuiwu-xingye-shuju-zhinenghua-jiejuefangan/shuiwu_xingye_shuju_zhinenghua_jiejuefangan-2024.3.6.0-py3-none-any.whl/ShuiwuXingyeShujuZhinenghuaJiejuefangan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuiwu-xingye-shuju-zhinenghua-jiejuefangan
+https://github.com/apachecn/shuiwu-xingye-shuju-zhinenghua-jiejuefangan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
