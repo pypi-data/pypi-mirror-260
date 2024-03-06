164223c1117e0c73d811f0820263d174b6c8ac04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:05:05 +0000

    Add package ccnp-shiyan-shouce-wuming/2024.3.5.0/ccnp-shiyan-shouce-wuming-2024.3.5.0.tar.gz

---
+++ packages/ccnp-shiyan-shouce-wuming/ccnp-shiyan-shouce-wuming-2024.3.5.0.tar.gz/ccnp-shiyan-shouce-wuming-2024.3.5.0/CcnpShiyanShouceWuming/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ccnp-shiyan-shouce-wuming
+https://github.com/apachecn/ccnp-shiyan-shouce-wuming"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
