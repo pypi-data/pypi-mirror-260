35a98d1189c656b7a5b9e214c564e1386b22dbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:04:51 +0000

    Add package backtrader-finam/3.0.1/backtrader_finam-3.0.1.tar.gz

---
+++ packages/backtrader-finam/backtrader_finam-3.0.1.tar.gz/backtrader_finam-3.0.1/FinamPy/Config.py
@@ -0,0 +1,8 @@
+# Чтобы получить торговый токен:
+# 1. Открыть счет в "Финаме" https://open.finam.ru/registration
+# 2. Зарегистрироваться в сервисе Comon https://www.comon.ru/
+# 3. В личном кабинете Comon получить торговый токен https://www.comon.ru/my/trade-api/tokens
+
+class Config:
+    ClientIds = ('<Торговый счет>',)  # Торговые счета
+    AccessToken = '<Токен>'  # Торговый токен
