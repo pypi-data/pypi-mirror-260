dd67011e21fa593bd5923f3c75e8c8890a670ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:05:10 +0000

    Add package maidr/0.1.0/maidr-0.1.0.tar.gz

---
+++ packages/maidr/maidr-0.1.0.tar.gz/maidr-0.1.0/maidr/core/enum/maidr_key.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class MaidrKey(Enum):
+    """
+    Enumeration class representing keys used in the Maidr application.
+    """
+
+    AXES = "axes"
+    CAPTION = "caption"
+    DATA = "data"
+    FILL = "fill"
+    LABEL = "label"
+    LEVEL = "level"
+    ID = "id"
+    ORIENTATION = "orientation"
+    SELECTOR = "selector"
+    SUBTITLE = "subtitle"
+    TITLE = "title"
+    TYPE = "type"
+    X = "x"
+    Y = "y"
---
+++ packages/maidr/maidr-0.1.0.tar.gz/maidr-0.1.0/maidr/core/enum/plot_type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class PlotType(Enum):
+    """
+    Enumeration class for different plot types.
+    Each plot type corresponds to a different kind of MAIDR representation.
+    """
+
+    BAR = "bar"
