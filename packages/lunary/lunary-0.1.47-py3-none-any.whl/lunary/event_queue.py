86f5176c1a1518a4ba7b98e1410dc594f40a2cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:05 +0000

    Add package lunary/0.1.47/lunary-0.1.47-py3-none-any.whl

---
+++ packages/lunary/lunary-0.1.47-py3-none-any.whl/lunary/event_queue.py
@@ -0,0 +1,23 @@
+import threading
+
+
+class EventQueue:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.events = []
+
+    def append(self, event):
+        with self.lock:
+            if isinstance(event, dict):
+                self.events.append(event)
+
+    def get_batch(self):
+        if self.lock.acquire(False): # non-blocking
+            try:
+                events = self.events
+                self.events = []
+                return events
+            finally:
+                self.lock.release()
+        else:
+            return []
