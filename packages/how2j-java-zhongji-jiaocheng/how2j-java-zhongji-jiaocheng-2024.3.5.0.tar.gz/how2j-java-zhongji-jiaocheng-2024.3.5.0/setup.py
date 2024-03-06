bb6b5d25aa8a62c9cbe0fe2861295c8aede1b2ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:04 +0000

    Add package how2j-java-zhongji-jiaocheng/2024.3.5.0/how2j-java-zhongji-jiaocheng-2024.3.5.0.tar.gz

---
+++ packages/how2j-java-zhongji-jiaocheng/how2j-java-zhongji-jiaocheng-2024.3.5.0.tar.gz/how2j-java-zhongji-jiaocheng-2024.3.5.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import How2jJavaZhongjiJiaocheng
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('How2jJavaZhongjiJiaocheng'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="how2j-java-zhongji-jiaocheng",
+    version=How2jJavaZhongjiJiaocheng.__version__,
+    url="https://github.com/apachecn/how2j-java-zhongji-jiaocheng",
+    author=How2jJavaZhongjiJiaocheng.__author__,
+    author_email=How2jJavaZhongjiJiaocheng.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="HOW2J Java 中级教程",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "how2j-java-zhongji-jiaocheng=How2jJavaZhongjiJiaocheng.__main__:main",
+            "How2jJavaZhongjiJiaocheng=How2jJavaZhongjiJiaocheng.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
