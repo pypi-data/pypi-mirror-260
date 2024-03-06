1fea5d66e8dc0f54157ffb8bb82da5bd1eebc080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:04:23 +0000

    Add package ajaxify/0.1/ajaxify-0.1.tar.gz

---
+++ packages/ajaxify/ajaxify-0.1.tar.gz/ajaxify-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ajaxify",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # list of minimal dependencies required to run the package
+    ],
+    # other metadata
+)
