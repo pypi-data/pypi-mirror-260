6fc092d1a957228b730b81ce123171f1b301a6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:09 +0000

    Add package ddtrace/2.7.1/ddtrace-2.7.1-cp38-cp38-musllinux_1_1_aarch64.whl

---
+++ packages/ddtrace/ddtrace-2.7.1-cp38-cp38-musllinux_1_1_aarch64.whl/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/project/ddtrace/internal/datadog/profiling/dd_wrapper/src/interface.cpp" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/interface.cpp.o" "gcc" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/interface.cpp.o.d"
+  "/project/ddtrace/internal/datadog/profiling/dd_wrapper/src/profile.cpp" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/profile.cpp.o" "gcc" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/profile.cpp.o.d"
+  "/project/ddtrace/internal/datadog/profiling/dd_wrapper/src/sample.cpp" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/sample.cpp.o" "gcc" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/sample.cpp.o.d"
+  "/project/ddtrace/internal/datadog/profiling/dd_wrapper/src/sample_manager.cpp" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/sample_manager.cpp.o" "gcc" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/sample_manager.cpp.o.d"
+  "/project/ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader.cpp" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/uploader.cpp.o" "gcc" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/uploader.cpp.o.d"
+  "/project/ddtrace/internal/datadog/profiling/dd_wrapper/src/uploader_builder.cpp" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/uploader_builder.cpp.o" "gcc" "/project/build/lib.linux-aarch64-cpython-38/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/src/uploader_builder.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ packages/ddtrace/ddtrace-2.7.1-cp38-cp38-musllinux_1_1_aarch64.whl/ddtrace/internal/datadog/profiling/CMakeFiles/dd_wrapper.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "/project/build/ddtrace.internal.datadog.profiling/libdd_wrapper.pdb"
+  "/project/build/ddtrace.internal.datadog.profiling/libdd_wrapper.so"
+  "CMakeFiles/dd_wrapper.dir/src/interface.cpp.o"
+  "CMakeFiles/dd_wrapper.dir/src/interface.cpp.o.d"
+  "CMakeFiles/dd_wrapper.dir/src/profile.cpp.o"
+  "CMakeFiles/dd_wrapper.dir/src/profile.cpp.o.d"
+  "CMakeFiles/dd_wrapper.dir/src/sample.cpp.o"
+  "CMakeFiles/dd_wrapper.dir/src/sample.cpp.o.d"
+  "CMakeFiles/dd_wrapper.dir/src/sample_manager.cpp.o"
+  "CMakeFiles/dd_wrapper.dir/src/sample_manager.cpp.o.d"
+  "CMakeFiles/dd_wrapper.dir/src/uploader.cpp.o"
+  "CMakeFiles/dd_wrapper.dir/src/uploader.cpp.o.d"
+  "CMakeFiles/dd_wrapper.dir/src/uploader_builder.cpp.o"
+  "CMakeFiles/dd_wrapper.dir/src/uploader_builder.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dd_wrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
