644ddb875e0a46d3f79eccf583276c03cb093c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:05:36 +0000

    Add package pupil-labs-uvc/1.0.0/pupil_labs_uvc-1.0.0-cp36-cp36m-win_amd64.whl

---
+++ packages/pupil-labs-uvc/pupil_labs_uvc-1.0.0-cp36-cp36m-win_amd64.whl/uvc/__init__.py
@@ -0,0 +1,67 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_1_4():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pupil_labs_uvc.libs'))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if not is_pyinstaller or os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-pupil_labs_uvc-1.0.0')
+        if not is_pyinstaller or os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, '.load-order-pupil_labs_uvc-1.0.0')) as file:
+                load_order = file.read().split()
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if not is_pyinstaller or os.path.isfile(lib_path):
+                    ctypes.WinDLL(lib_path)
+
+
+_delvewheel_init_patch_1_1_4()
+del _delvewheel_init_patch_1_1_4
+# end delvewheel patch
+
+import logging
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version
+
+# .uvc_bindings expects `logger` to be present
+logger = logging.getLogger(__name__)
+
+from .uvc_bindings import (  # noqa: E402
+    Capture,
+    Device_List,
+    InitError,
+    OpenError,
+    StreamError,
+    device_list,
+    get_time_monotonic,
+    is_accessible,
+)
+
+try:
+    __version__ = version("pupil-labs-uvc")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+__all__ = [
+    "__version__",
+    "Capture",
+    "device_list",
+    "Device_List",
+    "get_time_monotonic",
+    "InitError",
+    "is_accessible",
+    "OpenError",
+    "StreamError",
+]
