5d3eaed607c65742734f68fec2746c6909e7c91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:06:03 +0000

    Add package gougleai/1.0.5.4/gougleai-1.0.5.4.tar.gz

---
+++ packages/gougleai/gougleai-1.0.5.4.tar.gz/gougleai-1.0.5.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gougleai',
+    version='1.0.5.4',
+    author='Gougle AI LLC',
+    author_email='gouglellc@gmail.com',
+    description='The Python package for Gougle AI API.',
+    long_description=open('README.md', "r").read(),
+    long_description_content_type='text/markdown',
+    url='https://www.github.com/gougle-official/gougleai-python',
+    packages=find_packages(),
+    py_modules=['gougleai'],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+    ],
+    package_data={'': ['../LICENSE']}
+)
