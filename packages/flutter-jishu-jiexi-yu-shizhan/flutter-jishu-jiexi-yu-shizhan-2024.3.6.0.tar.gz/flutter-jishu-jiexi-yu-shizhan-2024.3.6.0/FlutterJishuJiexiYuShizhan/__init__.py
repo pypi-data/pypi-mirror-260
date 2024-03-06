ad036034e87640aba0279449cc1d10ac658f650a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:20 +0000

    Add package flutter-jishu-jiexi-yu-shizhan/2024.3.6.0/flutter-jishu-jiexi-yu-shizhan-2024.3.6.0.tar.gz

---
+++ packages/flutter-jishu-jiexi-yu-shizhan/flutter-jishu-jiexi-yu-shizhan-2024.3.6.0.tar.gz/flutter-jishu-jiexi-yu-shizhan-2024.3.6.0/FlutterJishuJiexiYuShizhan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""flutter-jishu-jiexi-yu-shizhan
+https://github.com/apachecn/flutter-jishu-jiexi-yu-shizhan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
