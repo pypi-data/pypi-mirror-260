5b9cb2c4c0508501a08c0a1a60778c47f3be9a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:06:04 +0000

    Add package azure-functions-extension-base/1.0.0a1/azure-functions-extension-base-1.0.0a1.tar.gz

---
+++ packages/azure-functions-extension-base/azure-functions-extension-base-1.0.0a1.tar.gz/azure-functions-extension-base-1.0.0a1/setup.py
@@ -0,0 +1,21 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='azure-functions-extension-base',
+    version='1.0.0a1',
+    author='Azure Functions team at Microsoft Corp.',
+    author_email='azurefunctions@microsoft.com',
+    description='Base Python worker extension for Azure Functions.',
+    packages=find_packages(exclude=[
+        'azure.functions.extension', 'azure.functions', 'azure', 'tests'
+    ]),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7'
+)
