6250c6a6a46b48115fcb7651d4224b2577be240b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:38 +0000

    Add package infiltra/3.4/infiltra-3.4.tar.gz

---
+++ packages/infiltra/infiltra-3.4.tar.gz/infiltra-3.4/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='infiltra',
+    version='3.4',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+        'pydig',
+        'ascii_magic',
+        'pyfiglet',
+        'pipx'
+    ],
+    entry_points={
+        'console_scripts': [
+            'infiltra=infiltra.infiltra:main',
+        ],
+    },
+    # Include additional files into the package
+    include_package_data=True,
+    package_data={
+        'infiltra': [
+            'aort/utils/*.json',
+            'version.txt',
+            '*.json',
+            '*.sh',
+            '*.yaml',
+            '*.png',
+            'venv/*',
+            'eyewitness/*',
+            'bbot/*',
+            'aort/*',
+            'website_enum/*'
+            'submenus/*'
+            'nuclei-templates/**/*',
+        ],
+    },
+    # Metadata
+    author='@jivy26',
+    author_email='jivy26@gmail.com',
+    description='CLI Based to that Automates Various Pentest Tools',
+    license='MIT',
+    keywords='infiltra penetration cybersecurity scanning',
+)
