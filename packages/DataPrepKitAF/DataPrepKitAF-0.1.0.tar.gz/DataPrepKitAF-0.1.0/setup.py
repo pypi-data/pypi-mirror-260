9f2bc05096a4eb39fd9d3e4c2c4db90d73852375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:05:01 +0000

    Add package DataPrepKitAF/0.1.0/DataPrepKitAF-0.1.0.tar.gz

---
+++ packages/DataPrepKitAF/DataPrepKitAF-0.1.0.tar.gz/DataPrepKitAF-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = "DataPrepKitAF",
+    version = "0.1.0",
+    author = "Abdulaziz Fahad",
+    description= "Data preperation package",
+    packages = find_packages(),
+    classifiers= ["Programming Language :: Python :: 3",
+                  "Operating System :: OS Independent",
+                  "License :: OSI Approved :: MIT License"]
+)
