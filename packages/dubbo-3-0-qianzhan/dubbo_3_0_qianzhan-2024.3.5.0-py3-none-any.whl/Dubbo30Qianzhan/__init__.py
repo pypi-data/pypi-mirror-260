e4b295e76e0f7a12fe004aae6a97b98bfd001da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:52 +0000

    Add package dubbo-3-0-qianzhan/2024.3.5.0/dubbo_3_0_qianzhan-2024.3.5.0-py3-none-any.whl

---
+++ packages/dubbo-3-0-qianzhan/dubbo_3_0_qianzhan-2024.3.5.0-py3-none-any.whl/Dubbo30Qianzhan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dubbo-3-0-qianzhan
+https://github.com/apachecn/dubbo-3-0-qianzhan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
