638d5741e5c3278c44575b76782fa82ac3208d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:04:39 +0000

    Add package gadapt/0.2.21/gadapt-0.2.21.tar.gz

---
+++ packages/gadapt/gadapt-0.2.21.tar.gz/gadapt-0.2.21/gadapt/operations/variable_update/base_variable_updater.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class BaseVariableUpdater(ABC):
+
+    """
+    Base class for variable update
+    """
+
+    @abstractmethod
+    def update_variables(self, population):
+        pass
