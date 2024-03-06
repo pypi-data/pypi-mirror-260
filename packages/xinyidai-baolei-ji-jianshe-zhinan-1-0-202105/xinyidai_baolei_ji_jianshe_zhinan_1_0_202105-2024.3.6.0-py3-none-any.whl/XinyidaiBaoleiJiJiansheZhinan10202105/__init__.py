156250741c670186a805309453661b60319d4d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:16 +0000

    Add package xinyidai-baolei-ji-jianshe-zhinan-1-0-202105/2024.3.6.0/xinyidai_baolei_ji_jianshe_zhinan_1_0_202105-2024.3.6.0-py3-none-any.whl

---
+++ packages/xinyidai-baolei-ji-jianshe-zhinan-1-0-202105/xinyidai_baolei_ji_jianshe_zhinan_1_0_202105-2024.3.6.0-py3-none-any.whl/XinyidaiBaoleiJiJiansheZhinan10202105/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xinyidai-baolei-ji-jianshe-zhinan-1-0-202105
+https://github.com/apachecn/xinyidai-baolei-ji-jianshe-zhinan-1-0-202105"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
