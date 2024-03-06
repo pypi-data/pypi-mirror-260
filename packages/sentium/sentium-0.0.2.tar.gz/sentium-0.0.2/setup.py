5264e598d24d97ca02b17cd566cdc2a381b36171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:29 +0000

    Add package sentium/0.0.2/sentium-0.0.2.tar.gz

---
+++ packages/sentium/sentium-0.0.2.tar.gz/sentium-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+import sys
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+        subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
+
+setup(
+    name='sentium',
+    packages=['sentium'],
+    version='0.0.2',
+    license='MIT',
+    description='Semantic Enhancement through Neural Taxonomy - an Interpretable and Understandable Model',
+    author='Nick S.H Oh',
+    author_email='research@socius.org',
+    url='https://github.com/socius-org/sentibank',
+    download_url='https://github.com/socius-org/sentibank/archive/refs/tags/0.2.2.tar.gz',
+    keywords=[
+        'AI', 
+        'Social Science', 
+        'Sentiment Analysis', 
+        'Rule Based Sentiment Analysis', 
+        'Sentiment Lexicon',
+    ],
+    install_requires=[
+        'spacy >= 3.7.2',
+        'rich == 13.4.2',
+    ],
+    include_package_data=True,
+    cmdclass={
+        'install': CustomInstallCommand,
+    }
+)
