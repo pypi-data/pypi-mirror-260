4feda40ed54fae218179578103c5872c3fae94fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:05:13 +0000

    Add package afeng-py-tools/0.0.118/afeng_py_tools-0.0.118-py3-none-any.whl

---
+++ packages/afeng-py-tools/afeng_py_tools-0.0.118-py3-none-any.whl/afeng_tools/fastapi_tool/common/po_service/help_info_po_service.py
@@ -0,0 +1,9 @@
+from afeng_tools.fastapi_tool.common.po_service.po_service import PoService
+
+
+class HelpInfoPoService(PoService):
+    """
+    使用示例：help_info_po_service = HelpInfoPoService(app_info.db_code, HelpInfoPo)
+    """
+
+    pass
