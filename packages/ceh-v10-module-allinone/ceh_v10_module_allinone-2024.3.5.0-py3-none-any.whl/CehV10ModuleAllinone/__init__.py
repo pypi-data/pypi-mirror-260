058673acede3b707527ae760b496d47c4d2c4d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:05:07 +0000

    Add package ceh-v10-module-allinone/2024.3.5.0/ceh_v10_module_allinone-2024.3.5.0-py3-none-any.whl

---
+++ packages/ceh-v10-module-allinone/ceh_v10_module_allinone-2024.3.5.0-py3-none-any.whl/CehV10ModuleAllinone/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ceh-v10-module-allinone
+https://github.com/apachecn/ceh-v10-module-allinone"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
