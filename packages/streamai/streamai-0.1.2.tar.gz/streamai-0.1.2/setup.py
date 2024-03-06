a8b7a9eb9ae3053659ac887138e228c82b8f0cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:04:47 +0000

    Add package streamai/0.1.2/streamai-0.1.2.tar.gz

---
+++ packages/streamai/streamai-0.1.2.tar.gz/streamai-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(name='streamai',
+      version='0.1.2',
+      description='finetune latest open source llms in less than 10 line of code.',
+      author='Navdeep Dhakar',
+      license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+      ],
+      install_requires=['fastapi', 'uvicorn', 'fire'],
+      python_requires='>=3.6',
+      packages=find_packages(),
+      include_package_data=True)
---
+++ packages/streamai/streamai-0.1.2.tar.gz/streamai-0.1.2/streamai/mistral/__init__.py
@@ -0,0 +1,3 @@
+from streamai.mistral.train import Trainmodel as AutoTrainMistral
+from streamai.mistral.inference import main as Loadmodel
+from streamai.mistral.inference import evaluate as Evalmodel
