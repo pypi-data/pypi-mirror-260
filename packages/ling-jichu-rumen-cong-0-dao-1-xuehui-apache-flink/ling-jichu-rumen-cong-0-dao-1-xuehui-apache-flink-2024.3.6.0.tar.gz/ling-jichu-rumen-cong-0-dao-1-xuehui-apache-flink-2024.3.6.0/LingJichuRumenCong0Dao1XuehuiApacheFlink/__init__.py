6593e646d309932fa70dd25770243f2883534c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:32 +0000

    Add package ling-jichu-rumen-cong-0-dao-1-xuehui-apache-flink/2024.3.6.0/ling-jichu-rumen-cong-0-dao-1-xuehui-apache-flink-2024.3.6.0.tar.gz

---
+++ packages/ling-jichu-rumen-cong-0-dao-1-xuehui-apache-flink/ling-jichu-rumen-cong-0-dao-1-xuehui-apache-flink-2024.3.6.0.tar.gz/ling-jichu-rumen-cong-0-dao-1-xuehui-apache-flink-2024.3.6.0/LingJichuRumenCong0Dao1XuehuiApacheFlink/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ling-jichu-rumen-cong-0-dao-1-xuehui-apache-flink
+https://github.com/apachecn/ling-jichu-rumen-cong-0-dao-1-xuehui-apache-flink"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
