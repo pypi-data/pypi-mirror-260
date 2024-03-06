85133492615c5e1186a8702a4526226eb778b775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 17:04:25 +0000

    Add package bilby-lisa/1.0.0a3/bilby_lisa-1.0.0a3.tar.gz

---
+++ packages/bilby-lisa/bilby_lisa-1.0.0a3.tar.gz/bilby_lisa-1.0.0a3/README.md
@@ -0,0 +1,29 @@
+# bilby_lisa
+
+## Description
+
+Plug in for [`bilby`](https://lscsoft.docs.ligo.org/bilby/) to perform Bayesian
+inference with LISA. See [this paper](https://arxiv.org/abs/2312.13039) for
+details.
+
+## Installation instructions
+
+To install ``bilby_lisa``, please see the installation instructions
+[`here`](https://bilby-lisa.readthedocs.io/en/latest/installation.html).
+
+## Citing
+
+If you make use of this code, please cite the
+[`bilby_lisa` paper](https://arxiv.org/abs/2312.13039)
+
+```
+@article{Hoy:2023ndx,
+    author = "Hoy, Charlie and Nuttall, Laura K.",
+    title = "{BILBY in space: Bayesian inference for transient gravitational-wave signals observed with LISA}",
+    eprint = "2312.13039",
+    archivePrefix = "arXiv",
+    primaryClass = "astro-ph.IM",
+    month = "12",
+    year = "2023"
+}
+```
---
+++ packages/bilby-lisa/bilby_lisa-1.0.0a3.tar.gz/bilby_lisa-1.0.0a3/bilby_lisa/parallel_bilby.py
@@ -0,0 +1,52 @@
+# Licensed under an MIT style license -- see LICENSE.md
+
+from .bilby_pipe import DataGenerationInput
+from parallel_bilby.generation import (
+    ParallelBilbyDataGenerationInput as _DGInput, main
+)
+
+__author__ = ["Charlie Hoy <charlie.hoy@ligo.org>"]
+
+
+class ParallelBilbyDataGenerationInput(DataGenerationInput, _DGInput):
+    """Extends ParallelBilbyDataGenerationInput to include additional
+    options for LISA
+    """
+    pass
+
+
+def create_generation_parser():
+    """Extends the BilbyArgParser for bilby_pipe to include additional
+    options for LISA
+    """
+    from parallel_bilby.parser.generation import create_generation_parser
+    from .parser import add_LISA_options_to_parser
+    parser = create_generation_parser()
+    parser = add_LISA_options_to_parser(parser)
+    # change the defaults for LISA data analysis
+    parser.set_defaults(
+        generation_executable_parser=(
+            "bilby_lisa.parallel_bilby.create_generation_parser"
+        ),
+        generation_input_class=(
+            "bilby_lisa.parallel_bilby.ParallelBilbyDataGenerationInput"
+        ),
+    )
+    return parser
+
+
+def parallel_bilby_generation_main():
+    """A modified, top-level interface for parallel_bilby_generation which
+    first checks the `bilby_lisa` installation
+    """
+    from .utils import get_version_info, version_error_string
+    import parallel_bilby
+
+    get_version_info()
+    if "e4e2f7b" not in parallel_bilby.__version__:
+        raise ImportError(
+            version_error_string.format(
+                'parallel_bilby', parallel_bilby.__version__
+            )
+        )
+    main()
