ae8ac9c850516cc179a53e22fd087c20782db482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:05:27 +0000

    Add package chinapub-ccna-zhongwen-jiaocai/2024.3.5.0/chinapub-ccna-zhongwen-jiaocai-2024.3.5.0.tar.gz

---
+++ packages/chinapub-ccna-zhongwen-jiaocai/chinapub-ccna-zhongwen-jiaocai-2024.3.5.0.tar.gz/chinapub-ccna-zhongwen-jiaocai-2024.3.5.0/ChinapubCcnaZhongwenJiaocai/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chinapub-ccna-zhongwen-jiaocai
+https://github.com/apachecn/chinapub-ccna-zhongwen-jiaocai"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
