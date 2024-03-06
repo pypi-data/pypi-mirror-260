80f4b8869c735723175ad2a87df64da67b6dd522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:39 +0000

    Add package agogos/0.1.2/agogos-0.1.2.tar.gz

---
+++ packages/agogos/agogos-0.1.2.tar.gz/agogos-0.1.2/agogos/pipeline.py
@@ -0,0 +1,153 @@
+from dataclasses import dataclass
+from typing import Any
+
+from agogos.training_system import TrainingSystem
+
+from agogos.transforming_system import TransformingSystem
+from joblib import hash
+
+from agogos._core._base import _Base
+
+
+@dataclass
+class Pipeline(_Base):
+    """A pipeline of systems that can be trained and predicted.
+
+    ### Parameters:
+    - x_sys (TransformingSystem | None): The system to transform the input data.
+    - y_sys (TransformingSystem | None): The system to transform the labelled data.
+    - train_sys (TrainingSystem | None): The system to train the data.
+    - pred_sys (TransformingSystem | None): The system to transform the predictions.
+    - label_sys (TransformingSystem | None): The system to transform the labels.
+
+    ### Methods:
+    ```python
+    def train(self, x: Any, y: Any, train_args: dict[str, Any] = {}) -> tuple[Any, Any]: # Train the system.
+
+    def predict(self, x: Any, pred_args: dict[str, Any] = {}) -> Any: # Predict the output of the system.
+
+    def get_hash(self) -> str: # Get the hash of the pipeline.
+    ```
+
+    ### Usage:
+    ```python
+    from agogos.pipeline import Pipeline
+
+    x_sys = CustomTransformingSystem()
+    y_sys = CustomTransformingSystem()
+    train_sys = CustomTrainingSystem()
+    pred_sys = CustomTransformingSystem()
+    label_sys = CustomTransformingSystem()
+
+    pipeline = Pipeline(x_sys=x_sys, y_sys=y_sys, train_sys=train_sys, pred_sys=pred_sys, label_sys=label_sys)
+    trained_x, trained_y = pipeline.train(x, y)
+    predictions = pipeline.predict(x)
+    ```
+    """
+
+    x_sys: TransformingSystem | None = None
+    y_sys: TransformingSystem | None = None
+    train_sys: TrainingSystem | None = None
+    pred_sys: TransformingSystem | None = None
+    label_sys: TransformingSystem | None = None
+
+    def train(self, x: Any, y: Any, train_args: dict[str, Any] = {}) -> tuple[Any, Any]:
+        """Train the system.
+
+        :param x: The input to the system.
+        :param y: The expected output of the system.
+        :param train_args: The arguments to pass to the training system. (Default is {})
+        :return: The input and output of the system.
+        """
+        if self.x_sys is not None:
+            x_sys_args = (
+                train_args["x_sys"] if train_args and "x_sys" in train_args else {}
+            )
+            x = self.x_sys.transform(x, x_sys_args)
+        if self.y_sys is not None:
+            y_sys_args = (
+                train_args["y_sys"] if train_args and "y_sys" in train_args else {}
+            )
+            y = self.y_sys.transform(y, y_sys_args)
+        if self.train_sys is not None:
+            train_sys_args = (
+                train_args["train_sys"]
+                if train_args and "train_sys" in train_args
+                else {}
+            )
+            x, y = self.train_sys.train(x, y, train_sys_args)
+        if self.pred_sys is not None:
+            pred_sys_args = (
+                train_args["pred_sys"]
+                if train_args and "pred_sys" in train_args
+                else {}
+            )
+            x = self.pred_sys.transform(x, pred_sys_args)
+        if self.label_sys is not None:
+            label_sys_args = (
+                train_args["label_sys"]
+                if train_args and "label_sys" in train_args
+                else {}
+            )
+            y = self.label_sys.transform(y, label_sys_args)
+
+        return x, y
+
+    def predict(self, x: Any, pred_args: dict[str, Any] = {}) -> Any:
+        """Predict the output of the system.
+
+        :param x: The input to the system.
+        :param pred_args: The arguments to pass to the prediction system. (Default is {})
+        :return: The output of the system.
+        """
+        if self.x_sys is not None:
+            x_sys_args = (
+                pred_args["x_sys"] if pred_args and "x_sys" in pred_args else {}
+            )
+            x = self.x_sys.transform(x, x_sys_args)
+        if self.train_sys is not None:
+            train_sys_args = (
+                pred_args["train_sys"] if pred_args and "train_sys" in pred_args else {}
+            )
+            x = self.train_sys.predict(x, train_sys_args)
+        if self.pred_sys is not None:
+            pred_sys_args = (
+                pred_args["pred_sys"] if pred_args and "pred_sys" in pred_args else {}
+            )
+            x = self.pred_sys.transform(x, pred_sys_args)
+
+        return x
+
+    def _set_hash(self, prev_hash: str) -> None:
+        """Set the hash of the pipeline.
+
+        :param prev_hash: The hash of the previous block.
+        """
+        self._hash = prev_hash
+
+        xy_hash = ""
+        if self.x_sys is not None:
+            xy_hash += self.x_sys.get_hash()
+        if self.y_sys is not None:
+            xy_hash += self.y_sys.get_hash()
+
+        if xy_hash != "":
+            self._hash = hash(self._hash + xy_hash)
+
+        if self.train_sys is not None:
+            self.train_sys._set_hash(self._hash)
+            training_hash = self.train_sys.get_hash()
+            if training_hash != "":
+                self._hash = hash(self._hash + training_hash)
+
+        predlabel_hash = ""
+        if self.pred_sys is not None:
+            predlabel_hash += self.pred_sys.get_hash()
+        if self.label_sys is not None:
+            predlabel_hash += self.label_sys.get_hash()
+
+        if predlabel_hash != "":
+            if self._hash == "":
+                self._hash = predlabel_hash
+            else:
+                self._hash = hash(self._hash + predlabel_hash)
---
+++ packages/agogos/agogos-0.1.2.tar.gz/agogos-0.1.2/tests/test_transforming_system.py
@@ -0,0 +1,148 @@
+import numpy as np
+import pytest
+from agogos.transformer import Transformer
+from agogos.transforming_system import TransformingSystem
+
+
+class TestTransformingSystem:
+    def test_transforming_system_init(self):
+        transforming_system = TransformingSystem()
+        assert transforming_system is not None
+
+    def test_transforming_system_init_with_steps(self):
+        class SubTransformer(Transformer):
+            def transform(self, x):
+                return x
+
+        block1 = SubTransformer()
+        transforming_system = TransformingSystem(steps=[block1])
+        assert transforming_system is not None
+
+    def test_transforming_system_wrong_step(self):
+        class SubTransformer:
+            def transform(self, x):
+                return x
+
+        with pytest.raises(AssertionError):
+            TransformingSystem(steps=[SubTransformer()])
+
+    def test_transforming_system_steps_changed(self):
+        class SubTransformer:
+            def transform(self, x):
+                return x
+
+        block1 = SubTransformer()
+        transforming_system = TransformingSystem()
+        transforming_system.steps = [block1]
+        with pytest.raises(TypeError):
+            transforming_system.transform([1, 2, 3])
+
+    def test_transforming_system_transform_1_block(self):
+        class SubTransformer(Transformer):
+            def transform(self, x):
+                return x
+
+        block1 = SubTransformer()
+        transforming_system = TransformingSystem(steps=[block1])
+        assert transforming_system.transform([1, 2, 3]) == [1, 2, 3]
+
+    def test_transforming_system_transform_1_block_with_args(self):
+        class SubTransformer(Transformer):
+            def transform(self, data):
+                return data
+
+        block1 = SubTransformer()
+        transforming_system = TransformingSystem(steps=[block1])
+        assert transforming_system.transform([1, 2, 3], {"SubTransformer": {}}) == [
+            1,
+            2,
+            3,
+        ]
+
+    def test_transforming_system_transform_2_blocks(self):
+        class SubTransformer(Transformer):
+            def transform(self, x):
+                return x * 2
+
+        block1 = SubTransformer()
+        block2 = SubTransformer()
+        transforming_system = TransformingSystem(steps=[block1, block2])
+        result = transforming_system.transform(np.array([1, 2, 3]))
+        assert np.array_equal(result, np.array([4, 8, 12]))
+
+    def test_transformsys_with_transformsys(self):
+        class SubTransformer(Transformer):
+            def transform(self, x):
+                return x * 2
+
+        block1 = SubTransformer()
+        block2 = TransformingSystem(steps=[block1])
+        transforming_system = TransformingSystem(steps=[block2])
+        result = transforming_system.transform(np.array([1, 2, 3]))
+        assert np.array_equal(result, np.array([2, 4, 6]))
+
+    def test_transforming_system_transform_with_args(self):
+        class SubTransformer(Transformer):
+            def transform(self, data, multiplier=2):
+                return data * multiplier
+
+        block1 = SubTransformer()
+        transforming_system = TransformingSystem(steps=[block1])
+        result = transforming_system.transform(
+            np.array([1, 2, 3]), {"SubTransformer": {"multiplier": 2}}
+        )
+        assert np.array_equal(result, np.array([2, 4, 6]))
+
+    def test_transforming_system_transform_with_args_2_blocks(self):
+        class SubTransformer(Transformer):
+            def transform(self, data, multiplier=2):
+                return data * multiplier
+
+        block1 = SubTransformer()
+        block2 = SubTransformer()
+        transforming_system = TransformingSystem(steps=[block1, block2])
+        result = transforming_system.transform(
+            np.array([1, 2, 3]), {"SubTransformer": {"multiplier": 2}}
+        )
+        assert np.array_equal(result, np.array([4, 8, 12]))
+
+    def test_transforming_system_transform_with_recursive_args(self):
+        class SubTransformer(Transformer):
+            def transform(self, data, multiplier=2):
+                return data * multiplier
+
+        block1 = SubTransformer()
+        block2 = SubTransformer()
+        block3 = TransformingSystem(steps=[block2])
+        block4 = TransformingSystem(steps=[block3])
+        transforming_system = TransformingSystem(steps=[block1, block4])
+        assert np.array_equal(
+            transforming_system.transform(
+                np.array([1, 2, 3]), {"SubTransformer": {"multiplier": 2}}
+            ),
+            np.array([4, 8, 12]),
+        )
+        assert np.array_equal(
+            transforming_system.transform(
+                np.array([1, 2, 3]),
+                {
+                    "TransformingSystem": {
+                        "TransformingSystem": {"SubTransformer": {"multiplier": 3}}
+                    }
+                },
+            ),
+            np.array([6, 12, 18]),
+        )
+
+    def test_transforming_system_predict(self):
+        class SubTransformer(Transformer):
+            def transform(self, x):
+                return x
+
+        block1 = SubTransformer()
+        transforming_system = TransformingSystem(steps=[block1])
+        assert transforming_system.predict([1, 2, 3]) == [1, 2, 3]
+
+    def test_transforming_system_empty_hash(self):
+        transforming_system = TransformingSystem()
+        assert transforming_system.get_hash() == ""
