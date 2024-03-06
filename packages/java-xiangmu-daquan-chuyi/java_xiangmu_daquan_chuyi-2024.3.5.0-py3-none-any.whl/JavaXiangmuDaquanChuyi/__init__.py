ceb729c208a2943ea4cfca4dc2e86be9dfe6c91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:26 +0000

    Add package java-xiangmu-daquan-chuyi/2024.3.5.0/java_xiangmu_daquan_chuyi-2024.3.5.0-py3-none-any.whl

---
+++ packages/java-xiangmu-daquan-chuyi/java_xiangmu_daquan_chuyi-2024.3.5.0-py3-none-any.whl/JavaXiangmuDaquanChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""java-xiangmu-daquan-chuyi
+https://github.com/apachecn/java-xiangmu-daquan-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
