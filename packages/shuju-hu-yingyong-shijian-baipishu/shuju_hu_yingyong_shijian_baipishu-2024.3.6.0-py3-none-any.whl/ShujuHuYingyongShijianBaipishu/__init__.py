96ecd2c0bd9942a690579df114a7a4f567a1e146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:19 +0000

    Add package shuju-hu-yingyong-shijian-baipishu/2024.3.6.0/shuju_hu_yingyong_shijian_baipishu-2024.3.6.0-py3-none-any.whl

---
+++ packages/shuju-hu-yingyong-shijian-baipishu/shuju_hu_yingyong_shijian_baipishu-2024.3.6.0-py3-none-any.whl/ShujuHuYingyongShijianBaipishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuju-hu-yingyong-shijian-baipishu
+https://github.com/apachecn/shuju-hu-yingyong-shijian-baipishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
