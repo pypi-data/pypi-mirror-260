5409d8de0f1feb1f726afbee1166a81ef04ca1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:05:15 +0000

    Add package knative-yun-yuansheng-yingyong-kaifa-zhinan/2024.3.6.0/knative-yun-yuansheng-yingyong-kaifa-zhinan-2024.3.6.0.tar.gz

---
+++ packages/knative-yun-yuansheng-yingyong-kaifa-zhinan/knative-yun-yuansheng-yingyong-kaifa-zhinan-2024.3.6.0.tar.gz/knative-yun-yuansheng-yingyong-kaifa-zhinan-2024.3.6.0/KnativeYunYuanshengYingyongKaifaZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""knative-yun-yuansheng-yingyong-kaifa-zhinan
+https://github.com/apachecn/knative-yun-yuansheng-yingyong-kaifa-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
