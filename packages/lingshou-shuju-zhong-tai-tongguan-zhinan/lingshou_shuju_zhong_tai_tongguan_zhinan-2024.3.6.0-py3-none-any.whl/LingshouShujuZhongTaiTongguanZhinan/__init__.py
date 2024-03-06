9d2a971b23042701fd729bb0467b857a406f954b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:35 +0000

    Add package lingshou-shuju-zhong-tai-tongguan-zhinan/2024.3.6.0/lingshou_shuju_zhong_tai_tongguan_zhinan-2024.3.6.0-py3-none-any.whl

---
+++ packages/lingshou-shuju-zhong-tai-tongguan-zhinan/lingshou_shuju_zhong_tai_tongguan_zhinan-2024.3.6.0-py3-none-any.whl/LingshouShujuZhongTaiTongguanZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingshou-shuju-zhong-tai-tongguan-zhinan
+https://github.com/apachecn/lingshou-shuju-zhong-tai-tongguan-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
