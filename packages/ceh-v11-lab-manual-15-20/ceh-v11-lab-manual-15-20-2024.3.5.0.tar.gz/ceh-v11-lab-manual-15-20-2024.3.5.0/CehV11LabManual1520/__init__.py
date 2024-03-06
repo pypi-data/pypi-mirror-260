abeb71ecb521af34399dfa96d3d047cb2cab95d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:05:01 +0000

    Add package ceh-v11-lab-manual-15-20/2024.3.5.0/ceh-v11-lab-manual-15-20-2024.3.5.0.tar.gz

---
+++ packages/ceh-v11-lab-manual-15-20/ceh-v11-lab-manual-15-20-2024.3.5.0.tar.gz/ceh-v11-lab-manual-15-20-2024.3.5.0/CehV11LabManual1520/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ceh-v11-lab-manual-15-20
+https://github.com/apachecn/ceh-v11-lab-manual-15-20"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
