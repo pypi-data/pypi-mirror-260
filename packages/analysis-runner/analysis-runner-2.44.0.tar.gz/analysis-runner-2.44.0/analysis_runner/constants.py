59de6b5a43615bcb1657fdbd06b52eda482e0e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:17 +0000

    Add package analysis-runner/2.44.0/analysis-runner-2.44.0.tar.gz

---
+++ packages/analysis-runner/analysis-runner-2.44.0.tar.gz/analysis-runner-2.44.0/analysis_runner/constants.py
@@ -0,0 +1,28 @@
+"""Constants for analysis-runner"""
+
+SERVER_ENDPOINT = 'https://server-a2pko7ameq-ts.a.run.app'
+SERVER_TEST_ENDPOINT = 'https://server-test-a2pko7ameq-ts.a.run.app'
+ANALYSIS_RUNNER_PROJECT_ID = 'analysis-runner'
+CROMWELL_URL = 'https://cromwell.populationgenomics.org.au'
+CROMWELL_AUDIENCE = (
+    '717631777761-ec4u8pffntsekut9kef58hts126v7usl.apps.googleusercontent.com'
+)
+GCLOUD_ACTIVATE_AUTH = (
+    'gcloud -q auth activate-service-account --key-file=/gsa-key/key.json'
+)
+
+
+def get_server_endpoint(
+    server_url: str | None = SERVER_ENDPOINT, is_test: bool = False
+):
+    """
+    Get the server endpoint {production / test}
+    Do it in a function so it's easy to fix if the logic changes
+    """
+    if is_test:
+        return SERVER_TEST_ENDPOINT
+
+    if not server_url:
+        return SERVER_ENDPOINT
+
+    return server_url
