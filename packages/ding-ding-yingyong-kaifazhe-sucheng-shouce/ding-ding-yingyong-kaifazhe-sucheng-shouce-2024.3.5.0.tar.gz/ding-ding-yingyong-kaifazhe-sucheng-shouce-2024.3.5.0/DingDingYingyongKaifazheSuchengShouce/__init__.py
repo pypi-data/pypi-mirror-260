33c2f346550a7a1e6e1f5f283cd143a025a2e046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:39 +0000

    Add package ding-ding-yingyong-kaifazhe-sucheng-shouce/2024.3.5.0/ding-ding-yingyong-kaifazhe-sucheng-shouce-2024.3.5.0.tar.gz

---
+++ packages/ding-ding-yingyong-kaifazhe-sucheng-shouce/ding-ding-yingyong-kaifazhe-sucheng-shouce-2024.3.5.0.tar.gz/ding-ding-yingyong-kaifazhe-sucheng-shouce-2024.3.5.0/DingDingYingyongKaifazheSuchengShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ding-ding-yingyong-kaifazhe-sucheng-shouce
+https://github.com/apachecn/ding-ding-yingyong-kaifazhe-sucheng-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
