3aa83d576e2577b3ce2c2282fcd80afabba0fe5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:16 +0000

    Add package audicium-extension-base/0.1/audicium-extension-base-0.1.tar.gz

---
+++ packages/audicium-extension-base/audicium-extension-base-0.1.tar.gz/audicium-extension-base-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="audicium-extension-base",
+    version="0.1",
+    packages=find_packages(),
+    description="Base classes and models to create an audicium extension",
+    author="RA341",
+    author_email="",
+    url="https://github.com/RA341/audicium_extension_base",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
