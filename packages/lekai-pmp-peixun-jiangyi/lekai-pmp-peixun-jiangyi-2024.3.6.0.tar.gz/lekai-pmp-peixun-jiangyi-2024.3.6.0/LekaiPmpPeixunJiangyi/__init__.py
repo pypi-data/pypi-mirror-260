19b27cd69b46e706d57d0b98691920cece469894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:29 +0000

    Add package lekai-pmp-peixun-jiangyi/2024.3.6.0/lekai-pmp-peixun-jiangyi-2024.3.6.0.tar.gz

---
+++ packages/lekai-pmp-peixun-jiangyi/lekai-pmp-peixun-jiangyi-2024.3.6.0.tar.gz/lekai-pmp-peixun-jiangyi-2024.3.6.0/LekaiPmpPeixunJiangyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lekai-pmp-peixun-jiangyi
+https://github.com/apachecn/lekai-pmp-peixun-jiangyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
