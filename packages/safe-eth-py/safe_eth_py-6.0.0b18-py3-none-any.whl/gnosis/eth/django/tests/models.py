70893f0bbe5c3e127fd4cd0a4f7c22964a9b80f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:28 +0000

    Add package safe-eth-py/6.0.0b18/safe_eth_py-6.0.0b18-py3-none-any.whl

---
+++ packages/safe-eth-py/safe_eth_py-6.0.0b18-py3-none-any.whl/gnosis/eth/django/tests/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from ..models import (
+    EthereumAddressField,
+    EthereumAddressV2Field,
+    Keccak256Field,
+    Sha3HashField,
+    Uint96Field,
+    Uint256Field,
+)
+
+
+class EthereumAddress(models.Model):
+    value = EthereumAddressField(null=True)
+
+
+class EthereumAddressV2(models.Model):
+    value = EthereumAddressV2Field(null=True)
+
+
+class Uint256(models.Model):
+    value = Uint256Field(null=True)
+
+
+class Uint96(models.Model):
+    value = Uint96Field(null=True)
+
+
+class Sha3Hash(models.Model):
+    value = Sha3HashField(null=True)
+
+
+class Keccak256Hash(models.Model):
+    value = Keccak256Field(null=True)
