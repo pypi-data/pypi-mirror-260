828d1dd07a8dbb6887f3a047b15e7b4a34c083f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:08 +0000

    Add package yunduan-sheji-yu-shijian-saipao/2024.3.6.0/yunduan-sheji-yu-shijian-saipao-2024.3.6.0.tar.gz

---
+++ packages/yunduan-sheji-yu-shijian-saipao/yunduan-sheji-yu-shijian-saipao-2024.3.6.0.tar.gz/yunduan-sheji-yu-shijian-saipao-2024.3.6.0/YunduanShejiYuShijianSaipao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yunduan-sheji-yu-shijian-saipao
+https://github.com/apachecn/yunduan-sheji-yu-shijian-saipao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
