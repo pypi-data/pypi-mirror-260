5a9b1b8a6c49034a6a8ae360b7800a45d7d1e8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:49 +0000

    Add package jihe-yuanben-yingwen-wenziban/2024.3.6.0/jihe-yuanben-yingwen-wenziban-2024.3.6.0.tar.gz

---
+++ packages/jihe-yuanben-yingwen-wenziban/jihe-yuanben-yingwen-wenziban-2024.3.6.0.tar.gz/jihe-yuanben-yingwen-wenziban-2024.3.6.0/JiheYuanbenYingwenWenziban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jihe-yuanben-yingwen-wenziban
+https://github.com/apachecn/jihe-yuanben-yingwen-wenziban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
