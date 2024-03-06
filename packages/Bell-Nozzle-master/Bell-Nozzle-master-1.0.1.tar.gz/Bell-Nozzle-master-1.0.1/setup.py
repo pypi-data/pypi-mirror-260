bbc25454b8b4cf18353a6b9c9bc54f66aa694413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:04:56 +0000

    Add package Bell-Nozzle-master/1.0.1/Bell-Nozzle-master-1.0.1.tar.gz

---
+++ packages/Bell-Nozzle-master/Bell-Nozzle-master-1.0.1.tar.gz/Bell-Nozzle-master-1.0.1/setup.py
@@ -0,0 +1,36 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '1.0.1'
+DESCRIPTION = 'Tools for designing bell nozzles'
+LONG_DESCRIPTION = 'Tools for designing bell nozzles'
+
+# Setting up
+setup(
+    name="Bell-Nozzle-master",
+    version=VERSION,
+    author="",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(include=["Bell-Nozzle-master", "Bell-Nozzle-master.*"], ),
+    install_requires=['arcade'],
+    keywords=['python','nozzle','bell nozzle'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
