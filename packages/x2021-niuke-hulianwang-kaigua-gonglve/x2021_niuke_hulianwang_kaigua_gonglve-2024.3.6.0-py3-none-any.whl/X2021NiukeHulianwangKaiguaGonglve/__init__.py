f2c307fd06dea6df723dcf08a3a46b04d4d51571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:06 +0000

    Add package x2021-niuke-hulianwang-kaigua-gonglve/2024.3.6.0/x2021_niuke_hulianwang_kaigua_gonglve-2024.3.6.0-py3-none-any.whl

---
+++ packages/x2021-niuke-hulianwang-kaigua-gonglve/x2021_niuke_hulianwang_kaigua_gonglve-2024.3.6.0-py3-none-any.whl/X2021NiukeHulianwangKaiguaGonglve/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x2021-niuke-hulianwang-kaigua-gonglve
+https://github.com/apachecn/x2021-niuke-hulianwang-kaigua-gonglve"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
