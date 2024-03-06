f8622560518f9d8e3c5cd6c510e15f077135b60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:04:55 +0000

    Add package python-opencv3-jisuanji-shijue-miji-chuyi/2024.3.6.0/python_opencv3_jisuanji_shijue_miji_chuyi-2024.3.6.0-py3-none-any.whl

---
+++ packages/python-opencv3-jisuanji-shijue-miji-chuyi/python_opencv3_jisuanji_shijue_miji_chuyi-2024.3.6.0-py3-none-any.whl/PythonOpencv3JisuanjiShijueMijiChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""python-opencv3-jisuanji-shijue-miji-chuyi
+https://github.com/apachecn/python-opencv3-jisuanji-shijue-miji-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
