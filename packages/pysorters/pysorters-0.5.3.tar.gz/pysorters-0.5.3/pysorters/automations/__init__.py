f5adda5ed9120acc5ebcdb9d5f5fd3f3bbf35e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:17 +0000

    Add package pysorters/0.5.3/pysorters-0.5.3.tar.gz

---
+++ packages/pysorters/pysorters-0.5.3.tar.gz/pysorters-0.5.3/pysorters/automations/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module contains the automations for the pysorters_project package.
+"""
+
+import pysorters.automations.filepy
+from pysorters.automations import filepy
+from pysorters.automations.filepy import Pyxplorer, Sweepy
