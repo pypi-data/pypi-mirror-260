df3fe991024d99be4d21209a695c0f0216aa634e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:04:21 +0000

    Add package viadot2/2.0a17/viadot2-2.0a17.tar.gz

---
+++ packages/viadot2/viadot2-2.0a17.tar.gz/viadot2-2.0a17/setup.py
@@ -0,0 +1,46 @@
+import os
+import re
+
+import setuptools
+
+
+def get_version(package: str):
+    """Return package version as listed in `__version__` in `init.py`."""
+    init_py = open(os.path.join(package, "__init__.py")).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+extras = {
+    "sap": ["pyrfc==2.5.0", "sql-metadata==2.3.0"],
+}
+
+setuptools.setup(
+    name="viadot2",
+    version=get_version("viadot"),
+    author="Alessio Civitillo",
+    maintainer="Michal Zawadzki",
+    maintainer_email="mzawadzki@dyvenia.com",
+    description="A simple data ingestion library to guide data flows from some places to other places",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dyvenia/viadot",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "sharepy>=2.0.0, <3.0.0",
+        "pyodbc>=4.0.32, <5.0.0",
+        "databricks-connect>=11.3.0, <11.4.0",
+        "adlfs>=2022.9.1",
+        "pyarrow>=7.0.0, <12.0.0",
+        "openpyxl>=3.0.10, <4.0.0",
+    ],
+    extras=extras,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
