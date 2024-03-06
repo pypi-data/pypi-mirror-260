a3b73c5639e06a087df72f476ee84e8af242940b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:47 +0000

    Add package jian-zhi-qianduan-offer/2024.3.5.0/jian-zhi-qianduan-offer-2024.3.5.0.tar.gz

---
+++ packages/jian-zhi-qianduan-offer/jian-zhi-qianduan-offer-2024.3.5.0.tar.gz/jian-zhi-qianduan-offer-2024.3.5.0/JianZhiQianduanOffer/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jian-zhi-qianduan-offer
+https://github.com/apachecn/jian-zhi-qianduan-offer"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
