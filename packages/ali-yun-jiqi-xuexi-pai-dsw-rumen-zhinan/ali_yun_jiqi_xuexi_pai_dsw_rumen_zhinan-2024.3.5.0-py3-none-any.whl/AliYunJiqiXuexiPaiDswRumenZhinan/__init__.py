32d448c9173df037c6f1878a94c18ffa8bea4055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:04:35 +0000

    Add package ali-yun-jiqi-xuexi-pai-dsw-rumen-zhinan/2024.3.5.0/ali_yun_jiqi_xuexi_pai_dsw_rumen_zhinan-2024.3.5.0-py3-none-any.whl

---
+++ packages/ali-yun-jiqi-xuexi-pai-dsw-rumen-zhinan/ali_yun_jiqi_xuexi_pai_dsw_rumen_zhinan-2024.3.5.0-py3-none-any.whl/AliYunJiqiXuexiPaiDswRumenZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ali-yun-jiqi-xuexi-pai-dsw-rumen-zhinan
+https://github.com/apachecn/ali-yun-jiqi-xuexi-pai-dsw-rumen-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
