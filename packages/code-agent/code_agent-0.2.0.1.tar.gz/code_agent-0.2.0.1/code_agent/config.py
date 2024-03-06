48ccbea6333ef244a2a96db53a2b1ce99f8bf8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:04:45 +0000

    Add package code-agent/0.2.0.1/code_agent-0.2.0.1.tar.gz

---
+++ packages/code-agent/code_agent-0.2.0.1.tar.gz/code_agent-0.2.0.1/code_agent/config.py
@@ -0,0 +1,15 @@
+from os.path import join
+from dynaconf import Dynaconf
+from code_agent.definitions import ROOT_DIR
+
+settings = Dynaconf(
+    envvar_prefix="DYNACONF",
+    settings_files=[join(ROOT_DIR, f) for f in [
+        'code_agent/settings/commit_maker_config.toml',
+        'code_agent/settings/code_refactorer_config.toml',
+
+    ]]
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
