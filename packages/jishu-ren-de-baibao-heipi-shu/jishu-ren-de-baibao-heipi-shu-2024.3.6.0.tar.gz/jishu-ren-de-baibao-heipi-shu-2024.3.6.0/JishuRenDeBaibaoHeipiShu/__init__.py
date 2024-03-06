47b26fb4767f7ba98b1a180ecfbc6a214ec976da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:04:40 +0000

    Add package jishu-ren-de-baibao-heipi-shu/2024.3.6.0/jishu-ren-de-baibao-heipi-shu-2024.3.6.0.tar.gz

---
+++ packages/jishu-ren-de-baibao-heipi-shu/jishu-ren-de-baibao-heipi-shu-2024.3.6.0.tar.gz/jishu-ren-de-baibao-heipi-shu-2024.3.6.0/JishuRenDeBaibaoHeipiShu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jishu-ren-de-baibao-heipi-shu
+https://github.com/apachecn/jishu-ren-de-baibao-heipi-shu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
