0e7e2d8e1807086f8f74aed1382c9da62c0723a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 16:05:47 +0000

    Add package bamboost/0.5.1/bamboost-0.5.1-py3-none-any.whl

---
+++ packages/bamboost/bamboost-0.5.1-py3-none-any.whl/bamboost/__init__.py
@@ -0,0 +1,21 @@
+__author__ = "florez@ethz.ch"
+__copyright__ = ""
+__license__ = "LGPLv3"
+__version__ = "0.5.1"
+
+import logging
+import os
+
+from .manager import Manager
+from .simulation import Simulation
+from .simulation_writer import SimulationWriter
+
+
+def set_log_level(level: int = 30):
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", level))
+
+
+# Set default logging level to WARNING (30)
+set_log_level(30)
