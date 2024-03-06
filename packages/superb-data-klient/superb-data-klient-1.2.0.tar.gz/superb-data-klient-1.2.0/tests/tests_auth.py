3491367711b294b9cd81b0e3a239bea910e9f3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 17:04:34 +0000

    Add package superb-data-klient/1.2.0/superb-data-klient-1.2.0.tar.gz

---
+++ packages/superb-data-klient/superb-data-klient-1.2.0.tar.gz/superb-data-klient-1.2.0/tests/tests_auth.py
@@ -0,0 +1,22 @@
+import unittest
+import webbrowser
+import superbdataklient as sdk
+
+
+class AuthenticationTestSDKClient(unittest.TestCase):
+    env = 'sdk-dev'
+
+    def test_get_organization_all(self):
+        """
+        instantiating a SDKClient should raise EnvironmentError in a no-browser-environment
+        """
+        try:
+            webbrowser.get()
+            sdk.SDKClient(env=self.env)
+        except webbrowser.Error:
+            with self.assertRaises(EnvironmentError):
+                sdk.SDKClient(env=self.env)
+
+
+if __name__ == '__main__':
+    unittest.main()
