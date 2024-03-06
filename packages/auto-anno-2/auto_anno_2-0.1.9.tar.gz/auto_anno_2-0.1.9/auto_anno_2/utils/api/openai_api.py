3de95948631a73a69aedd00e6599c9513e5e4507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:38 +0000

    Add package auto-anno-2/0.1.9/auto_anno_2-0.1.9.tar.gz

---
+++ packages/auto-anno-2/auto_anno_2-0.1.9.tar.gz/auto_anno_2-0.1.9/auto_anno_2/utils/api/openai_api.py
@@ -0,0 +1,48 @@
+import openai
+
+i = 0
+
+def chat_openai(user):
+    from ...local_config import config
+    openai_key = config['openai']['key']
+    if type(openai_key) == list:
+        openai_keys = openai_key
+    else:
+        openai_keys = [openai_key]
+
+    # Set up your API key
+    openai.api_key = openai_keys[0]
+    global i
+    i += 1
+    if i >= len(openai_keys):
+        i = 0
+    openai.api_key = openai_keys[i]
+    # openai.api_base = "https://api.openai.com/v1"
+    # openai.api_base = "http://47.89.230.109/v1"
+    # openai.api_base = "https://api.tekii.cn/v1"
+    # openai.api_base = "https://api.aiproxy.io/v1"
+    openai.api_base = config['openai'].get('api_base', 'https://api.openai.com/v1')
+    model = config['openai'].get('model', 'gpt-3.5-turbo')
+    # Call the OpenAI API
+    completion = openai.ChatCompletion.create(
+                    model=model,
+                    messages=[
+                        {"role": "user", "content": f"{user}"},
+                    ]
+                )
+
+    # Extract the output and parse the JSON array
+    content = completion.choices[0].message.content
+
+    return content
+
+if __name__ == '__main__':
+    content = chat_openai('你好')
+    print(content)
+    # import flask
+    # app = flask.Flask(__name__)
+    # @app.route('/chat', methods=['POST'])
+    # def chat_api():
+    #     user = flask.request.form.get('user')
+    #     return chat(user)
+    # app.run(port=5000, host='0.0.0.0')
