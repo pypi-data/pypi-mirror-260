dfb3e569e3c5e23351211aa35f0066de204398ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:05:17 +0000

    Add package NeuroNetworkByHands/0.1.1/neuronetworkbyhands-0.1.1.tar.gz

---
+++ packages/NeuroNetworkByHands/neuronetworkbyhands-0.1.1.tar.gz/neuronetworkbyhands-0.1.1/NeuroNetworkByHands/NeuroNetwork/Layer.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+
