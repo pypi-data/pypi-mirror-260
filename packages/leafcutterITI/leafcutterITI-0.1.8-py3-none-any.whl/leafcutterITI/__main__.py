243822cf1590833ec5b008675846790d088fbd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:08 +0000

    Add package leafcutterITI/0.1.8/leafcutterITI-0.1.8-py3-none-any.whl

---
+++ packages/leafcutterITI/leafcutterITI-0.1.8-py3-none-any.whl/leafcutterITI/__main__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from runpy import run_path
+
+pkg_dir = Path(__file__).resolve().parent
+
+def leafcutterITI_clustering():
+    script_pth = pkg_dir / "clustering" / "leafcutterITI_clustering.py"
+    run_path(str(script_pth), run_name="__main__")
+    
+def leafcutterITI_map_gen():
+    script_pth = pkg_dir / "map_gen" / "LeafcutterITI_map_gen.py"
+    run_path(str(script_pth), run_name="__main__")
