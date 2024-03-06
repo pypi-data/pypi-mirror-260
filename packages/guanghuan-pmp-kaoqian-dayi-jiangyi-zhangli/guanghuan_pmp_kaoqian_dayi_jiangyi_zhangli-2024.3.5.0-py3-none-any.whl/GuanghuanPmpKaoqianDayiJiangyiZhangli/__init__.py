f47e8b2531f44a1c86fde6e71f40f9f349ad6d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:25 +0000

    Add package guanghuan-pmp-kaoqian-dayi-jiangyi-zhangli/2024.3.5.0/guanghuan_pmp_kaoqian_dayi_jiangyi_zhangli-2024.3.5.0-py3-none-any.whl

---
+++ packages/guanghuan-pmp-kaoqian-dayi-jiangyi-zhangli/guanghuan_pmp_kaoqian_dayi_jiangyi_zhangli-2024.3.5.0-py3-none-any.whl/GuanghuanPmpKaoqianDayiJiangyiZhangli/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""guanghuan-pmp-kaoqian-dayi-jiangyi-zhangli
+https://github.com/apachecn/guanghuan-pmp-kaoqian-dayi-jiangyi-zhangli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
