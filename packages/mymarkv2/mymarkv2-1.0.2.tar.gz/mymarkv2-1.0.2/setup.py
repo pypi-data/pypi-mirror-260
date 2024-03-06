bde8442f8dbc504dd02835b65599890101e06f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:05:04 +0000

    Add package mymarkv2/1.0.2/mymarkv2-1.0.2.tar.gz

---
+++ packages/mymarkv2/mymarkv2-1.0.2.tar.gz/mymarkv2-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mymarkv2",
+    version="1.0.2",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "mymark=marker_tool.mymark:main",
+        ],
+    },
+    install_requires=[
+        "requests==2.31.0",
+    ],
+)
