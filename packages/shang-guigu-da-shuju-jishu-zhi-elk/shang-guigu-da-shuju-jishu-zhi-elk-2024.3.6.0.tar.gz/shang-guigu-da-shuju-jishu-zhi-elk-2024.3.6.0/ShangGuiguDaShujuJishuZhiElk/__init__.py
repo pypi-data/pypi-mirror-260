5e29bcbeefffa0467cd60dcf33fa46b3054dacbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:04:54 +0000

    Add package shang-guigu-da-shuju-jishu-zhi-elk/2024.3.6.0/shang-guigu-da-shuju-jishu-zhi-elk-2024.3.6.0.tar.gz

---
+++ packages/shang-guigu-da-shuju-jishu-zhi-elk/shang-guigu-da-shuju-jishu-zhi-elk-2024.3.6.0.tar.gz/shang-guigu-da-shuju-jishu-zhi-elk-2024.3.6.0/ShangGuiguDaShujuJishuZhiElk/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shang-guigu-da-shuju-jishu-zhi-elk
+https://github.com/apachecn/shang-guigu-da-shuju-jishu-zhi-elk"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
