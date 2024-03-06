af932eb441c68004da9364f18e4a68cc346f68bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:45 +0000

    Add package ceh-v8-module-4-5/2024.3.6.0/ceh_v8_module_4_5-2024.3.6.0-py3-none-any.whl

---
+++ packages/ceh-v8-module-4-5/ceh_v8_module_4_5-2024.3.6.0-py3-none-any.whl/CehV8Module45/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ceh-v8-module-4-5
+https://github.com/apachecn/ceh-v8-module-4-5"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
