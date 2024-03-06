c49f15244903139d9e6dde8b9c128d8c07416e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:28 +0000

    Add package x2020-zhiyuan-dahui-wenji-heji-disi-bufen/2024.3.6.0/x2020_zhiyuan_dahui_wenji_heji_disi_bufen-2024.3.6.0-py3-none-any.whl

---
+++ packages/x2020-zhiyuan-dahui-wenji-heji-disi-bufen/x2020_zhiyuan_dahui_wenji_heji_disi_bufen-2024.3.6.0-py3-none-any.whl/X2020ZhiyuanDahuiWenjiHejiDisiBufen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x2020-zhiyuan-dahui-wenji-heji-disi-bufen
+https://github.com/apachecn/x2020-zhiyuan-dahui-wenji-heji-disi-bufen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
