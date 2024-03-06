4ad33086249424c6c0f2c5f2b3760f0003d465e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 15:05:10 +0000

    Add package ceh-v9-module-7-12/2024.3.6.0/ceh_v9_module_7_12-2024.3.6.0-py3-none-any.whl

---
+++ packages/ceh-v9-module-7-12/ceh_v9_module_7_12-2024.3.6.0-py3-none-any.whl/CehV9Module712/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ceh-v9-module-7-12
+https://github.com/apachecn/ceh-v9-module-7-12"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
