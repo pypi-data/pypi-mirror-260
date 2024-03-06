daca35b1888ff861488b7a50f7eecdce40acba96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:04:05 +0000

    Add package how2j-java-kuangjia-jiaocheng/2024.3.5.0/how2j-java-kuangjia-jiaocheng-2024.3.5.0.tar.gz

---
+++ packages/how2j-java-kuangjia-jiaocheng/how2j-java-kuangjia-jiaocheng-2024.3.5.0.tar.gz/how2j-java-kuangjia-jiaocheng-2024.3.5.0/How2jJavaKuangjiaJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""how2j-java-kuangjia-jiaocheng
+https://github.com/apachecn/how2j-java-kuangjia-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
