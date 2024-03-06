6eae6e4e9b338ef9837d523fe814bc8c47121a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:40 +0000

    Add package dts-kongzhitai-rumen-yiben-tong/2024.3.5.0/dts_kongzhitai_rumen_yiben_tong-2024.3.5.0-py3-none-any.whl

---
+++ packages/dts-kongzhitai-rumen-yiben-tong/dts_kongzhitai_rumen_yiben_tong-2024.3.5.0-py3-none-any.whl/DtsKongzhitaiRumenYibenTong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dts-kongzhitai-rumen-yiben-tong
+https://github.com/apachecn/dts-kongzhitai-rumen-yiben-tong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
