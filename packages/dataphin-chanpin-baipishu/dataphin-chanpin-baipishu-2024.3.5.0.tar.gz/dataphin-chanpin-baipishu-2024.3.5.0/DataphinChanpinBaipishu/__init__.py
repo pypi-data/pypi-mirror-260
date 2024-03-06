2b72c6c3cf8d3f0e94197fb8f57a49cc5a7ea97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 04:03:36 +0000

    Add package dataphin-chanpin-baipishu/2024.3.5.0/dataphin-chanpin-baipishu-2024.3.5.0.tar.gz

---
+++ packages/dataphin-chanpin-baipishu/dataphin-chanpin-baipishu-2024.3.5.0.tar.gz/dataphin-chanpin-baipishu-2024.3.5.0/DataphinChanpinBaipishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dataphin-chanpin-baipishu
+https://github.com/apachecn/dataphin-chanpin-baipishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
