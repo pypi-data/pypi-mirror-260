9b09892a69a836b802dfac3cd25c9e587021c525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 05:05:39 +0000

    Add package leetcode-cookbook/2024.3.6.0/leetcode-cookbook-2024.3.6.0.tar.gz

---
+++ packages/leetcode-cookbook/leetcode-cookbook-2024.3.6.0.tar.gz/leetcode-cookbook-2024.3.6.0/LeetcodeCookbook/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""leetcode-cookbook
+https://github.com/apachecn/leetcode-cookbook"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
