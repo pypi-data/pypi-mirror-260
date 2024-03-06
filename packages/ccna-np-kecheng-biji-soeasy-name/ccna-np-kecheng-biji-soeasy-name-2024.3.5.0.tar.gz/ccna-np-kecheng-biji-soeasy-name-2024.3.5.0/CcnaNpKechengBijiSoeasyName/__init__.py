64427c74593b76cd79a7aa0c96dce3d7ff7b319c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:04:53 +0000

    Add package ccna-np-kecheng-biji-soeasy-name/2024.3.5.0/ccna-np-kecheng-biji-soeasy-name-2024.3.5.0.tar.gz

---
+++ packages/ccna-np-kecheng-biji-soeasy-name/ccna-np-kecheng-biji-soeasy-name-2024.3.5.0.tar.gz/ccna-np-kecheng-biji-soeasy-name-2024.3.5.0/CcnaNpKechengBijiSoeasyName/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ccna-np-kecheng-biji-soeasy-name
+https://github.com/apachecn/ccna-np-kecheng-biji-soeasy-name"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.5.0"
