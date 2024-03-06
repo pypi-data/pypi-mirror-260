18887ac3f44a9eeec8f507471e6ae8f68bd558f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 07:04:36 +0000

    Add package shishi-ji-weilai/2024.3.6.0/shishi_ji_weilai-2024.3.6.0-py3-none-any.whl

---
+++ packages/shishi-ji-weilai/shishi_ji_weilai-2024.3.6.0-py3-none-any.whl/ShishiJiWeilai/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shishi-ji-weilai
+https://github.com/apachecn/shishi-ji-weilai"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
