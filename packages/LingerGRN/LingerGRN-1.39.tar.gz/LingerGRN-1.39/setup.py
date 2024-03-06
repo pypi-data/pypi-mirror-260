3911c2a138b246c05f1d253f741fba75e43fbffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 03:05:36 +0000

    Add package LingerGRN/1.39/LingerGRN-1.39.tar.gz

---
+++ packages/LingerGRN/LingerGRN-1.39.tar.gz/LingerGRN-1.39/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='LingerGRN',
+    version='1.39',
+    description='Gene regulatory network inference',
+    author='Kaya Yuan',
+    author_email='qyyuan33@gmail.com',
+    packages=['LingerGRN'],
+    license = "MIT",
+    url='https://github.com/Durenlab/LINGER',
+    install_requires=['torch', 'scipy', 'numpy', 'pandas', 'shap==0.44.0', 'scikit-learn', 'joblib','matplotlib','seaborn'],
+     scripts=['/zfs/durenlab/palmetto/Kaya/SC_NET/code/github/combine/LINGER/LingerGRN/scripts/extract_overlap_regions_LINGER.sh','/zfs/durenlab/palmetto/Kaya/SC_NET/code/github/combine/LINGER/LingerGRN/scripts/extract_overlap_regions_baseline.sh'],
+)
