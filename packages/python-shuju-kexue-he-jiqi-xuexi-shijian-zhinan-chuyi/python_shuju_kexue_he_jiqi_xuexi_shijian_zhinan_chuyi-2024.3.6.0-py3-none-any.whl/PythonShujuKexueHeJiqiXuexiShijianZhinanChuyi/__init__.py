324fd44e992957086de94a577255b3f19c1ba9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 06:05:02 +0000

    Add package python-shuju-kexue-he-jiqi-xuexi-shijian-zhinan-chuyi/2024.3.6.0/python_shuju_kexue_he_jiqi_xuexi_shijian_zhinan_chuyi-2024.3.6.0-py3-none-any.whl

---
+++ packages/python-shuju-kexue-he-jiqi-xuexi-shijian-zhinan-chuyi/python_shuju_kexue_he_jiqi_xuexi_shijian_zhinan_chuyi-2024.3.6.0-py3-none-any.whl/PythonShujuKexueHeJiqiXuexiShijianZhinanChuyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""python-shuju-kexue-he-jiqi-xuexi-shijian-zhinan-chuyi
+https://github.com/apachecn/python-shuju-kexue-he-jiqi-xuexi-shijian-zhinan-chuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
