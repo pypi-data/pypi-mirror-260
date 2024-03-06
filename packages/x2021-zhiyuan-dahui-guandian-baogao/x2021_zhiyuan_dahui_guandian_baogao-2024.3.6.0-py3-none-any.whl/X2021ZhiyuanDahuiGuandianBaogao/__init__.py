e0e1a8a3cc90aed50ce2b75257d70bca06c154c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 14:04:07 +0000

    Add package x2021-zhiyuan-dahui-guandian-baogao/2024.3.6.0/x2021_zhiyuan_dahui_guandian_baogao-2024.3.6.0-py3-none-any.whl

---
+++ packages/x2021-zhiyuan-dahui-guandian-baogao/x2021_zhiyuan_dahui_guandian_baogao-2024.3.6.0-py3-none-any.whl/X2021ZhiyuanDahuiGuandianBaogao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x2021-zhiyuan-dahui-guandian-baogao
+https://github.com/apachecn/x2021-zhiyuan-dahui-guandian-baogao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2024.3.6.0"
