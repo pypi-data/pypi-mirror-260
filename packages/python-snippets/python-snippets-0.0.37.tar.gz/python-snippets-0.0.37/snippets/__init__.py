6c85dda88c97a47fa009092fec89b24eaaa8ba23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-06 17:04:19 +0000

    Add package python-snippets/0.0.37/python-snippets-0.0.37.tar.gz

---
+++ packages/python-snippets/python-snippets-0.0.37.tar.gz/python-snippets-0.0.37/snippets/__init__.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# -*- coding utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     __init__.py.py
+   Author :       chenhao
+   time：          2021/10/18 14:34
+   Description :
+-------------------------------------------------
+"""
+
+import os
+SNIPPETS_ENV = os.environ.get("SNIPPETS_ENV", "prod")
+
+
+from snippets.perf import *
+from snippets.logs import *
+from snippets.decorators import *
+from snippets.utils import *
+from snippets.mixin import *
+from snippets.evaluate import *
+import os
