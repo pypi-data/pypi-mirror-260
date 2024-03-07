e31b66fa0e8a5ae7dc7e7119be80a1768e194cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 17:04:55 +0000

    Add package hashtagger/0.1.2/hashtagger-0.1.2.tar.gz

---
+++ packages/hashtagger/hashtagger-0.1.2.tar.gz/hashtagger-0.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python',
+  'Programming Language :: Python :: 3',
+  'Programming Language :: Python :: 3.7',
+  'Programming Language :: Python :: 3.8',
+  'Programming Language :: Python :: 3.9',
+  'Programming Language :: Python :: 3.10',
+  'Programming Language :: Python :: 3.11',
+  'Programming Language :: Python :: 3.12',  # Hypothetical future version
+]
+ 
+setup(
+  name='hashtagger',
+  version='0.1.2',
+  description='A hashtag generator using tensorflow and nltk',
+  long_description = (open('README.rst').read() + '\n\n' +
+                   open('CHANGELOG.txt').read()),
+  
+  url='',  
+  author='Meet Jethwa',
+  author_email='meetjethwa3@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='tagger', 
+  packages=find_packages(),
+  install_requires=[
+        "opencv-python",
+        "numpy",
+        "tensorflow",
+        "nltk",
+    ] 
+)
---
+++ packages/hashtagger/hashtagger-0.1.2.tar.gz/hashtagger-0.1.2/test.py
@@ -0,0 +1,21 @@
+from hashtag import hashtagger
+
+# Create an instance of YourLibrary
+your_library = hashtagger()
+
+# Specify the path to the image you want to process
+image_path = "C:\\Users\\Tina\\OneDrive\\Desktop\\car.jpg"  # Replace with the path to your image
+
+try:
+    # Use the recognize_objects method to recognize objects in the image
+    decoded_predictions = your_library.recognize_objects(image_path)
+
+    # Use the generate_tags method to generate tags for the recognized objects
+    tags = your_library.generate_tags(decoded_predictions)
+
+    print("Recognized objects and tags:")
+    for tag in tags:
+        print(tag)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
