6732bea0d59589b9a902ebb37301a1b8d37c5937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:07:35 +0000

    Add package melting-schemas/1.0.5/melting_schemas-1.0.5.tar.gz

---
+++ packages/melting-schemas/melting_schemas-1.0.5.tar.gz/melting_schemas-1.0.5/melting_schemas/templating/__init__.py
@@ -0,0 +1,11 @@
+from .prompt import (
+    ChatPrompt,
+    ChatPromptTemplate,
+    CreateCompletionPrompt,
+    GeneratedFields,
+    GetCompletionPrompt,
+    UpdateChatPrompt,
+    UpdateChatTemplateData,
+    UpdateCompletionPrompt,
+    UpdateSettings,
+)
