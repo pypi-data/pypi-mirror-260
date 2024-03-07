cb1c071eb0c54c27ab6ea29cff8ee7e79be414bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:05:24 +0000

    Add package PyCAI2/1/PyCAI2-1.tar.gz

---
+++ packages/PyCAI2/PyCAI2-1.tar.gz/PyCAI2-1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyCAI2',
+    version='1',
+    packages=find_packages(),
+    description='Un Oficial Python wraper for character ai',
+    author='Tokai Falco',
+    author_email='otsronca@gmail.com',
+    url='https://github.com/FalcoTK/PyCAI2/',
+)
