f26095e0f8b3ece5423f748b0d4b88da163904e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:06:28 +0000

    Add package robustcheck/1.1.0/robustcheck-1.1.0.tar.gz

---
+++ packages/robustcheck/robustcheck-1.1.0.tar.gz/robustcheck-1.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "matplotlib >= 3.7.0",
+    "numpy >= 1.18.0",
+    "mlflow >= 1.2.0",
+    "tqdm >= 4.64.1",
+]
+
+setup(
+    name="robustcheck",
+    packages=find_packages(),
+    description="Tool for quick assessment of image classifiers robustness",
+    url="https://github.com/andreiilie1/RobustCheck",
+    download_url="https://github.com/andreiilie1/RobustCheck/archive/refs/tags/v1.1.0.tar.gz",
+    author="Andrei Ilie",
+    author_email="andrei0758@gmail.com",
+    include_package_data=True,
+    python_requires=">=3.8.0",
+    install_requires=requirements,
+)
