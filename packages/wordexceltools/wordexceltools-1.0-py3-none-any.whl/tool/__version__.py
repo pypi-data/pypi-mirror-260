9b70f271ef2bf019b9c4a18e002af320e1c822e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 17:04:49 +0000

    Add package wordexceltools/1.0/wordexceltools-1.0-py3-none-any.whl

---
+++ packages/wordexceltools/wordexceltools-1.0-py3-none-any.whl/tool/__version__.py
@@ -0,0 +1,12 @@
+"""
+Author       : Sunmy
+Date         : 2024-03-07 02:13:01
+LastEditTime : 2024-03-07 02:15:16
+LastEditors  : Sunmy ldspdvs@gmail.com
+FilePath     : __version__.py
+Description  :  
+"""
+
+VERSION = (0, 1, 4)
+
+__version__ = '.'.join(map(str, VERSION))
