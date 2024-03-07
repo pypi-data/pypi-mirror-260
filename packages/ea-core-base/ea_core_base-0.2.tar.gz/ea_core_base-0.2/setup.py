122320e6c57920577ee9fdde73635e7bb71dc3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:45 +0000

    Add package ea-core-base/0.2/ea_core_base-0.2.tar.gz

---
+++ packages/ea-core-base/ea_core_base-0.2.tar.gz/ea_core_base-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ea_core_base',
+    version='0.2',
+    packages=find_packages(),
+    py_modules=['energy_analysis_input','energy_analysis_output'],  
+    description='Energy Analysis Core Base',
+    author='Kambiz Ezzati',
+    author_email='kezzati@armstrongfluidtechnology.com',
+    install_requires=[],  
+)
