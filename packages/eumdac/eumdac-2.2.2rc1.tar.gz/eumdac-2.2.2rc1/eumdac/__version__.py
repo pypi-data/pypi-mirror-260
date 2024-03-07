f49e0725227af6b9c588826a8ab0d129e05dc49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:06:53 +0000

    Add package eumdac/2.2.2rc1/eumdac-2.2.2rc1.tar.gz

---
+++ packages/eumdac/eumdac-2.2.2rc1.tar.gz/eumdac-2.2.2rc1/eumdac/__version__.py
@@ -0,0 +1,11 @@
+"""Module containing package information for setup.py and __init__.py"""
+__title__ = "eumdac"
+__description__ = "EUMETSAT Data Access Client"
+__url__ = "https://gitlab.eumetsat.int/eumetlab/data-services/eumdac"
+__version__ = "2.2.2-rc1"
+__documentation__ = (
+    "https://user.eumetsat.int/resources/user-guides/eumetsat-data-access-client-eumdac-guide"
+)
+__author__ = "EUMETSAT"
+__author_email__ = "ops@eumetsat.int"
+__license__ = "MIT"
