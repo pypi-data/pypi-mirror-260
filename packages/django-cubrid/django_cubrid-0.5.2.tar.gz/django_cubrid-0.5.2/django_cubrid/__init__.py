5116734af866028dc16038fcf5b00daabbaf76fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:44 +0000

    Add package django-cubrid/0.5.2/django_cubrid-0.5.2.tar.gz

---
+++ packages/django-cubrid/django_cubrid-0.5.2.tar.gz/django_cubrid-0.5.2/django_cubrid/__init__.py
@@ -0,0 +1,10 @@
+"""
+Django-CUBRID Backend
+
+This package provides a Django database backend for CUBRID, a comprehensive open-source
+relational database management system (RDBMS) highly optimized for web applications.
+The Django-CUBRID backend facilitates seamless integration and management of CUBRID
+databases within Django projects.
+"""
+
+__version__ = "0.5.2"
---
+++ packages/django-cubrid/django_cubrid-0.5.2.tar.gz/django_cubrid-0.5.2/setup.py
@@ -0,0 +1,36 @@
+"""Setup script for the django_cubrid Python package."""
+
+from setuptools import setup, find_packages
+
+with open('django_cubrid/__init__.py', "r", encoding='utf-8') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().strip("'").strip('"')
+            break
+
+with open('README.md', "r", encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='django_cubrid',
+    version=version,
+    description='A Django database backend for CUBRID',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Casian Andrei',
+    author_email='casian@zco.ro',
+    url='https://github.com/zimbrul-co/django_cubrid',
+    packages=find_packages(),
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'Django>=4.2',
+        'cubrid_db>=0.7.1',
+    ],
+    python_requires='>=3.9',
+    license='BSD-3-Clause',
+)
