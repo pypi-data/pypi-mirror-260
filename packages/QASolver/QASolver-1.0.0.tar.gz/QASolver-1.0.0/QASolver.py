3d46b32aa121671ca9a48d653637741a492093a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:38 +0000

    Add package QASolver/1.0.0/QASolver-1.0.0.tar.gz

---
+++ packages/QASolver/QASolver-1.0.0.tar.gz/QASolver-1.0.0/QASolver.py
@@ -0,0 +1,25 @@
+from typing import List 
+from questions import questions
+from solutions import solutions
+
+class BookSolutions:
+    def __init__(self):
+        self.questions = questions
+        self.solutions = solutions
+
+    def find_solution(self, query: str) -> List[tuple[str, str, str]] | str|None:
+        query_lower = query.lower()
+        matching_solutions: List[tuple[str, str, str]] = []
+        for key, value in self.questions.items():
+            if len(query_lower) > 10 and query_lower in value.lower():
+                matching_solutions.append((key, value, self.solutions[key]))
+            elif query_lower == key:
+                matching_solutions.append((key, value, self.solutions[key]))
+
+        if matching_solutions:
+            for key, question, value in matching_solutions:
+                print(f"Question: {key} {question} \nSolution:\n{value}\n")
+        return "Solution not found" if not matching_solutions else None
+
+book_solutions = BookSolutions()
+book_solutions.find_solution('Favorite Places: Make a dictionary called favorite_places. Think of three names to use as keys in the dictionary, and store one to three favorite places for each person. Loop through the dictionary, and print each person’s name and their favorite places.')
