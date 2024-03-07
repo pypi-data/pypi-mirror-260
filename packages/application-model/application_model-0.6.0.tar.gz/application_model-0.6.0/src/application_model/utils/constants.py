17c6b8b7c87a810597aef073716ae1a50eb8b526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:41 +0000

    Add package application-model/0.6.0/application_model-0.6.0.tar.gz

---
+++ packages/application-model/application_model-0.6.0.tar.gz/application_model-0.6.0/src/application_model/utils/constants.py
@@ -0,0 +1,7 @@
+
+PAYLOAD_SCHEMA = [
+    'ume-profession', 'ume-zipcode', 'ume-age_on_application', 'ume-segment', 'ume-retailer', 'ume-state', 
+    'bvsIncome-CLASSRENDAV2', 'bvsIncome-RendaPresumida', 'bvsP5-Score', 'bvsSubP5-Fintechs', 
+    'bvsSubP5-CartaoCredito', 'bvsSubP5-CreditoPessoal', 'bvsSubP5-VAR_MoveisEletrodomesticos', 
+    'bvsSubP5-VAR_VestuarioAcessorios', 'bvsSubP5-FinancialmentoVeiculos'
+]
