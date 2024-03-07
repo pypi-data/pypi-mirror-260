fe324f68956b1fde5dad9d2bd4120d8f9ad7fe5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:05:21 +0000

    Add package celescope/2.0.8/celescope-2.0.8-py3-none-any.whl

---
+++ packages/celescope/celescope-2.0.8-py3-none-any.whl/celescope/dynaseq/__init__.py
@@ -0,0 +1,17 @@
+STEPS = [
+    'sample',
+    'starsolo',
+    'analysis',
+    'conversion',
+    'substitution',
+    'replacement']
+
+__ASSAY__ = 'dynaseq'
+
+IMPORT_DICT = {
+    'analysis': 'celescope.rna',
+}
+
+
+#count
+DYNA_MATRIX_DIR_SUFFIX = ['labeled','unlabeled']
---
+++ packages/celescope/celescope-2.0.8-py3-none-any.whl/celescope/rna/__init__.py
@@ -0,0 +1,14 @@
+STEPS = [
+    'mkref',
+    'sample',
+    'starsolo',
+    'analysis',
+    'cells',
+]
+__ASSAY__ = 'rna'
+REMOVE_FROM_MULTI = {
+    'mkref',
+    'cells',
+}
+
+
