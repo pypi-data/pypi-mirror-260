f7fe8ca57418f776fd301826f5058729df0061d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:07:42 +0000

    Add package qfpy/2.0.0/qfpy-2.0.0.tar.gz

---
+++ packages/qfpy/qfpy-2.0.0.tar.gz/qfpy-2.0.0/src/qfpy/ffmpeg.py
@@ -0,0 +1,24 @@
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+
+def concat_video(files: list[str], output_file: str):
+    """
+    合并视频
+    
+    后处理：删除原视频
+
+    注意：只使用 CPU，合并大量视频很慢
+    """
+    clips = [VideoFileClip(f) for f in files]
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile(output_file)
+    for f in files:
+        f.unlink()
+
+
+if __name__ == "__main__":
+    filelist = [
+        "S:/rather/pic/daoayao/2021-08-06-vid_258.mp4",
+        "S:/rather/pic/daoayao/2021-08-08-vid_256.mp4",
+    ]
+    print(concat_video(filelist, "output.mp4"))
