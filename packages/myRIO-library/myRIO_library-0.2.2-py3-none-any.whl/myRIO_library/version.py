2f29fe8f3339e45ca4905d99ec17e57e19d08de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:32 +0000

    Add package myRIO-library/0.2.2/myRIO_library-0.2.2-py3-none-any.whl

---
+++ packages/myRIO-library/myRIO_library-0.2.2-py3-none-any.whl/myRIO_library/version.py
@@ -0,0 +1,9 @@
+# version.py
+__version__ = "0.2.2"
+
+""" version notes
+0.2.0: added __del__ method to myRio class to close the connection
+       to the myRIO (2024/03/05)
+0.2.2: fixed the Default.lvbitx error: now the file is distributed 
+       with the package. We use the relative folder of __file__ (2024/3/6)
+"""
