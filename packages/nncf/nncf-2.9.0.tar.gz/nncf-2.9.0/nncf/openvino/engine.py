e23794c61f3b66e3e6f273eaf6fc054ce3dfad58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:21 +0000

    Add package nncf/2.9.0/nncf-2.9.0.tar.gz

---
+++ packages/nncf/nncf-2.9.0.tar.gz/nncf-2.9.0/nncf/openvino/engine.py
@@ -0,0 +1,112 @@
+# Copyright (c) 2024 Intel Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict, List, Tuple, Union
+
+import numpy as np
+import openvino.runtime as ov
+
+import nncf
+from nncf.common.engine import Engine
+from nncf.openvino.graph.model_utils import model_has_state
+from nncf.parameters import TargetDevice
+
+
+class OVCompiledModelEngine(Engine):
+    """
+    Implementation of the engine to infer OpenVINO compiled model.
+
+    OVCompiledModelEngine uses
+    [OpenVINO Runtime](https://docs.openvino.ai/latest/openvino_docs_OV_UG_OV_Runtime_User_Guide.html)
+    to infer the compiled model.
+    """
+
+    def __init__(self, compiled_model: ov.CompiledModel, stateful: bool):
+        self.infer_request = compiled_model.create_infer_request()
+        self.reset_state = stateful and hasattr(self.infer_request, "reset_state")
+        self.input_tensor_names = set()
+        self.number_of_inputs = len(compiled_model.inputs)
+        for model_input in compiled_model.inputs:
+            self.input_tensor_names.update(model_input.get_names())
+
+    def _check_input_data_format(
+        self, input_data: Union[np.ndarray, List[np.ndarray], Tuple[np.ndarray], Dict[str, np.ndarray]]
+    ) -> None:
+        """
+        Checks correspondence of the model input names and the passed data.
+        If there is a mismatch, the method throws a more specific and readable error than
+        original error raised by the compiled model.
+
+        :param input_data: Provided inputs to infer the model.
+        """
+        actual_num_inputs = 1 if isinstance(input_data, np.ndarray) else len(input_data)
+        if actual_num_inputs != self.number_of_inputs:
+            raise nncf.ValidationError(
+                f"Model expects {self.number_of_inputs} inputs, but {actual_num_inputs} are provided."
+            )
+        if isinstance(input_data, dict):
+            for name in input_data:
+                if isinstance(name, str) and name not in self.input_tensor_names:
+                    raise nncf.ValidationError(f"Missing a required input: {name} to run the model.")
+
+    def infer(
+        self, input_data: Union[np.ndarray, List[np.ndarray], Tuple[np.ndarray], Dict[str, np.ndarray]]
+    ) -> Dict[str, np.ndarray]:
+        """
+        Runs model on the provided input via OpenVINO Runtime.
+        Returns the dictionary of model outputs by node names.
+
+        :param input_data: Inputs for the model.
+        :return output_data: Model's output.
+        """
+        self._check_input_data_format(input_data)
+
+        if self.reset_state:
+            self.infer_request.reset_state()
+
+        model_outputs = self.infer_request.infer(input_data, share_inputs=True)
+
+        output_data = {}
+        for tensor, value in model_outputs.items():
+            for tensor_name in tensor.get_names():
+                output_data[tensor_name] = value
+        return output_data
+
+
+class OVNativeEngine(Engine):
+    """
+    Implementation of the engine for OpenVINO backend.
+
+    OVNativeEngine uses
+    [OpenVINO Runtime](https://docs.openvino.ai/latest/openvino_docs_OV_UG_OV_Runtime_User_Guide.html)
+    to infer the model.
+    """
+
+    def __init__(self, model: ov.Model, target_device: TargetDevice = TargetDevice.CPU):
+        if target_device == TargetDevice.ANY:
+            target_device = TargetDevice.CPU
+
+        ie = ov.Core()
+        stateful = model_has_state(model)
+        compiled_model = ie.compile_model(model, target_device.value)
+        self.engine = OVCompiledModelEngine(compiled_model, stateful)
+
+    def infer(
+        self, input_data: Union[np.ndarray, List[np.ndarray], Tuple[np.ndarray], Dict[str, np.ndarray]]
+    ) -> Dict[str, np.ndarray]:
+        """
+        Runs model on the provided input via OpenVINO Runtime.
+        Returns the dictionary of model outputs by node names.
+
+        :param input_data: Inputs for the model.
+        :return output_data: Model's output.
+        """
+        return self.engine.infer(input_data)
---
+++ packages/nncf/nncf-2.9.0.tar.gz/nncf-2.9.0/nncf/tensorflow/__init__.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2024 Intel Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Base subpackage for NNCF TensorFlow functionality.
+"""
+
+import tensorflow
+from packaging import version
+
+import nncf
+from nncf import nncf_logger
+from nncf.common.logging.logger import warn_bkc_version_mismatch
+from nncf.version import BKC_TF_VERSION
+
+try:
+    _tf_version = tensorflow.__version__
+    tensorflow_version = version.parse(_tf_version).base_version
+except:
+    nncf_logger.debug("Could not parse tensorflow version")
+    _tf_version = "0.0.0"
+    tensorflow_version = version.parse(_tf_version).base_version
+tensorflow_version_major, tensorflow_version_minor = tuple(map(int, tensorflow_version.split(".")))[:2]
+if not tensorflow_version.startswith(BKC_TF_VERSION[:-2]):
+    warn_bkc_version_mismatch("tensorflow", BKC_TF_VERSION, _tf_version)
+elif not (tensorflow_version_major == 2 and 8 <= tensorflow_version_minor <= 13):
+    raise nncf.UnsupportedVersionError(
+        f"NNCF only supports 2.8.4 <= tensorflow <= 2.13.*, while current tensorflow version is {_tf_version}"
+    )
+
+
+from nncf.common.accuracy_aware_training.training_loop import (
+    AdaptiveCompressionTrainingLoop as AdaptiveCompressionTrainingLoop,
+)
+from nncf.common.accuracy_aware_training.training_loop import (
+    EarlyExitCompressionTrainingLoop as EarlyExitCompressionTrainingLoop,
+)
+from nncf.tensorflow.helpers import create_compressed_model as create_compressed_model
+from nncf.tensorflow.helpers.callback_creation import create_compression_callbacks as create_compression_callbacks
+from nncf.tensorflow.initialization import register_default_init_args as register_default_init_args
+from nncf.tensorflow.pruning.filter_pruning import algorithm as filter_pruning_algorithm
+
+# Required for correct COMPRESSION_ALGORITHMS registry functioning
+from nncf.tensorflow.quantization import algorithm as quantization_algorithm
+from nncf.tensorflow.sparsity.magnitude import algorithm as magnitude_sparsity_algorithm
+from nncf.tensorflow.sparsity.rb import algorithm as rb_sparsity_algorithm
