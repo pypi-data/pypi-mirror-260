74e709cfa36b1f8a2439e8353222256f3783aed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:06:45 +0000

    Add package ert/9.0.0rc2/ert-9.0.0rc2-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-9.0.0rc2-cp310-cp310-macosx_10_15_x86_64.whl/ert/gui/ertwidgets/storage_widget.py
@@ -0,0 +1,66 @@
+from qtpy.QtCore import QSortFilterProxyModel, Qt
+from qtpy.QtWidgets import (
+    QLineEdit,
+    QTreeView,
+    QVBoxLayout,
+    QWidget,
+)
+
+from ert.config import ErtConfig
+from ert.gui.ertnotifier import ErtNotifier
+from ert.gui.ertwidgets.caselist import AddWidget
+from ert.gui.ertwidgets.create_experiment_dialog import CreateExperimentDialog
+from ert.gui.ertwidgets.models.storage_model import StorageModel
+
+
+class StorageWidget(QWidget):
+    def __init__(
+        self, notifier: ErtNotifier, ert_config: ErtConfig, ensemble_size: int
+    ):
+        QWidget.__init__(self)
+
+        self._notifier = notifier
+        self._ert_config = ert_config
+        self._ensemble_size = ensemble_size
+
+        tree_view = QTreeView(self)
+        storage_model = StorageModel(self._notifier.storage)
+        notifier.storage_changed.connect(storage_model.reloadStorage)
+        notifier.ertChanged.connect(
+            lambda: storage_model.reloadStorage(self._notifier.storage)
+        )
+
+        search_bar = QLineEdit(self)
+        search_bar.setPlaceholderText("Filter")
+        proxy_model = QSortFilterProxyModel()
+        proxy_model.setFilterKeyColumn(-1)  # Search all columns.
+        proxy_model.setSourceModel(storage_model)
+        proxy_model.sort(0, Qt.SortOrder.AscendingOrder)
+
+        tree_view.setModel(proxy_model)
+        search_bar.textChanged.connect(proxy_model.setFilterFixedString)
+
+        self._create_experiment_button = AddWidget(self.add_item)
+
+        layout = QVBoxLayout()
+        layout.addWidget(search_bar)
+        layout.addWidget(tree_view)
+        layout.addWidget(self._create_experiment_button)
+
+        self.setLayout(layout)
+
+    def add_item(self) -> None:
+        create_experiment_dialog = CreateExperimentDialog(parent=self)
+        create_experiment_dialog.show()
+        if create_experiment_dialog.exec_():
+            ensemble = self._notifier.storage.create_experiment(
+                parameters=self._ert_config.ensemble_config.parameter_configuration,
+                responses=self._ert_config.ensemble_config.response_configuration,
+                observations=self._ert_config.observations,
+                name=create_experiment_dialog.experiment_name,
+            ).create_ensemble(
+                name=create_experiment_dialog.ensemble_name,
+                ensemble_size=self._ensemble_size,
+            )
+            self._notifier.set_current_case(ensemble)
+            self._notifier.ertChanged.emit()
