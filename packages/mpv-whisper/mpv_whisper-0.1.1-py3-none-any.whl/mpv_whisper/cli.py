654c47e0a32503237d15737f4e58e00c557e4687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 17:04:46 +0000

    Add package mpv-whisper/0.1.1/mpv_whisper-0.1.1-py3-none-any.whl

---
+++ packages/mpv-whisper/mpv_whisper-0.1.1-py3-none-any.whl/mpv_whisper/cli.py
@@ -0,0 +1,14 @@
+import sys
+
+from .monitor import MPVMonitor
+from .transcribe import get_model
+
+
+def cli():
+    get_model()
+
+    monitor = MPVMonitor()
+    if len(sys.argv) > 1:
+        monitor.command("loadfile", sys.argv[1])
+
+    monitor.block()
