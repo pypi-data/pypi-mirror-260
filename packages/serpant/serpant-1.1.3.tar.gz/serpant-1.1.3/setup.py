e768da8ada82b98f4b8b5ac8e80aeea3704d1429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:31 +0000

    Add package serpant/1.1.3/serpant-1.1.3.tar.gz

---
+++ packages/serpant/serpant-1.1.3.tar.gz/serpant-1.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='serpant',
+    version='1.1.3',
+    description='A CLI tool for scanning directories for vulnerabilities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Bahadir Nural',
+    author_email='bahadir.nural@outlook.com',
+    url='https://github.com/Latrodect/wss-repo-vulnerability-finder',
+    packages=find_packages(),
+    install_requires=[
+        'termcolor',
+        'colorlog',
+        'alive-progress'
+    ],
+    entry_points={
+        'console_scripts': [
+            'serpant=src.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
