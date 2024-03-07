b4a4abd01e0e85db7dd7f16ed506709c944d4c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 14:04:29 +0000

    Add package kod-norm-str/0.2.44/kod-norm-str-0.2.44.tar.gz

---
+++ packages/kod-norm-str/kod-norm-str-0.2.44.tar.gz/kod-norm-str-0.2.44/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kod-norm-str",
+    version="0.2.44",
+    author="Nestor Rivera",
+    author_email="kod3000@gmail.com",
+    description="A package for normalizing and generating a unique identifier of "
+                "strings. Great for when you need to search for exact matches.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/kod3000/kod-normalize-str",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
