6be7fbbac66dfbff67a6e8d475867c302814eb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 17:05:52 +0000

    Add package commitit/0.4/commitit-0.4.tar.gz

---
+++ packages/commitit/commitit-0.4.tar.gz/commitit-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="commitit",
+    version="0.4",
+    packages=find_packages(),
+    description="It is not how good you are, it is about how good people think you are",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Didas G. Junior"
+)
