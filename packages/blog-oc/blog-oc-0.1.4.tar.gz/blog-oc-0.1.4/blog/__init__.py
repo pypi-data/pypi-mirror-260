37c94aa968a157b1d054567f4e2b5ce500cf3f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:07:15 +0000

    Add package blog-oc/0.1.4/blog-oc-0.1.4.tar.gz

---
+++ packages/blog-oc/blog-oc-0.1.4.tar.gz/blog-oc-0.1.4/blog/__init__.py
@@ -0,0 +1,15 @@
+# coding=utf8
+""" Blog
+
+Exposes internal modules
+"""
+
+__author__		= "Chris Nasr"
+__copyright__	= "Ouroboros Coding Inc."
+__email__		= "chris@ouroboroscoding.com"
+__created__		= "2023-11-28"
+
+__all__ = ['errors', 'service']
+
+# Local
+from blog import errors, service
