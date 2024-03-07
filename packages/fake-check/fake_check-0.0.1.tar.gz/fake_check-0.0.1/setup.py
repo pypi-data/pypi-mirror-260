9824fd25be50507bec3283bba2694582174365eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 16:04:22 +0000

    Add package fake-check/0.0.1/fake_check-0.0.1.tar.gz

---
+++ packages/fake-check/fake_check-0.0.1.tar.gz/fake_check-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from setuptools import find_packages
+
+
+requirements = []
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name = "DeepCheck",
+    version = "0.0.1",
+    author = "PhospheneAI",
+    author_email = "sanjith.kumar@phospheneai.com",
+    description = "baseline deepcheck module",
+    url = "https://github.com/SANJITH-KUMAR-20/SizeInvariantTransformer-for-Deepfake-detection",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers = [
+        "Programming Language :: Python :: 3.11"
+    ],
+    python_requires = ">=3.11"
+)
