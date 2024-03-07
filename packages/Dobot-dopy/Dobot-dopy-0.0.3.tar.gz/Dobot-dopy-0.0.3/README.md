7f0efa25647903bfdfacc5aa79466da07a610c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 16:04:26 +0000

    Add package Dobot-dopy/0.0.3/Dobot-dopy-0.0.3.tar.gz

---
+++ packages/Dobot-dopy/Dobot-dopy-0.0.3.tar.gz/Dobot-dopy-0.0.3/README.md
@@ -0,0 +1,10 @@
+# DobotPyControl
+
+https://www.dobot-robots.com/
+
+A Python library for controlling Dobot robots over TCP/IP. 
+This project provides a simple and efficient way to send commands and manage Dobot robot operations, including movements, payload settings, and digital I/O operations. 
+Designed for ease of use and integration, DobotPyControl allows developers and hobbyists alike to automate tasks, prototype ideas, and build complex robotic applications with Dobot robots.
+
+해당 프로젝트는 TCP/IP로 두봇을 쉽게 사용하기 위한 프로젝트입니다.
+2024.03.06 부터 시작된 오픈소스 프로젝트로 열심히 업데이트 하겠습니다.
---
+++ packages/Dobot-dopy/Dobot-dopy-0.0.3.tar.gz/Dobot-dopy-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Dobot-dopy',
+    version='0.0.3',
+    description='Dobot python : Python library for streamlined TCP/IP communication with Dobot robots, enabling easy automation and control.',
+    author='lunachanwool',
+    author_email='lunachanwool@gmail.com',
+    url='https://github.com/lunachanwool/Dobot-Dopy',
+    install_requires=['numpy'],
+    packages=find_packages(exclude=[]),
+    keywords=['dobot', 'dopy', 'dobot python', 'python dobot', 'dobot tcp', 'dobot ip','dobot tcp/ip'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
