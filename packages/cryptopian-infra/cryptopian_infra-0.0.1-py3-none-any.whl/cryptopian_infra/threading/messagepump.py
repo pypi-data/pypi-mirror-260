136839a4a9824242d3eeb69d7a50740ae9a05b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 17:06:01 +0000

    Add package cryptopian-infra/0.0.1/cryptopian_infra-0.0.1-py3-none-any.whl

---
+++ packages/cryptopian-infra/cryptopian_infra-0.0.1-py3-none-any.whl/cryptopian_infra/threading/messagepump.py
@@ -0,0 +1,35 @@
+import logging
+import queue
+import threading
+from abc import abstractmethod, ABC
+
+
+class MessagePump(ABC):
+    def __init__(self, thread_name: str):
+        self.logger = logging.getLogger(f"{__class__.__name__}[{thread_name}]")
+        self.message_queue = queue.Queue()
+        self.process_exit = False
+        self.pump_thread = threading.Thread(target=self.process_messages, name=thread_name, daemon=True)
+        self.pump_thread.start()
+
+    def post_message(self, message):
+        self.message_queue.put(message)
+
+    def process_messages(self):
+        while True:
+            message = self.message_queue.get()  # This will block until a message is available
+            if message is not None:
+                try:
+                    self.handle_message(message)
+                except:
+                    self.logger.exception('Error processing message')
+            elif self.process_exit:
+                break
+
+    @abstractmethod
+    def handle_message(self, message):
+        pass
+
+    def set_process_exit(self):
+        self.process_exit = True
+        self.message_queue.put(None)
