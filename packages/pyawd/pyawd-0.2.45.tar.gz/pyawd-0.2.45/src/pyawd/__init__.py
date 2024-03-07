152826b09e3f87b76d2eab5ed72dedc72606fe39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:56 +0000

    Add package pyawd/0.2.45/pyawd-0.2.45.tar.gz

---
+++ packages/pyawd/pyawd-0.2.45.tar.gz/pyawd-0.2.45/src/pyawd/__init__.py
@@ -0,0 +1,15 @@
+# pyawd
+# Tribel Pascal - pascal.tribel@ulb.be
+"""
+Pyawd (standing for Pytorch Acoustic Wave Dataset) is a powerful tool for building datasets containing custom simulations of the propagation of Acoustic Wave through a given medium.
+This equation is given by
+
+$\\frac{d^2u}{dt^2} = c\\nabla^2u$
+
+where $u$ is the displacement field, and can be either a scalar field or a vector field, $c$ is the wave propagation speed and $\\nabla^2$ is the laplacian operator.
+
+It uses the finite differences scheme (implemented in the Devito package) to solve the Acoustic Wave Equation, and offers convenient tools for the customisation of the parameters, the handling of the data, the visualisation of the simulations.
+"""
+from pyawd.ScalarAcousticWaveDataset import ScalarAcousticWaveDataset
+from pyawd.VectorAcousticWaveDataset import VectorAcousticWaveDataset
+from pyawd.Marmousi import Marmousi
