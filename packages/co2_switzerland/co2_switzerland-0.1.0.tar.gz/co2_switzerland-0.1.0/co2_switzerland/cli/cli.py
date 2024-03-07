54ed8c7425d91351f724827fe0bb36f13951151a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:08:31 +0000

    Add package co2_switzerland/0.1.0/co2_switzerland-0.1.0.tar.gz

---
+++ packages/co2_switzerland/co2_switzerland-0.1.0.tar.gz/co2_switzerland-0.1.0/co2_switzerland/cli/cli.py
@@ -0,0 +1,10 @@
+import typer
+from loguru import logger
+
+cli = typer.Typer(no_args_is_help=True)
+
+
+@cli.command()
+def hello():
+    print("this is an hello from co2_switzerland")
+    logger.error("here")
