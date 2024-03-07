56948bfa7b91efd35fc5f03b19cd6b27cf202d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 16:04:22 +0000

    Add package msrk-package/0.1/msrk-package-0.1.tar.gz

---
+++ packages/msrk-package/msrk-package-0.1.tar.gz/msrk-package-0.1/README.md
@@ -0,0 +1,3 @@
+# Example Package
+
+This is a simple example package. You can use this to learn how to publish a package to PyPI.
---
+++ packages/msrk-package/msrk-package-0.1.tar.gz/msrk-package-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='msrk-package',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple example package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='test',
+    author_email='mohan@cure53.de',
+    url='https://github.com/yourusername/example_package',
+    # Add more information as needed
+)
