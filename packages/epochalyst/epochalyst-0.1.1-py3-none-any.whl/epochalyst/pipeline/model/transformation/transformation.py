95877694d0472a6b13476c547ad943a2fef10247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:06:30 +0000

    Add package epochalyst/0.1.1/epochalyst-0.1.1-py3-none-any.whl

---
+++ packages/epochalyst/epochalyst-0.1.1-py3-none-any.whl/epochalyst/pipeline/model/transformation/transformation.py
@@ -0,0 +1,26 @@
+from typing import Any
+from agogos.transforming_system import TransformingSystem
+
+from epochalyst._core._logging._logger import _Logger
+
+
+class TransformationPipeline(TransformingSystem, _Logger):
+    """TransformationPipeline is the class used to create the pipeline for the transformation of the data.
+
+    :param steps: The steps to transform the data. Can be a list of Transformers, TransformationPipelines, or a combination of both.
+    :param title: The title of the pipeline. (Default: "Transformation Pipeline")
+    """
+
+    title: str = "Transformation Pipeline"  # The title of the pipeline since transformation pipeline can be used for multiple purposes. (Feature, Label, etc.)
+
+    def transform(self, x: Any, transform_args: dict[str, Any] = {}) -> Any:
+        """Transform the input data.
+
+        :param x: The input data.
+        :return: The transformed data.
+        """
+
+        if self.steps:
+            self.log_section_separator(self.title)
+
+        return super().transform(x, transform_args)
