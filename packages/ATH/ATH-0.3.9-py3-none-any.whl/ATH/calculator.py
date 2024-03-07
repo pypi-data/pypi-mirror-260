c7ab195e56c5a0c1ee8ec59dbe597dca9d9ec95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:06:53 +0000

    Add package ATH/0.3.9/ATH-0.3.9-py3-none-any.whl

---
+++ packages/ATH/ATH-0.3.9-py3-none-any.whl/ATH/calculator.py
@@ -0,0 +1,21 @@
+def add_numbers(*args):
+    total = sum(args)
+    return total
+
+def subtract_numbers(*args):
+    result = args[0]
+    for num in args[1:]:
+        result -= num
+    return result
+
+def multiply_numbers(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+def divide_numbers(*args):
+    result = args[0]
+    for num in args[1:]:
+        result /= num
+    return result
