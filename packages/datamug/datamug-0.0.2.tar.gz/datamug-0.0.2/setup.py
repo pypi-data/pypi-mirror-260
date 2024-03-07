f4de03db62214165e0e2c5e5d58362277955fd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:05:04 +0000

    Add package datamug/0.0.2/datamug-0.0.2.tar.gz

---
+++ packages/datamug/datamug-0.0.2.tar.gz/datamug-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Python package to generate training data with LLMs for LLMs'
+LONG_DESCRIPTION = 'Python package to generate training data with LLMs for LLMs'
+
+# Setting up
+setup(
+    name="datamug",
+    version=VERSION,
+    author="Erfan Varedi",
+    author_email="erfanvaredi@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    keywords=['python', 'llm', 'fine-tuning', 'data-generation'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
