1cc58eb935eff0d944eeddc7596570fcd2c4c183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:06:28 +0000

    Add package specpart/0.0.4/specpart-0.0.4.tar.gz

---
+++ packages/specpart/specpart-0.0.4.tar.gz/specpart-0.0.4/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from numpy.distutils.core import setup, Extension
+
+
+setup(
+    name="specpart",
+    ext_modules=[
+        Extension(
+            name='specpart',
+            sources=[
+                "src/specpart/specpart.f90",
+                "src/specpart/specpart.pyf",
+            ]
+        )
+    ]
+)
