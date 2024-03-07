c686817da6682dcc46dc3ed08547f2ff37942bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 14:05:48 +0000

    Add package tgedr-dataops/0.0.27/tgedr_dataops-0.0.27-py3-none-any.whl

---
+++ packages/tgedr-dataops/tgedr_dataops-0.0.27-py3-none-any.whl/tgedr/dataops/sink/sink.py
@@ -0,0 +1,44 @@
+import abc
+from typing import Any, Dict, Optional
+
+from tgedr.dataops.chain import Chain
+
+
+class SinkException(Exception):
+    pass
+
+
+class SinkInterface(metaclass=abc.ABCMeta):
+    """
+    def put(self, context: Optional[Dict[str, Any]] = None) -> Any:
+        raise NotImplementedError()
+    """
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (
+            hasattr(subclass, "put")
+            and callable(subclass.put)
+            and hasattr(subclass, "delete")
+            and callable(subclass.delete)
+        ) or NotImplemented
+
+
+@SinkInterface.register
+class Sink(abc.ABC):
+    def __init__(self, config: Optional[Dict[str, Any]] = None):
+        self._config = config
+
+    @abc.abstractmethod
+    def put(self, context: Optional[Dict[str, Any]] = None) -> Any:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def delete(self, context: Optional[Dict[str, Any]] = None):
+        raise NotImplementedError()
+
+
+@SinkInterface.register
+class SinkChain(Chain, abc.ABC):
+    def execute(self, context: Optional[Dict[str, Any]] = None) -> Any:
+        return self.put(context=context)
---
+++ packages/tgedr-dataops/tgedr_dataops-0.0.27-py3-none-any.whl/tgedr/dataops/source/s3_file_source.py
@@ -0,0 +1,61 @@
+import logging
+import os
+from typing import Any, Dict, List, Optional
+from tgedr.dataops.commons.s3_connector import S3Connector
+from tgedr.dataops.source.source import Source, SourceException
+
+
+logger = logging.getLogger(__name__)
+
+
+class S3FileSource(Source, S3Connector):
+    CONTEXT_KEY_SOURCE = "source"
+    CONTEXT_KEY_TARGET = "target"
+
+    def __init__(self, config: Optional[Dict[str, Any]] = None):
+        Source.__init__(self, config=config)
+        S3Connector.__init__(self)
+
+    def __derive_local_file(self, target: str, isdir: bool, file: str):
+        logger.debug(f"[__derive_local_file|in] ({target}, {isdir}, {file})")
+
+        if isdir:
+            result = os.path.join(target, file)
+            basedir = os.path.dirname(result)
+            if not os.path.exists(basedir):
+                logger.debug(f"[__derive_local_file] creating folder: {basedir}")
+                os.mkdir(basedir)
+        else:
+            result = target
+
+        logger.debug(f"[__derive_local_file|out] => {result}")
+        return result
+
+    def get(self, context: Optional[Dict[str, Any]] = None) -> Any:
+        logger.info(f"[get|in] ({context})")
+
+        result: List[str] = []
+        if self.CONTEXT_KEY_SOURCE not in context:
+            raise SourceException(f"you must provide context for {self.CONTEXT_KEY_SOURCE}")
+        if self.CONTEXT_KEY_TARGET not in context:
+            raise SourceException(f"you must provide context for {self.CONTEXT_KEY_TARGET}")
+
+        path = context[self.CONTEXT_KEY_SOURCE]
+        path_elements = path.split("/")
+        bucket = path_elements[0]
+        key = "/".join(path_elements[1:])
+        target = context[self.CONTEXT_KEY_TARGET]
+        target_is_dir = os.path.isdir(target)
+
+        objs = self._client.list_objects_v2(Bucket=bucket, Prefix=key)
+        files = [entry["Key"] for entry in objs["Contents"] if not (entry["Key"]).endswith("/")]
+
+        for file in files:
+            logger.debug(f"[get] found file: {file}")
+            local_file = self.__derive_local_file(target=target, isdir=target_is_dir, file=file)
+            logger.debug(f"[get] bucket: {bucket}   key: {key}   file: {file}   local_file: {local_file}")
+            self._client.download_file(Bucket=bucket, Key=file, Filename=local_file)
+            result.append(local_file)
+
+        logger.info(f"[get|out] => {result}")
+        return result
