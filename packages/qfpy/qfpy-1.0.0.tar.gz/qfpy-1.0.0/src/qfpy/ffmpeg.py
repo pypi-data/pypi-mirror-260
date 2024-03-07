276a3d37a59767f9f941352a075d4beefb343198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:53 +0000

    Add package qfpy/1.0.0/qfpy-1.0.0.tar.gz

---
+++ packages/qfpy/qfpy-1.0.0.tar.gz/qfpy-1.0.0/src/qfpy/ffmpeg.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+
+def concat_video(files: list[Path], output_file: Path):
+    """
+    合并视频
+    """
+    clips = [VideoFileClip(f) for f in files]
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile(output_file)
+
+
+if __name__ == "__main__":
+    filelist = [
+        "S:/rather/pic/daoayao/2021-08-06-vid_258.mp4",
+        "S:/rather/pic/daoayao/2021-08-08-vid_256.mp4",
+    ]
+    print(concat_video(filelist, "output.mp4"))
