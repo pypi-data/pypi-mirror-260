88cdda14a590196bee123e84f8cae487e2943590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:07:51 +0000

    Add package web3db/0.0.1/web3db-0.0.1.tar.gz

---
+++ packages/web3db/web3db-0.0.1.tar.gz/web3db-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="web3db",
+    version='0.0.1',
+    author="timer",
+    author_email="timerkhan2002@gmail.com",
+    description='Web3 database',
+    packages=find_packages(),
+    install_requires=[
+        'python-gnupg', 'loguru',
+        'sqlalchemy', 'asyncpg',
+        'eth_account', 'aptos-sdk', 'solana', 'base58'
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ]
+)
