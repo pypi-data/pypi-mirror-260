97545fd3255e551d6121b83c9693b0da264c61b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:55 +0000

    Add package bionumpy/1.0.6/bionumpy-1.0.6-py2.py3-none-any.whl

---
+++ packages/bionumpy/bionumpy-1.0.6-py2.py3-none-any.whl/bionumpy/encodings/__init__.py
@@ -0,0 +1,65 @@
+from ..encoded_array import BaseEncoding, Encoding, NumericEncoding
+from .alphabet_encoding import (AlphabetEncoding, DNAEncoding, RNAENcoding,
+                                AminoAcidEncoding, ACGTnEncoding,
+                                CigarOpEncoding, BamEncoding, StrandEncoding)
+
+
+__all__ = ["BaseEncoding", "Encoding",
+           "AlphabetEncoding",  "AminoAcidEncoding"]
+
+
+# class StrandEncoding(Encoding):
+#     MIN_CODE = ord("+")
+# 
+#     @classmethod
+#     def encode(cls, bytes_array):
+#         return (bytes_array & np.uint8(2)) >> np.uint8(1)
+# 
+#     @classmethod
+#     def decode(cls, strands):
+#         return 2 * strands + cls.MIN_CODE
+
+
+class DigitEncodingFactory(NumericEncoding):
+    def __init__(self, min_code):
+        self._min_code = ord(min_code)
+
+    def _encode(self, bytes_array):
+        return bytes_array - self._min_code
+
+    def _decode(self, digits):
+        return digits + self._min_code
+
+    def __repr__(self):
+        return "DigitEncoding(min_code=%d)" % self._min_code
+
+
+DigitEncoding = DigitEncodingFactory("0")
+QualityEncoding = DigitEncodingFactory("!")
+CigarEncoding = DigitEncodingFactory(chr(0))
+
+
+def set_backend(lib):
+    #from ..cupy_compatible.encodings.alphabet_encoding import CPAlphabetEncoding
+    #from ..cupy_compatible.encodings.alphabet_encoding import CPACTGEncoding
+    #from ..cupy_compatible.encodings.alphabet_encoding import CPAminoAcidEncoding
+    
+    #sys.modules[__name__].AlphabetEncoding = CPAlphabetEncoding
+    #sys.modules[__name__].ACTGEncoding = CPACTGEncoding
+    #sys.modules[__name__].AminoAcidEncoding = CPAminoAcidEncoding
+
+    from . import base_encoding
+    base_encoding.np = lib
+
+    from . import alphabet_encoding
+    alphabet_encoding.np = lib
+
+    from . import kmer_encodings
+    kmer_encodings.np = lib
+
+    from . import alphabet_encoding
+    alphabet_encoding.np = lib
+
+    # Have to reinitalize the encodings since these where initialized with numpy
+    from .alphabet_encoding import DNAEncoding
+    DNAEncoding._initialize(force=True)
