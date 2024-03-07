7d8bb2f1d93b595f57dd6a78d4ca540876d97355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 16:04:19 +0000

    Add package aioli-sdk/0.1.1rc0/aioli-sdk-0.1.1rc0.tar.gz

---
+++ packages/aioli-sdk/aioli-sdk-0.1.1rc0.tar.gz/aioli-sdk-0.1.1rc0/aiolirest/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from aiolirest.api.authentication_api import AuthenticationApi
+from aiolirest.api.deployments_api import DeploymentsApi
+from aiolirest.api.information_api import InformationApi
+from aiolirest.api.models_api import ModelsApi
+from aiolirest.api.registries_api import RegistriesApi
+from aiolirest.api.roles_api import RolesApi
+from aiolirest.api.services_api import ServicesApi
+from aiolirest.api.users_api import UsersApi
+
