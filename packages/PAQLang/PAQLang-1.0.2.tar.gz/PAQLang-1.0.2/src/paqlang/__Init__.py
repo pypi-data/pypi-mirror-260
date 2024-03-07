477aa7587c0162d432f7b22bbaa785cd2c8d8ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:49 +0000

    Add package PAQLang/1.0.2/PAQLang-1.0.2.tar.gz

---
+++ packages/PAQLang/PAQLang-1.0.2.tar.gz/PAQLang-1.0.2/src/paqlang/__Init__.py
@@ -0,0 +1,11 @@
+from .program import *
+from .utils import *
+# from .program import pgm
+# from .utils import get_json, aio_reads, freads
+
+__all__ = [
+    pgm,
+    get_json, 
+    aio_reads, 
+    freads
+]
