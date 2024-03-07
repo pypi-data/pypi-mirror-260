db32366c3fc04fcceaeab5ffd03652a31029d7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 17:05:27 +0000

    Add package kmService/0.0.0.dev7/kmservice-0.0.0.dev7.tar.gz

---
+++ packages/kmService/kmservice-0.0.0.dev7.tar.gz/kmservice-0.0.0.dev7/kmService/__init__.py
@@ -0,0 +1,10 @@
+from .km_service import KmResponse, KmService, KmServiceBuilder, get_km_service
+
+__version__ = "0.0.0.dev7"
+
+__all__ = [
+    "KmService",
+    "KmResponse",
+    "KmServiceBuilder",
+    "get_km_service",
+]
