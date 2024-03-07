d698ff911f947391035fcc339c17bb89f8e611f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 15:04:24 +0000

    Add package bless/0.2.6/bless-0.2.6.tar.gz

---
+++ packages/bless/bless-0.2.6.tar.gz/bless-0.2.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools  # type: ignore
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bless",
+    version="0.2.6",
+    author="Kevin Davis",
+    author_email="kevincarrolldavis@gmail.com",
+    description="A Bluetooth Low Energy Server supplement to Bleak",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kevincar/bless",
+    package_data={"bless": ["py.typed"]},
+    packages=setuptools.find_packages(exclude=("test", "examples")),
+    include_package_data=True,
+    install_requires=[
+        "bleak",
+        "pywin32;platform_system=='Windows'",
+        "dbus_next;platform_system=='Linux'",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
