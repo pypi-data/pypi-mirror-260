aa4f3ae826c438399fe645cf096374d1aafb9ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 17:05:39 +0000

    Add package commitit/0.1/commitit-0.1.tar.gz

---
+++ packages/commitit/commitit-0.1.tar.gz/commitit-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="commitit",
+    version="0.1",
+    packages=find_packages(),
+    description="It is not how good you are, it is about how good people think you are",
+    author="Didas G. Junior"
+)
