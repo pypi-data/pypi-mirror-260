30c9f6380978efe7fc270c4719fb800ffa759d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:05:10 +0000

    Add package epochalyst/0.1.0/epochalyst-0.1.0-py3-none-any.whl

---
+++ packages/epochalyst/epochalyst-0.1.0-py3-none-any.whl/epochalyst/pipeline/model/training/training.py
@@ -0,0 +1,30 @@
+from typing import Any
+from agogos.training_system import TrainingSystem
+
+from epochalyst._core._logging._logger import _Logger
+
+
+class TrainingPipeline(TrainingSystem, _Logger):
+    """The training pipeline. This is the class used to create the pipeline for the training of the model.
+
+    :param steps: The steps to train the model.
+    """
+
+    def __post_init__(self) -> None:
+        """Post init method for the Pipeline class."""
+        super().__post_init__()
+
+    def train(
+        self, x: Any, y: Any, train_args: dict[str, Any] | None = None
+    ) -> tuple[Any, Any]:
+        """Train the system.
+
+        :param x: The input to the system.
+        :param y: The expected output of the system.
+        :return: The input and output of the system.
+        """
+
+        if self.steps:
+            self.log_section_separator("Training Pipeline")
+
+        return super().train(x, y, train_args)
