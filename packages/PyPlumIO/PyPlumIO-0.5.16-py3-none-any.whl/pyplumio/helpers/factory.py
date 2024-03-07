b8e961ee96728f7ac15b871a5be81b8ad439d0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:33 +0000

    Add package PyPlumIO/0.5.16/PyPlumIO-0.5.16-py3-none-any.whl

---
+++ packages/PyPlumIO/PyPlumIO-0.5.16-py3-none-any.whl/pyplumio/helpers/factory.py
@@ -0,0 +1,20 @@
+"""Contains a factory helper."""
+from __future__ import annotations
+
+import importlib
+import logging
+from typing import Any
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def factory(class_path: str, **kwargs: Any) -> Any:
+    """Return class instance from the class path."""
+    try:
+        module_name, class_name = class_path.rsplit(".", 1)
+        return getattr(
+            importlib.import_module("." + module_name, "pyplumio"), class_name
+        )(**kwargs)
+    except Exception:
+        _LOGGER.error("Failed to load module (%s)", class_path)
+        raise
