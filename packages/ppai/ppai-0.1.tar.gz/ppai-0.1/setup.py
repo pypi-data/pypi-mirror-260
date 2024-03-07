088697f117910265d2e341981e5ac2ce5aa98c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:41 +0000

    Add package ppai/0.1/ppai-0.1.tar.gz

---
+++ packages/ppai/ppai-0.1.tar.gz/ppai-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ppai',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/freihandlabor/peakprivacyai-python',
+    license='MIT',
+    author='freihandlabor GmbH',
+    author_email='support@freihandlabor.com',
+    description='The official PeakPrivacy library for the AI API',
+)
