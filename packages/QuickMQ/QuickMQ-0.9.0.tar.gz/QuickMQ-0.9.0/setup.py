a30f45e29ee5aeb016f22fdb96a7521e8d5e0a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:29 +0000

    Add package QuickMQ/0.9.0/QuickMQ-0.9.0.tar.gz

---
+++ packages/QuickMQ/QuickMQ-0.9.0.tar.gz/QuickMQ-0.9.0/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+# All metadata is contained in pyproject.toml
+# Keep setup.py for backwards compatibility
+setup()
---
+++ packages/QuickMQ/QuickMQ-0.9.0.tar.gz/QuickMQ-0.9.0/src/quickmq/utils.py
@@ -0,0 +1,24 @@
+"""
+quickmq.utils
+~~~~~~~~~~~~~
+
+Useful functions for quickmq, currently just message encoding/decoding.
+"""
+
+import json
+from typing import Dict, List, TypeAlias, Union
+
+from .exceptions import EncodingError
+
+JSON: TypeAlias = Union[Dict[str, "JSON"], List["JSON"], str, int, float, bool, None]
+
+
+def encode_message(message: Union[JSON, bytearray, bytes]) -> bytes:
+    if isinstance(message, bytes):
+        return message
+    elif isinstance(message, bytearray):
+        return bytes(message)
+    try:
+        return bytes(json.dumps(message), encoding="utf-8")
+    except (UnicodeEncodeError, TypeError) as e:
+        raise EncodingError(f"Could not encode message {message}") from e
