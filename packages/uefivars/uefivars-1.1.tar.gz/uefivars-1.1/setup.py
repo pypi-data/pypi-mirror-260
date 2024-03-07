f50be099f9e289156abcc1e570bca20466e9a43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:05:03 +0000

    Add package uefivars/1.1/uefivars-1.1.tar.gz

---
+++ packages/uefivars/uefivars-1.1.tar.gz/uefivars-1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="uefivars",
+    version="1.1",
+    author="Amazon Web Services",
+    author_email="graf@amazon.com",
+    description="UEFI variable store tools",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/awslabs/python-uefivars",
+    packages=setuptools.find_packages(),
+    install_requires=['google-crc32c', 'deepdiff'],
+    entry_points={
+        'console_scripts': [
+            'uefivars.py = pyuefivars:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
