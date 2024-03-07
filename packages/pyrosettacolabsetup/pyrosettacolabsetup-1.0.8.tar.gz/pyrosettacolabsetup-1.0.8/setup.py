aae9db35f008c12304a7ac20c3c3d8855143eb5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 16:04:30 +0000

    Add package pyrosettacolabsetup/1.0.8/pyrosettacolabsetup-1.0.8.tar.gz

---
+++ packages/pyrosettacolabsetup/pyrosettacolabsetup-1.0.8.tar.gz/pyrosettacolabsetup-1.0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyrosettacolabsetup',
+      version='1.0.8',
+      description='Mounts Google Drive for PyRosetta use in Google Colaboratory',
+      url='https://github.com/RosettaCommons/pyrosetta_colab_setup',
+      author='Sergey Lyskov',
+      author_email='sergey.lyskov@jhu.edu',
+      license='MIT',
+      packages=['pyrosettacolabsetup'],
+      zip_safe=False)
