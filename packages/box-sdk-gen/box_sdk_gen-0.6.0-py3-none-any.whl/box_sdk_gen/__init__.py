64859fab97b74e6fc18b2907bfbdd3c98866cbec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 16:04:12 +0000

    Add package box-sdk-gen/0.6.0/box_sdk_gen-0.6.0-py3-none-any.whl

---
+++ packages/box-sdk-gen/box_sdk_gen-0.6.0-py3-none-any.whl/box_sdk_gen/__init__.py
@@ -0,0 +1,13 @@
+from .box import *
+
+from .internal import *
+
+from .networking import *
+
+from .serialization import *
+
+from .schemas import *
+
+from .managers import *
+
+from .client import *
