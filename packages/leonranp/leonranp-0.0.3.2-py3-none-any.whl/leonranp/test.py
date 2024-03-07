1284d88a1d12791fb9e8d45b4961b3711b162a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:34 +0000

    Add package leonranp/0.0.3.2/leonranp-0.0.3.2-py3-none-any.whl

---
+++ packages/leonranp/leonranp-0.0.3.2-py3-none-any.whl/leonranp/test.py
@@ -0,0 +1,14 @@
+from __init__ import *
+randstr()
+print('---')
+print(randcode(120))
+print('---')
+
+a = randcode(3)
+print(a)
+b = input("aa")
+
+if a == b:
+    print("yes")
+else:
+    print("no")
