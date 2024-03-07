eda0eec4dd63c02a5a8965f4d6eb216e50899d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:49 +0000

    Add package cosi-atmosphere/0.1.0a0/cosi_atmosphere-0.1.0a0.tar.gz

---
+++ packages/cosi-atmosphere/cosi_atmosphere-0.1.0a0.tar.gz/cosi_atmosphere-0.1.0a0/setup.py
@@ -0,0 +1,14 @@
+# Imports:
+from setuptools import setup, find_packages
+
+# Setup:
+setup(
+    name='cosi_atmosphere',
+    version="0.1.0a0",
+    url='https://github.com/cositools/cosi-atmosphere.git',
+    author='COSI Team',
+    author_email='christopher.m.karwin@nasa.gov',
+    packages=find_packages(),
+    install_requires = ['histpy==1.0.2','pandas','healpy','pymsis'],
+    description = 'Tools for calculating atmospheric response and background'
+)
