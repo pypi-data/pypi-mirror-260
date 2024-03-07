b31bd394ad9f58abf052f48e1e54ddc0fde314d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 16:04:12 +0000

    Add package babelone/0.1.3/babelone-0.1.3.tar.gz

---
+++ packages/babelone/babelone-0.1.3.tar.gz/babelone-0.1.3/tests/outputs/setupy__generate_setup.py
@@ -0,0 +1,13 @@
+"""Installation configuration and package metadata for babelone-test."""
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    setup(
+        package_name="babelone-test",
+        version="v0.1.1",
+        install_requires=["flask", "pydantic==2.6.1"],
+        setup_requires=[],
+        extra_requires={"dev": ["pytest", "hypothesis>=6.98.1"]},
+        entry_points={"console_scripts": ["hello-world = timmins:hello_world"], "gui_scripts": []},
+    )
