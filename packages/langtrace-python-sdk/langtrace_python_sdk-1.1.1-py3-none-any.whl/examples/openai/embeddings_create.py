e5167ce19bb73b858c2ae92cee2eae2f37cfb492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 14:04:21 +0000

    Add package langtrace-python-sdk/1.1.1/langtrace_python_sdk-1.1.1-py3-none-any.whl

---
+++ packages/langtrace-python-sdk/langtrace_python_sdk-1.1.1-py3-none-any.whl/examples/openai/embeddings_create.py
@@ -0,0 +1,20 @@
+from dotenv import find_dotenv, load_dotenv
+from openai import OpenAI
+
+from src.langtrace_python_sdk import langtrace
+from src.langtrace_python_sdk.utils.with_root_span import \
+    with_langtrace_root_span
+
+_ = load_dotenv(find_dotenv())
+
+langtrace.init(batch=True, log_spans_to_console=True,
+               write_to_remote_url=False)
+client = OpenAI()
+
+
+@with_langtrace_root_span()
+def embeddings_create():
+    result = client.embeddings.create(
+        model="text-embedding-ada-002",
+        input="Once upon a time, there was a frog.",
+    )
