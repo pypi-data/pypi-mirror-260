4809e11460335e0daa6fe979c09f9b8cec26b8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 15:04:40 +0000

    Add package gtauthclient/0.0.4/gtauthclient-0.0.4.tar.gz

---
+++ packages/gtauthclient/gtauthclient-0.0.4.tar.gz/gtauthclient-0.0.4/gtauthclient/__init__.py
@@ -0,0 +1,22 @@
+import jwt
+from fastapi import Header, HTTPException
+
+
+class GTAuthClient:
+    def __init__(self, key: str):
+        self.key = key
+
+    def verify_user(self, authorization: str = Header(None)):
+        try:
+            if authorization == self.key:
+                return {"sub": "ADMIN", "role": "ADMIN"}
+            if "bearer" in authorization.lower():
+                scheme, _, api_key = authorization.partition(" ")
+                authorization = api_key
+            return jwt.decode(
+                jwt=api_key,
+                key=self.key,
+                algorithms=["HS256"],
+            )
+        except:
+            raise HTTPException(status_code=401, detail="Invalid token.")
