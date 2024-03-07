372fc1665eedcb0157942a64ba2450ef9a72580e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:28 +0000

    Add package agixt/1.4.57/agixt-1.4.57.tar.gz

---
+++ packages/agixt/agixt-1.4.57.tar.gz/agixt-1.4.57/agixt/providers/gpt4free.py
@@ -0,0 +1,78 @@
+import logging
+import asyncio
+
+from g4f.Provider import RetryProvider
+from g4f.models import ModelUtils, gpt_35_turbo, default
+
+
+class Gpt4freeProvider:
+    def __init__(
+        self,
+        AI_MODEL: str = gpt_35_turbo.name,
+        MAX_TOKENS: int = 4096,
+        AI_TEMPERATURE: float = 0.7,
+        AI_TOP_P: float = 0.7,
+        WAIT_BETWEEN_REQUESTS: int = 1,
+        WAIT_AFTER_FAILURE: int = 3,
+        **kwargs,
+    ):
+        self.requirements = ["g4f", "httpx"]
+        if not AI_MODEL:
+            self.AI_MODEL = default
+        elif AI_MODEL in ModelUtils.convert:
+            self.AI_MODEL = ModelUtils.convert[AI_MODEL]
+        else:
+            self.AI_MODEL = ModelUtils.convert["gpt-3.5-turbo"]
+        self.AI_TEMPERATURE = AI_TEMPERATURE if AI_TEMPERATURE else 0.7
+        self.MAX_TOKENS = MAX_TOKENS if MAX_TOKENS else 4096
+        self.AI_TOP_P = AI_TOP_P if AI_TOP_P else 0.7
+        self.WAIT_BETWEEN_REQUESTS = (
+            WAIT_BETWEEN_REQUESTS if WAIT_BETWEEN_REQUESTS else 1
+        )
+        self.WAIT_AFTER_FAILURE = WAIT_AFTER_FAILURE if WAIT_AFTER_FAILURE else 3
+
+    async def inference(self, prompt, tokens: int = 0):
+        max_new_tokens = (
+            int(self.MAX_TOKENS) - int(tokens) if tokens > 0 else self.MAX_TOKENS
+        )
+        model = self.AI_MODEL
+        provider = model.best_provider
+        if provider:
+            append_model = f" and model: {model.name}" if model.name else ""
+            logging.info(f"[Gpt4Free] Use provider: {provider.__name__}{append_model}")
+        try:
+            return (
+                await asyncio.gather(
+                    provider.create_async(
+                        model=model.name,
+                        messages=[{"role": "user", "content": prompt}],
+                        max_tokens=max_new_tokens,
+                        temperature=float(self.AI_TEMPERATURE),
+                        top_p=float(self.AI_TOP_P),
+                    ),
+                    asyncio.sleep(int(self.WAIT_BETWEEN_REQUESTS)),
+                )
+            )[0]
+        except Exception as e:
+            if int(self.WAIT_AFTER_FAILURE) > 0:
+                await asyncio.sleep(int(self.WAIT_AFTER_FAILURE))
+            raise e
+        finally:
+            if provider and isinstance(provider, RetryProvider):
+                if hasattr(provider, "exceptions"):
+                    for provider_name in provider.exceptions:
+                        error = provider.exceptions[provider_name]
+                        logging.error(f"[Gpt4Free] {provider_name}: {error}")
+
+
+if __name__ == "__main__":
+    import asyncio, time
+
+    async def run_test():
+        petal = Gpt4freeProvider()
+        start = time.time()
+        response = await petal.inference("What is the meaning of life?")
+        print(response)
+        print(f"{round(time.time()-start, 2)} secs")
+
+    asyncio.run(run_test())
---
+++ packages/agixt/agixt-1.4.57.tar.gz/agixt-1.4.57/docs/1-Getting started/Screenshots.md
@@ -0,0 +1,11 @@
+![image](https://github.com/Josh-XT/AGiXT/assets/102809327/944c9600-d67f-45da-ac1e-715e4c9d3912)
+
+![Screenshot from 2023-08-10 16-13-15](https://github.com/Josh-XT/AGiXT/assets/102809327/e4d0eb5a-06e8-460f-b915-7ab9fb0d6114)
+
+![Screenshot from 2023-08-10 16-13-27](https://github.com/Josh-XT/AGiXT/assets/102809327/cbc30e69-733c-4ceb-801b-20dfc377ac81)
+
+![Screenshot from 2023-08-10 16-14-03](https://github.com/Josh-XT/AGiXT/assets/102809327/bf893e47-a85e-4f72-ad60-578316646b29)
+![Screenshot from 2023-08-10 16-14-09](https://github.com/Josh-XT/AGiXT/assets/102809327/07350145-97a5-4cbd-8c8f-9543b1b05970)
+![Screenshot from 2023-08-10 16-14-19](https://github.com/Josh-XT/AGiXT/assets/102809327/79932253-4089-42b3-9bda-218ea51d0f22)
+![Screenshot from 2023-08-10 16-16-19](https://github.com/Josh-XT/AGiXT/assets/102809327/b6d2d501-3ee8-4a99-afe1-cb4f0f2c3aab)
+![Screenshot from 2023-08-10 16-16-43](https://github.com/Josh-XT/AGiXT/assets/102809327/003c9688-3377-4ebe-be91-a64df6bbe10e)
