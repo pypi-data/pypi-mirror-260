0fdf6b3db82003af5d3eddb16ca66abb0a432550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 17:05:39 +0000

    Add package commitit/0.1/commitit-0.1-py3-none-any.whl

---
+++ packages/commitit/commitit-0.1-py3-none-any.whl/hcommit/git.py
@@ -0,0 +1,23 @@
+import os
+
+
+def make_commit(days: int):
+    if days < 1:
+        return os.system('git push')
+    else:
+        dates = f'{days} days ago'
+        with open('data.txt', 'a') as file:
+            file.write(f'{dates}\n')
+
+        # Staging
+        os.system('git add data.txt')
+
+        # Commit
+        os.system('git commit --date="'+dates+'" -m "first commit"')
+
+        return days * make_commit(days-1)
+
+
+numberOfDays = input(
+    "Enter the number of days from today you want to commit: ")
+make_commit(numberOfDays)
