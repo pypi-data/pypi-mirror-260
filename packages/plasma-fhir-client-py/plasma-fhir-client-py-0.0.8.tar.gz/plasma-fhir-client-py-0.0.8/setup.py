d9416ded3f1c80fd9c4d63605726f65b9870521c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 17:04:35 +0000

    Add package plasma-fhir-client-py/0.0.8/plasma-fhir-client-py-0.0.8.tar.gz

---
+++ packages/plasma-fhir-client-py/plasma-fhir-client-py-0.0.8.tar.gz/plasma-fhir-client-py-0.0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plasma-fhir-client-py',
+    version='0.0.8',
+    author='Eric Morgan',
+    author_email='plasmafhir@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'base64'
+    ],
+    tests_require=['pytest'],
+    test_suite='tests',
+    description='Plasma FHIR Client for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://plasmahealth.net',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
