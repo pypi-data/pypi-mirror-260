549527270ed1a3eed839cb75a71b58e1a5c46bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:06:57 +0000

    Add package xtable-py/0.0.21/xtable_py-0.0.21-py3-none-any.whl

---
+++ packages/xtable-py/xtable_py-0.0.21-py3-none-any.whl/xtable_py/airflow_plugin.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.plugins_manager import AirflowPlugin
+
+import xtable_cmd
+
+
+class XtableOperator(BaseOperator):
+
+    @apply_defaults
+    def __init__(self, config: Path, catalog:Path, jars: Path, *args, **kwargs):
+        super(XtableOperator, self).__init__(*args, **kwargs)
+        self.config: Path = config
+        self.catalog: Path = catalog
+        self.jars: Path = jars
+
+    def execute(self, context):
+        xtable_cmd.setup(jars=self.jars)
+        xtable_cmd.sync(config=self.config, catalog=self.catalog, jars=self.jars)
+
+
+class XtablePlugin(AirflowPlugin):
+    # The name of your plugin (str)
+    name = "xtable"
+    operators = [XtableOperator]
