16e4424ea0b1419ffd0ad9d16ea5e7c576b6853b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:46 +0000

    Add package milosz7wpdm/0.1.1/milosz7wpdm-0.1.1.tar.gz

---
+++ packages/milosz7wpdm/milosz7wpdm-0.1.1.tar.gz/milosz7wpdm-0.1.1/src/milosz7wpdm/utils.py
@@ -0,0 +1,17 @@
+def foo(x,y):
+    """
+
+    :param x:
+    :param y:
+    :return:
+    """
+    return x+y
+
+
+def bar(x,y):
+    """
+    :param x:
+    :param y:
+    :return:
+    """
+    return x-y
