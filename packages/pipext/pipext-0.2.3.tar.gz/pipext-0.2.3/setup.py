6d9f56a887ccc89ab649cbc8ddf276749bbcbffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 03:04:59 +0000

    Add package pipext/0.2.3/pipext-0.2.3.tar.gz

---
+++ packages/pipext/pipext-0.2.3.tar.gz/pipext-0.2.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='pipext',
+    version='0.2.3',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'pipext=pipext.main:main',
+        ],
+    },
+    author='Usman Mukhtar',
+    description='An extended pip installer that updates requirements.txt',
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+)
