870c1f1ca21c9d989003f53b1d0d1c0dff2a73af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 05:04:29 +0000

    Add package kfactory/0.12.2/kfactory-0.12.2.tar.gz

---
+++ packages/kfactory/kfactory-0.12.2.tar.gz/kfactory-0.12.2/src/kfactory/__init__.py
@@ -0,0 +1,77 @@
+"""KFactory package. Utilities for creating photonic devices.
+
+Uses the klayout package as a backend.
+
+"""
+
+# The import order matters, we need to first import the important stuff.
+# isort:skip_file
+
+__version__ = "0.12.2"
+
+import klayout.db as kdb
+import klayout.lay as lay
+import klayout.rdb as rdb
+from .kcell import (
+    KCell,
+    Instance,
+    Port,
+    Ports,
+    cell,
+    kcl,
+    KCLayout,
+    save_layout_options,
+    LayerEnum,
+    LayerStack,
+    show,
+    polygon_from_array,
+    dpolygon_from_array,
+    VKCell,
+    VInstance,
+)
+from . import cells, placer, routing, port, technology, enclosure, utils
+from .conf import config
+from .enclosure import LayerEnclosure, KCellEnclosure
+from .grid import grid_dbu, flexgrid_dbu, grid, flexgrid
+
+from aenum import constant  # type: ignore[import-untyped,unused-ignore]
+
+logger = config.logger
+
+__all__ = [
+    "KCell",
+    "Instance",
+    "Port",
+    "Ports",
+    "cell",
+    "kcl",
+    "KCLayout",
+    "default_save",
+    "kdb",
+    "lay",
+    "rdb",
+    "port",
+    "cells",
+    "grid",
+    "flexgrid",
+    "grid_dbu",
+    "flexgrid_dbu",
+    "placer",
+    "routing",
+    "utils",
+    "show",
+    "config",
+    "enclosure",
+    "LayerStack",
+    "LayerEnum",
+    "logger",
+    "polygon_from_array",
+    "dpolygon_from_array",
+    "technology",
+    "LayerEnclosure",
+    "KCellEnclosure",
+    "constant",
+    "save_layout_options",
+    "VKCell",
+    "VInstance",
+]
