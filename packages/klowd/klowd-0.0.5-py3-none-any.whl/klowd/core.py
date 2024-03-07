c09f1825d781e4ed00be0e2fd1b4825e27dae22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 15:04:55 +0000

    Add package klowd/0.0.5/klowd-0.0.5-py3-none-any.whl

---
+++ packages/klowd/klowd-0.0.5-py3-none-any.whl/klowd/core.py
@@ -0,0 +1,16 @@
+import anywidget
+import traitlets
+
+def kloudhello(number):
+    print("hello from kloud")
+
+class KloudToken(anywidget.AnyWidget):
+    _esm = """
+    function render({ model, el }) {
+        model.set(\"value\", window.kloud_token ?? \"n/a\");
+        model.save_changes();
+    };
+    export default { render };
+    """
+    _css = ""
+    value = traitlets.Unicode("default").tag(sync=True)
