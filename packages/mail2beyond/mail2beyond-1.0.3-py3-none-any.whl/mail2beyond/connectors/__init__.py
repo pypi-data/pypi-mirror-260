849c501240b57bc5aee4544fce49c32edc161a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 04:06:31 +0000

    Add package mail2beyond/1.0.3/mail2beyond-1.0.3-py3-none-any.whl

---
+++ packages/mail2beyond/mail2beyond-1.0.3-py3-none-any.whl/mail2beyond/connectors/__init__.py
@@ -0,0 +1,7 @@
+"""Module that groups built-in connectors for mail2beyond to use."""
+from . import void
+from . import smtp
+from . import slack
+from . import discord
+from . import google_chat
+from . import microsoft_teams
