150bba3463da5f4a48a81d0c03c8dc44c823a839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 16:04:43 +0000

    Add package nigep/0.1.19/nigep-0.1.19-py3-none-any.whl

---
+++ packages/nigep/nigep-0.1.19-py3-none-any.whl/nigep/lib/consts.py
@@ -0,0 +1,18 @@
+NOISE_LEVELS = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+
+NIGEP_AVAILABLE_KWARGS = [
+    'execution_name',
+    'model',
+    'batch_size',
+    'input_shape',
+    'x_data',
+    'y_data',
+    'target_names',
+    'class_mode',
+    'k_fold_n',
+    'epochs',
+    'callbacks',
+    'noise_levels',
+    'save_models',
+    'evaluate_models',
+]
