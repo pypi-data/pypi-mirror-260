eb109e7382f6c1307aa2dcff00753c0759c9e6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-07 16:04:18 +0000

    Add package imutesting/0.0.10/imutesting-0.0.10-py3-none-any.whl

---
+++ packages/imutesting/imutesting-0.0.10-py3-none-any.whl/norm/calcu.py
@@ -0,0 +1,17 @@
+class calculating:
+    def __init__(self):
+        pass
+
+    # 四元数乘法-Quaternion Multiplication
+    @staticmethod
+    def multiplication(a,b):
+        return a*b
+
+    @staticmethod
+    def additon(a, b):
+        return a+b
+
+    @staticmethod
+    def substraction(a, b):
+        return a-b
+
