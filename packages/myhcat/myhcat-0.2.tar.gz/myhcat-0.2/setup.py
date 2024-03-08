fc871b7832b9b0a9f7441b03406f0a2dc9045436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:05:07 +0000

    Add package myhcat/0.2/myhcat-0.2.tar.gz

---
+++ packages/myhcat/myhcat-0.2.tar.gz/myhcat-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myhcat',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        # 任何依赖项都在这里列出
+        'httpcat-sdk'
+    ],
+    author='dwge1',
+    author_email='dwge1234@outlook.com',
+    description='myhcat',
+    license='MIT',
+    keywords='myhcat',
+    url='https://github.com/dwge1/myhcat'
+)
+
