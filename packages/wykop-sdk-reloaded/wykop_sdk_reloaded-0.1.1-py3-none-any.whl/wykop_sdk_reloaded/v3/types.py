67d29b9b656751b4e7d528feef5bad6dbb4a27b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:05:06 +0000

    Add package wykop-sdk-reloaded/0.1.1/wykop_sdk_reloaded-0.1.1-py3-none-any.whl

---
+++ packages/wykop-sdk-reloaded/wykop_sdk_reloaded-0.1.1-py3-none-any.whl/wykop_sdk_reloaded/v3/types.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class LinkCommentSortType(Enum):
+    NEWEST = "newest"
+    BEST = "best"
+    OLDEST = "oldest"
+
+
+class LinkCommentVoteType(Enum):
+    UP = "up"
+    DOWN = "down"
+
+
+class LinkType(Enum):
+    HOMEPAGE = "homepage"
+    UPCOMING = "upcoming"
+
+
+class LinkVoteDownReason(Enum):
+    DUPLICATE = 1
+    SPAM = 2
+    UNTRUE = 3
+    INAPPROPRIATE = 4
+    UNSUITABLE = 5
