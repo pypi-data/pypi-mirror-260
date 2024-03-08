57bc0a88a4c5369712c165c81989e381c23d436e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:05:35 +0000

    Add package systembridgegui/4.0.3/systembridgegui-4.0.3.tar.gz

---
+++ packages/systembridgegui/systembridgegui-4.0.3.tar.gz/systembridgegui-4.0.3/systembridgegui/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides systembridgegui version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update systembridgegui` to change this file.
+
+from incremental import Version
+
+__version__ = Version("systembridgegui", 4, 0, 3)
+__all__ = ["__version__"]
