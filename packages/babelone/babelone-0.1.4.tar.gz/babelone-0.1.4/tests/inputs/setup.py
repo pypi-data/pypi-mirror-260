dfa268d840d4006390f91507df11319d65ac9a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:05:23 +0000

    Add package babelone/0.1.4/babelone-0.1.4.tar.gz

---
+++ packages/babelone/babelone-0.1.4.tar.gz/babelone-0.1.4/tests/inputs/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+VERSION: str = "2.0"
+REQUIRES = [
+    "pydantic==2.6.2",
+    "fastapi",
+]
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="babelone-test",
+        version=VERSION,
+        author="Rahul D. Ghosal",
+        install_requires=REQUIRES,
+        extra_requires={
+            "dev": ["pytest", "hypothesis>=6.95.x"],
+            "PDF": ["ReportLab>=1.2", "RXP"],
+        },
+        entry_points={
+            "console_scripts": [
+                "hello-world = timmins:hello_world",
+            ],
+            "gui_scripts": [
+                "hello-world = timmins:hello_world",
+            ]
+        },
+    )
