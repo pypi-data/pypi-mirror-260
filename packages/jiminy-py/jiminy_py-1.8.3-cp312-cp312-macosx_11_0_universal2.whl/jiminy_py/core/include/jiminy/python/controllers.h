1f772f22c922de557444cfb027dd00e258c155fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:04:41 +0000

    Add package jiminy-py/1.8.3/jiminy_py-1.8.3-cp312-cp312-macosx_11_0_universal2.whl

---
+++ packages/jiminy-py/jiminy_py-1.8.3-cp312-cp312-macosx_11_0_universal2.whl/jiminy_py/core/include/jiminy/python/controllers.h
@@ -0,0 +1,13 @@
+#ifndef CONTROLLERS_PYTHON_H
+#define CONTROLLERS_PYTHON_H
+
+#include "jiminy/core/fwd.h"
+
+
+namespace jiminy::python
+{
+    void JIMINY_DLLAPI exposeAbstractController();
+    void JIMINY_DLLAPI exposeFunctionalController();
+}
+
+#endif  // CONTROLLERS_PYTHON_H
---
+++ packages/jiminy-py/jiminy_py-1.8.3-cp312-cp312-macosx_11_0_universal2.whl/jiminy_py/core/include/jiminy/python/generators.h
@@ -0,0 +1,12 @@
+#ifndef GENERATORS_PYTHON_H
+#define GENERATORS_PYTHON_H
+
+#include "jiminy/core/fwd.h"
+
+
+namespace jiminy::python
+{
+    void JIMINY_DLLAPI exposeGenerators();
+}
+
+#endif  // GENERATORS_PYTHON_H
