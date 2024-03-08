44635b600c8ad50f79658199b0a705b2d250037b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:05:00 +0000

    Add package PyQtUIkit/1.0.11/PyQtUIkit-1.0.11.tar.gz

---
+++ packages/PyQtUIkit/PyQtUIkit-1.0.11.tar.gz/PyQtUIkit-1.0.11/setup.py
@@ -0,0 +1,30 @@
+"""Скрипт Setup.py для проекта по упаковке."""
+
+from setuptools import setup, find_packages
+
+from PyQtUIkit.core._version import VERSION
+
+
+def readme():
+    with open('readme.md', 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+def requirements():
+    with open('requirements.txt', 'r', encoding='utf-8') as f:
+        return f.read().split()
+
+
+if __name__ == '__main__':
+    setup(
+        name='PyQtUIkit',
+        author='SergeiKrivko',
+        url='https://github.com/SergeiKrivko',
+        long_description=readme(),
+        version=VERSION,
+        package_dir={'PyQtUIkit': 'PyQtUIkit'},
+        packages=find_packages(include=['PyQtUIkit*']),
+        description='A PyQtUIkit package.',
+        install_requires=requirements(),
+        python_requires='>=3.11'
+    )
