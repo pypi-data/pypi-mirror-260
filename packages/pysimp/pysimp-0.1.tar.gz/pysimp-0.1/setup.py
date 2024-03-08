7bf691c1eb3e9deb876334eb90ffcea8453e4642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:05:13 +0000

    Add package pysimp/0.1/pysimp-0.1.tar.gz

---
+++ packages/pysimp/pysimp-0.1.tar.gz/pysimp-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+    name="pysimp",
+    version="0.1",
+    author="vinoth",
+    athor_email="binotsuke2006@gmail.com",
+    packages=find_packages(),
+    )
