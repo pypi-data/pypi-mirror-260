d8c751230c2a3b3ed37eef02b6cd18ab65b6e63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:04:59 +0000

    Add package PRGenius/1.0.0/PRGenius-1.0.0.tar.gz

---
+++ packages/PRGenius/PRGenius-1.0.0.tar.gz/PRGenius-1.0.0/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='PRGenius', 
+    version='1.0.0',  
+    packages=find_packages(), 
+    install_requires=[
+        'requests', 
+        'openai',
+        'python-dotenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'prgenius=prgenius.pr_creator:main',  
+        ],
+    },
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='Automates PR creation on GitHub using GPT-3 for description generation based on commit messages.',
+    long_description=read('README.md'),  
+    long_description_content_type='text/markdown',
+    keywords='GitHub PR automation GPT-3',
+    url='https://github.com/yourusername/PRGenius',  
+    license='MIT',  
+    classifiers=[
+        'Development Status :: 3 - Alpha',  
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
