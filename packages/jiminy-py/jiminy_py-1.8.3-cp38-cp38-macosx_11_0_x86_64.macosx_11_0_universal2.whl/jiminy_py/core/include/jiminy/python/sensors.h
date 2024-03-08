219a79c7d77957298f7a5d4c031fb52b0c3c851b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:04:45 +0000

    Add package jiminy-py/1.8.3/jiminy_py-1.8.3-cp38-cp38-macosx_11_0_x86_64.macosx_11_0_universal2.whl

---
+++ packages/jiminy-py/jiminy_py-1.8.3-cp38-cp38-macosx_11_0_x86_64.macosx_11_0_universal2.whl/jiminy_py/core/include/jiminy/python/sensors.h
@@ -0,0 +1,14 @@
+#ifndef SENSORS_PYTHON_H
+#define SENSORS_PYTHON_H
+
+#include "jiminy/core/fwd.h"
+
+
+namespace jiminy::python
+{
+    void JIMINY_DLLAPI exposeSensorMeasurementTree();
+    void JIMINY_DLLAPI exposeAbstractSensor();
+    void JIMINY_DLLAPI exposeBasicSensors();
+}
+
+#endif  // SENSORS_PYTHON_H
