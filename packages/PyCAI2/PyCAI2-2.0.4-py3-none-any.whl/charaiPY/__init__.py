ac6a457deeb0e4670dbc7a212680e107a0a8d5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:04:56 +0000

    Add package PyCAI2/2.0.4/PyCAI2-2.0.4-py3-none-any.whl

---
+++ packages/PyCAI2/PyCAI2-2.0.4-py3-none-any.whl/charaiPY/__init__.py
@@ -0,0 +1,3 @@
+from charaiPY.nPyCAI2 import PyCAI2, PyAsyncCAI2
+
+__all__ = ['PyCAI2', 'PyAsyncCAI2']
---
+++ packages/PyCAI2/PyCAI2-2.0.4-py3-none-any.whl/charaiPY/erorr.py
@@ -0,0 +1,14 @@
+class PyCAI2(Exception):
+    pass
+
+class ServerError(PyCAI2):
+    pass
+
+class LabelError(PyCAI2):
+    pass
+
+class AuthError(PyCAI2):
+    pass
+
+class PostTypeError(PyCAI2):
+    pass
