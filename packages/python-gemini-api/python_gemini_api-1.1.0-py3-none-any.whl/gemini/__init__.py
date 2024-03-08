88d3462d67b1e41bdf822fcc1365d4c8aaa232f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:04:58 +0000

    Add package python-gemini-api/1.1.0/python_gemini_api-1.1.0-py3-none-any.whl

---
+++ packages/python-gemini-api/python_gemini_api-1.1.0-py3-none-any.whl/gemini/__init__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Daniel Park, Antonio Cheang, MIT License
+from os import environ
+from .client import GeminiClient
+from .core import Gemini
+from .src.model.output import GeminiImage, GeminiCandidate, GeminiModelOutput
+from .src.misc import *
+from .src.parser import *
+from .src.tools import *
+from .src.tools.google import *
+
+gemini_api_key = environ.get("GEMINI_COOKIES")
+
+__version__ = "1.1.0"
+__author__ = (
+    "daniel park <parkminwoo1991@gmail.com>, antonio cheang <teapotv8@proton.me>"
+)
---
+++ packages/python-gemini-api/python_gemini_api-1.1.0-py3-none-any.whl/gemini/src/tools/user_content.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+
+class UserContent(ABC):
+    @property
+    @abstractmethod
+    def key(self) -> str:
+        pass
+
+    # Markdown representation as a long string
+    @property
+    @abstractmethod
+    def markdown_text(self) -> str:
+        pass
