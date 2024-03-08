69eb02b5f7ab3a8fab48c63d0fccba03aa4a5c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:04:41 +0000

    Add package imioweb.core/1.0a4/imioweb.core-1.0a4.tar.gz

---
+++ packages/imioweb.core/imioweb.core-1.0a4.tar.gz/imioweb.core-1.0a4/src/imioweb/core/browser/contact_info.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from plone.autoform import directives
+from plone.formwidget.recaptcha.widget import ReCaptchaFieldWidget
+from Products.CMFPlone.browser.contact_info import ContactForm as BaseContactForm
+from Products.CMFPlone.browser.interfaces import IContactForm as IBaseContactForm
+from zope import schema
+
+
+class IContactForm(IBaseContactForm):
+
+    directives.widget("recaptcha", ReCaptchaFieldWidget)
+    recaptcha = schema.TextLine(
+        title=u'ReCaptcha',
+        required=True
+    )
+
+
+class ContactForm(BaseContactForm):
+
+    schema = IContactForm
