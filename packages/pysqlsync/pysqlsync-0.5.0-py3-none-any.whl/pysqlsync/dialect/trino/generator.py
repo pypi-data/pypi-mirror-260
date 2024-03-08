5fd887ec4720530630104f0793250463b640d9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:04:45 +0000

    Add package pysqlsync/0.5.0/pysqlsync-0.5.0-py3-none-any.whl

---
+++ packages/pysqlsync/pysqlsync-0.5.0-py3-none-any.whl/pysqlsync/dialect/trino/generator.py
@@ -0,0 +1,29 @@
+from pysqlsync.base import BaseGenerator, GeneratorOptions
+from pysqlsync.formation.py_to_sql import (
+    ArrayMode,
+    DataclassConverter,
+    DataclassConverterOptions,
+    EnumMode,
+    NamespaceMapping,
+    StructMode,
+)
+from pysqlsync.util.typing import override
+
+
+class TrinoGenerator(BaseGenerator):
+    def __init__(self, options: GeneratorOptions) -> None:
+        super().__init__(options)
+        self.converter = DataclassConverter(
+            options=DataclassConverterOptions(
+                enum_mode=options.enum_mode or EnumMode.CHECK,
+                struct_mode=options.struct_mode or StructMode.JSON,
+                array_mode=options.array_mode or ArrayMode.JSON,
+                namespaces=NamespaceMapping(options.namespaces),
+                foreign_constraints=options.foreign_constraints,
+                skip_annotations=options.skip_annotations,
+            )
+        )
+
+    @override
+    def placeholder(self, index: int) -> str:
+        raise NotImplementedError("operation not supported for Trino")
