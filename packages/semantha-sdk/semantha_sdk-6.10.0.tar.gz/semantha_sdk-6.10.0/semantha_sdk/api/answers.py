cb369de578543604b1d1565fa1cb32a308fe3731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:04:24 +0000

    Add package semantha-sdk/6.10.0/semantha_sdk-6.10.0.tar.gz

---
+++ packages/semantha-sdk/semantha_sdk-6.10.0.tar.gz/semantha_sdk-6.10.0/semantha_sdk/api/answers.py
@@ -0,0 +1,56 @@
+from semantha_sdk.model.answer import Answer
+from semantha_sdk.model.answer import AnswerSchema
+from semantha_sdk.rest.rest_client import MediaType
+from semantha_sdk.rest.rest_client import RestClient, RestEndpoint
+
+class AnswersEndpoint(RestEndpoint):
+    """ author semantha, this is a generated class do not change manually! 
+    
+    """
+
+    @property
+    def _endpoint(self) -> str:
+        return self._parent_endpoint + "/answers"
+
+    def __init__(
+        self,
+        session: RestClient,
+        parent_endpoint: str,
+    ) -> None:
+        super().__init__(session, parent_endpoint)
+
+    
+    def post(
+        self,
+        question: str = None,
+        maxreferences: int = None,
+        similaritythreshold: float = None,
+        language: str = None,
+    ) -> Answer:
+        """
+        Finds references in library for a given question and uses references to generate an answer.
+            The 'maxReferences' parameter determines how many library items are used. The language of the answer can be changed via the 'language' parameter.
+        Args:
+        question (str): The text to find in the library.
+    maxreferences (int): Maximum number of returned results.
+    similaritythreshold (float): Threshold for the similarity score. semantha will not deliver results with a sentence score lower than the threshold.
+            In general, the higher the threshold, the more precise the results.
+    language (str): The language for the answer text.
+        """
+        q_params = {}
+        response = self._session.post(
+            url=self._endpoint,
+            body={
+                "question": question,
+                "maxreferences": maxreferences,
+                "similaritythreshold": similaritythreshold,
+                "language": language,
+            },
+            headers=RestClient.to_header(MediaType.JSON),
+            q_params=q_params
+        ).execute()
+        return response.to(AnswerSchema)
+
+    
+    
+    
---
+++ packages/semantha-sdk/semantha_sdk-6.10.0.tar.gz/semantha_sdk-6.10.0/semantha_sdk/api/sentence.py
@@ -0,0 +1,38 @@
+from semantha_sdk.model.sentence import Sentence
+from semantha_sdk.model.sentence import SentenceSchema
+from semantha_sdk.rest.rest_client import MediaType
+from semantha_sdk.rest.rest_client import RestClient, RestEndpoint
+
+class SentenceEndpoint(RestEndpoint):
+    """ author semantha, this is a generated class do not change manually! 
+    
+    """
+
+    @property
+    def _endpoint(self) -> str:
+        return self._parent_endpoint + f"/{self._id}"
+
+    def __init__(
+        self,
+        session: RestClient,
+        parent_endpoint: str,
+        id: str,
+    ) -> None:
+        super().__init__(session, parent_endpoint)
+        self._id = id
+
+    def get(
+        self,
+    ) -> Sentence:
+        """
+        Get one single sentence of specific document.
+        Args:
+            """
+        q_params = {}
+    
+        return self._session.get(self._endpoint, q_params=q_params, headers=RestClient.to_header(MediaType.JSON)).execute().to(SentenceSchema)
+
+    
+    
+    
+    
