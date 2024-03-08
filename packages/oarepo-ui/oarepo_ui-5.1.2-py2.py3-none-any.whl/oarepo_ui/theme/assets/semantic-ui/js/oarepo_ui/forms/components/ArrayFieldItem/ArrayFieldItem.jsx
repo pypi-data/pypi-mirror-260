7ece86aa4cde96b8b496c8cb58d93388af8f4e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:04:54 +0000

    Add package oarepo-ui/5.1.2/oarepo_ui-5.1.2-py2.py3-none-any.whl

---
+++ packages/oarepo-ui/oarepo_ui-5.1.2-py2.py3-none-any.whl/oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/components/ArrayFieldItem/ArrayFieldItem.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { GroupField } from "react-invenio-forms";
+import { Form, Button, Icon } from "semantic-ui-react";
+import { i18next } from "@translations/oarepo_ui/i18next";
+import PropTypes from "prop-types";
+
+export const ArrayFieldItem = ({
+  arrayHelpers,
+  indexPath,
+  children,
+  className,
+  removeButton: RemoveButton,
+  removeButtonProps,
+  ...uiProps
+}) => {
+  const [highlighted, setHighlighted] = useState(false);
+  return (
+    <GroupField
+      className={`${highlighted ? "highlighted" : ""} ${className}`}
+      {...uiProps}
+    >
+      {children}
+      <Form.Field>
+        {RemoveButton ? (
+          <RemoveButton
+            arrayHelpers={arrayHelpers}
+            indexPath={indexPath}
+            {...removeButtonProps}
+          />
+        ) : (
+          <Button
+            style={{ marginTop: "1.75rem" }}
+            aria-label={i18next.t("Remove field")}
+            className="close-btn"
+            icon
+            onClick={() => {
+              arrayHelpers.remove(indexPath);
+            }}
+            onMouseEnter={() => setHighlighted(true)}
+            onMouseLeave={() => setHighlighted(false)}
+          >
+            <Icon name="close" />
+          </Button>
+        )}
+      </Form.Field>
+    </GroupField>
+  );
+};
+
+ArrayFieldItem.propTypes = {
+  arrayHelpers: PropTypes.object,
+  indexPath: PropTypes.number,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  removeButton: PropTypes.node,
+  removeButtonProps: PropTypes.object,
+};
+
+ArrayFieldItem.defaultProps = {
+  className: "invenio-group-field",
+  removeButton: undefined,
+  removeButtonProps: {},
+};
