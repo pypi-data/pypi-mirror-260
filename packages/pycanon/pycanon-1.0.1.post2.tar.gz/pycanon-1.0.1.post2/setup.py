668af05ed296f31b031a3c3444304987b0089eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:05:15 +0000

    Add package pycanon/1.0.1.post2/pycanon-1.0.1.post2.tar.gz

---
+++ packages/pycanon/pycanon-1.0.1.post2.tar.gz/pycanon-1.0.1.post2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+# Passing requirements as file only works on setuptools >= 62.6
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    install_requires=requirements,
+)
---
+++ packages/pycanon/pycanon-1.0.1.post2.tar.gz/pycanon-1.0.1.post2/tests/test_metrics.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pytest
+
+from pycanon import anonymity
+from pycanon.anonymity.utils import aux_functions
+
+
+class TestMathScores:
+    qi = ["Teacher", "Gender", "Ethnic", "Freeredu", "wesson"]
+    sa = ["Score"]
+    file_name = "./data/processed/StudentsMath_Score.csv"
+    data = aux_functions.read_file(file_name)
+
+    def test_k_anon(self):
+        assert 1 == anonymity.k_anonymity(self.data, self.qi)
+
+    def test_l_div(self):
+        assert 1 == anonymity.l_diversity(self.data, self.qi, self.sa)
+
+    def test_entropy_l(self):
+        assert 1 == anonymity.entropy_l_diversity(self.data, self.qi, self.sa)
+
+    def test_c_div(self):
+        assert (np.nan, 1) == anonymity.recursive_c_l_diversity(
+            self.data, self.qi, self.sa
+        )
+
+    def test_alpha(self):
+        assert (1, 1) == anonymity.alpha_k_anonymity(self.data, self.qi, self.sa)
+
+    def test_basic_beta(self):
+        assert 71.0 == pytest.approx(
+            anonymity.basic_beta_likeness(self.data, self.qi, self.sa)
+        )
+
+    def test_enhanced_beta(self):
+        assert 5.375278407684164 == pytest.approx(
+            anonymity.enhanced_beta_likeness(self.data, self.qi, self.sa)
+        )
+
+    def test_delta_disclosure(self):
+        assert 4.276666119016055 == pytest.approx(
+            anonymity.delta_disclosure(self.data, self.qi, self.sa)
+        )
+
+    def test_t_clos(self):
+        assert 0.4165919952210274 == pytest.approx(
+            anonymity.t_closeness(self.data, self.qi, self.sa)
+        )
