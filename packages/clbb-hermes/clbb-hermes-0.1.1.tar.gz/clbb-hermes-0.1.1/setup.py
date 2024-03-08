ac5c73d8a055c65149d6a7b622daf63ffb953428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:04:44 +0000

    Add package clbb-hermes/0.1.1/clbb-hermes-0.1.1.tar.gz

---
+++ packages/clbb-hermes/clbb-hermes-0.1.1.tar.gz/clbb-hermes-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clbb-hermes',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'scipy',
+        'numpy',
+        'geopandas',
+        'osmnx',
+        'networkx',
+        'pandana',
+        'osmnet',
+        'pyarrow',
+        'openpyxl'
+    ],
+)
