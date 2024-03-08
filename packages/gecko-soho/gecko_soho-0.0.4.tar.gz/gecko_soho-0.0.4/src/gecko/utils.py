ec177c2dac6f472c6c39411225a12b5a2e45f6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:04:21 +0000

    Add package gecko-soho/0.0.4/gecko_soho-0.0.4.tar.gz

---
+++ packages/gecko-soho/gecko_soho-0.0.4.tar.gz/gecko_soho-0.0.4/src/gecko/utils.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from PIL.JpegImagePlugin import JpegImageFile
+from IPython import display
+
+
+def create_gif(images: List[JpegImageFile], output_path: str) -> None:
+    images[0].save(output_path, save_all=True, append_images=images, loop=2<<10)
+    return
+
+
+def display_gif(gif_path):
+    return display.HTML('<img src="{}">'.format(gif_path))
