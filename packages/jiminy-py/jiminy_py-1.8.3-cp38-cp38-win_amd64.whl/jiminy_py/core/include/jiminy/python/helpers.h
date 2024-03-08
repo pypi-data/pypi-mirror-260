108bb67eed5f11af416a369b2683cc0d418812c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:04:52 +0000

    Add package jiminy-py/1.8.3/jiminy_py-1.8.3-cp38-cp38-win_amd64.whl

---
+++ packages/jiminy-py/jiminy_py-1.8.3-cp38-cp38-win_amd64.whl/jiminy_py/core/include/jiminy/python/helpers.h
@@ -0,0 +1,12 @@
+#ifndef HELPERS_PYTHON_H
+#define HELPERS_PYTHON_H
+
+#include "jiminy/core/fwd.h"
+
+
+namespace jiminy::python
+{
+    void JIMINY_DLLAPI exposeHelpers();
+}
+
+#endif  // HELPERS_PYTHON_H
