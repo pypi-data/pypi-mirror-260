1796dffd025a0830afb51c394fadd575f81a1928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:04:55 +0000

    Add package tiledbsoma/1.7.3/tiledbsoma-1.7.3-cp310-cp310-macosx_11_0_x86_64.whl

---
+++ packages/tiledbsoma/tiledbsoma-1.7.3-cp310-cp310-macosx_11_0_x86_64.whl/tiledbsoma/io/__init__.py
@@ -0,0 +1,39 @@
+from ._registration import (
+    ExperimentAmbientLabelMapping,
+)
+from .ingest import (
+    add_matrix_to_collection,
+    add_X_layer,
+    append_obs,
+    append_var,
+    append_X,
+    create_from_matrix,
+    from_anndata,
+    from_h5ad,
+    register_anndatas,
+    register_h5ads,
+    to_anndata,
+    to_h5ad,
+    update_matrix,
+    update_obs,
+    update_var,
+)
+
+__all__ = (
+    "add_matrix_to_collection",
+    "add_X_layer",
+    "append_obs",
+    "append_var",
+    "append_X",
+    "create_from_matrix",
+    "from_anndata",
+    "from_h5ad",
+    "register_h5ads",
+    "register_anndatas",
+    "to_anndata",
+    "to_h5ad",
+    "update_matrix",
+    "update_obs",
+    "update_var",
+    "ExperimentAmbientLabelMapping",
+)
